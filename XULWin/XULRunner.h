f9baab8631dc7d2078f0cb4410e44d4029f9e078
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2009-09-24 18:57:40 +0000

    Introduce XULRunner. XULRunner now parses prefs.js to know which XUL file is the main one.

---
+++ XULWin/XULRunner.h
@@ -0,0 +1,21 @@
+#ifndef XULRUNNER_H_INCLUDED
+#define XULRUNNER_H_INCLUDED
+
+
+#include <string>
+
+
+namespace XULWin
+{
+    
+    class XULRunner
+    {
+    public:
+        void run(const std::string & inApplicationIniFile);
+    
+    };
+
+} // XULWin
+
+
+#endif // XULRUNNER_H_INCLUDED

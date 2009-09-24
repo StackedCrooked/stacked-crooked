562ae3f9beb14d1f2fe1c6a00c638d182765b03a
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2009-09-24 12:23:31 +0000

    Add Image file (forgot in prev commits).

---
+++ trunk/XULWin/Image.h
@@ -0,0 +1,29 @@
+#ifndef IMAGE_H_INCLUDED
+#define IMAGE_H_INCLUDED
+
+
+#include "Element.h"
+
+
+namespace XULWin
+{
+
+    class Image : public Element
+    {
+    public:
+        static ElementPtr Create(Element * inParent, const AttributesMapping & inAttr)
+        { return Element::Create<Image>(inParent, inAttr); }
+
+        static const char * Type() { return "image"; }
+
+        std::string value() const;
+    
+    private:
+        friend class Element;
+        Image(Element * inParent);
+    };
+
+
+} // namespace XULWin
+
+#endif // IMAGE_H_INCLUDED

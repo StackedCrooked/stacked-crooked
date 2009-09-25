59fecb07932ebc95ef4b9bf4b5e46e81ee35deb1
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2009-09-25 21:43:42 +0000

    Add xulrunner 'shout' sample.

--- trunk/xulrunnersamples/widgets/run.bat
+++ trunk/xulrunnersamples/widgets/run.bat
@@ -1,3 +1,3 @@
-rmdir /S /Q "C:\Documents and Settings\fr\Local Settings\Application Data\Nobody"
-rmdir /S /Q "C:\Documents and Settings\fr\Application Data\Nobody"
+rmdir /S /Q "C:\Documents and Settings\fr\Local Settings\Application Data\XULWin"
+rmdir /S /Q "C:\Documents and Settings\fr\Application Data\XULWin"
 xulrunner.exe application.ini

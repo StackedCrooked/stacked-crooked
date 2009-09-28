34c873bcb8d0f9ed4abf323374f11b328211c6d5
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2009-09-28 11:39:47 +0000

    Add Conversions.cpp and Conversions.h files.

---
+++ XULWin/Conversions.h
@@ -0,0 +1,35 @@
+#ifndef CONVERSIONS_H_INCLUDED
+#define CONVERSIONS_H_INCLUDED
+
+
+#include "Layout.h"
+#include <string>
+
+
+namespace XULWin
+{
+
+    std::string Int2String(int inValue);
+    
+    int String2Int(const std::string & inValue, int inDefault);
+    
+    int String2Int(const std::string & inValue);
+    
+    bool String2Bool(const std::string & inString, bool inDefault);
+    
+    std::string Bool2String(bool inBool);
+    
+    Orientation String2Orientation(const std::string & inValue, Orientation inDefault);
+    
+    std::string Orientation2String(Orientation inOrient);
+    
+    Alignment String2Align(const std::string & inValue, Alignment inDefault);
+    
+    std::string Align2String(Alignment inAlign);
+    
+    int CssString2Size(const std::string & inString, int inDefault);
+
+} // namespace XULWin
+
+
+#endif // CONVERSIONS_H_INCLUDED

16d66ce3f0169d87873d115ab240932e63907cb2
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2009-10-07 14:03:18 +0000

    Remove WS_TABSTOP on NativeControl's level, and move to subclass constructors where appropriate.

--- trunk/Test/main.cpp
+++ trunk/Test/main.cpp
@@ -156,11 +156,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     //runConfigSample();
     XULTest::Tester tester;
-    tester.runXULSample("hello");
+    //tester.runXULSample("hello");
     //tester.runXULSample("widgets");
     //tester.runXULSample("tabbox");
     //tester.runXULSample("treeview");
-    //tester.runXULSample("configpanel");
+    tester.runXULSample("configpanel");
     //tester.runXULSample("shout");
     //tester.runXULSample("svg");
     return 0;

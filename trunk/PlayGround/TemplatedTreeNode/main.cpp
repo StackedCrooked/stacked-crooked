0773cd171d6161af2cb9e7caa2bdb403405ae65d
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2009-10-20 21:10:05 +0000

    Added ContainerPolicy, however, index-based getChild doesn't work anymore. Todo: add iterators.

--- trunk/PlayGround/TemplatedTreeNode/main.cpp
+++ trunk/PlayGround/TemplatedTreeNode/main.cpp
@@ -1,11 +1,31 @@
+#include <set>
 #include <string>
 #include <vector>
 #include <boost/shared_ptr.hpp>
 #include <assert.h>
+#include <iterator>
 
+template<class DataType>
+struct ContainerPolicy_Set
+{
+    typedef std::set<DataType> Container;
+    static void insert(Container & ioContainer, const DataType & inValue)
+    {
+        ioContainer.insert(inValue);
+    }
+};
+template<class DataType>
+struct ContainerPolicy_Vector
+{
+    typedef std::vector<DataType> Container;
+    static void insert(Container & ioContainer, const DataType & inValue)
+    {
+        ioContainer.push_back(inValue);
+    }
+};
 
 template <class PointeeType>
-struct NormalPointerPolicy
+struct PointerPolicy_Normal
 {
     typedef PointeeType* PointerType;
 
@@ -16,7 +36,7 @@ struct NormalPointerPolicy
 };
 
 template <class PointeeType>
-struct SharedPointerPolicy
+struct PointerPolicy_Shared
 {
     typedef boost::shared_ptr<PointeeType> PointerType;
 
@@ -27,7 +47,7 @@ struct SharedPointerPolicy
 };
 
 
-template <class DataType, template <class> class PointerPolicy>
+template <class DataType, template <class> class ContainerPolicy, template <class> class PointerPolicy>
 class GenericNode
 {
 public:    
@@ -35,20 +55,14 @@ public:
 
     GenericNode(const DataType & inData) : mData(inData) { }
 
-    typedef GenericNode<DataType, PointerPolicy> This;
-
+    typedef GenericNode<DataType, ContainerPolicy, PointerPolicy> This;
     typedef typename PointerPolicy<This>::PointerType ChildPtr;
-    
-    const This * getChild(size_t idx) const
-    { return PointerPolicy<This>::getRaw(mChildren[idx]); }
-
-    This * getChild(size_t idx)
-    { return PointerPolicy<This>::getRaw(mChildren[idx]); }
+    typedef typename ContainerPolicy<ChildPtr>::Container Container;
 
     void addChild(This * inItem)
     { 
         ChildPtr item(inItem);
-        mChildren.push_back(item);
+        ContainerPolicy<ChildPtr>::insert(mChildren, item);
     }
 
     const DataType & data() const
@@ -59,11 +73,11 @@ public:
 
 private:
     DataType mData;
-    std::vector<ChildPtr> mChildren;
+    Container mChildren;
 };
 
-typedef GenericNode<std::string, NormalPointerPolicy> SimpleNode;
-typedef GenericNode<std::string, SharedPointerPolicy> SmartNode;
+typedef GenericNode<std::string, ContainerPolicy_Vector, PointerPolicy_Normal> SimpleNode;
+typedef GenericNode<std::string, ContainerPolicy_Set, PointerPolicy_Shared> SmartNode;
 
 
 int main()
@@ -71,17 +85,16 @@ int main()
     SimpleNode simpleNode;
     simpleNode.addChild(new SimpleNode("test1"));
     simpleNode.addChild(new SimpleNode("test2"));
-    SimpleNode * a = simpleNode.getChild(0);
-    assert(a->data() == "test1");
-    const SimpleNode * b = static_cast<const SimpleNode>(simpleNode).getChild(1);
-    assert(b->data() == "test2");
+    //SimpleNode * a = simpleNode.getChild(0);
+    //assert(a->data() == "test1");
+    //const SimpleNode * b = static_cast<const SimpleNode>(simpleNode).getChild(1);
+    //assert(b->data() == "test2");
 
     SmartNode smartNode;
     smartNode.addChild(new SmartNode("test3"));
     smartNode.addChild(new SmartNode("test4"));
-    SmartNode * c = smartNode.getChild(0);
-    assert(c->data() == "test3");
-    SmartNode * d = static_cast<const SmartNode>(smartNode).getChild(1);
-    assert(d->data() == "test4");
+    //SmartNode::iterator it = smartNode.begin();
+    //SmartNode * c = *it;
+    //assert(c->data() == "test3");
     return 0;
 }

06d59209e8d45998ea57ceae3d9395226d0afa21
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2009-10-20 22:26:58 +0000

    Add iterator. Add ContainerPolicy_Set_CustomOrdering as illustration.

--- PlayGround/TemplatedTreeNode/main.cpp
+++ PlayGround/TemplatedTreeNode/main.cpp
@@ -4,6 +4,8 @@
 #include <boost/shared_ptr.hpp>
 #include <assert.h>
 #include <iterator>
+#include <iostream>
+
 
 template<class DataType>
 struct ContainerPolicy_Set
@@ -14,6 +16,7 @@ struct ContainerPolicy_Set
         ioContainer.insert(inValue);
     }
 };
+
 template<class DataType>
 struct ContainerPolicy_Vector
 {
@@ -59,6 +62,91 @@ public:
     typedef typename PointerPolicy<This>::PointerType ChildPtr;
     typedef typename ContainerPolicy<ChildPtr>::Container Container;
 
+
+    // simple iterator
+    class const_iterator : public std::iterator<std::forward_iterator_tag, This>
+    {
+    public:
+        bool operator== (const const_iterator & rhs) const
+        { return mIterator == rhs.mIterator; }
+        
+        bool operator!= (const const_iterator & rhs) const
+        { return mIterator != rhs.mIterator; }
+
+        // preincrement
+        const_iterator operator++()
+        {
+            return const_iterator(++mIterator);
+        }
+
+        // postincrement
+        const_iterator operator++(int)
+        {
+            const_iterator tmp = *this;
+            ++*this;
+            return tmp;
+        }
+
+        // return reference to class object
+        const ChildPtr & operator*() { return *mIterator; }
+
+        // return pointer to class object
+        const ChildPtr * operator->() { return &(*mIterator); }
+
+    private:
+        friend class This;
+        const_iterator(const typename Container::const_iterator & inIterator) : mIterator(inIterator) { }
+        typename Container::const_iterator mIterator;
+    };
+
+    class iterator : public std::iterator<std::forward_iterator_tag, This>
+    {
+    public:
+        bool operator== (const iterator & rhs) const
+        { return mIterator == rhs.mIterator; }
+        
+        bool operator!= (const iterator & rhs) const
+        { return mIterator != rhs.mIterator; }
+
+        // preincrement
+        iterator operator++()
+        {
+            return iterator(++mIterator);
+        }
+
+        // postincrement
+        iterator operator++(int)
+        {
+            iterator tmp = *this;
+            ++*this;
+            return tmp;
+        }
+
+        // return reference to class object
+        const ChildPtr & operator*() { return *mIterator; }
+
+        // return pointer to class object
+        const ChildPtr * operator->() { return &(*mIterator); }
+
+    private:
+        friend class This;
+        iterator(const typename Container::iterator & inIterator) : mIterator(inIterator) { }
+        typename Container::iterator mIterator;
+    };
+
+    iterator begin()
+    {
+        Container::iterator it = mChildren.begin();
+        return iterator(it);
+    }
+
+    iterator end() { return iterator(mChildren.end()); }
+
+    const_iterator begin() const { return const_iterator(mChildren.begin()); }
+
+    const_iterator end() const { return const_iterator(mChildren.end()); }
+
+
     void addChild(This * inItem)
     { 
         ChildPtr item(inItem);
@@ -80,21 +168,114 @@ typedef GenericNode<std::string, ContainerPolicy_Vector, PointerPolicy_Normal> S
 typedef GenericNode<std::string, ContainerPolicy_Set, PointerPolicy_Shared> SmartNode;
 
 
+template<class Container>
+void iterateContainer(Container & inContainer)
+{
+    {
+        std::cout << "Non-const postincrement" << std::endl;
+        Container::iterator it = inContainer.begin();
+        while (it != inContainer.end())
+        {
+            Container::ChildPtr child = (*it);
+            std::string data = (*it)->data();
+            std::cout << data << std::endl;
+            it++;
+        }
+    }
+    {
+        std::cout << "Non-const preincrement" << std::endl;
+        Container::iterator it = inContainer.begin();
+        while (it != inContainer.end())
+        {
+            Container::ChildPtr child = (*it);
+            std::string data = (*it)->data();
+            std::cout << data << std::endl;
+            ++it;
+        }
+    }
+}
+
+template<class Container>
+void iterateContainer(const Container & inContainer)
+{
+    {
+        std::cout << "Const postincrement" << std::endl;
+        Container::const_iterator it = inContainer.begin();
+        while (it != inContainer.end())
+        {
+            Container::ChildPtr child = (*it);
+            std::string data = (*it)->data();
+            std::cout << data << std::endl;
+            it++;
+        }
+    }
+    {
+        std::cout << "Const preincrement" << std::endl;
+        Container::const_iterator it = inContainer.begin();
+        while (it != inContainer.end())
+        {
+            Container::ChildPtr child = (*it);
+            std::string data = (*it)->data();
+            std::cout << data << std::endl;
+            ++it;
+        }
+    }
+}
+
+
+template<class NodeConfig>
+void testConfig()
+{
+    NodeConfig node;
+    node.addChild(new NodeConfig("cup"));
+    node.addChild(new NodeConfig("orange"));
+    node.addChild(new NodeConfig("house"));
+    node.addChild(new NodeConfig("zorro"));
+    iterateContainer(node);
+
+    const NodeConfig & constSimpleNode(node);
+    iterateContainer(constSimpleNode);
+}
+
+
+template<class NodeT>
+struct ContainerPolicy_Set_CustomOrdering
+{
+    struct CustomOrder
+    {
+        bool operator () (NodeT lhs, NodeT rhs)
+        {
+            return lhs->data() < rhs->data();
+        }
+    };
+    typedef std::set<NodeT, CustomOrder> Container;
+
+    static void insert(Container & ioContainer, const NodeT & inValue)
+    {
+        ioContainer.insert(inValue);
+    }
+};
+
+
 int main()
 {
-    SimpleNode simpleNode;
-    simpleNode.addChild(new SimpleNode("test1"));
-    simpleNode.addChild(new SimpleNode("test2"));
-    //SimpleNode * a = simpleNode.getChild(0);
-    //assert(a->data() == "test1");
-    //const SimpleNode * b = static_cast<const SimpleNode>(simpleNode).getChild(1);
-    //assert(b->data() == "test2");
+    std::cout << "Vector, normal pointer" << std::endl;
+    testConfig<GenericNode<std::string, ContainerPolicy_Vector, PointerPolicy_Normal> >();
 
-    SmartNode smartNode;
-    smartNode.addChild(new SmartNode("test3"));
-    smartNode.addChild(new SmartNode("test4"));
-    //SmartNode::iterator it = smartNode.begin();
-    //SmartNode * c = *it;
-    //assert(c->data() == "test3");
+    std::cout << "Vector, shared pointer" << std::endl;
+    testConfig<GenericNode<std::string, ContainerPolicy_Vector, PointerPolicy_Shared> >();
+
+    std::cout << "Set, normal pointer" << std::endl;
+    testConfig<GenericNode<std::string, ContainerPolicy_Set, PointerPolicy_Normal> >();
+
+    std::cout << "Set, shared pointer" << std::endl;
+    testConfig<GenericNode<std::string, ContainerPolicy_Set, PointerPolicy_Shared> >();
+
+    std::cout << "Set, normal pointer, custom ordening" << std::endl;
+    testConfig<GenericNode<std::string, ContainerPolicy_Set_CustomOrdering, PointerPolicy_Normal> >();
+
+    std::cout << "Type 'q' + ENTER to quit";
+    char c;
+    std::cin >> c;
     return 0;
 }

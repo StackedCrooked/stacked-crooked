01cfd7ee09899a1d39148b6045fabf6918d7a5a7
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2009-11-05 19:48:18 +0000

    Some docs cleanup.

--- trunk/VideoConverter/ProcessUtils.rb
+++ trunk/VideoConverter/ProcessUtils.rb
@@ -3,17 +3,17 @@ require 'open3'
 # Helper functions when running processes
 module ProcessUtils
 
-  # Runs a subprocess and applies handlers for stdout and stderr.
+  # Runs a subprocess.
   # +command+:: command line string to be executed by the system
-  # +outhandler+:: +Proc+ object that takes the stdout pipe (of type +IO+) as first and only parameter. This value may be +nil+ if you don't want to process the standard output stream.
-  # +errhandler+:: +Proc+ object that takes the stderr pipe (of type +IO+) as first and only parameter. This value may be +nil+ if you don't want to process the standard error stream.
-  def execute_and_handle(command, outhandler, errhandler)
+  # +outpipe+:: +Proc+ object that takes the stdout pipe (of type +IO+) as first and only parameter.
+  # +errpipe+:: +Proc+ object that takes the stderr pipe (of type +IO+) as first and only parameter.
+  def execute_and_handle(command, outpipe, errpipe)
     Open3.popen3(command) do |_, stdout, stderr|
-      if (outhandler)
-        outhandler.call(stdout)
+      if (outpipe)
+        outpipe.call(stdout)
       end
-      if (errhandler)
-        errhandler.call(stderr)
+      if (errpipe)
+        errpipe.call(stderr)
       end
     end
   end

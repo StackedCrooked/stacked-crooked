0bf3c9ec7038fc295096d9b321b504a20783bea9
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2009-11-05 16:33:43 +0000

    Compatibalize documentation with RDoc.

--- VideoConverter/ProcessUtils.rb
+++ VideoConverter/ProcessUtils.rb
@@ -4,9 +4,9 @@ module ProcessUtils
 
   # Runs a subprocess and applies handlers for stdout and stderr
   # Params:
-  # - command: command line string to be executed by the system
-  # - outhandler: proc object that takes a pipe object as first and only param
-  # - errhandler: proc object that takes a pipe object as first and only param
+  # * [+command+]     command line string to be executed by the system
+  # * [+outhandler+]  proc object that takes a pipe object as first and only param (may be nil)
+  # * [+errhandler+]  proc object that takes a pipe object as first and only param (may be nil)
   def execute_and_handle(command, outhandler, errhandler)
     Open3.popen3(command) do |_, stdout, stderr|
       if (outhandler)

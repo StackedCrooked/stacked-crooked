48e62d934f602e51805fb15e96e34af1a69dbf72
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2009-11-06 23:37:14 +0000

    Introduce Mutex and ConditionVariable in Thread system. Cleanup.

--- VideoConverter/RemoteControl.rb
+++ VideoConverter/RemoteControl.rb
@@ -3,16 +3,30 @@ require 'rubygems'
 require 'sinatra'
 
 $converter = VideoConverter.new
+$mutex = Mutex.new
+$cv = ConditionVariable.new
+
 
 get '/convert' do
   Thread.new do 
-    $converter.convert("mitch.mpg", "mitch.avi")
-    "Conversion progress has started, progress is now: " + $converter.get_progress().to_s
+    progress_handler = Proc.new do |progress|
+      $mutex.synchronize do
+        $progress = progress
+        $cv.signal
+      end
+    end
+    $converter.convert("mitch.mpg",
+                       "mitch.avi",
+                       progress_handler)
   end
+  "Conversion progress has started, progress is now: " + $progress.to_s
 end
 
 get '/progress' do
-  "Progress is now: " + $converter.get_progress().to_s
+  $mutex.synchronize do
+    $cv.wait($mutex)
+    "Progress is now: " + $progress.to_s
+  end
 end
 
 

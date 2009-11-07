b834b764293f7ed23632c028fb796ca4839be600
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2009-11-07 02:17:49 +0000

    Update documentation.

--- trunk/VideoConverter/VideoConverter.rb
+++ trunk/VideoConverter/VideoConverter.rb
@@ -1,23 +1,27 @@
 require 'FFMPEG.rb'
 
-# Provides an interface to video conversion utilities
+# VideoConverter enables converting of videos. It is based on FFMPEG.
 class VideoConverter
+
+  # VideoConverter constructor.
+  # +video+:: identifier for the to be converted video file
   def initialize(video)
     @converter = FFMPEG.new
     @video = File.dirname(__FILE__) + "/../TestVideos/" + video
     @progress = 0
-    @duration = get_duration
     @mutex = Mutex.new
     @cv = ConditionVariable.new
   end
 
+  # Returns duration of the video file in seconds.
   def get_duration
-    if @duration == nil
+    if not @duration
       @duration = @converter.get_duration(@video)
     end
     @duration
   end
 
+  # Returns the progress in seconds of the conversion process.
   def get_progress
     result = 0
     if @mutex
@@ -29,10 +33,8 @@ class VideoConverter
     result
   end
 
-  # Executes a simple ffmpeg convert command of the form <tt>ffmpeg -i <inputfile> <outputfile></tt>
-  # +input_file+:: path to the input video file
-  # +output_file+:: path for the output video file
-  # +progress_handler+:: +Proc+ object that takes a progress value (in seconds).
+  # Converts the video. The target codec is deduced from the +ouput_file+'s file extension.
+  # +output_file+:: name of the output video file
   def convert_to(output_file)
     @out_video = File.dirname(__FILE__) + "/" + output_file
     system("rm #{@out_video}")

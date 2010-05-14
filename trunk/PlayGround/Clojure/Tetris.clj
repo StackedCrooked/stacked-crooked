c2dbaa79548648c5a338f2384009cded9d20ae76
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-05-14 06:25:19 +0000

    Introduce new "grid" data structure. Blocks can be stacked now.

--- trunk/PlayGround/Clojure/Tetris.clj
+++ trunk/PlayGround/Clojure/Tetris.clj
@@ -1,14 +1,46 @@
+
+
+(in-ns 'tetris)
+(clojure.core/use 'clojure.core)
 (import java.awt.Color)
 (import java.awt.Dimension)
 (import java.awt.Toolkit)
 (import java.awt.event.KeyListener)
 (import javax.swing.JFrame)
 (import javax.swing.JPanel)
-
 (use 'clojure.contrib.math)
 
+; Grid data structure
+; -------------------
+(defstruct grid :width :height)
 
+(defn create-grid [w h initial-value]
+  (struct-map grid
+    :width  w
+    :height h
+    :data   (ref (vec (repeat (* w h) initial-value)))))
 
+(defn create-grid-with-data [w h data]
+  (struct-map grid
+    :width w
+    :height h
+    :data (ref data)))
+    
+(defn get-grid [g x y]
+  (let [data  (deref (g :data))
+        idx   (+ x (* (g :width) y)) ]
+    (nth data idx)))
+    
+(defn set-grid [g x y value]
+  (let [data  (deref (g :data))
+        idx   (+ x (* (g :width) y)) ]
+    (dosync (alter (g :data) (fn [_] (assoc data idx value))))))
+
+(defn get-grid-rows [g]
+  (partition (g :width) (deref (g :data))))
+
+; Shuffle utility function
+; ------------------------    
 (defn sort-by-mem-keys 
   "Like clojure/sort-by but only calls keyfn once on each item in coll." 
   ([keyfn coll] 
@@ -18,16 +50,14 @@
            sorted-pairs (sort (fn [x y] (.compare comp (first x) (first y))) 
                               key-vals)] 
        (map second sorted-pairs))))
-       
+
 (defn shuffle 
-  "Returns a seq of coll shuffled in O(n log n) time. Space is at least 
-O(2N). Each item in coll is 
-  assigned a random number which becomes the sort key." 
-  [coll] 
+  "Returns a seq of coll shuffled in O(n log n) time.
+   Space is at least O(2N). Each item in coll is assigned
+   a random number which becomes the sort key." 
+  [coll]
   (println "shuffle")
   (sort-by-mem-keys (fn [_] (rand)) coll))
-
-
 
 
 (def prefs {
@@ -39,141 +69,98 @@ O(2N). Each item in coll is
   :screen-height 400
   :border-left 1
   :border-left-debug 12
-  :border-top 2
-})
+  :border-top 2 })
 
-(def field
-  (atom
-    (vec
-      (doall
-        (for [_ (range (prefs :num-rows))]
-          (vec (repeat (prefs :num-columns) 0)))))))
-
-(defn get-field [rowIdx colIdx]
-  (nth (nth @field rowIdx) colIdx))
-
-(defn set-field [rowIdx colIdx value]
-  (swap! field #(update-in % [rowIdx colIdx] (constantly value))))
 
 (def i-block {
   :value 1
-  :grids
-  [
-    [ [ 0 1 ]
-      [ 0 1 ]
-      [ 0 1 ]
-      [ 0 1 ] ]    
+  :grids [  (create-grid-with-data 2 4 [ 0 1
+                                         0 1
+                                         0 1
+                                         0 1])
 
-    [ [ 0 0 0 0 ]
-      [ 1 1 1 1 ] ]
-  ]  
-})
+            (create-grid-with-data 4 1 [ 1 1 1 1 ])]})
 
 (def j-block {
   :value 2
-  :grids
-  [
-    [ [ 0 2 0 ]
-      [ 0 2 0 ]
-      [ 2 2 0 ] ]
+  :grids [  (create-grid-with-data 3 3 [ 0 2 0
+                                         0 2 0
+                                         2 2 0 ])
 
-    [ [ 0 0 0 ]
-      [ 2 0 0 ]
-      [ 2 2 2 ] ]
+            (create-grid-with-data 3 3 [ 0 0 0
+                                         2 0 0
+                                         2 2 2 ])
 
-    [ [ 2 2 0 ]
-      [ 2 0 0 ]
-      [ 2 0 0 ] ]
+            (create-grid-with-data 3 3 [ 2 2 0
+                                         2 0 0
+                                         2 0 0 ])
 
-    [ [ 0 0 0 ]
-      [ 2 2 2 ]
-      [ 0 0 2 ] ]
-  ]
-})
+            (create-grid-with-data 3 3 [ 0 0 0
+                                         2 2 2
+                                         0 0 2 ])]})
 
 (def l-block {
   :value 3
-  :grids
-  [
-    [ [ 3 0 0 ]
-      [ 3 0 0 ]
-      [ 3 3 0 ] ]
+  :grids [  (create-grid-with-data 3 3 [ 3 0 0
+                                         3 0 0
+                                         3 3 0 ])
 
-    [ [ 0 0 0 ]
-      [ 3 3 3 ]
-      [ 3 0 0 ] ]
+            (create-grid-with-data 3 3 [ 0 0 0
+                                         3 3 3
+                                         3 0 0 ])
 
-    [ [ 3 3 0 ]
-      [ 0 3 0 ]
-      [ 0 3 0 ] ]
+            (create-grid-with-data 3 3 [ 3 3 0
+                                         0 3 0
+                                         0 3 0 ])
 
-    [ [ 0 0 0 ]
-      [ 0 0 3 ]
-      [ 3 3 3 ] ]
-  ]
-})
+            (create-grid-with-data 3 3 [ 0 0 0
+                                         0 0 3
+                                         3 3 3 ])]})
+
 
 (def o-block {
   :value 4
-  :grids
-  [
-    [ [ 0 0 0 ]
-      [ 0 4 4 ]
-      [ 0 4 4 ] ]
-  ]  
-})
+  :grids [ (create-grid-with-data 2 2 [ 4 4 4 4 ])]})
 
 (def s-block {
   :value 5
-  :grids
-  [
-    [ [ 0 5 5 ]
-      [ 5 5 0 ] ] 
-   
-    [ [ 0 5 0 ]
-      [ 0 5 5 ]
-      [ 0 0 5 ] ]
-  ]  
-})
+  :grids [ (create-grid-with-data 3 2 [  0 5 5 
+                                         5 5 0 ])
+
+           (create-grid-with-data 3 3 [  0 5 0 
+                                         0 5 5
+                                         0 0 5 ])]})
 
 (def t-block {
   :value 6
-  :grids
-  [
-    [ [ 6 6 6 ]
-      [ 0 6 0 ] ]
-      
-    [ [ 0 6 0 ]
-      [ 6 6 0 ]
-      [ 0 6 0 ] ]
-      
-    [ [ 0 6 0 ]
-      [ 6 6 6 ] ]
-      
-    [ [ 0 6 0 ]
-      [ 0 6 6 ]
-      [ 0 6 0 ] ]
-  ]  
-})
+  :grids [ (create-grid-with-data 3 2 [  6 6 6
+                                         0 6 0 ])
+
+           (create-grid-with-data 3 3 [  0 6 0 
+                                         6 6 0
+                                         0 6 0 ]) 
+
+           (create-grid-with-data 3 2 [  0 6 0
+                                         6 6 6 ]) 
+
+           (create-grid-with-data 3 3 [  0 6 0
+                                         0 6 6
+                                         0 6 0 ])]})
 
 (def z-block {
   :value 7
-  :grids
-  [
-    [ [ 7 7 0 ]
-      [ 0 7 7 ] ]  
-  
-    [ [ 0 0 7 ]
-      [ 0 7 7 ]
-      [ 0 7 0 ] ]
-  ]  
-})
+  :grids [ (create-grid-with-data 3 2 [ 7 7 0
+                                        0 7 7 ])  
 
+           (create-grid-with-data 3 3 [ 0 0 7
+                                        0 7 7
+                                        0 7 0 ])]})
+  
 (def active-block {
     :type (ref l-block)
     :rotation (ref 0)
-    :rowIdx (ref 0)
-    :colIdx (ref 0) })
+    :x (ref 0)
+    :y (ref 0) })
 
 (def block-types [i-block j-block l-block o-block s-block t-block z-block])
 
@@ -183,6 +170,7 @@ O(2N). Each item in coll is
 
 (def bag-index (ref 0))
 
+(def global-field (create-grid 10 20 0))
 
 (defn random-block []
   (let [ bag       (deref bag-of-blocks)
@@ -232,161 +220,129 @@ O(2N). Each item in coll is
     (.drawString text (* w x) (* h y))))
 
 (defn draw-block [g block]
-  (let [  block-type  (deref (block :type))
-          row      	  (deref (block :rowIdx))
-          col         (deref (block :colIdx))
+  (let [  x  	        (deref (block :x))
+          y           (deref (block :y))
           rotation    (deref (block :rotation))
-          grids       (block-type :grids)
+          grids       ((deref (block :type)) :grids)
           grid-idx    (mod rotation (count grids))
-          rows        (nth grids grid-idx)
-          num-rows    (count rows)]
-  (dotimes [ri num-rows]
-    (let [current-row   (nth rows ri)
-          num-columns   (count current-row) ]
-      (dotimes [ci num-columns]
-        (let [cell-value (nth current-row ci)]
+          active-grid (grids grid-idx)]
+  (dotimes [i (active-grid :width)]
+      (dotimes [j (active-grid :height)]
+        (let [cell-value (get-grid active-grid i j)]
           (if-not (zero? cell-value)
-            (let [x	(+ (prefs :border-left) (+ col ci))
-                  y	(+ (prefs :border-top) (+ row ri))]                  
-            (draw-rectangle g x y
-                            (prefs :block-width)
-                            (prefs :block-height)
-                            (get-color cell-value))))))))))
+            (let [paint-x   (+ (prefs :border-left) (+ x i))
+                  paint-y   (+ (prefs :border-top) (+ y j))]
+              (draw-rectangle g
+                              paint-x
+                              paint-y
+                              (prefs :block-width)
+                              (prefs :block-height)
+                              (get-color cell-value)))))))))
 
 (defn draw-field [g field]
-  (let [ numRows (count @field) ]
-    (dotimes [ rowIdx numRows ]
-      (let [currentRow   (nth @field rowIdx)
-            numCols      (count currentRow) ]
-        (dotimes [ colIdx numCols ]
-          (draw-rectangle g (+ (prefs :border-left) colIdx)
-                            (+ (prefs :border-top) rowIdx)
-                            (prefs :block-width)
-                            (prefs :block-height)
-                            (get-color (currentRow colIdx))))))))
+  (dotimes [x (field :width)]
+    (dotimes [y (field :height)]
+      (draw-rectangle g (+ (prefs :border-left) x)
+                        (+ (prefs :border-top) y)
+                        (prefs :block-width)
+                        (prefs :block-height)
+                        (get-color (get-grid field x y))))))
 
-(defn draw-debug [g field]
-  (let [ numRows (count @field) ]
-    (dotimes [ rowIdx numRows ]
-      (let [currentRow   (nth @field rowIdx)
-            numCols      (count currentRow) ]    
-        (dotimes [ colIdx numCols ]
-          (draw-text g (+ (prefs :border-left-debug) colIdx)
-                       (+ (prefs :border-top) rowIdx)
-                       (prefs :block-width)
-                       (prefs :block-height)
-                       (str (get-field rowIdx colIdx))))))))
+(defn draw-all [g f b]
+  (draw-field g f)
+  (draw-block g b))
 
-(defn draw-all [g]
-  (let [ b active-block ]
-    (draw-field g field)
-    (draw-debug g field)
-    (draw-block g b)))
-    
+(defn check-position-valid [field block]
+  (let [stop-condition (ref false)
+        grids          ((deref (block :type)) :grids)
+        grid-idx       (mod (deref (block :rotation)) (count grids))
+        grid           (grids grid-idx)  ]
+      (loop [ x 0 ]
+        (when (and (not (deref stop-condition))
+                   (< x (grid :width)))
+          (loop [ y 0 ]
+            (when (and (not (deref stop-condition))
+                       (< y (grid :height)))
+              (let [ block-value (get-grid grid x y)
+                     field-x     (+ x (deref (block :x)))
+                     field-y     (+ y (deref (block :y))) ]
+                (if (not (zero? block-value))
+                  (if-not
+                    (and (>= field-x 0)
+                         (< field-x (field :width))
+                         (< field-y (field :height))
+                         (zero? (get-grid field field-x field-y)))
+                    (dosync (alter stop-condition (fn [_] true))))))
+              (recur (inc y))))
+          (recur (inc x))))
+  (not (deref stop-condition))))
+
+
 (defn rotate [block]
   (dosync (alter (block :rotation) inc)))
-  
 
 (defn next-block []
   (dosync
-    (alter (active-block :type) (fn [oldBlock] (random-block)))
-    (alter (active-block :rotation) (fn [oldBlock] 0))
-    (alter (active-block :rowIdx) (fn [oldBlock] 0))
-    (alter (active-block :colIdx) (fn [oldBlock] 5))))
+    (alter (active-block :type) (fn [_] (random-block)))
+    (alter (active-block :rotation) (fn [_] 0))
+    (alter (active-block :x)
+      (fn [_]
+        (let [  block-type  (deref (active-block :type))
+                block-x     (deref (active-block :x))
+                block-y     (deref (active-block :y))
+                grids       (block-type :grids)
+                grid-idx    (mod (deref (active-block :rotation)) (count grids))
+                block-grid  (grids grid-idx)]
+          (int (/ (- (global-field :width)
+                     (block-grid :width)) 2)))))
+    (alter (active-block :y) (fn [_] 0))))
 
-  
-(defn first-non-zero-element [row]
-  (count (take-while #(zero? %) row)))
+(defn move-left [f b]
+  (dosync (alter (b :x) dec))
+  (if-not (check-position-valid f b)
+    (dosync (alter (b :x) inc))))
+    
+(defn move-right [f b]
+  (dosync (alter (b :x) inc))
+  (if-not (check-position-valid f b)
+    (dosync (alter (b :x) dec))))
 
-(defn first-if [collection predicate]
-  (count (take-while (fn [n] (not (predicate n))) collection)))
-  
-(defn last-if [collection predicate]
-  (- (dec (count collection)) (first-if (rseq collection) predicate)))
-
-(defn max-x [b]
-  (let [block-type  (deref (b :type))
-        rotation    (deref (b :rotation))
-        grids       (block-type :grids)
-        grid-idx    (mod rotation (count grids))
-        rows        (nth grids grid-idx) ]
-    (last-if
-      (reduce 
-        (fn [r1 r2]
-          (let [lastNonZeroValue (fn [row] (last-if row (comp not zero?)))]
-            (if (> (lastNonZeroValue r1) (lastNonZeroValue r2)) r1 r2)))
-        rows)
-      (comp not zero?))))
-
-(defn max-y [b]
-  (let [block-type  (deref (b :type))
-        rotation    (deref (b :rotation))
-        grids       (block-type :grids)
-        grid-idx    (mod rotation (count grids))
-        rows        (nth grids grid-idx) ]
-    (- (dec (count rows)) (count (take-while (fn [row] (zero? (reduce max row))) (rseq rows))))))
-
-(defn min-x [b]
-  (let [block-type  (deref (b :type))
-        rotation    (deref (b :rotation))
-        grids       (block-type :grids)
-        grid-idx    (mod rotation (count grids))
-        rows        (nth grids grid-idx) ]
-    (* -1 (first-non-zero-element
-      (reduce (fn [r1 r2]
-                (if (< (first-non-zero-element r1)
-                       (first-non-zero-element r2))
-  			      r1 r2))
-              rows)))))
-
-(defn move-left [b]
-  (if (< (min-x b) (deref (b :colIdx)))
-      (dosync (alter (b :colIdx) dec))))
-
-(defn move-right [b]
-  (if (< (+ (deref (b :colIdx)) (max-x b)) (dec (prefs :num-columns)))
-    (dosync (alter (b :colIdx) inc))))
-
-(defn commit-block [block]
+(defn commit-block [field block]
   (let [  block-type  (deref (block :type))
-          rowIdx   	  (deref (block :rowIdx))
-          colIdx      (deref (block :colIdx))
-          rotation    (deref (block :rotation))
+          block-x     (deref (block :x))
+          block-y     (deref (block :y))
           grids       (block-type :grids)
-          grid-idx    (mod rotation (count grids))
-          rows        (nth grids grid-idx)
-          num-rows    (count rows)]
-    (dotimes [ri num-rows]
-      (let [current-row   (nth rows ri)
-            num-columns   (count current-row)]
-        (dotimes [ci num-columns]
-          (let [cell-value (nth current-row ci)]
-            (if-not (zero? cell-value)
-              (let [c	(+ colIdx ci)
-                    r	(+ rowIdx ri)]
-                (set-field r c cell-value)))))))
-    (next-block)))
+          grid-idx    (mod (deref (block :rotation)) (count grids))
+          block-grid  (grids grid-idx)]
+    (dotimes [x (block-grid :width)]
+      (dotimes [y (block-grid :height)]
+        (let [value (get-grid block-grid x y)]
+            (if-not (zero? value)
+              (set-grid field (+ x block-x) (+ y block-y) value)))))))
 
-(defn move-down [b]
-  (if (< (+ (deref (b :rowIdx)) (max-y b)) (dec (prefs :num-rows)))
-    (dosync (alter (b :rowIdx) inc))
-    (commit-block b)))
+(defn move-down [f b]
+  (dosync (alter (b :y) inc))
+  (if (not (check-position-valid f b))
+    (do
+      (dosync (alter (b :y) dec))
+      (commit-block f b)
+      (next-block))))
 
 (defn create-panel []
   (doto
     (proxy [JPanel KeyListener] []
       (paintComponent [g]
         (proxy-super paintComponent g)
-        (draw-all g))
+        (draw-all g global-field active-block))
       (getPreferredSize [] (Dimension. (prefs :screen-width)
                                        (prefs :screen-height)))
       (keyPressed [e]
         (let [keyCode (.getKeyCode e)]          
           (.repaint this)
-          (if (== 37 keyCode) (move-left active-block)
+          (if (== 37 keyCode) (move-left global-field active-block)
           (if (== 38 keyCode) (rotate active-block)
-          (if (== 39 keyCode) (move-right active-block)
-          (if (== 40 keyCode) (move-down active-block)
+          (if (== 39 keyCode) (move-right global-field active-block)
+          (if (== 40 keyCode) (move-down global-field active-block)
           (if (== 32 keyCode) (next-block)
                               (println keyCode))))))))
       (keyReleased [e])
@@ -408,8 +364,7 @@ O(2N). Each item in coll is
     (dosync (alter bag-of-blocks shuffle))
     (next-block)
     (loop []
-      ;(.repaint panel)
       (Thread/sleep 10)
       (recur))))
 
-;(main)
+(main)

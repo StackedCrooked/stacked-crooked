c50c290be921f8b23e2142d0deda6bedd912558d
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2010-05-19 19:29:53 +0000

    Add C++ code for calculating prime numbers.

--- PlayGround/DynamicAttributes/main.cpp
+++ PlayGround/DynamicAttributes/main.cpp
@@ -1,3 +1,7 @@
+#include "Attribute.h"
+#include "AttributeContainer.h"
+#include "CommonAttributes.h"
+#include "TypedAttribute.h"
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
 #include <boost/lexical_cast.hpp>
@@ -10,217 +14,6 @@
 namespace XULWin
 {
 
-    class Attribute
-    {
-    public:
-        Attribute(const std::string & inName) : mName(inName) {}
-
-        virtual ~Attribute() {}
-
-        //virtual bool getStringValue(std::string & outValue) = 0;
-
-        //virtual bool setStringValue(const std::string & inValue) = 0;
-
-        const std::string & name() const { return mName; }
-
-    private:
-        std::string mName;
-    };
-
-
-    template<class ValueT, class ConstRefT>
-    class TypedAttribute : public Attribute
-    {
-    public:
-        typedef ConstRefT ConstRefType;
-        typedef ValueT ValueType;
-        
-        // We have to return a value type for the getter because we don't know how the variable
-        // will be stored and retrieved.
-        typedef boost::function<bool(ValueType&)> Getter;
-        typedef boost::function<bool(ConstRefType)> Setter;
-
-        TypedAttribute(const std::string & inName, Getter inGetter, Setter inSetter) :
-            Attribute(inName),
-            mGetter(inGetter),
-            mSetter(inSetter)
-        {
-        }
-
-        bool getValue(ValueType & outValue) const
-        {
-            return mGetter(outValue);
-        }
-
-        bool setValue(ConstRefType inValue)
-        {
-            return mSetter(inValue);
-        }
-
-        //virtual bool getStringValue(std::string & outValue)
-        //{
-        //    ValueType value;
-        //    if (getValue(value))
-        //    {
-        //        outValue = boost::lexical_cast<std::string>(value);
-        //        return true;
-        //    }
-        //    return false;
-        //}
-
-        //virtual bool setStringValue(const std::string & inValue)
-        //{            
-        //    ValueType value = boost::lexical_cast<ValueType>(inValue);
-        //    return setValue(value);
-        //}
-
-    private:
-        Getter mGetter;
-        Setter mSetter;
-    };
-
-
-    class AttributeContainer
-    {
-    public:
-        template<class T>
-        void registerAttribute(typename T::Getter inGetter,
-                               typename T::Setter inSetter)
-        {
-            if (mAttributes.find(T::Name()) == mAttributes.end())
-            {
-                mAttributes.insert(std::make_pair(T::Name(), new T(inGetter, inSetter)));
-                return;
-            }
-            std::stringstream ss;
-            ss << "An attribute with name '" << T::Name() << "' already exists.";
-            std::string msg = ss.str();
-            throw std::runtime_error(msg.c_str());
-        }
-
-
-        template<class T>
-        const T & attribute() const
-        {
-            Attributes::const_iterator it = mAttributes.find(T::Name());
-            if (it == mAttributes.end())
-            {
-                std::stringstream ss;
-                ss << "No attribute found with name '" << T::Name() << "'.";
-                std::string msg = ss.str();
-                throw std::runtime_error(msg.c_str());
-            }
-            T * attr = dynamic_cast<T*>(it->second);
-            if (!attr)
-            {
-                throw std::runtime_error("Dynamic cast failed.");
-            }
-            return *attr;
-        }
-
-
-        template<class T>
-        T & attribute()
-        {
-            return const_cast<T &>(static_cast<const AttributeContainer*>(this)->attribute<T>());
-        }
-
-
-        bool getAttribute(const std::string & inName, Attribute *& outAttribute) const
-        {
-            Attributes::const_iterator it = mAttributes.find(inName);
-            if (it != mAttributes.end())
-            {
-                outAttribute = it->second;
-                return true;
-            }
-            return false;
-        }
-
-
-        template<class T>
-        typename T::ValueType get() const
-        {
-            const T & attr = attribute<T>();
-            T::ValueType result;
-            if (!attr.getValue(result))
-            {
-                std::stringstream ss;
-                ss << "No attribute found with name '" << T::Name() << "'.";
-                std::string msg = ss.str();
-                throw std::runtime_error(msg.c_str());
-            }
-            return result;
-        }
-
-
-        template<class T>
-        void set(typename T::ConstRefType inValue)
-        {
-            T & attr = attribute<T>();
-            if (!attr.setValue(inValue))
-            {
-                throw std::runtime_error("Failed to set attribute value.");
-            }
-        }
-
-
-    private:
-        typedef std::map<std::string, Attribute*> Attributes;
-        Attributes mAttributes;
-    };
-
-    
-    //**
-    // * DECLARE_ATTRIBUTE
-    // * 
-    // * This macro declares a XUL attribute.
-    // */
-    #define DECLARE_ATTRIBUTE(classname, valuetype, reftype, attributename)                           \
-        class classname : public TypedAttribute<valuetype, reftype>                                   \
-        {                                                                                             \
-        public:                                                                                       \
-            typedef TypedAttribute<valuetype, reftype> Super;                                         \
-            classname(Super::Getter inGetter, Super::Setter inSetter) :                               \
-                Super(Name(), inGetter, inSetter)                                                     \
-            {                                                                                         \
-            }                                                                                         \
-                                                                                                      \
-            static const char * Name() { return attributename; }                                      \
-                                                                                                      \
-        };
-
-    //**
-    // * DECLARE_INT_ATTRIBUTE
-    // *
-    // * Declares a XUL attribute that has an int value.
-    // */
-    #define DECLARE_INT_ATTRIBUTE(classname, attributename) \
-        DECLARE_ATTRIBUTE(classname, int, int, attributename)
-
-    //**
-    // * DECLARE_BOOL_ATTRIBUTE
-    // *
-    // * Declares a XUL attribute that has a boolean value.
-    // */
-    #define DECLARE_BOOL_ATTRIBUTE(classname, attributename) \
-        DECLARE_ATTRIBUTE(classname, bool, bool, attributename)
-
-    //**
-    // * DECLARE_STRING_ATTRIBUTE
-    // *
-    // * Declares a XUL attribute that has a std::string value.
-    // */
-    #define DECLARE_STRING_ATTRIBUTE(classname, attributename) \
-        DECLARE_ATTRIBUTE(classname, std::string, const std::string &, attributename)
-
-    //**
-    // * DECLARE_FUNCTOR_ATTRIBUTE
-    // *
-    // */
-    #define DECLARE_FUNCTOR_ATTRIBUTE(classname, attributename) \
-        DECLARE_ATTRIBUTE(classname, boost::function<void()>, const boost::function<void()> &, attributename)
-
     //**
     // * DECLARE_HWND_ATTRIBUTE
     // *
@@ -229,15 +22,7 @@ namespace XULWin
         DECLARE_ATTRIBUTE(classname, HWND, HWND, attributename)
 
     
-    DECLARE_INT_ATTRIBUTE(Width, "width")
-    DECLARE_INT_ATTRIBUTE(Height, "height")
-    DECLARE_BOOL_ATTRIBUTE(Hidden, "hidden")
-    DECLARE_STRING_ATTRIBUTE(Title, "title")
-    DECLARE_FUNCTOR_ATTRIBUTE(Factory, "Component_Constructor");
-    DECLARE_FUNCTOR_ATTRIBUTE(Destructor, "Component_Destructor");
-    DECLARE_FUNCTOR_ATTRIBUTE(Initializer, "Component_Initializer");
     DECLARE_HWND_ATTRIBUTE(Handle, "hwnd");
-
 
 
     template<class State>
@@ -429,9 +214,11 @@ bool setParent(WinComponent * inComponent, const WinComponent * inParent)
     return true;
 }
 
-#define REGISTER_ATTRIBUTE(attribute, object) \
-    object->registerAttribute<attribute>(boost::bind(&get##attribute, object, _1), \
-                                         boost::bind(&set##attribute, object, _1));
+
+#define ADD_ATTRIBUTE(AttributeType, obj) \
+    obj->addAttribute<AttributeType>(boost::bind(&get##AttributeType, obj, _1), \
+                                     boost::bind(&set##AttributeType, obj, _1));
+
 
 class ComponentFactory
 {
@@ -439,17 +226,18 @@ public:
     std::auto_ptr<WinComponent> makeComponent(WinComponent * inParent)
     {
         std::auto_ptr<WinComponent> comp(new WinComponent);
-        REGISTER_ATTRIBUTE(Factory, comp.get())
-        REGISTER_ATTRIBUTE(Initializer, comp.get())
-        REGISTER_ATTRIBUTE(Destructor, comp.get())
-        REGISTER_ATTRIBUTE(Title, comp.get())
-        REGISTER_ATTRIBUTE(Width, comp.get())
-        REGISTER_ATTRIBUTE(Height, comp.get())
-        REGISTER_ATTRIBUTE(Handle, comp.get())
-        REGISTER_ATTRIBUTE(Parent, comp.get())
+        ADD_ATTRIBUTE(Factory, comp.get())
+        ADD_ATTRIBUTE(Initializer, comp.get())
+        ADD_ATTRIBUTE(Destructor, comp.get())
+        ADD_ATTRIBUTE(Title, comp.get())
+        ADD_ATTRIBUTE(Width, comp.get())
+        ADD_ATTRIBUTE(Height, comp.get())
+        ADD_ATTRIBUTE(Handle, comp.get())
+        ADD_ATTRIBUTE(Parent, comp.get())
         return comp;
     }
 };
+
 
 int main()
 {   
@@ -458,14 +246,16 @@ int main()
     comp->create();
     comp->init();
 
-    int w = 0;
-    comp->get<Title>();
+	std::string title = comp->get<Title>();
+	assert(title.empty());
+	comp->set<Title>("test");
+	title = comp->get<Title>();
+	assert(title == "test");
+
     comp->set<Width>(80);
+	int w = comp->get<Width>();
 
-    const std::string & title = comp->get<Title>();
-    comp->set<Title>("test");
-
-    std::string widthAsString;
-    Width & width = comp->attribute<Width>();
+    Width & width = comp->getAttribute<Width>();
+	width.getValue(w);
     return 0;
 }
--- PlayGround/M4/play.m4
+++ PlayGround/M4/play.m4
@@ -15,19 +15,13 @@ define(`FOREACH', `for (int _idx = 0; _idx != $1.size(); ++_idx)
 
 int main()
 {
-    // foreach
-    FOREACH(str, char c,
-        printf("ASCII %d", c);
-        printf("String representation: %c.\n", c);)
-
-
-    std::vector<std::string> sentence;
-    sentence.push_back("one");
-    sentence.push_back("two");
-    sentence.push_back("three");
+    std::vector<std::string> wordList;
+    wordList.push_back("one");
+    wordList.push_back("two");
+    wordList.push_back("three");
 
     // nested foreach
-    FOREACH(sentence, const std::string & word,
+    FOREACH(wordList, const std::string & word,
         FOREACH(word, char c,
             // print the character
             printf("Char: %c.\n", c);))

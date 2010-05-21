674d0a6dc7fa07eb46d22481824c44852208de8c
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2010-05-21 19:19:01 +0000

    Cleanup repository.

--- BBB/asterisk/06_create_red5_sip_accounts.sh
+++
@@ -1,3 +0,0 @@
-sudo wget http://bigbluebutton.googlecode.com/files/asterisk-create-sip-accounts.sh
-sudo chmod a+x asterisk-create-sip-accounts.sh
-sudo ./asterisk-create-sip-accounts.sh
--- Linux/install-common-dev-tools.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install gcc make 
-sudo apt-get install autoconf automake libtool
-sudo apt-get install cvs 
-sudo apt-get install patch
-sudo apt-get install subversion

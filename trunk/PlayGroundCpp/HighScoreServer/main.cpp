c1a1ba4de53f71cafdcc0d902151c2c457c14351
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-05-22 00:36:46 +0000

    Cleanup main.cpp, simplify HTTPServer class.

--- trunk/PlayGroundCpp/HighScoreServer/main.cpp
+++ trunk/PlayGroundCpp/HighScoreServer/main.cpp
@@ -40,9 +40,6 @@
 #include "Poco/Net/HTTPServerResponse.h"
 #include "Poco/Net/HTTPServerParams.h"
 #include "Poco/Net/ServerSocket.h"
-#include "Poco/Timestamp.h"
-#include "Poco/DateTimeFormatter.h"
-#include "Poco/DateTimeFormat.h"
 #include "Poco/Exception.h"
 #include "Poco/ThreadPool.h"
 #include "Poco/Util/ServerApplication.h"
@@ -59,9 +56,6 @@ using Poco::Net::HTTPServer;
 using Poco::Net::HTTPServerRequest;
 using Poco::Net::HTTPServerResponse;
 using Poco::Net::HTTPServerParams;
-using Poco::Timestamp;
-using Poco::DateTimeFormatter;
-using Poco::DateTimeFormat;
 using Poco::ThreadPool;
 using Poco::Util::ServerApplication;
 using Poco::Util::Application;
@@ -70,162 +64,64 @@ using Poco::Util::OptionSet;
 using Poco::Util::HelpFormatter;
 
 
-class TimeRequestHandler: public HTTPRequestHandler
-	/// Return a HTML document with the current date and time.
+class HighScoreRequestHandler: public HTTPRequestHandler
 {
-public:
-	TimeRequestHandler(const std::string& format): 
-		_format(format)
-	{
-	}
-	
+public:	
+	// Return a HTML document with the HighScores
 	void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
 	{
 		Application& app = Application::instance();
 		app.logger().information("Request from " + request.clientAddress().toString());
 
-		Timestamp now;
-		std::string dt(DateTimeFormatter::format(now, _format));
-
 		response.setChunkedTransferEncoding(true);
 		response.setContentType("text/html");
 
 		std::ostream& ostr = response.send();
-		ostr << "<html><head><title>HighScoreServer powered by POCO C++ Libraries</title>";
-		ostr << "<meta http-equiv=\"refresh\" content=\"1\"></head>";
-		ostr << "<body><p style=\"text-align: center; font-size: 48px;\">";
-		ostr << dt;
-		ostr << "</p></body></html>";
+		ostr << "<html><head><body>You have nice high scores!</body></html>";
 	}
-	
-private:
-	std::string _format;
 };
 
 
-class TimeRequestHandlerFactory: public HTTPRequestHandlerFactory
+class HighScoreRequestHandlerFactory: public HTTPRequestHandlerFactory
 {
 public:
-	TimeRequestHandlerFactory(const std::string& format):
-		_format(format)
-	{
-	}
 
 	HTTPRequestHandler* createRequestHandler(const HTTPServerRequest& request)
 	{
 		if (request.getURI() == "/")
-			return new TimeRequestHandler(_format);
+        {
+			return new HighScoreRequestHandler;
+        }
 		else
+        {
 			return 0;
+        }
 	}
-	
-private:
-	std::string _format;
 };
 
 
 class HighScoreServer: public Poco::Util::ServerApplication
-	/// The main application class.
-	///
-	/// This class handles command-line arguments and
-	/// configuration files.
-	/// Start the HighScoreServer executable with the help
-	/// option (/help on Windows, --help on Unix) for
-	/// the available command line options.
-	///
-	/// To use the sample configuration file (HighScoreServer.properties),
-	/// copy the file to the directory where the HighScoreServer executable
-	/// resides. If you start the debug version of the HighScoreServer
-	/// (HTTPTimeServerd[.exe]), you must also create a copy of the configuration
-	/// file named HTTPTimeServerd.properties. In the configuration file, you
-	/// can specify the port on which the server is listening (default
-	/// 9980) and the format of the date/time string sent back to the client.
-	///
-	/// To test the TimeServer you can use any web browser (http://localhost:9980/).
 {
-public:
-	HighScoreServer(): _helpRequested(false)
-	{
-	}
-	
-	~HighScoreServer()
-	{
-	}
-
 protected:
-	void initialize(Application& self)
-	{
-		loadConfiguration(); // load default configuration files, if present
-		ServerApplication::initialize(self);
-	}
-		
-	void uninitialize()
-	{
-		ServerApplication::uninitialize();
-	}
-
-	void defineOptions(OptionSet& options)
-	{
-		ServerApplication::defineOptions(options);
-		
-		options.addOption(
-			Option("help", "h", "display help information on command line arguments")
-				.required(false)
-				.repeatable(false));
-	}
-
-	void handleOption(const std::string& name, const std::string& value)
-	{
-		ServerApplication::handleOption(name, value);
-
-		if (name == "help")
-			_helpRequested = true;
-	}
-
-	void displayHelp()
-	{
-		HelpFormatter helpFormatter(options());
-		helpFormatter.setCommand(commandName());
-		helpFormatter.setUsage("OPTIONS");
-		helpFormatter.setHeader("A web server that serves the current date and time.");
-		helpFormatter.format(std::cout);
-	}
 
 	int main(const std::vector<std::string>& args)
 	{
-		if (_helpRequested)
-		{
-			displayHelp();
-		}
-		else
-		{
-			// get parameters from configuration file
-			unsigned short port = (unsigned short) config().getInt("HighScoreServer.port", 9980);
-			std::string format(config().getString("HighScoreServer.format", DateTimeFormat::SORTABLE_FORMAT));
-			int maxQueued  = config().getInt("HighScoreServer.maxQueued", 100);
-			int maxThreads = config().getInt("HighScoreServer.maxThreads", 16);
-			ThreadPool::defaultPool().addCapacity(maxThreads);
-			
-			HTTPServerParams* pParams = new HTTPServerParams;
-			pParams->setMaxQueued(maxQueued);
-			pParams->setMaxThreads(maxThreads);
-			
-			// set-up a server socket
-			ServerSocket svs(port);
-			// set-up a HTTPServer instance
-			HTTPServer srv(new TimeRequestHandlerFactory(format), svs, pParams);
-			// start the HTTPServer
-			srv.start();
-			// wait for CTRL-C or kill
-			waitForTerminationRequest();
-			// Stop the HTTPServer
-			srv.stop();
-		}
+		int port = config().getInt("HighScoreServer.port", 9980);
+		int maxQueued  = config().getInt("HighScoreServer.maxQueued", 100);
+		int maxThreads = config().getInt("HighScoreServer.maxThreads", 16);
+		ThreadPool::defaultPool().addCapacity(maxThreads);
+		
+		HTTPServerParams* pParams = new HTTPServerParams;
+		pParams->setMaxQueued(maxQueued);
+		pParams->setMaxThreads(maxThreads);
+
+		ServerSocket serverSocket(port);
+		HTTPServer httpServer(new HighScoreRequestHandlerFactory, serverSocket, pParams);
+		httpServer.start();
+		waitForTerminationRequest();
+		httpServer.stop();
 		return Application::EXIT_OK;
 	}
-	
-private:
-	bool _helpRequested;
 };
 
 

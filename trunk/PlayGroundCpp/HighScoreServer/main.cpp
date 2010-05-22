1f663d93e449571fae643d0f3869e6b0055446f1
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-05-22 01:49:57 +0000

    Fix Poco Data project settings. Use Unicode in all projects.

--- trunk/PlayGroundCpp/HighScoreServer/main.cpp
+++ trunk/PlayGroundCpp/HighScoreServer/main.cpp
@@ -1,12 +1,10 @@
 #include "HighScoreServer.h"
-#include "SQLiteSupport.h"
 
 using namespace HSServer;
 
 
 int main(int argc, char** argv)
 {
-    SQLiteSupport sqlite;
     HighScoreServer app;
 	return app.run(argc, argv);
 }

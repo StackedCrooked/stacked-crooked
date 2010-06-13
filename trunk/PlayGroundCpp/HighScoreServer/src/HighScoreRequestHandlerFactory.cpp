0f13f091bbe5c18a779118a40f23fc7f997c2fec
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-06-13 04:23:41 +0000

    Log all requests.

--- trunk/PlayGroundCpp/HighScoreServer/src/HighScoreRequestHandlerFactory.cpp
+++ trunk/PlayGroundCpp/HighScoreServer/src/HighScoreRequestHandlerFactory.cpp
@@ -2,6 +2,7 @@
 #include "Poco/String.h"
 #include "Poco/StringTokenizer.h"
 #include "Poco/Net/HTTPRequestHandler.h"
+#include "Poco/Util/Application.h"
 
 
 namespace HSServer
@@ -9,7 +10,10 @@ namespace HSServer
 
     Poco::Net::HTTPRequestHandler *
     HighScoreRequestHandlerFactory::createRequestHandler(const Poco::Net::HTTPServerRequest& inRequest)
-    {        
+    {
+        // Log the request
+        Poco::Util::Application::instance().logger().information("Request with uri: " + inRequest.getURI());
+
         if (inRequest.getURI().find("/method=GetAllHighScores") == 0)
         {
             return GetAllHighScores::Create(inRequest.getURI());

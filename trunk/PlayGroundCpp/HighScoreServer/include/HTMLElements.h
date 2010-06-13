8f3042edc6fdebb7c5cc21690795a0309316e268
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-06-13 08:01:49 +0000

    Improved HTML component system.

--- trunk/PlayGroundCpp/HighScoreServer/include/HTMLElements.h
+++ trunk/PlayGroundCpp/HighScoreServer/include/HTMLElements.h
@@ -3,6 +3,7 @@
 
 
 #include <iostream>
+#include <stack>
 #include <string>
 
 
@@ -10,23 +11,81 @@ namespace HTML
 {
     static const size_t cIndent = 2;
 
+    /**
+     * Provides a scope where a certain output stream is defined.
+     */
+    class CurrentOutStream
+    {
+    public:
+        CurrentOutStream(std::ostream & outStream);
+
+        ~CurrentOutStream();
+
+        static std::ostream & CurrentlyActive();
+
+    private:
+        std::ostream & mOutStream;
+        static std::stack<CurrentOutStream*> sInstanceStack;
+    };
+
+    void Write(const std::string & inText);
+
+    std::string Whitespace(size_t n);
+
+    // Requires that a CurrentOutStream object is currently existing.
     class HTMLElement
     {
     public:
-        HTMLElement(const std::string & inTagName, std::ostream & outStream);
+        
+        HTMLElement(const std::string & inTagName);
 
-        HTMLElement(const std::string & inTagName, const std::string& inText, std::ostream & outStream);
+        HTMLElement(const std::string & inTagName, const std::string & inText);
 
-        ~HTMLElement();
+        virtual ~HTMLElement();
 
     private:
-        std::string whitespace(size_t n);
+
+        std::string openTag();
+        std::string closeTag();
+        std::string selfClosingTag();
+        std::string completeNode();
+
+        static std::string OpenTag(const std::string & inTagName);
+        static std::string CloseTag(const std::string & inTagName);
+        static std::string OpenCloseTag(const std::string & inTagName);
+        static std::string Surround(const std::string & inTagName, const std::string & inText);
 
         std::string mTagName;
         std::string mText;
         std::ostream & mOutStream;
-        static size_t sDepth;
     };
+
+
+    #define DECLARE_HTML_ELEMENT(ELEMENTNAME, TAGNAME) \
+        class ELEMENTNAME : public HTMLElement                                                                  \
+        {                                                                                                       \
+        public:                                                                                                 \
+            ELEMENTNAME() : HTMLElement(TAGNAME) {}                                                             \
+            ELEMENTNAME(const std::string & inText) : HTMLElement(TAGNAME, inText) {}                           \
+        };
+
+
+    DECLARE_HTML_ELEMENT(Html, "html");
+    DECLARE_HTML_ELEMENT(Head, "head");
+    DECLARE_HTML_ELEMENT(Body, "body");
+    DECLARE_HTML_ELEMENT(H1, "h1");
+    DECLARE_HTML_ELEMENT(H2, "h2");
+    DECLARE_HTML_ELEMENT(H3, "h3");
+    DECLARE_HTML_ELEMENT(H4, "h4");
+    DECLARE_HTML_ELEMENT(P, "p");
+    DECLARE_HTML_ELEMENT(B, "b");
+    DECLARE_HTML_ELEMENT(I, "i");
+    DECLARE_HTML_ELEMENT(U, "u");
+    DECLARE_HTML_ELEMENT(Blockquote, "blockquote");
+    DECLARE_HTML_ELEMENT(Br, "br");
+    DECLARE_HTML_ELEMENT(Table, "table");
+    DECLARE_HTML_ELEMENT(Tr, "tr");
+    DECLARE_HTML_ELEMENT(Td, "td");
 
 } // HTML
 
--- trunk/PlayGroundCpp/HighScoreServer/src/HTMLElements.cpp
+++ trunk/PlayGroundCpp/HighScoreServer/src/HTMLElements.cpp
@@ -3,39 +3,45 @@
 
 namespace HTML
 {
-    size_t HTMLElement::sDepth(0);
+    static size_t sDepth(0);
+
+    std::stack<CurrentOutStream *> CurrentOutStream::sInstanceStack;
 
 
-    HTMLElement::HTMLElement(const std::string & inTagName, std::ostream & outStream) :
-        mTagName(inTagName),
+    CurrentOutStream::CurrentOutStream(std::ostream & outStream) :
         mOutStream(outStream)
     {
-        mOutStream << whitespace(sDepth) << "<" << mTagName << ">\n";
-        sDepth++;
+        sInstanceStack.push(this);
     }
 
 
-    HTMLElement::HTMLElement(const std::string & inTagName, const std::string& inText, std::ostream & outStream) :
-        mTagName(inTagName),
-        mText(inText),
-        mOutStream(outStream)
+    CurrentOutStream::~CurrentOutStream()
     {
-        mOutStream << "" << whitespace(sDepth) << "<" << mTagName << ">" << mText << "</" << mTagName << ">\n";
-        sDepth++;
+        sInstanceStack.pop();
     }
 
 
-    HTMLElement::~HTMLElement()
+    std::ostream & CurrentOutStream::CurrentlyActive()
     {
-        sDepth--;
-        if (mText.empty())
+        if (sInstanceStack.empty())
         {
-            mOutStream << whitespace(sDepth) << "</" << mTagName << ">\n";
+            throw std::runtime_error("There is no currenly active HTML output stream.");
         }
+        return sInstanceStack.top()->mOutStream;
     }
 
 
-    std::string HTMLElement::whitespace(size_t n)
+    void Write(const std::string & inText)
+    {
+        if (inText.empty())
+        {
+            return;
+        }
+        CurrentOutStream::CurrentlyActive() << Whitespace(sDepth) << inText;
+    }
+
+
+    std::string Whitespace(size_t n)
     {
         std::string result;
         for (size_t idx = 0; idx != cIndent*n; ++idx)
@@ -43,6 +49,83 @@ namespace HTML
             result += " ";
         }
         return result;
+    }
+
+
+    HTMLElement::HTMLElement(const std::string & inTagName) :
+        mTagName(inTagName),
+        mOutStream(CurrentOutStream::CurrentlyActive())
+    {
+        mOutStream << Whitespace(sDepth) << openTag() << "\n";
+        sDepth++;
+    }
+
+
+    HTMLElement::HTMLElement(const std::string & inTagName, const std::string& inText) :
+        mTagName(inTagName),
+        mText(inText),
+        mOutStream(CurrentOutStream::CurrentlyActive())
+    {
+        mOutStream << Whitespace(sDepth) << completeNode() << "\n";
+        // Note: don't increment sDepth here. It won't have children.
+    }
+
+
+    HTMLElement::~HTMLElement()
+    {
+        if (mText.empty())
+        {
+            sDepth--;
+            mOutStream << Whitespace(sDepth) << closeTag() << "\n";            
+        }
+    }
+
+
+    std::string HTMLElement::openTag()
+    {
+        return OpenTag(mTagName);
+    }
+
+
+    std::string HTMLElement::closeTag()
+    {
+        return CloseTag(mTagName);
+    }
+
+
+    std::string HTMLElement::selfClosingTag()
+    {
+        return OpenCloseTag(mTagName);
+    }
+
+
+    std::string HTMLElement::completeNode()
+    {
+        return Surround(mTagName, mText);
+    }
+
+    
+    std::string HTMLElement::OpenTag(const std::string & inTagName)
+    {
+        return "<" + inTagName + ">";
+    }
+
+
+    std::string HTMLElement::CloseTag(const std::string & inTagName)
+    {
+        return "</" + inTagName + ">";
+    }
+
+
+    std::string HTMLElement::OpenCloseTag(const std::string & inTagName)
+    {
+        return "<" + inTagName + "/>";
+    }
+
+
+    std::string HTMLElement::Surround(const std::string & inTagName, const std::string & inText)
+    {
+        return OpenTag(inTagName) + inText + CloseTag(inTagName);
     }
 
 

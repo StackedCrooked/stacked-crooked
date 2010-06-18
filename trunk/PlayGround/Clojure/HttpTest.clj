27e196a5dab5ec9a6a9f412ba30be59a6faa933c
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-06-18 21:10:52 +0000

    Add HTTP sample.

---
+++ trunk/PlayGround/Clojure/HttpTest.clj
@@ -0,0 +1,20 @@
+(import java.net.URL)
+(import java.io.InputStreamReader)
+(import java.io.BufferedReader)
+(defn get-stream [url-string]
+    (let [url           (URL. url-string)
+          connection    (.openConnection url)
+          is            (.getInputStream connection)
+          isr           (InputStreamReader. is)
+          in            (BufferedReader. isr)]
+        in))
+(defn get-url [url-string]
+    (let [in      (get-stream url-string)]
+        (loop [line (.readLine in)
+               result (str)]
+            (if (not (nil? line))
+                (do (recur (.readLine in) (str result line)))
+                result))))
+
+; Print Google homepage HTML
+(printn (get-url "http://www.google.com"))

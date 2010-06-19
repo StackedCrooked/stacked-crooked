f34c70421295ded9a7e17980ad6d7f7b3b4d76bf
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-06-19 18:40:36 +0000

    Use atoms instead of refs.

--- trunk/PlayGround/Clojure/Tetris.clj
+++ trunk/PlayGround/Clojure/Tetris.clj
@@ -1,3 +1,17 @@
+; TODO
+; ----
+; - Support cancel button
+; - High-score tabular formatting
+; - Create an uber-jar with Leiningen (requires Linux -> VirtualBox)
+; - Create a self-contained executable (IzPack)
+; - Create an applet (if possible)
+; v lines
+; v level
+; v show score http://tetris.wikia.com/wiki/Scoring
+; v game over message
+; v drop block space bar
+;(main)
+
 (in-ns 'tetris)
 (clojure.core/use 'clojure.core)
 (import java.awt.Color)
@@ -82,18 +96,16 @@
   (struct-map grid
     :width  w
     :height h
-    :data   (ref (vec (repeat (* w h) initial-value)))))
+    :data   (atom (vec (repeat (* w h) initial-value)))))
 
 (defn reset-grid [g initial-value]
-  (alter (g :data)
-           (fn [_] (vec (repeat (* (g :width) (g :height))
-                                   initial-value)))))
+  (reset! (g :data) (vec (repeat (* (g :width) (g :height)) initial-value))))
 
 (defn create-grid-with-data [w h data]
   (struct-map grid
     :width w
     :height h
-    :data (ref data)))
+    :data (atom data)))
     
 (defn get-grid [g x y]
   (let [data  @(g :data)
@@ -103,7 +115,7 @@
 (defn set-grid [g x y value]
   (let [data  @(g :data)
         idx   (+ x (* (g :width) y)) ]
-    (alter (g :data) (fn [_] (assoc data idx value)))))
+    (reset! (g :data) (assoc data idx value))))
 
 (defn get-grid-rows [g]
   (partition (g :width) @(g :data)))
@@ -232,71 +244,70 @@
                                         0 7 0 ])]})
 
 (def active-block {
-    :type (ref l-block)
-    :rotation (ref 0)
-    :x (ref 0)
-    :y (ref 0) })
+    :type (atom l-block)
+    :rotation (atom 0)
+    :x (atom 0)
+    :y (atom 0) })
 
 (def global-field (create-grid 10 20 0))
 
 (def block-types
   [i-block j-block l-block o-block s-block t-block z-block])
 
-(def bag-of-blocks (ref block-types)) ;(into block-types block-types)))
+(def bag-of-blocks (atom block-types)) ;(into block-types block-types)))
 
-(def bag-index (ref 0))
+(def bag-index (atom 0))
 
-(def next-blocks (ref []))
+(def next-blocks (atom []))
 (def num-next-blocks 1)
 (def is-game-over (atom false))
-(def user-name (ref (str)))
-(def hs-xml (ref nil))
+(def user-name (atom (str)))
+(def hs-xml (atom nil))
 
 (defn current-time-ms [] (.getTime (java.util.Date.)))
-(def timer (ref (new Timer)))
-(def frame-count (ref 0))
-(def start-time (ref (current-time-ms)))
-(def fps (ref 0))
+(def timer (atom (new Timer)))
+(def frame-count (atom 0))
+(def start-time (atom (current-time-ms)))
+(def fps (atom 0))
 
 ; stats
 (def stats {
-  :score (ref 0)
-  :lines (ref 0)
-  :singles (ref 0)
-  :doubles (ref 0)
-  :triples (ref 0)
-  :tetrises (ref 0)
-  :i-blocks (ref 0)
-  :j-blocks (ref 0)
-  :l-blocks (ref 0)
-  :o-blocks (ref 0)
-  :s-blocks (ref 0)
-  :t-blocks (ref 0)
-  :z-blocks (ref 0)
+  :score (atom 0)
+  :lines (atom 0)
+  :singles (atom 0)
+  :doubles (atom 0)
+  :triples (atom 0)
+  :tetrises (atom 0)
+  :i-blocks (atom 0)
+  :j-blocks (atom 0)
+  :l-blocks (atom 0)
+  :o-blocks (atom 0)
+  :s-blocks (atom 0)
+  :t-blocks (atom 0)
+  :z-blocks (atom 0)
 })
 
 (defn reset-stats []
-  (alter (stats :score) (fn [_] 0))
-  (alter (stats :lines) (fn [_] 0))
-  (alter (stats :singles) (fn [_] 0))
-  (alter (stats :doubles) (fn [_] 0))
-  (alter (stats :triples) (fn [_] 0))
-  (alter (stats :tetrises) (fn [_] 0))
-  (alter (stats :i-blocks) (fn [_] 0))
-  (alter (stats :j-blocks) (fn [_] 0))
-  (alter (stats :l-blocks) (fn [_] 0))
-  (alter (stats :o-blocks) (fn [_] 0))
-  (alter (stats :s-blocks) (fn [_] 0))
-  (alter (stats :t-blocks) (fn [_] 0))
-  (alter (stats :z-blocks) (fn [_] 0)))
+  (reset! (stats :score) 0)
+  (reset! (stats :lines) 0)
+  (reset! (stats :singles) 0)
+  (reset! (stats :doubles) 0)
+  (reset! (stats :triples) 0)
+  (reset! (stats :tetrises) 0)
+  (reset! (stats :i-blocks) 0)
+  (reset! (stats :j-blocks) 0)
+  (reset! (stats :l-blocks) 0)
+  (reset! (stats :o-blocks) 0)
+  (reset! (stats :s-blocks) 0)
+  (reset! (stats :t-blocks) 0)
+  (reset! (stats :z-blocks) 0))
 
 
 (defn init-blocks []
-  (dosync    
     (reset-grid global-field 0)
-    (alter bag-of-blocks ( fn [_] (shuffle block-types)))
-    (alter (active-block :type) (fn [_] (first @bag-of-blocks)))
-    (alter (active-block :x)
+    (reset! bag-of-blocks (shuffle block-types))
+    (reset! (active-block :type) (first @bag-of-blocks))
+    (swap! (active-block :x)
           (fn [x]
             (let [  block-type  @(active-block :type)
                     block-x     x
@@ -308,27 +319,26 @@
                          (block-grid :width)) 2)))))
     (if-not (zero? num-next-blocks)
       (do
-        (alter next-blocks
-          (fn [_] (subvec (vec @bag-of-blocks) 1 (inc num-next-blocks))))))
-    (alter bag-of-blocks
-           (fn [_] (drop (inc num-next-blocks) @bag-of-blocks)))))
+        (reset! next-blocks
+          (subvec (vec @bag-of-blocks) 1 (inc num-next-blocks)))))
+    (reset! bag-of-blocks
+           (drop (inc num-next-blocks) @bag-of-blocks)))
 
 (declare check-position-valid)
 (declare do-game-over)
 
 (defn next-block []
-  (alter  (active-block :type)
-          (fn [_]
+  (reset!  (active-block :type)
             (if-not (zero? num-next-blocks)
               (first @next-blocks)
-              (first @bag-of-blocks))))
+              (first @bag-of-blocks)))
   (if-not (zero? num-next-blocks)
-    (alter next-blocks (fn [nb] (conj (vec (drop 1 nb)) (first @bag-of-blocks)))))
-  (alter bag-of-blocks (fn [v] (vec (rest v))))
+    (swap! next-blocks (fn [nb] (conj (vec (drop 1 nb)) (first @bag-of-blocks)))))
+  (swap! bag-of-blocks (fn [v] (vec (rest v))))
   (if (empty? @bag-of-blocks)
-    (alter bag-of-blocks (fn [_] (shuffle block-types))))
-  (alter (active-block :rotation) (fn [_] 0))
-  (alter (active-block :x)
+    (reset! bag-of-blocks (shuffle block-types)))
+  (reset! (active-block :rotation) 0)
+  (swap! (active-block :x)
     (fn [x]
       (let [  block-type  @(active-block :type)
               block-x     x
@@ -338,7 +348,7 @@
               block-grid  (grids grid-idx)]
         (int (/ (- (global-field :width)
                    (block-grid :width)) 2)))))
-  (alter (active-block :y) (fn [_] 0))
+  (reset! (active-block :y) 0)
   (if-not (check-position-valid global-field active-block)
     (if (= false @is-game-over)
       (do-game-over))))
@@ -375,20 +385,19 @@
                 (if (= false @is-game-over)
                   (move-down global-field active-block))))]
     (.cancel @timer)
-    (alter timer (fn [_] (new Timer)))
+    (reset! timer (new Timer))
     (.scheduleAtFixedRate @timer task (long interval) (long interval))))
 
 (defn random-block []
-  (dosync
     (let [ bag       @bag-of-blocks
            bag-size  (count bag)    ]
     (if (== @bag-index bag-size)
       (do
-        (alter bag-index (fn [n] 0))
-        (alter bag-of-blocks shuffle)))
+        (reset! bag-index 0)
+        (swap! bag-of-blocks shuffle)))
     (do
-      (alter bag-index inc)
-      (nth bag @bag-index)))))
+      (swap! bag-index inc)
+      (nth bag @bag-index))))
   
 (defn get-color [grid-value]
   (let [color-table [ Color/BLACK
@@ -503,10 +512,9 @@
         time-interval       (if-not (zero? time-interval-s)
                               time-interval-s
                               1)]
-    (dosync
-      (alter fps (fn [_] (int (round (/ @frame-count time-interval)))))
-      (alter frame-count (fn [_] 0))
-      (alter start-time ( fn [_] (current-time-ms))))))
+      (reset! fps (int (round (/ @frame-count time-interval))))
+      (reset! frame-count 0)
+      (reset! start-time (current-time-ms))))
 
 (defn draw-fps [g]
   (doto g
@@ -649,22 +657,19 @@
                              (zero? (get-grid field field-x field-y))))))))
 
 (defn rotate [field block]
-  (dosync
-    (alter (block :rotation) inc)
+    (swap! (block :rotation) inc)
     (if-not (check-position-valid field block)
-      (alter (block :rotation) dec))))
+      (swap! (block :rotation) dec)))
 
 (defn move-left [f b]
-  (dosync
-    (alter (b :x) dec)
+    (swap! (b :x) dec)
     (if-not (check-position-valid f b)
-    (alter (b :x) inc))))
+    (swap! (b :x) inc)))
 
 (defn move-right [f b]
-  (dosync
-    (alter (b :x) inc)
+    (swap! (b :x) inc)
     (if-not (check-position-valid f b)
-      (alter (b :x) dec))))
+      (swap! (b :x) dec)))
 
 (defn update-score [num-lines-scored]
   (let [level   (get-level @(stats :lines))
@@ -673,7 +678,7 @@
                 (if (== num-lines-scored 3) (* 300 (inc num-lines-scored))
                 (if (== num-lines-scored 4) (* 1200 (inc num-lines-scored))
                 (println "ERROR: num-lines-scored is: " num-lines-scored))))) ]
-    (alter (stats :score) (fn [old-score] (+ old-score points)))))
+    (swap! (stats :score) (fn [old-score] (+ old-score points)))))
       
       
 (defn contains [collection value]
@@ -690,15 +695,14 @@
           ]
     (if-not (zero? num-lines-scored)
       (do
-        (alter (field :data)
-          (fn [_]
-            (let [zeroes (vec (repeat (* (field :width) num-lines-scored) 0))]
-              (vec (flatten (conj zeroes remaining-rows))))))
-        (alter (stats :lines) (fn [lines] (+ lines num-lines-scored)))
-        (if (== num-lines-scored 1) (alter (stats :singles) inc)
-        (if (== num-lines-scored 2) (alter (stats :doubles) inc)
-        (if (== num-lines-scored 3) (alter (stats :triples) inc)
-        (if (== num-lines-scored 4) (alter (stats :tetrises) inc)))))
+        (reset! (field :data)
+          (let [zeroes (vec (repeat (* (field :width) num-lines-scored) 0))]
+            (vec (flatten (conj zeroes remaining-rows)))))
+        (swap! (stats :lines) (fn [lines] (+ lines num-lines-scored)))
+        (if (== num-lines-scored 1) (swap! (stats :singles) inc)
+        (if (== num-lines-scored 2) (swap! (stats :doubles) inc)
+        (if (== num-lines-scored 3) (swap! (stats :triples) inc)
+        (if (== num-lines-scored 4) (swap! (stats :tetrises) inc)))))
         (update-score num-lines-scored))
       (set-game-speed (get-game-speed (get-level @(stats :lines)))))))
 
@@ -716,29 +720,27 @@
               (set-grid field (+ x block-x) (+ y block-y) value)))))))
 
 (defn move-down [f b]
-  (dosync
-    (alter (b :y) inc)
+    (swap! (b :y) inc)
     (if-not (check-position-valid f b)
       (do
-        (alter (b :y) dec)
+        (swap! (b :y) dec)
         (commit-block f b)
         (clear-lines f)
-        (next-block)))))
+        (next-block))))
 
 (defn drop-block [f b]
-  (dosync
     (loop []
       (when (check-position-valid f b)
-        (alter (b :y) inc)
+        (swap! (b :y) inc)
         (recur)))
-    (alter (b :y) dec)
+    (swap! (b :y) dec)
     (commit-block f b)
     (clear-lines f)
-    (next-block)))
+    (next-block))
 
 (defn reset-game []
   (reset! is-game-over false)
-  (alter hs-xml (fn [_] nil))
+  (reset! hs-xml nil)
   (reset-stats))
 
 (defn create-panel []
@@ -747,7 +749,7 @@
       (paintComponent [g]
         (proxy-super paintComponent g)
         (draw-all this g global-field active-block)
-        (dosync (alter frame-count inc)))
+        (swap! frame-count inc))
       (getPreferredSize [] (Dimension. window-width
                                        window-height))
       (keyPressed [e]
@@ -759,7 +761,7 @@
             (if (== 40 keyCode) (move-down global-field active-block)
             (if (== 32 keyCode) (drop-block global-field active-block))))))
           (if (== 10 keyCode)
-              (dosync (reset-game))))))
+              (reset-game)))))
       (keyReleased [e])
       (keyTyped [e]))
     (.setFocusable true)))
@@ -775,8 +777,7 @@
     (.addKeyListener panel panel)
     (center-in-screen frame)
     (init-blocks)    
-    (dosync
-      (set-game-speed (get-game-speed (get-level @(stats :lines)))))
+    (set-game-speed (get-game-speed (get-level @(stats :lines))))
     (loop []
       (Thread/sleep 10)
       (.repaint panel)
@@ -793,21 +794,12 @@
   (if-not (= true @is-game-over)
     (do
       (reset! is-game-over true)
-      (alter user-name (fn [_] (get-user-name)))
+      (reset! user-name (get-user-name))
       (if-not (nil? @user-name)
         (let [url         (str "http://" domain "/hs")
               post-body   (str "name=" (uri-encode @user-name) "&score=" @(stats :score))]
           (println "Post request" (do-post-request url post-body))))
-      (alter hs-xml (fn [_] (clojure.xml/parse (str "http://" domain "/hof.xml"))))
+      (reset! hs-xml (clojure.xml/parse (str "http://" domain "/hof.xml")))
       (init-blocks))))
 
-; TODO
-; ----
-; - high-score (internet?)
-; - distribute as applet (if feasible)
-; v lines
-; v level
-; v show score http://tetris.wikia.com/wiki/Scoring
-; v game over message
-; v drop block space bar
-;(main)
+(main)

bb3a0676597bb8472dd83c27c3a51e1ed92c31c2
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2010-06-30 20:32:12 +0000

    CommitSucceeded has content-type text/html. Remove spaces in cHTMLTemplate. CommitSucceeded now also has the h1 "High Score Server" heading.

--- PlayGroundCpp/HighScoreServer/include/Utils.h
+++ PlayGroundCpp/HighScoreServer/include/Utils.h
@@ -83,18 +83,17 @@ namespace HSServer
 
 
     static const char * cHTMLTemplate = 
-        "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"                          "
-        "                      \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">        "
-        "<html xmlns=\"http://www.w3.org/1999/xhtml\">                                             "
-        "<head>                                                                                    "                                        
-        "<title>{{title}}</title>                                                                  "
-        "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />                 "
-        "<meta http-equiv=\"Content-Style-Type\" content=\"text/css\" />                           "
-        "</head>                                                                                   "
-        "<body>                                                                                    "
-        "{{body}}                                                                                  "
-        "</body>                                                                                   "
-        "</html>                                                                                   ";
+        "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
+        "<html xmlns=\"http://www.w3.org/1999/xhtml\">"
+        "<head>"
+        "<title>{{title}}</title>"
+        "<meta http-equiv=\"Content-Type\"content=\"text/html; charset=UTF-8\"/>"
+        "<meta http-equiv=\"Content-Style-Type\"content=\"text/css\"/>"
+        "</head>"
+        "<body>"
+        "{{body}}"
+        "</body>"
+        "</html>";
 
 
     std::string MakeHTMLDocument(const std::string & inTitle, const std::string & inBody);

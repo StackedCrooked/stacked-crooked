f98f301790a14368b7010e9cc60e9202b5b0dc8e
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2010-07-02 23:51:17 +0000

    Cleanup formatting.

--- PlayGround/Clojure/Tetris.clj
+++ PlayGround/Clojure/Tetris.clj
@@ -155,10 +155,10 @@
   :field-y 1
   :num-rows 20
   :num-columns 10
-  :block-width 32
-  :block-height 32 })
+  :block-width 20
+  :block-height 20 })
 
-(def window-side-width 200)
+(def window-side-width 100)
 (def window-width (+ (* (+ 2 (prefs :num-columns)) (prefs :block-width)) window-side-width))
 (def window-height (* (+ 2 (prefs :num-rows)) (prefs :block-height)))
 
@@ -529,7 +529,7 @@
 (defn get-tetris-font [g]
   (.deriveFont (.getFont g)
                (int java.awt.Font/BOLD)
-               (float 14)))
+               (float 12)))
     
 (defn get-text-rect [g str font]
   (.getStringBounds (.getFontMetrics g (get-tetris-font g)) str g))
@@ -601,9 +601,9 @@
                           (if (and (< i num-entries) (< i 10))
                             (let [xml-entry       (nth xml-entries i)
                                   full-user-name  (uri-decode ((xml-entry :attrs) :name))
-                                  user-name       (apply str (take 24 full-user-name))
+                                  user-name       (apply str (take 18 full-user-name))
                                   score           ((xml-entry :attrs) :score) ]
-                              (recur (inc i) (conj result {:name user-name :score score :color Color/YELLOW})))
+                              (recur (inc i) (conj result {:name user-name :score score :color Color/GREEN})))
                             result)) ]
     (dotimes [i (count hs-entries)]
       (let [entry           (hs-entries i)

370c01b578d9cb7fc71af455e97276712fa17649
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-11-13 02:27:03 +0000

    Cleanup.

--- trunk/PlayGroundCpp/VideoPlayer/MainWindow.cpp
+++ trunk/PlayGroundCpp/VideoPlayer/MainWindow.cpp
@@ -2,23 +2,15 @@
 #include <QFileDialog>
 
 
-#define trace() \
-    qDebug("%s:%d", __FUNCTION__, __LINE__)
-
-#define trace_msg(msg) \
-    qDebug("%s:%d:%d", __FUNCTION__, __LINE__, (msg))
-
-
-
 const char * cPipeline =
-    " filesrc location=\"{{path}}\" ! decodebin2 name=d d. ! queue ! ffmpegcolorspace ! osxvideosink d. ! queue ! audioconvert ! audioresample ! autoaudiosink ";
+    "filesrc location=\"{{path}}\" ! decodebin2 name=d"
+    " d. ! queue ! ffmpegcolorspace ! osxvideosink"
+    " d. ! queue ! audioconvert ! audioresample ! autoaudiosink";
 
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
-    Q_ASSERT(testAttribute(Qt::WA_NativeWindow));
-
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Video"), "$HOME", tr("Video Files (*.avi *.mkv *.ogm *.wmv)"));
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Video"), "/", tr("Video Files (*.avi *.mkv *.ogm *.wmv)"));
     QString desc = cPipeline;
     desc.replace("{{path}}", fileName);
     mPipeline.reset(Gst::Pipeline::Parse(desc.toAscii().data()));

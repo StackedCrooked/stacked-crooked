b334dd507da6d836cffe02971c347e182a08ad43
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-11-13 01:35:16 +0000

    Select a file at startup.

--- trunk/PlayGroundCpp/VideoPlayer/MainWindow.cpp
+++ trunk/PlayGroundCpp/VideoPlayer/MainWindow.cpp
@@ -1,4 +1,5 @@
 #include "MainWindow.h"
+#include <QFileDialog>
 
 
 #define trace() \
@@ -8,11 +9,21 @@
     qDebug("%s:%d:%d", __FUNCTION__, __LINE__, (msg))
 
 
+
+const char * cPipeline =
+    " filesrc location=\"{{path}}\" ! decodebin2 name=d "
+    " d. ! queue ! ffmpegcolorspace ! osxvideosink sync=false "
+    " d. ! queue ! audioconvert ! audioresample ! autoaudiosink ";
+
+
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 {
     Q_ASSERT(testAttribute(Qt::WA_NativeWindow));
 
-    mPipeline.reset(Gst::Pipeline::Parse("videotestsrc ! osxvideosink sync=false"));
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Video"), "$HOME", tr("Video Files (*.avi *.mkv *.ogm *.wmv)"));
+    QString desc = cPipeline;
+    desc.replace("{{path}}", fileName);
+    mPipeline.reset(Gst::Pipeline::Parse(desc.toAscii().data()));
     mGstWidget.reset(new Gst::Widget(mPipeline, reinterpret_cast<void*>(winId())));
     Gst::Pipeline::SetState(mPipeline, GST_STATE_PLAYING);
 }

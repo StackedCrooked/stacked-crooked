81f7c9f9c531c197ec763350f27d8f60df16759d
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2010-11-24 21:07:59 +0000

    It works! (I suspect that adjusting the domain in application.ini was the key.).

--- PlayGroundCpp/XPCOMSample/MyComponent.h
+++ PlayGroundCpp/XPCOMSample/MyComponent.h
@@ -4,8 +4,8 @@
 
 #include "IMyComponent.h"
 
-#define MY_COMPONENT_CONTRACTID "@mydomain.com/XPCOMSample/MyComponent;1"
-#define MY_COMPONENT_CLASSNAME "A Simple XPCOM Sample"
+#define MY_COMPONENT_CONTRACTID "@stacked-crooked.com/XPCOMSample/CrookedComponent;1"
+#define MY_COMPONENT_CLASSNAME "A Crooked XPCOM Sample"
 #define MY_COMPONENT_CID { 0x99d79656, 0xf804, 0x11df, { 0xaee5, 0x08, 0x00, 0x27, 0xa4, 0x54, 0x55 } } 
 
 

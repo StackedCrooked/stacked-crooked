09384f9dbd82bf2bb297818a6829fdb724f131bb
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2010-11-25 00:03:31 +0000

    Add XULRunner v1.9.1

---
+++ PlayGroundCpp/3rdParty/XULRunnerSDK/1.9.1/xulrunner-sdk/include/system_wrappers/gtk/gtkfixed.h
@@ -0,0 +1,4 @@
+#pragma GCC system_header
+#pragma GCC visibility push(default)
+#include_next <gtk/gtkfixed.h>
+#pragma GCC visibility pop
---
+++ PlayGroundCpp/3rdParty/XULRunnerSDK/1.9.1/xulrunner-sdk/include/system_wrappers/gtk/gtkimage.h
@@ -0,0 +1,4 @@
+#pragma GCC system_header
+#pragma GCC visibility push(default)
+#include_next <gtk/gtkimage.h>
+#pragma GCC visibility pop

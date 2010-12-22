8004117d72b98f7c6bd6a445d62e8f224f7d0dc5
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2010-12-22 21:23:14 +0000

    Add GoogleTest sample.

---
+++ trunk/PlayGroundCpp/GoogleTest/main.cpp
@@ -0,0 +1,55 @@
+#include "gtest/gtest.h"
+
+
+class Summator
+{
+public:
+    Summator(int inA, int inB) : mA(inA), mB(inB) {}
+
+    int result() const { return mA + mB; }
+
+private:
+    int mA, mB;
+};
+
+
+class SummatorTest : public ::testing::Test
+{
+protected:
+    virtual void SetUp();
+    virtual void TearDown();
+};
+
+
+void SummatorTest::SetUp()
+{
+}
+
+
+void SummatorTest::TearDown()
+{
+}
+
+
+TEST_F(SummatorTest, ResultIsCorrect)
+{
+    Summator sum(3, 4);
+    EXPECT_EQ(sum.result(), 7);
+    EXPECT_TRUE(sum.result() > 6);
+    EXPECT_TRUE(sum.result() < 8);
+}
+
+
+TEST_F(SummatorTest, ResultIsOdd)
+{
+    Summator sum(1, 2);
+    EXPECT_TRUE(sum.result() % 2 == 1);
+}
+
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+

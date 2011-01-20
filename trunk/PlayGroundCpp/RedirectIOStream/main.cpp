5816a4db639e92c837b49d1a39f26f671be30f98
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-01-20 20:44:05 +0000

    Remove namespace.

--- trunk/PlayGroundCpp/RedirectIOStream/main.cpp
+++ trunk/PlayGroundCpp/RedirectIOStream/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	std::cout << "Before mute" << std::endl;
 	{
-		Crooked::ScopedMuter mute(std::cout);
+		ScopedMuter mute(std::cout);
 		std::cout << "During mute." << std::endl;
 	}
 	std::cout << "After mute" << std::endl;

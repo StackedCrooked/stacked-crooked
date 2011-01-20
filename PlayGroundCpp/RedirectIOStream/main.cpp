50da18bffbc6b85d0a76406041ac85e264c26361
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-01-20 20:22:36 +0000

    Add RedirectIOStream sample.

---
+++ PlayGroundCpp/RedirectIOStream/main.cpp
@@ -0,0 +1,14 @@
+#include "ScopedMuter.h"
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Before mute" << std::endl;
+	{
+		Crooked::ScopedMuter mute(std::cout);
+		std::cout << "During mute." << std::endl;
+	}
+	std::cout << "After mute" << std::endl;
+	return 0;
+}

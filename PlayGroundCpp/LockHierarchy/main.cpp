b354f308a613656d862e9ff73e7ec4fb372651ab
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-03-17 17:12:04 +0000

    Cleanup whitespace.

--- PlayGroundCpp/LockHierarchy/main.cpp
+++ PlayGroundCpp/LockHierarchy/main.cpp
@@ -7,13 +7,13 @@
 typedef Poco::Mutex Mutex;
 void LockMutex(Mutex & inMutex)
 {
-	inMutex.lock();
+    inMutex.lock();
 }
 
 
 void UnlockMutex(Mutex & inMutex)
 {
-	inMutex.unlock();
+    inMutex.unlock();
 }
 
 
@@ -25,26 +25,26 @@ template<class _RankedMutexType>
 class RankedMutexLock
 {
 public:
-	typedef _RankedMutexType RankedMutex;
-	RankedMutexLock(_RankedMutexType & inRankedMutex) :
-		mRankedMutex(inRankedMutex)
-	{
+    typedef _RankedMutexType RankedMutex;
+    RankedMutexLock(_RankedMutexType & inRankedMutex) :
+        mRankedMutex(inRankedMutex)
+    {
         CheckRank();
-		LockMutex(mRankedMutex.getMutex());
-	}
+        LockMutex(mRankedMutex.getMutex());
+    }
 
-	~RankedMutexLock()
-	{
-		UnlockMutex(mRankedMutex.getMutex());
+    ~RankedMutexLock()
+    {
+        UnlockMutex(mRankedMutex.getMutex());
 
         LockMutex(sGlobalRanksMutex);
         sGlobalRanks.pop_back();
         UnlockMutex(sGlobalRanksMutex);
-	}
+    }
 
 private:
-	RankedMutexLock(const RankedMutexLock &);
-	RankedMutexLock& operator=(const RankedMutexLock &);
+    RankedMutexLock(const RankedMutexLock &);
+    RankedMutexLock& operator=(const RankedMutexLock &);
 
     void CheckRank()
     {
@@ -65,7 +65,7 @@ private:
         }
     }
 
-	RankedMutex & mRankedMutex;
+    RankedMutex & mRankedMutex;
 };
 
 
@@ -97,31 +97,31 @@ template<class _MutexType>
 class MutexWrapper : public AbstractMutexWrapper
 {
 public:
-	typedef _MutexType MutexType;
+    typedef _MutexType MutexType;
 
     MutexWrapper(int inRank) :
         AbstractMutexWrapper(inRank)
     {
     }
 
-	virtual ~MutexWrapper()
-	{
+    virtual ~MutexWrapper()
+    {
 
-	}
+    }
 
-	MutexType & getMutex()
-	{
-		return mMutex;
-	}
+    MutexType & getMutex()
+    {
+        return mMutex;
+    }
 
-	const MutexType & getMutex() const
-	{
-		return mMutex;
-	}
+    const MutexType & getMutex() const
+    {
+        return mMutex;
+    }
 
 protected:
 private:
-	MutexType mMutex;
+    MutexType mMutex;
     int mRank;
 };
 
@@ -131,14 +131,14 @@ class BottomMutex : public MutexWrapper<_MutexType>
 {
 public:
     typedef MutexWrapper<_MutexType> Super;
-	typedef BottomMutex This;
+    typedef BottomMutex This;
     typedef typename Super::MutexType MutexType;
-	typedef RankedMutexLock<This> ScopedLock;
+    typedef RankedMutexLock<This> ScopedLock;
 
-	enum
+    enum
     {
-		Rank = 0
-	};
+        Rank = 0
+    };
 
 
     BottomMutex() :
@@ -152,27 +152,24 @@ template<class _LowerRankedMutex>
 class RankedMutex : public MutexWrapper<typename _LowerRankedMutex::MutexType>
 {
 public:
-	typedef MutexWrapper<typename _LowerRankedMutex::MutexType> Super;
-	typedef RankedMutex<_LowerRankedMutex> This;
-	typedef RankedMutexLock<This> ScopedLock;
+    typedef MutexWrapper<typename _LowerRankedMutex::MutexType> Super;
+    typedef RankedMutex<_LowerRankedMutex> This;
+    typedef RankedMutexLock<This> ScopedLock;
 
-	enum
-	{
-		Rank = _LowerRankedMutex::Rank + 1
-	};
+    enum
+    {
+        Rank = _LowerRankedMutex::Rank + 1
+    };
 
     RankedMutex() :
         Super(Rank)
     {
     }
-	
-	virtual ~RankedMutex()
-	{
-	}
+
+    virtual ~RankedMutex()
+    {
+    }
 };
-
-
-
 
 
 typedef BottomMutex<Mutex  > L0Mutex;
@@ -184,38 +181,38 @@ typedef RankedMutex<L3Mutex>      L4Mutex;
 
 int main()
 {
-	L0Mutex m0;
-	L1Mutex m1;
-	L2Mutex m2;
+    L0Mutex m0;
+    L1Mutex m1;
+    L2Mutex m2;
 
-	try
-	{
-		std::cout << "Expect success: "; 
-		L2Mutex::ScopedLock l2(m2);
-		L1Mutex::ScopedLock l1(m1);
-		L0Mutex::ScopedLock l0(m0);
-		std::cout << "OK" << std::endl;
-	}
-	catch (const std::exception & exc)
-	{
-		std::cout << exc.what() << std::endl;
-	}
-
-
-	try
-	{
-		std::cout << "Expect exception: "; 
-		L1Mutex::ScopedLock l1(m1);
-		L0Mutex::ScopedLock l0(m0);
-		L2Mutex::ScopedLock l2(m2);
-		std::cout << "Huh?" << std::endl;
-	}
-	catch (const std::exception & exc)
-	{
-		std::cout << "Indeed: " << exc.what() << std::endl;
-	}
+    try
+    {
+        std::cout << "Expect success: ";
+        L2Mutex::ScopedLock l2(m2);
+        L1Mutex::ScopedLock l1(m1);
+        L0Mutex::ScopedLock l0(m0);
+        std::cout << "OK" << std::endl;
+    }
+    catch (const std::exception & exc)
+    {
+        std::cout << exc.what() << std::endl;
+    }
 
 
-	return 0;
+    try
+    {
+        std::cout << "Expect exception: ";
+        L1Mutex::ScopedLock l1(m1);
+        L0Mutex::ScopedLock l0(m0);
+        L2Mutex::ScopedLock l2(m2);
+        std::cout << "Huh?" << std::endl;
+    }
+    catch (const std::exception & exc)
+    {
+        std::cout << "Indeed: " << exc.what() << std::endl;
+    }
+
+
+    return 0;
 }
 

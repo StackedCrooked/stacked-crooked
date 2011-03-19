08ef3500530d9df949fe52d48254297513eee4d6
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-03-19 04:46:33 +0000

    Add more info.

--- trunk/Utilities/RAMDisk/MacOSX/MountRAMDisk.sh
+++ trunk/Utilities/RAMDisk/MacOSX/MountRAMDisk.sh
@@ -1,4 +1,6 @@
-# Clustersize seems to be 512 bytes. So ram://1165430 is actually 569 MB
-# Formula: RequestedMB * 2048 = MagicNumber
-diskutil erasevolume HFS+ "ramdisk" `hdiutil attach -nomount ram://2048000`
+#!/bin/sh
+# Clustersize seems to be 512 bytes.
+# Formula: RequestedMB * 2048 = NumberOfClusters
+# Create a 1 GB volume by specifying ram://2048000
+diskutil erasevolume HFS+ "RAMDisk" `hdiutil attach -nomount ram://2048000`
 

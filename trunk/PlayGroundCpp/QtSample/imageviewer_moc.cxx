cd08a0c31deb2b3f3998f808bd71cf6db41c5c57
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-04-07 20:27:41 +0000

    Cross-compile.

--- trunk/PlayGroundCpp/QtSample/imageviewer_moc.cxx
+++ trunk/PlayGroundCpp/QtSample/imageviewer_moc.cxx
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'imageviewer.h'
 **
-** Created: Thu Apr 7 21:17:56 2011
+** Created: Thu Apr 7 22:20:48 2011
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.0)
 **
 ** WARNING! All changes made in this file will be lost!

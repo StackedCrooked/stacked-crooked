9aa86d9832c9c4c8144517cdabd102b67eaa05e7
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-04-28 23:51:54 +0000

    Use factory functions.

--- PlayGround/RemoveCopyIf/main.cpp
+++ PlayGround/RemoveCopyIf/main.cpp
@@ -5,52 +5,51 @@
 
 // Overload that takes a function pointer
 template<class ForwardIterator, class OutputIterator, class ArgType>
-void copy_if(ForwardIterator begin, ForwardIterator end, OutputIterator out, bool (*inPredicate)(ArgType))
+OutputIterator copy_if(ForwardIterator begin, ForwardIterator end, OutputIterator out, bool (*inPredicate)(ArgType))
 {
-    typedef std::pointer_to_unary_function<ArgType, bool> Adapter;
-    std::remove_copy_if(begin, end, out, std::unary_negate<Adapter>(Adapter(inPredicate)));
+    return std::remove_copy_if(begin, end, out, std::not1(std::ptr_fun(inPredicate)));
 }
 
 // Overload that takes a function object
 template<class ForwardIterator, class OutputIterator, class Functor>
-void copy_if(ForwardIterator begin, ForwardIterator end, OutputIterator out, Functor inFunctor)
+OutputIterator copy_if(ForwardIterator begin, ForwardIterator end, OutputIterator out, Functor inPredicate)
 {
-    std::remove_copy_if(begin, end, out, std::unary_negate<Functor>(inFunctor));
+    return std::remove_copy_if(begin, end, out, std::not1(inPredicate));
 }
-
+ 
 bool is_odd(int inValue)
 {
     return inValue % 2 == 1;
 }
-
+ 
 bool is_odd_const_ref(const int & inValue)
 {
     return inValue % 2 == 1;
 }
-
+ 
 struct is_odd_functor : public std::unary_function<int, bool>
 {
     bool operator() (const int & inValue) const { return inValue % 2 == 1; }
 };
-
+ 
 int main()
 {
-	std::vector<int> numbers;
-	numbers.push_back(0);
-	numbers.push_back(1);
-	numbers.push_back(2);
-	numbers.push_back(3);
-
-	std::vector<int> copy;
-
+    std::vector<int> numbers;
+    numbers.push_back(0);
+    numbers.push_back(1);
+    numbers.push_back(2);
+    numbers.push_back(3);
+ 
+    std::vector<int> copy;
+ 
     // Functor: Ok
-	copy_if(numbers.begin(), numbers.end(), std::back_inserter(copy), is_odd_functor());
-
+    copy_if(numbers.begin(), numbers.end(), std::back_inserter(copy), is_odd_functor());
+ 
     // Function pointer: Ok
-	copy_if(numbers.begin(), numbers.end(), std::back_inserter(copy), is_odd);
-
+    copy_if(numbers.begin(), numbers.end(), std::back_inserter(copy), is_odd);
+ 
     // Function pointer that takes const ref: Compiler error
-	copy_if(numbers.begin(), numbers.end(), std::back_inserter(copy), is_odd_const_ref);
-	return 0;
+    copy_if(numbers.begin(), numbers.end(), std::back_inserter(copy), is_odd_const_ref);
+    return 0;
 }
-
+ 

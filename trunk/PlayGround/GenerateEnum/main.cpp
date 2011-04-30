9e197ad8cc0045eba99e0fc4fba8c8d8687d6acd
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-04-30 06:40:04 +0000

    Make the tests more complete.

--- trunk/PlayGround/GenerateEnum/main.cpp
+++ trunk/PlayGround/GenerateEnum/main.cpp
@@ -4,11 +4,10 @@
 #include <iostream>
 
 
-#define DEFINE_ENUM(Tag, Size, Enumerator) \
-    enum Tag { BOOST_PP_LIST_ENUM(BOOST_PP_TUPLE_TO_LIST(Size, Enumerator)) };
-
 /**
- * EnumInfo<EnumType> provides useful meta data beloning to an enum type:
+ * EnumInfo<EnumType> provides useful meta data beloning to an enum type.
+ *
+ * Details:
  *   - name() return the enumeration's tag name.
  *   - values is an array containing all enumerator values.
  *   - names is an array containing all enumerator names.
@@ -23,10 +22,28 @@
 template<typename T>
 struct EnumInfo;
 
+
+/**
+ * EnumeratorInfo<EnumType, Enumerator> provides useful metadata for the requested Enumerator value.
+ *
+ * Details:
+ *   - name() returns the Enumerator name
+ *   - value() returns the Enumerator value
+ */
+template<class TEnum, TEnum TEnumerator>
+struct EnumeratorInfo;
+
+
+//
+// Macros for internal use
+//
+#define DEFINE_ENUM(Tag, Size, Enumerator) \
+    enum Tag { BOOST_PP_LIST_ENUM(BOOST_PP_TUPLE_TO_LIST(Size, Enumerator)) };
+
 #define VALUE_TO_STRING(Dummy0, Dummy1, Element) \
     BOOST_PP_STRINGIZE(Element)
 
-#define DEFINE_ENUMTYPEINFO_SPECIALIZATION(Tag, Size, Enumerator, First, Last) \
+#define DEFINE_ENUMINFO_SPECIALIZATION(Tag, Size, Enumerator, First, Last) \
     template<> struct EnumInfo<Tag> \
     { \
         static const char * name() { return #Tag; } \
@@ -41,13 +58,13 @@ struct EnumInfo;
             for (std::size_t idx = 0; idx < size(); ++idx) { \
                 if (names[idx] == inName) return values[idx]; \
             } \
-            throw std::runtime_error("Color name not found: " + inName); \
+            throw std::runtime_error("Invalid enumerator name: " + inName); \
         } \
         static const char * ToString(Tag inValue) { \
             for (std::size_t idx = 0; idx < size(); ++idx) { \
                 if (values[idx] == inValue) return names[idx]; \
             } \
-            throw std::runtime_error("Color index not found: " + boost::lexical_cast<std::string>(inValue)); \
+            throw std::runtime_error("Invalid enumerator value: " + boost::lexical_cast<std::string>(inValue)); \
         } \
     };\
     EnumInfo<Tag>::Values EnumInfo<Tag>::values = { \
@@ -58,16 +75,7 @@ struct EnumInfo;
     };
 
 
-/**
- * EnumeratorInfo<EnumType, Enumerator> provides useful metadata for the requested Enumerator value.
- *   - name() returns the Enumerator name
- *   - value() returns the Enumerator value
- */
-template<class TEnum, TEnum TEnumerator>
-struct EnumeratorInfo;
-
-
-#define DEFINE_ENUMVALUEINFO_SPECIALIZATION(Dummy, Enum, Enumerator) \
+#define DEFINE_ENUMERATORINFO_SPECIALIZATION(Dummy, Enum, Enumerator) \
     template<> struct EnumeratorInfo<Enum, Enumerator> { \
         typedef Enum EnumType; \
         static const char * name() { return #Enumerator; } \
@@ -75,51 +83,65 @@ struct EnumeratorInfo;
     };
 
 
-//
-// Macro for defining a new enum.
-//
+/**
+ * The ENUM macro allows you to define a new enum along with accompanying
+ * code for parsing and serializing the enumerator values.
+ *
+ * Usage example:
+ *
+ *    ENUM(HTTPRequestMethod, 9, (HEAD, GET, POST, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH))
+ *
+ *
+ */
 #define ENUM(Tag, Size, Values) \
     DEFINE_ENUM(Tag, Size, Values) \
-    DEFINE_ENUMTYPEINFO_SPECIALIZATION( \
+    DEFINE_ENUMINFO_SPECIALIZATION( \
         Tag, \
         Size, \
         Values, \
         BOOST_PP_LIST_FIRST(BOOST_PP_TUPLE_TO_LIST(Size, Values)), \
         BOOST_PP_LIST_FIRST(BOOST_PP_LIST_REVERSE(BOOST_PP_TUPLE_TO_LIST(Size, Values)))) \
     BOOST_PP_LIST_FOR_EACH( \
-        DEFINE_ENUMVALUEINFO_SPECIALIZATION, \
+        DEFINE_ENUMERATORINFO_SPECIALIZATION, \
         Tag, \
         BOOST_PP_TUPLE_TO_LIST(Size, Values))
 
 
 //
+// Tests
+//
+
+
 // Define the Color enum
-//
-ENUM(Color, 7, (Red, Orange, Yellow, Green, Blue, Indigo, Violet));
+ENUM(Color, 7, (Red, Orange, Yellow, Green, Blue, Indigo, Violet))
 
-
-//
 // Define the Note enum
-//
 ENUM(Note, 7, (Do, Re, Mi, Fa, Sol, La, Si))
+
+// Define the HTTPRequestMethod enum
+ENUM(HTTPRequestMethod, 9, (HEAD, GET, POST, PUT, DELETE, TRACE, OPTIONS, CONNECT, PATCH))
+
+
+void PauseConsole(const std::string & inMessage)
+{
+    std::string message = "\"" + inMessage + "\"";
+    #ifdef _WIN32
+    std::string command = "pause ";
+    #else
+    std::string command = "read -p ";
+    #endif
+    system((command + message).c_str());
+}
 
 
 template<class Enum>
 void TestEnum()
 {
-    std::cout << "Tag: " << EnumInfo<Enum>::name() << std::endl;
-    std::cout << "First: " << EnumInfo<Enum>::first() << std::endl;
-    std::cout << "Last: " << EnumInfo<Enum>::last() << std::endl;
-    std::cout << "Size: " << EnumInfo<Enum>::size() << std::endl;
-    for (int i = 0; i < EnumInfo<Enum>::size(); ++i)
-    {
-        std::cout << EnumInfo<Enum>::names[i] << "=" << EnumInfo<Enum>::values[i] << std::endl;
-    }
-
+    std::cout << "=== Enum Test ===" << std::endl;
     try
     {
         {
-            std::cout << "Please input a color name. (Possible values are ";
+            std::cout << "Please input an enumerator name. (Possible values are ";
             for (Enum i = EnumInfo<Enum>::first(); i <= EnumInfo<Enum>::last(); i = static_cast<Enum>(static_cast<int>(i) + 1))
             {
                 if (i != EnumInfo<Enum>::first())
@@ -129,12 +151,12 @@ void TestEnum()
                 std::cout << EnumInfo<Enum>::names[i];
             }
             std::cout << "): ";
-            std::string colorName;
-            std::cin >> colorName;
+            std::string enumeratorName;
+            std::cin >> enumeratorName;
 
-            Enum enumValue = EnumInfo<Enum>::FromString(colorName);
-            std::cout << "This color has the value: " << enumValue << "." << std::endl
-                      << "And it's name is: "         << EnumInfo<Enum>::ToString(enumValue)
+            Enum enumValue = EnumInfo<Enum>::FromString(enumeratorName);
+            std::cout << "This enumerator has the value: " << enumValue << "." << std::endl
+                      << "And its name is: "         << EnumInfo<Enum>::ToString(enumValue)
                       << std::endl << std::flush;
 
         }
@@ -155,31 +177,44 @@ void TestEnum()
             if (enumerator >= EnumInfo<Enum>::first() && enumerator <= EnumInfo<Enum>::last())
             {
                 Enum enumValue = static_cast<Enum>(enumerator);
-                std::cout << "The corresponding color name is: " << EnumInfo<Enum>::ToString(enumValue) << std::endl << std::flush;
+                std::cout << "The corresponding enumerator name is: " << EnumInfo<Enum>::ToString(enumValue) << std::endl << std::flush;
             }
 
         }
-        std::cout << std::endl << std::flush;
     }
     catch (const std::exception & exc)
     {
-        std::cerr << exc.what() << std::endl;
+        std::cerr << exc.what() << std::endl << std::flush;
     }
+
+    PauseConsole("Press any key to start the next test (or quit if this was the last one).");
+    std::cout << std::endl;
 }
+
 
 template<class TEnumType, TEnumType Enumerator>
 void TestEnumerator()
 {
+    std::cout << "=== Enumerator Test ===" << std::endl;
     typedef typename EnumeratorInfo<TEnumType, Enumerator>::EnumType Enum;
     std::cout << "The enumerator value is: " << EnumeratorInfo<Enum, Enumerator>::value() << "." << std::endl;
     std::cout << "The enumerator name is: " << EnumeratorInfo<Enum, Enumerator>::name() << "." << std::endl;
-    std::cout << "The enumerator belongs to the " << EnumInfo<Enum>::name() << " enum." << std::endl;
-    std::cout << std::endl << std::flush;
+    std::cout << "The enumerator belongs to the " << EnumInfo<Enum>::name() << " enum." << std::endl << std::endl;
+
+    PauseConsole("Press any key to start the next test (or quit if this was the last one).");
+    std::cout << std::endl;
 }
 
 
 int main()
 {
+    PauseConsole("\nWarning: This test requires user interaction. This may get on your nerves.\nPress any key to continue.");
+    std::cout << std::endl;
+
+    TestEnum<HTTPRequestMethod>();
+    TestEnumerator<HTTPRequestMethod, GET>();
+    TestEnumerator<HTTPRequestMethod, PUT>();
+
     TestEnum<Note>();
     TestEnumerator<Note, Sol>();
     TestEnumerator<Note, Si>();
@@ -187,6 +222,8 @@ int main()
     TestEnum<Color>();
     TestEnumerator<Color, Yellow>();
     TestEnumerator<Color, Orange>();
+
+    std::cout << std::endl << "We are complete." << std::endl << std::flush;
     return 0;
 }
 

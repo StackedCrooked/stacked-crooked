5aa75661377480ecdcb02ee6d8fd4b9670a4e2a9
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-04-30 04:17:54 +0000

    Rename EnumInfo to EnumTypeInfo.

--- PlayGround/GenerateEnum/main.cpp
+++ PlayGround/GenerateEnum/main.cpp
@@ -7,15 +7,13 @@
 
 
 template<typename T>
-struct EnumInfo
-{
-};
+struct EnumTypeInfo;
 
 #define VALUE_TO_STRING(Dummy0, Dummy1, Element) \
     BOOST_PP_STRINGIZE(Element)
 
 #define DEFINE_ENUMINFO_SPECIALIZATION(Name, N, _Values, First, Last) \
-    template<> struct EnumInfo<Name> \
+    template<> struct EnumTypeInfo<Name> \
     { \
         static const char * name() { return #Name; } \
         typedef const Name Values[N]; \
@@ -26,10 +24,10 @@ struct EnumInfo
         static Name first() { return First; } \
         static Name last() { return Last; } \
     };\
-    EnumInfo<Name>::Values EnumInfo<Name>::values = { \
+    EnumTypeInfo<Name>::Values EnumTypeInfo<Name>::values = { \
         BOOST_PP_LIST_ENUM(BOOST_PP_TUPLE_TO_LIST(N, _Values)) \
     }; \
-    EnumInfo<Name>::Names EnumInfo<Name>::names = { \
+    EnumTypeInfo<Name>::Names EnumTypeInfo<Name>::names = { \
         BOOST_PP_LIST_ENUM(BOOST_PP_LIST_TRANSFORM(VALUE_TO_STRING, N, BOOST_PP_TUPLE_TO_LIST(N, _Values))) \
     };
 
@@ -49,13 +47,13 @@ ENUM(RGB, 3, (Red, Green, Blue))
 
 int main()
 {
-    std::cout << "Name: " << EnumInfo<RGB>::name() << std::endl;
-    std::cout << "First: " << EnumInfo<RGB>::first() << std::endl;
-    std::cout << "Last: " << EnumInfo<RGB>::last() << std::endl;
-    std::cout << "Size: " << EnumInfo<RGB>::size() << std::endl;
-    for (int i = 0; i < EnumInfo<RGB>::size(); ++i)
+    std::cout << "Name: " << EnumTypeInfo<RGB>::name() << std::endl;
+    std::cout << "First: " << EnumTypeInfo<RGB>::first() << std::endl;
+    std::cout << "Last: " << EnumTypeInfo<RGB>::last() << std::endl;
+    std::cout << "Size: " << EnumTypeInfo<RGB>::size() << std::endl;
+    for (int i = 0; i < EnumTypeInfo<RGB>::size(); ++i)
     {
-        std::cout << EnumInfo<RGB>::names[i] << "=" << EnumInfo<RGB>::values[i] << std::endl;
+        std::cout << EnumTypeInfo<RGB>::names[i] << "=" << EnumTypeInfo<RGB>::values[i] << std::endl;
     }
     return 0;
 }

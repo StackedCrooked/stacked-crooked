1f671abd8d98f7de9a6a5545eb76fd9af3ac1de3
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-04-30 04:14:49 +0000

    EnumTypeInfo now stores names and values array.

--- trunk/PlayGround/GenerateEnum/main.cpp
+++ trunk/PlayGround/GenerateEnum/main.cpp
@@ -11,13 +11,30 @@ struct EnumInfo
 {
 };
 
-#define DEFINE_ENUMINFO_SPECIALIZATION(Name, N, First, Last) \
+#define VALUE_TO_STRING(Dummy0, Dummy1, Element) \
+    BOOST_PP_STRINGIZE(Element)
+
+#define DEFINE_ENUMINFO_SPECIALIZATION(Name, N, _Values, First, Last) \
     template<> struct EnumInfo<Name> \
     { \
         static const char * name() { return #Name; } \
+        typedef const Name Values[N]; \
+        static Values values; \
+        typedef const char * Names[N]; \
+        static Names names; \
         static int size() { return N; } \
         static Name first() { return First; } \
         static Name last() { return Last; } \
+    };\
+    EnumInfo<Name>::Values EnumInfo<Name>::values = { BOOST_PP_LIST_ENUM(BOOST_PP_TUPLE_TO_LIST(N, _Values)) }; \
+    EnumInfo<Name>::Names EnumInfo<Name>::names = { \
+        BOOST_PP_LIST_ENUM( \
+            BOOST_PP_LIST_TRANSFORM( \
+                VALUE_TO_STRING, \
+                N, \
+                BOOST_PP_TUPLE_TO_LIST(N, _Values) \
+            ) \
+        ) \
     };
 
 
@@ -26,6 +43,7 @@ struct EnumInfo
     DEFINE_ENUMINFO_SPECIALIZATION( \
         Name, \
         N, \
+        Values, \
         BOOST_PP_LIST_FIRST(BOOST_PP_TUPLE_TO_LIST(N, Values)), \
         BOOST_PP_LIST_FIRST(BOOST_PP_LIST_REVERSE(BOOST_PP_TUPLE_TO_LIST(N, Values))))
 
@@ -39,5 +57,9 @@ int main()
     std::cout << "First: " << EnumInfo<RGB>::first() << std::endl;
     std::cout << "Last: " << EnumInfo<RGB>::last() << std::endl;
     std::cout << "Size: " << EnumInfo<RGB>::size() << std::endl;
+    for (int i = 0; i < EnumInfo<RGB>::size(); ++i)
+    {
+        std::cout << EnumInfo<RGB>::names[i] << "=" << EnumInfo<RGB>::values[i] << std::endl;
+    }
     return 0;
 }

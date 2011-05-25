2ae069c333582f095b523a8db2e2ea4ef0a39de4
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-05-25 19:52:16 +0000

    Cleanup.

--- PlayGroundCpp/RefcountPerformance/main.cpp
+++ PlayGroundCpp/RefcountPerformance/main.cpp
@@ -1,15 +1,13 @@
 #include "Poco/Stopwatch.h"
 #include <boost/intrusive_ptr.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/noncopyable.hpp>
 #include <boost/shared_ptr.hpp>
-#include <boost/ptr_container/ptr_set.hpp>
-#include <boost/ptr_container/ptr_vector.hpp>
 #include <ctime>
 #include <set>
 #include <string>
 #include <vector>
 #include <iostream>
-#include "Poco/RefCountedObject.h"
 
 
 class RefcountedObject : boost::noncopyable
@@ -26,8 +24,17 @@ protected:
     std::size_t mRefCount;
 };
 
-void intrusive_ptr_add_ref(RefcountedObject * obj) { ++obj->mRefCount; }
-void intrusive_ptr_release(RefcountedObject * obj) { if (--obj->mRefCount == 0) delete obj; }
+
+void intrusive_ptr_add_ref(RefcountedObject * obj)
+{
+    ++obj->mRefCount;
+}
+
+
+void intrusive_ptr_release(RefcountedObject * obj)
+{
+    if (--obj->mRefCount == 0) delete obj;
+}
 
 
 class MyClass : boost::noncopyable
@@ -35,7 +42,8 @@ class MyClass : boost::noncopyable
 public:
     MyClass() : mIdentifier(sInstanceCount++) {}
 
-    bool operator<(const MyClass & rhs) {
+    bool operator<(const MyClass & rhs)
+    {
         return mIdentifier < rhs.mIdentifier;
     }
 
@@ -43,6 +51,7 @@ private:
     std::size_t mIdentifier;
     static std::size_t sInstanceCount;
 };
+
 
 std::size_t MyClass::sInstanceCount = 0;
 
@@ -64,11 +73,13 @@ public:
     static std::size_t sInstanceCount;
 };
 
+
 std::size_t MyClassWithRefcount::sInstanceCount = 0;
 
 
 template<class Container>
-time_t TestCopy(const Container & inContainer, std::size_t n) {
+time_t TestCopy(const Container & inContainer, std::size_t n)
+{
     Poco::Stopwatch stopwatch;
     stopwatch.start();
     for (std::size_t idx = 0; idx < n; ++idx)
@@ -79,11 +90,12 @@ time_t TestCopy(const Container & inContainer, std::size_t n) {
 }
 
 
-void TestSet(std::size_t n) {
+void TestSet(std::size_t n)
+{
     std::cout << std::endl << "Set" << std::endl;
 
     {
-        std::set<boost::shared_ptr<MyClass> > theSet;
+        std::set< boost::shared_ptr< MyClass > > theSet;
         for (std::size_t idx = 0; idx < 256; ++idx)
         {
             boost::shared_ptr<MyClass> obj(new MyClass);
@@ -93,7 +105,7 @@ void TestSet(std::size_t n) {
     }
 
     {
-        std::set< boost::intrusive_ptr<MyClassWithRefcount> > theSet;
+        std::set< boost::intrusive_ptr< MyClassWithRefcount > > theSet;
         for (std::size_t idx = 0; idx < 256; ++idx)
         {
             theSet.insert(new MyClassWithRefcount);
@@ -103,21 +115,22 @@ void TestSet(std::size_t n) {
 }
 
 
-void TestVector(std::size_t n) {
+void TestVector(std::size_t n)
+{
     std::cout << std::endl << "Vector" << std::endl;
 
     {
-        std::vector<boost::shared_ptr<MyClass> > theVector;
+        std::vector< boost::shared_ptr< MyClass > > theVector;
         for (std::size_t idx = 0; idx < 256; ++idx)
         {
-            boost::shared_ptr<MyClass> obj(new MyClass);
+            boost::shared_ptr< MyClass > obj(new MyClass);
             theVector.push_back(obj);
         }
         std::cout << "shared_ptr: " << TestCopy(theVector, n) << "ms" << std::endl;
     }
 
     {
-        std::vector< boost::intrusive_ptr<MyClassWithRefcount> > theVector;
+        std::vector< boost::intrusive_ptr< MyClassWithRefcount > > theVector;
         for (std::size_t idx = 0; idx < 256; ++idx)
         {
             theVector.push_back(new MyClassWithRefcount);
@@ -127,12 +140,14 @@ void TestVector(std::size_t n) {
 }
 
 
-int main(int argc, char ** argv) {
+int main(int argc, char ** argv)
+{
     std::size_t n = 10000;
     if (argc == 2)
     {
-        n = boost::lexical_cast<std::size_t>(argv[1]);
+        n = boost::lexical_cast< std::size_t >(argv[1]);
     }
+
     TestVector(n);
     TestSet(n);
     return 0;

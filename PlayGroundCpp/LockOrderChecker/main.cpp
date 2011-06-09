03e4e00252548729d4be3be4e754a2df7bdcf3af
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-06-09 22:08:14 +0000

    Almost. But not.

--- PlayGroundCpp/LockOrderChecker/main.cpp
+++ PlayGroundCpp/LockOrderChecker/main.cpp
@@ -9,120 +9,260 @@
 #include <vector>
 
 
-
-template<class T>
-struct Node : boost::noncopyable
+template<class ValueType>
+struct ContainerPolicy_Set
 {
-    Node(const std::string & inName, const T & inT) :
-        mName(inName),
-        mT(inT)
+    typedef std::set<ValueType> Container;
+    static void insert(Container & ioContainer, const ValueType & inValue)
     {
+        ioContainer.insert(inValue);
     }
 
-    Node & append(Node & next)
+    static size_t size(const Container & ioContainer)
     {
-        mChildNodes.insert(&next);
-        return next;
+        return ioContainer.size();
     }
 
-    const std::string & name() const { return mName; }
-
-    T & value() { return mT; }
-
-    const T & value() const { return mT; }
-
-    typedef Node<T> This;
-    typedef std::set<This*> NodeSet;
-
-    std::string mName;
-    T mT;
-    NodeSet mChildNodes;
+    static bool empty(const Container & ioContainer)
+    {
+        return ioContainer.empty();
+    }
 };
 
 
-template<class T>
-bool HasCycles(const Node<T> & inNode, typename Node<T>::NodeSet & ioPreviousNodes, std::size_t inRecursionDepth)
+template<class ValueType>
+struct ContainerPolicy_Vector
 {
-    assert(inRecursionDepth < 100);
-
-    typedef typename Node<T>::NodeSet NodeSet;
-
-    const NodeSet & childNodes = inNode.mChildNodes;
-    std::size_t childCount = childNodes.size();
-    char firstLetter = inNode.name()[0];
-
-    if (!childNodes.empty())
+    typedef std::vector<ValueType> Container;
+    static void insert(Container & ioContainer, const ValueType & inValue)
     {
-        for (typename NodeSet::const_iterator it = childNodes.begin(),
-                                              end = childNodes.end();
+        ioContainer.push_back(inValue);
+    }
+
+    static size_t size(const Container & ioContainer)
+    {
+        return ioContainer.size();
+    }
+
+    static bool empty(const Container & ioContainer)
+    {
+        return ioContainer.empty();
+    }
+};
+
+
+template <class PointeeType>
+struct PointerPolicy_Normal_NoOwnership
+{
+    typedef PointeeType * PointerType;
+
+    static PointeeType * getRaw(PointerType p)
+    {
+        return p;
+    }
+
+    static void destroy(PointerType p) { }
+};
+
+
+template <class PointeeType>
+struct PointerPolicy_Normal_WithOwnership
+{
+    typedef PointeeType * PointerType;
+
+    static PointeeType * getRaw(PointerType p)
+    {
+        return p;
+    }
+
+    static void destroy(PointerType p)
+    {
+        delete p;
+    }
+};
+
+
+template <class PointeeType>
+struct PointerPolicy_Shared
+{
+    typedef boost::shared_ptr<PointeeType> PointerType;
+
+    static PointeeType * getRaw(PointerType p)
+    {
+        return p.get();
+    }
+
+    static void destroy(PointerType p) { }
+};
+
+
+template <class DataType,
+          template <class> class ContainerPolicy,
+          template <class> class PointerPolicy>
+class GenericNode
+{
+public:
+    GenericNode() { }
+
+    GenericNode(const DataType & inData) : mData(inData) { }
+
+    typedef GenericNode<DataType, ContainerPolicy, PointerPolicy> This;
+    typedef typename PointerPolicy<This>::PointerType ChildPtr;
+    typedef typename ContainerPolicy<ChildPtr>::Container Container;
+
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+
+    ~GenericNode()
+    {
+        const_iterator it = this->begin(), endIt = this->end();
+        for (; it != endIt; ++it)
+        {
+            PointerPolicy<This>::destroy(*it);
+        }
+    }
+
+    iterator begin()
+    {
+        return mChildren.begin();
+    }
+    iterator end()
+    {
+        return mChildren.end();
+    }
+
+    const_iterator begin() const
+    {
+        return mChildren.begin();
+    }
+    const_iterator end() const
+    {
+        return mChildren.end();
+    }
+
+    /**
+     * Takes ownership.
+     */
+    void insert(This * inItem)
+    {
+        ChildPtr item(inItem);
+        ContainerPolicy<ChildPtr>::insert(mChildren, item);
+    }
+
+    size_t size() const
+    {
+        return ContainerPolicy<ChildPtr>::size(mChildren);
+    }
+
+    bool empty() const
+    {
+        return ContainerPolicy<ChildPtr>::empty(mChildren);
+    }
+
+    const DataType & data() const
+    {
+        return mData;
+    }
+
+    void setData(const DataType & inData)
+    {
+        mData = inData;
+    }
+
+private:
+    DataType mData;
+    Container mChildren;
+};
+
+
+template<class T,
+         template<class> class C,
+         template<class> class P>
+bool HasCycles(const GenericNode<T, C, P> & inNode, typename GenericNode<T, C, P>::Container & ioPreviousNodes, std::size_t inRecursionDepth)
+{
+    if (inRecursionDepth >= 10)
+    {
+        return false;
+    }
+
+    typedef typename GenericNode<T, C, P>::Container Container;
+
+    if (!inNode.empty())
+    {
+        for (typename Container::const_iterator it = inNode.begin(),
+                                                end = inNode.end();
              it != end;
              ++it)
         {
-            Node<T> & child = **it;
+            GenericNode<T, C, P> & child = **it;
+            char value = child.data().mChar;
             if (ioPreviousNodes.find(&child) != ioPreviousNodes.end())
             {
                 return true;
             }
             else
             {
-                ioPreviousNodes.insert(&child);
                 if (HasCycles(child, ioPreviousNodes, inRecursionDepth + 1))
                 {
-                    childCount ++;
-                    firstLetter = 0;
                     return true;
                 }
+                ioPreviousNodes.insert(&child);
             }
+            value = '0';
         }
     }
     return false;
 }
 
 
-template<class T>
-bool HasCycles(const Node<T> & inNode)
+template<class T, template<class> class C, template<class> class P>
+bool HasCycles(const GenericNode<T, C, P> & inNode)
 {
-    typename Node<T>::NodeSet previousNodes; // empty for now
+    typename GenericNode<T, C, P>::Container previousNodes; // empty for now
     return HasCycles(inNode, previousNodes, 1);
 }
 
 
 struct Mutex
 {
+    Mutex(char inChar) : mChar(inChar) {}
+    char mChar;
 };
 
 
+template<class T>
+void Lock(T & a, T & b)
+{
+    a.insert(&b);
+}
 
+
+template<class T>
+void Lock(T & a, T & b, T& c)
+{
+    a.insert(&b);
+    b.insert(&c);
+}
+
+
+template<class T>
+void Lock(T & a, T & b, T& c, T & d)
+{
+    a.insert(&b);
+    b.insert(&c);
+    c.insert(&d);
+}
 
 
 int main()
 {
-    Node<int> a("a", 1);
+    typedef GenericNode<Mutex, ContainerPolicy_Set, PointerPolicy_Normal_NoOwnership> MutexNode;
+    MutexNode a('a'), b('b'), c('c'), d('d');
+
+    Lock(a, b);
+    Lock(a, c);
+    Lock(a, b, c);
     assert(!HasCycles(a));
-
-    Node<int> b("b", 2);
-    a.append(b);
-    assert(!HasCycles(a));
-
-    Node<int> c("c", 3);
-    a.append(b).append(c);
-    assert(!HasCycles(a));
-
-    Node<int> d("d", 4);
-    a.append(b).append(c).append(d);
-    assert(!HasCycles(a));
-
-    a.append(b).append(d);
-    assert(HasCycles(a));
-    assert(HasCycles(b));
-    assert(!HasCycles(c));
-    assert(!HasCycles(d));
-
-    a.append(b).append(d).append(c);
-    assert(HasCycles(a));
-    assert(HasCycles(b));
-    assert(HasCycles(c));
-    assert(HasCycles(d));
 
 
     std::cout << "Done!" << std::endl;

4dc725d8369f202a69770a6e40d8f8b925be6076
francis.rammeloo@gmail.com <StackedCrooked@users.noreply.github.com>
2011-06-10 15:28:12 +0000

    Basic implementation.

--- PlayGroundCpp/Threading/main.cpp
+++ PlayGroundCpp/Threading/main.cpp
@@ -1,10 +1,28 @@
 #include "Threading.h"
 #include <iostream>
 
+
+using namespace Threading;
+
+
+struct Point
+{
+    Point(int x_, int y_) : x(x_), y(y_) {}
+    int x;
+    int y;
+};
+
 int main()
 {
-    Threading::FastMutex theFastMutex;
-    Threading::FastMutex::ScopedLock theScopedLock(theFastMutex);
+    FastMutex theFastMutex;
+    FastMutex::ScopedLock theScopedLock(theFastMutex);
+
+    typedef ThreadSafe<Point> TSPoint;
+    TSPoint tsPoint(new Point(3, 4));
+    ScopedAccessor<Point> locker(tsPoint);
+    std::cout << "x: " << locker.get().x << std::endl;
+    std::cout << "y: " << locker.get().y << std::endl;
+
 
     std::cout << "Everything went better than expected." << std::endl << std::flush;
     return 0;

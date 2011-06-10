1101dd3abb2306f2eaa36780e370e28058976fee
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-06-10 21:32:21 +0000

    Now there is a decent test.

--- trunk/PlayGroundCpp/Threading/main.cpp
+++ trunk/PlayGroundCpp/Threading/main.cpp
@@ -8,99 +8,93 @@
 using namespace Threading;
 
 
-
-
 class Tester
 {
 public:
-	Tester() :
-		mCharacters(),
-		mAppendDigitsThread(),
-		mAppendLettersThread()
-	{
-	}
+    Tester() :
+        mCharacters(),
+        mAppendDigitsThread(),
+        mAppendLettersThread()
+    {
+    }
 
-	void start()
-	{
-		mControllerThread.reset(new boost::thread(boost::bind(&Tester::startImpl, this)));
-	}
+    void start()
+    {
+        mControllerThread.reset(new boost::thread(boost::bind(&Tester::startImpl, this)));
+    }
 
-	void stop()
-	{
-		mControllerThread->interrupt();
-	}
+    void stop()
+    {
+        mControllerThread->interrupt();
+    }
 
-	void print()
-	{
-		ScopedAccessor<Characters, PosixMutex> accessor(mCharacters);
-		Characters & characters = accessor.get();
-		for (std::size_t idx = 0; idx < characters.size(); ++idx)
-		{
-			std::cout << characters[idx];
-		}
-	}
+    void print()
+    {
+        ScopedAccessor<Characters, PosixMutex> accessor(mCharacters);
+        Characters & characters = accessor.get();
+        for (std::size_t idx = 0; idx < characters.size(); ++idx)
+        {
+            std::cout << characters[idx];
+        }
+    }
 
 
-	void startImpl()
-	{
-		mAppendDigitsThread.reset(new boost::thread(boost::bind(&Tester::appendLetters, this)));
-		mAppendLettersThread.reset(new boost::thread(boost::bind(&Tester::appendDigits, this)));
-	}
+    void startImpl()
+    {
+        mAppendDigitsThread.reset(new boost::thread(boost::bind(&Tester::appendLetters, this)));
+        mAppendLettersThread.reset(new boost::thread(boost::bind(&Tester::appendDigits, this)));
+    }
 
-	void appendDigits()
-	{
-		while (true)
-		{
+    void appendDigits()
+    {
+        while (true)
+        {
             ScopedAccessor<Characters, PosixMutex> accessor(mCharacters);
             Characters & characters = accessor.get();
 
-			for (char c = '0'; c <= '9'; ++c)
-			{
-				characters.push_back(c);
-			}
+            for (char c = '0'; c <= '9'; ++c)
+            {
+                characters.push_back(c);
+            }
             characters.push_back('\n');
-			boost::this_thread::interruption_point();
-		}
-	}
+            boost::this_thread::interruption_point();
+        }
+    }
 
-	void appendLetters()
-	{
-		while (true)
-		{
+    void appendLetters()
+    {
+        while (true)
+        {
             ScopedAccessor<Characters, PosixMutex> accessor(mCharacters);
             Characters & characters = accessor.get();
 
-			for (char c = 'A'; c <= 'Z'; ++c)
-			{
-				characters.push_back(c);
-			}
+            for (char c = 'A'; c <= 'Z'; ++c)
+            {
+                characters.push_back(c);
+            }
             characters.push_back('\n');
-			boost::this_thread::interruption_point();
-		}
-	}
+            boost::this_thread::interruption_point();
+        }
+    }
 
-	typedef std::vector<char> Characters;
-	ThreadSafe<Characters, PosixMutex> mCharacters;
+    typedef std::vector<char> Characters;
+    ThreadSafe<Characters, PosixMutex> mCharacters;
 
-	boost::scoped_ptr<boost::thread> mControllerThread;
-	boost::scoped_ptr<boost::thread> mAppendDigitsThread;
-	boost::scoped_ptr<boost::thread> mAppendLettersThread;
+    boost::scoped_ptr<boost::thread> mControllerThread;
+    boost::scoped_ptr<boost::thread> mAppendDigitsThread;
+    boost::scoped_ptr<boost::thread> mAppendLettersThread;
 };
 
 
 int main()
 {
-	std::cout << "Testing\n";
-	Tester tester;
-	std::cout << "Starting\n";
-	tester.start();
-	std::cout << "Sleep 1s\n";
-	sleep(1);
-	std::cout << "Stopping\n";
-	tester.stop();
-	std::cout << "Printing\n";
-	tester.print();
-	
+    Tester tester;
+    tester.start();
+    boost::xtime duration = {0, 500 * 1000 * 1000};
+    boost::this_thread::sleep(duration);
+    tester.stop();
+    tester.print();
+
 
     std::cout << "Everything went better than expected." << std::endl;
     return 0;

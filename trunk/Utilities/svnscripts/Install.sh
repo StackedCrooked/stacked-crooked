dedfe2bd2000459a9997567815aae5bdbaee9c74
francis.rammeloo@gmail.com <francis.rammeloo@gmail.com@78535834-c32d-11dd-927d-974589228060>
2011-07-26 20:28:02 +0000

    Use gvim for diffing.

--- trunk/Utilities/svnscripts/Install.sh
+++ trunk/Utilities/svnscripts/Install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make sure all scripts are executable
-chmod 755 fmdiff
+chmod 755 svndiffer
 chmod 755 svnadds
 chmod 755 svnchanges
 chmod 755 svndiff
@@ -13,11 +13,4 @@ ln -fs `pwd`/svndiff $HOME/bin/svndiff
 ln -fs `pwd`/svnunknowns $HOME/bin/svnunknowns 
 ln -fs `pwd`/svnadds $HOME/bin/svnadds 
 ln -fs `pwd`/svnmissing $HOME/bin/svnmissing 
-
-# fmdiff is a Mac only tool
-if [[ "`uname`" == 'Darwin' ]]; then
-    ln -fs `pwd`/fmdiff $HOME/bin/fmdiff 
-else
-    echo "Skipping fmdiff (Mac only)."
-fi
-
+ln -fs `pwd`/svndiffer $HOME/bin/svndiffer 

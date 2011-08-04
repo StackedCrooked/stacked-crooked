eecc2d0faa8bdf80bf390918b898ea64dece416e
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-08-04 18:55:02 +0000

    Add GetMALScore.sh

--- trunk/Playground/AnimeRatings/GetTitles.sh
+++ trunk/Playground/AnimeRatings/GetTitles.sh
@@ -1,1 +1,2 @@
-xpath Anime_of_2011.html '//a[@name="Pages_in_category"]/../..//li//a/@title'
+#!/bin/sh
+xpath Anime_of_2011.html '//a[@name="Pages_in_category"]/../..//li//a/@title' 2>/dev/null | sed "s/ title\=/\\`echo -e '\n\r'`/g" | sed 's/\"//g' | sed 's/\r//g'

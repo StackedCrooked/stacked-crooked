ea9f7fd5a1171d00464219f0c7777cc1094e3b5e
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-08-04 21:08:26 +0000

    Simple working version.

--- Playground/AnimeRatings/ChromePlugin/addratings.js
+++ Playground/AnimeRatings/ChromePlugin/addratings.js
@@ -10,8 +10,6 @@ function getMALScore(callback, titleNode) {
       }
     }
   }
-  // Note that any URL fetched here must be matched by a permission in
-  // the manifest.json file!
   var url = "http://myanimelist.net/api/anime/search.xml?q=" + titleNode.innerText;
   xmlhttp.open('GET', url, true);
   xmlhttp.send();
@@ -19,18 +17,12 @@ function getMALScore(callback, titleNode) {
 
 
 function parseXML(xml, titleNode) {
-    alert(xml);
     var parser = new DOMParser();
-    alert("parser: " + parser);
     var doc = parser.parseFromString(xml, "text/xml");
-    alert("doc: " + doc);
     var scores = doc.getElementsByTagName("score");
-    alert("scores: " + scores);
     if (scores.length > 0) {
         var score = scores[0];
-        alert("score: " + score);
         var actualScore = score.childNodes[0].nodeValue;
-        alert("actualScore: " + actualScore);
         titleNode.innerText = titleNode.innerText + " " + actualScore;
     }
 }
@@ -38,12 +30,11 @@ function parseXML(xml, titleNode) {
 
 function findTitles() {
     var titles = document.getElementsByTagName("a");
-    for (var i = 39; i < titles.length; ++i) {
+    for (var i = 0; i < titles.length; ++i) {
         var titleNode = titles[i];
         if (titleNode.innerText) {
             getMALScore(parseXML, titleNode);
         }
-        break;
     }
 }
 findTitles();

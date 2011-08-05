1f96229e9bf27701f3b334459759bfa5a99688a0
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-08-05 20:22:42 +0000

    Simplify example

--- Playground/AnimeRatings/ChromePlugin/addratings.js
+++ Playground/AnimeRatings/ChromePlugin/addratings.js
@@ -1,4 +1,15 @@
-alert("cookies");
-chrome.cookies.getAll({}, function(cookies) { alert("Yes cookies"); });
-alert("after cookies.");
-
+alert("1");
+var http = new window.XMLHttpRequest();
+alert("2");
+http.onreadystatechange = function(data) {
+  if (http.readyState == 4) {
+    alert(JSON.stringify(http));
+  }
+}
+alert("3");
+var url = "http://myanimelist.net/api/anime/search.xml?q=Madoka";
+alert("4");
+http.open('GET', url, true);
+alert("5");
+http.send();
+alert("6");

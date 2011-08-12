45d59aa4934bfac18f3d886dc0de44ab2bb053be
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-08-12 19:39:58 +0000

    Define entry point.

--- trunk/Playground/AnimeRatings/ChromePlugin/content.js
+++ trunk/Playground/AnimeRatings/ChromePlugin/content.js
@@ -35,39 +35,50 @@ function AnimeRatings() {
 
 
 try {
-	var ar = new AnimeRatings();
 
-	ar.getMWPages = function() {
-		var divs = document.getElementsByTagName("div");
-		for (var i = 0; i < divs.length; ++i) {
-			if (divs[i].id == "mw-pages") {
-				return divs[i];
-			}
-		}
-		throw "Could not find root node for the anime titles.";
-	};
 
-	ar.getLinks = function(mwpages) {
-		var result = [];
-		var lis = mwpages.getElementsByTagName("li");
-		for (var i = 0; i < lis.length; ++i) {
-			var links = lis[i].getElementsByTagName("a");
-			if (links.length > 0) {
-				result.push(links[0]);
-			}
-		}
-		if (result.length === 0) {
-			throw "No page links were found.";
-		}
-		return result;
-	};
+var ar = new AnimeRatings();
 
+ar.getMWPages = function() {
+	var divs = document.getElementsByTagName("div");
+	for (var i = 0; i < divs.length; ++i) {
+		if (divs[i].id == "mw-pages") {
+			return divs[i];
+		}
+	}
+	throw "Could not find root node for the anime titles.";
+};
+
+ar.getLinks = function(mwpages) {
+	var result = [];
+	var lis = mwpages.getElementsByTagName("li");
+	for (var i = 0; i < lis.length; ++i) {
+		var links = lis[i].getElementsByTagName("a");
+		if (links.length > 0) {
+			result.push(links[0]);
+		}
+	}
+	if (result.length === 0) {
+		throw "No page links were found.";
+	}
+	return result;
+};
+
+
+/// Define point
+ar.main = function() {
 	var links = ar.getLinks(ar.getMWPages());
 	for (var i = 0; i < links.length; ++i) {
 		ar.log(links[i].title);
 	}
-}
-catch (exc) {
+};
+
+
+/// Start application
+ar.main();
+
+
+} catch (exc) {
 	alert(JSON.stringify(exc));
 }
 

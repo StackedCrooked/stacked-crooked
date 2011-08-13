a2f1d98aeb82cc3477405aa9693b317f83b94418
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-08-13 00:45:31 +0000

    Improved Search.

--- trunk/Playground/AnimeRatings/SearchMAL.sh
+++ trunk/Playground/AnimeRatings/SearchMAL.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 URL=http://myanimelist.net/api/anime/search.xml
-wget --output-document=result.xml --user=$USER --password=$PASSWORD "$URL?q=$@"
+wget --output-document=result.xml --user=AnimeRatings --password=Chrome "$URL?q=$@" 1>/dev/null 2>/dev/null
+cat result.xml
+rm result.xml

1f24a9229d736c8e58c77b7a54be1bbe92a681d8
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-08-13 01:48:13 +0000

    Update SearchMAL.

--- trunk/Playground/AnimeRatings/SearchMAL.sh
+++ trunk/Playground/AnimeRatings/SearchMAL.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-URL=http://myanimelist.net/api/anime/search.xml
-wget --output-document=result.xml --user=AnimeRatings --password=Chrome "$URL?q=$@" 1>/dev/null 2>/dev/null
+wget --output-document=result.xml --user=AnimeRatings --password=Chrome $@ 1>/dev/null 2>/dev/null
 cat result.xml
 rm result.xml

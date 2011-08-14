2cb7b875f3e9aba38e1ef9c60232ae982e9a8fd5
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-08-14 18:07:20 +0000

    Pack now includes icons.

--- trunk/Playground/AnimeRatings/ChromePlugin/pack.sh
+++ trunk/Playground/AnimeRatings/ChromePlugin/pack.sh
@@ -2,5 +2,5 @@
 rm -rf anime-ratings
 rm -f anime-ratings.crx
 mkdir anime-ratings
-cp background.html content.js manifest.json anime-ratings/
+cp icon128.png icon16.png icon48.png background.html content.js manifest.json anime-ratings/
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=anime-ratings --pack-extension-key=/Volumes/MYBOOK/PrivateKeys/anime-ratings.pem

14269722ebb054c465f115cfacbb3fc771cb085c
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-08-14 21:11:07 +0000

    Pack deletes anime-ratings dir after zipping it.

--- Playground/AnimeRatings/ChromePlugin/pack.sh
+++ Playground/AnimeRatings/ChromePlugin/pack.sh
@@ -3,3 +3,4 @@ rm -rf anime-ratings
 rm -f anime-ratings.crx
 mkdir anime-ratings
 zip anime-ratings.zip icon128.png icon16.png icon48.png background.html content.js manifest.json
+rm -rf anime-ratings

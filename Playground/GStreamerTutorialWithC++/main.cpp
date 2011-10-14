864d6321ae1a4e4ff150e422eed0ee2300367f13
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-10-14 00:00:17 +0000

    Further cleanup.

--- Playground/GStreamerTutorialWithC++/main.cpp
+++ Playground/GStreamerTutorialWithC++/main.cpp
@@ -6,7 +6,7 @@
 #include <stdexcept>
 
 
-static gboolean OnBusMessage(GstBus * bus, GstMessage * msg, Gst::MainLoop * loop)
+static gboolean on_bus_message(GstBus * bus, GstMessage * msg, Gst::MainLoop * loop)
 {
     switch (GST_MESSAGE_TYPE(msg))
     {
@@ -45,8 +45,10 @@ static void on_pad_added(GstElement * element, GstPad * pad, gpointer data)
 
 void run(const std::string & inFileName)
 {
+    // Create a main loop object.
     Gst::MainLoop loop;
 
+    
     // Create gstreamer elements
     Gst::ScopedObject<GstElement> pipeline(Gst::Pipeline::Create("audio-player"));
     Gst::ScopedObject<GstElement> source(Gst::Element::Create(pipeline, "filesrc", "file-source"));
@@ -55,45 +57,49 @@ void run(const std::string & inFileName)
     Gst::ScopedObject<GstElement> conv(Gst::Element::Create(pipeline, "audioconvert",  "converter"));
     Gst::ScopedObject<GstElement> sink(Gst::Element::Create(pipeline, "autoaudiosink", "audio-output"));
 
-    // Set up the pipeline    
-
+    
     // We set the input filename to the source element
     g_object_set(G_OBJECT(source.get()), "location", inFileName.c_str(), NULL);
 
-    // we link the elements together
-    // file-source -> ogg-demuxer ~> vorbis-decoder -> converter -> alsa-output
+    
+    // Links elements
     Gst::Element::Link(source, demuxer);
     Gst::Element::Link(decoder, conv, sink);
     
     
-    // we add a message handler    
-    Gst::ScopedBusListener busListener(pipeline.get(), boost::bind(&OnBusMessage, _1, _2, &loop));
+    // We add a message handler. Also pass the loop as an extra argument.
+    Gst::ScopedBusListener busListener(pipeline.get(), boost::bind(&on_bus_message, _1, _2, &loop));
 
-
+    
     // Note that the demuxer will be linked to the decoder dynamically.
     // The reason is that Ogg may contain various streams (for example
     // audio and video). The source pad(s) will be created at run time,
     // by the demuxer when it detects the amount and nature of streams.
     // Therefore we connect a callback function which will be executed
-    // when the "pad-added" is emitted.
-    
+    // when the "pad-added" is emitted.    
     g_signal_connect(demuxer, "pad-added", G_CALLBACK(on_pad_added), decoder);
 
+    
     // Set the pipeline to "playing" state
     std::cout << "Now playing: " << inFileName << std::endl;    
     Gst::Pipeline::SetState(pipeline, GST_STATE_PLAYING);
+    
     
     // Start the event loop
     std::cout << "Running..." << std::endl;
     loop.run();
 
+    
     // Out of the main loop, clean up nicely
     std::cout << "Returned, stopping playback" << std::endl;
 
+    
+    // Stop the pipeline
     Gst::Element::SetState(pipeline, GST_STATE_NULL);
 
+    
     std::cout << "Deleting pipeline" << std::endl;
-    // Auto cleanup
+    // ScopedObject performs cleanup in its destructor.
 }
 
 

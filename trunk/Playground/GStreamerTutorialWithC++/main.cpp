c54fe30b64b067d192a55242cd59c07e56d5b938
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-10-14 00:18:05 +0000

    Further cleanup.

--- trunk/Playground/GStreamerTutorialWithC++/main.cpp
+++ trunk/Playground/GStreamerTutorialWithC++/main.cpp
@@ -1,24 +1,27 @@
-#include "Gst/Support.h"
 #include "Gst/BusListener.h"
 #include "Gst/MainLoop.h"
+#include "Gst/Support.h"
 #include <boost/bind.hpp>
 #include <iostream>
 #include <stdexcept>
 
 
-static gboolean on_bus_message(GstBus * bus, GstMessage * msg, Gst::MainLoop * loop)
+using namespace Gst;
+
+
+static gboolean on_bus_message(GstBus * bus, GstMessage * msg, MainLoop * loop)
 {
     switch (GST_MESSAGE_TYPE(msg))
     {
         case GST_MESSAGE_EOS:
         {
-            g_print("End of stream\n");
+            std::cout << "End of stream" << std::endl;
             loop->quit();
             break;
         }
         case GST_MESSAGE_ERROR:
         {
-            std::cout << Gst::Logging::GetLogMessage(msg) << std::endl;
+            std::cout << Logging::GetLogMessage(msg) << std::endl;
             loop->quit();
             break;
         }
@@ -34,42 +37,36 @@ static gboolean on_bus_message(GstBus * bus, GstMessage * msg, Gst::MainLoop * l
 static void on_pad_added(GstElement * element, GstPad * pad, gpointer data)
 {
     GstElement * decoder = (GstElement *) data;    
-    Gst::ScopedObject<GstPad> sinkpad(Gst::Element::GetStaticPad(decoder, "sink"));       
+    ScopedObject<GstPad> sinkpad(Element::GetStaticPad(decoder, "sink"));       
 
     // We can now link this pad with the vorbis-decoder sink pad.
     std::cout << "Dynamic pad created, linking demuxer/decoder" << std::endl;    
 
-    Gst::Pad::Link(pad, sinkpad);    
+    Pad::Link(pad, sinkpad);    
 }
 
 
 void run(const std::string & inFileName)
 {
     // Create a main loop object.
-    Gst::MainLoop loop;
-
+    MainLoop loop;
     
     // Create gstreamer elements
-    Gst::ScopedObject<GstElement> pipeline(Gst::Pipeline::Create("audio-player"));
-    Gst::ScopedObject<GstElement> source(Gst::Element::Create(pipeline, "filesrc", "file-source"));
-    Gst::ScopedObject<GstElement> demuxer(Gst::Element::Create(pipeline, "oggdemux",      "ogg-demuxer"));
-    Gst::ScopedObject<GstElement> decoder(Gst::Element::Create(pipeline, "vorbisdec",     "vorbis-decoder"));
-    Gst::ScopedObject<GstElement> conv(Gst::Element::Create(pipeline, "audioconvert",  "converter"));
-    Gst::ScopedObject<GstElement> sink(Gst::Element::Create(pipeline, "autoaudiosink", "audio-output"));
-
+    ScopedObject<GstElement> pipeline(Pipeline::Create("audio-player"));
+    ScopedObject<GstElement> source(Element::Create(pipeline, "filesrc", "file-source"));
+    ScopedObject<GstElement> demuxer(Element::Create(pipeline, "oggdemux",      "ogg-demuxer"));
+    ScopedObject<GstElement> decoder(Element::Create(pipeline, "vorbisdec",     "vorbis-decoder"));
+    ScopedObject<GstElement> conv(Element::Create(pipeline, "audioconvert",  "converter"));
+    ScopedObject<GstElement> sink(Element::Create(pipeline, "autoaudiosink", "audio-output"));
     
     // We set the input filename to the source element
     g_object_set(G_OBJECT(source.get()), "location", inFileName.c_str(), NULL);
+    
+    // Link the first half
+    Element::Link(source, demuxer);
 
-    
-    // Links elements
-    Gst::Element::Link(source, demuxer);
-    Gst::Element::Link(decoder, conv, sink);
-    
-    
-    // We add a message handler. Also pass the loop as an extra argument.
-    Gst::ScopedBusListener busListener(pipeline.get(), boost::bind(&on_bus_message, _1, _2, &loop));
-
+    // Link the second half
+    Element::Link(decoder, conv, sink);
     
     // Note that the demuxer will be linked to the decoder dynamically.
     // The reason is that Ogg may contain various streams (for example
@@ -78,30 +75,27 @@ void run(const std::string & inFileName)
     // Therefore we connect a callback function which will be executed
     // when the "pad-added" is emitted.    
     g_signal_connect(demuxer, "pad-added", G_CALLBACK(on_pad_added), decoder);
-
     
+    // We add a message handler. Also pass the loop as an extra argument.
+    ScopedBusListener busListener(pipeline.get(), boost::bind(&on_bus_message, _1, _2, &loop));
+
     // Set the pipeline to "playing" state
     std::cout << "Now playing: " << inFileName << std::endl;    
-    Gst::Pipeline::SetState(pipeline, GST_STATE_PLAYING);
-    
+    Pipeline::SetState(pipeline, GST_STATE_PLAYING);
     
     // Start the event loop
     std::cout << "Running..." << std::endl;
     loop.run();
-
     
     // Out of the main loop, clean up nicely
     std::cout << "Returned, stopping playback" << std::endl;
-
     
     // Stop the pipeline
-    Gst::Element::SetState(pipeline, GST_STATE_NULL);
-
+    Element::SetState(pipeline, GST_STATE_NULL);
     
     std::cout << "Deleting pipeline" << std::endl;
     // ScopedObject performs cleanup in its destructor.
 }
-
 
 
 int main(int argc, char * argv[])
@@ -109,7 +103,7 @@ int main(int argc, char * argv[])
     // Check input arguments
     if (argc != 2)
     {
-        g_printerr("Usage: %s <Ogg/Vorbis filename>\n", argv[0]);
+        std::cerr << "Usage: " << argv[0] << " <Ogg/Vorbis filename>" << std::endl;
         return -1;
     }
     
@@ -126,3 +120,4 @@ int main(int argc, char * argv[])
     }
     return 0;
 }
+

a0153aa47b62cff072de0ce129db4cd2de4822f7
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2011-11-05 19:54:59 +0000

    Add instructors for Unity disable of autohide.

--- Utilities/RAMDisk/MacOSX/MountRAMDisk.sh
+++ Utilities/RAMDisk/MacOSX/MountRAMDisk.sh
@@ -2,5 +2,5 @@
 # Clustersize seems to be 512 bytes.
 # Formula: RequestedMB * 2048 = NumberOfClusters
 # Create a 1 GB volume by specifying ram://2048000
-diskutil erasevolume HFS+ "RAMDisk" `hdiutil attach -nomount ram://2048000`
+diskutil erasevolume HFS+ "RAMDisk" `hdiutil attach -nomount ram://5000000`
 

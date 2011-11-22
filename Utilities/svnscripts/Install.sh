526577a9719065974cdf9a2642551db90d6026d0
francis.rammeloo@gmail.com <StackedCrooked@users.noreply.github.com>
2011-11-22 14:41:14 +0000

    Add svnlogimpl.

--- Utilities/svnscripts/Install.sh
+++ Utilities/svnscripts/Install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SVN_SCRIPTS="svnadds svnchanges svnconflicts svndiff svndiffer svnfind svnmissing svnunknowns"
+SVN_SCRIPTS="svnadds svnchanges svnconflicts svndiff svndiffer svnfind svnmissing svnunknowns svnlog svnlogimpl"
 
 for FILE in $SVN_SCRIPTS; do
     chmod +x $FILE

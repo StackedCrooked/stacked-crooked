c5cfcc75ddeac3e3f7550b5c2aae3f8260b9614d
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2011-12-02 17:42:53 +0000

    Update pack script.

--- trunk/Playground/AnimeRatings/ChromePlugin/pack.sh
+++ trunk/Playground/AnimeRatings/ChromePlugin/pack.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 rm -rf anime-ratings
-rm -f anime-ratings.crx
 mkdir anime-ratings
 zip anime-ratings.zip icon128.png icon16.png icon48.png background.html content.js manifest.json
 rm -rf anime-ratings

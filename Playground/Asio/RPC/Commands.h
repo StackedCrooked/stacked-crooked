f951dd99dde78625c5414be1a5edecba6755942f
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-01-27 20:10:31 +0000

    Use tuple args.

--- Playground/Asio/RPC/Commands.h
+++ Playground/Asio/RPC/Commands.h
@@ -4,77 +4,52 @@
 
 #include "RPC/Command.h"
 #include "RPC/RemoteObjects.h"
-#include <boost/tuple/tuple_io.hpp>
+
+
+namespace boost {
+namespace serialization {
+
+
+using boost::tuples::tuple;
+
+
+template<class Archive, class T0>
+void serialize(Archive & ar, tuple<T0> & value, const unsigned int)
+{
+    RPC::serialize_tuple(ar, value);
+}
+
+
+template<class Archive, typename T0, typename T1>
+void serialize(Archive & ar, tuple<T0, T1> & value, const unsigned int)
+{
+    RPC::serialize_tuple(ar, value);
+}
+
+
+template<class Archive, typename T0, typename T1, typename T2>
+void serialize(Archive & ar, tuple<T0, T1, T2> & value, const unsigned int)
+{
+    RPC::serialize_tuple(ar, value);
+}
+
+
+} // namespace serialization
+} // namespace boost
 
 
 namespace RPC {
 
 
-using boost::tuples::get;
 using boost::tuples::tuple;
-typedef std::string URL;
 
 
-template<class Tuple>
-struct Helper
+struct CreateStopwatch : public ConcreteCommand<RemoteStopwatch(tuple<RemoteServer, std::string>)>
 {
-    template<typename Archive>
-    static void serialize(Archive & ar, Tuple & tuple)
-    {
-        ar & tuple.get_head();
-        Helper<typename Tuple::tail_type>::serialize(ar, tuple.get_tail());
-    }
-};
-
-
-template<>
-struct Helper<boost::tuples::null_type>
-{
-    template<typename Archive>
-    static void serialize(Archive &, const boost::tuples::null_type &) { }
-};
-
-
-template<typename Archive, typename Tuple>
-void serialize_tuple(Archive & ar, Tuple & tuple)
-{
-
-    Helper<Tuple>::serialize(ar, tuple);
-}
-
-
-struct CreateStopwatchArgs
-{
-public:
-    CreateStopwatchArgs() {}
-
-    CreateStopwatchArgs(const RemoteServer & inServer, const std::string & inName) :
-        mTuple(inServer, inName)
-    {
-    }
-
-    const RemoteServer & server() const { return mTuple.get<0>(); }
-
-    const std::string & name() const { return mTuple.get<1>(); }
-
-    template<typename Archive>
-    void serialize(Archive & ar, const unsigned int)
-    {
-        serialize_tuple(ar, mTuple);
-    }
-
-    tuple<RemoteServer, std::string> mTuple;
-};
-
-
-struct CreateStopwatch : public ConcreteCommand<RemoteStopwatch(CreateStopwatchArgs)>
-{
-    typedef ConcreteCommand<RemoteStopwatch(CreateStopwatchArgs)> Super;
-
     static const char * CommandName() { return "CreateStopwatch"; }
 
-    CreateStopwatch(const RemoteServer & inServer, const std::string & inName) :
-        Super(CommandName(), CreateStopwatchArgs(inServer, inName))
+    CreateStopwatch(const Arg & inArgs) :
+        Super(CommandName(), inArgs)
     {
     }
 };

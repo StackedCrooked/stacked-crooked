fa005c3674b597a1091259930924175b7cebaf27
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-01-28 22:22:20 +0000

    Update todo list.

--- Playground/RPC/TupleSupport.h
+++ Playground/RPC/TupleSupport.h
@@ -1,8 +1,5 @@
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/serialization.hpp>
 #include <boost/tuple/tuple.hpp>
-#include <sstream>
 
 
 namespace boost {

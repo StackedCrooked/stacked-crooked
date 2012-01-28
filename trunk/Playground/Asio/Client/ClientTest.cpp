4b732918d5284b128259ba60fff8cd1ccbe30082
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-01-28 18:33:59 +0000

    Make UDPClient.

--- trunk/Playground/Asio/Client/ClientTest.cpp
+++ trunk/Playground/Asio/Client/ClientTest.cpp
@@ -17,29 +17,39 @@
 using boost::asio::ip::udp;
 
 
-std::string SendUDPMessage(const std::string & inURL,
-                           short inPort,
-                           const std::string & inMessage)
+class UDPClient
 {
+public:
+    UDPClient(const std::string & inURL, short inPort) :
+        io_service(),
+        socket(io_service, udp::endpoint(udp::v4(), 0)),
+        resolver(io_service),
+        query(udp::v4(), inURL.c_str(), boost::lexical_cast<std::string>(inPort).c_str()),
+        iterator(resolver.resolve(query))
+    {
+    }
+
+    std::string send(const std::string & inMessage)
+    {
+        socket.send_to(boost::asio::buffer(inMessage.c_str(), inMessage.size()), *iterator);
+
+        static const unsigned cMaxLength = 1024;
+        char reply[cMaxLength];
+        udp::endpoint sender_endpoint;
+        size_t reply_length = socket.receive_from(boost::asio::buffer(reply, cMaxLength), sender_endpoint);
+        return std::string(reply, reply_length);
+    }
+
     boost::asio::io_service io_service;
-
-    udp::socket socket(io_service, udp::endpoint(udp::v4(), 0));
-
-    udp::resolver resolver(io_service);
-    udp::resolver::query query(udp::v4(), inURL.c_str(), boost::lexical_cast<std::string>(inPort).c_str());
-    udp::resolver::iterator iterator = resolver.resolve(query);
-
-    socket.send_to(boost::asio::buffer(inMessage.c_str(), inMessage.size()), *iterator);
-
-    static const unsigned cMaxLength = 1024;
-    char reply[cMaxLength];
-    udp::endpoint sender_endpoint;
-    size_t reply_length = socket.receive_from(boost::asio::buffer(reply, cMaxLength), sender_endpoint);
-    return std::string(reply, reply_length);
-}
+    udp::socket socket;
+    udp::resolver resolver;
+    udp::resolver::query query;
+    udp::resolver::iterator iterator;
+};
 
 
 int main()
 {
-    std::cout << SendUDPMessage("127.0.0.1", 9001, "Hello") << std::endl;
+    UDPClient client("127.0.0.1", 9001);
+    std::cout << client.send("Hello") << std::endl;
 }

cedd52402152a78bb8945ef098bc54f60e6c4deb
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-01-29 14:44:43 +0000

    Expand serialize templates for tuples.

--- trunk/Playground/RPC/TupleSupport.h
+++ trunk/Playground/RPC/TupleSupport.h
@@ -33,13 +33,26 @@ void serialize(Archive & ar, tuple<T0, T1, T2> & t, const unsigned int)
 }
 
 
-template<typename Archive, typename T0, typename T1, typename T2, typename T3>
-void serialize(Archive & ar, tuple<T0, T1, T2, T3> & t, const unsigned int)
+template<typename Archive, typename T0, typename T1, typename T2, typename T3, typename T4>
+void serialize(Archive & ar, tuple<T0, T1, T2, T3, T4> & t, const unsigned int)
 {
     ar & get<0>(t);
     ar & get<1>(t);
     ar & get<2>(t);
     ar & get<3>(t);
+    ar & get<4>(t);
+}
+
+
+template<typename Archive, typename T0, typename T1, typename T2, typename T3, typename T4, typename T5>
+void serialize(Archive & ar, tuple<T0, T1, T2, T3, T4, T5> & t, const unsigned int)
+{
+    ar & get<0>(t);
+    ar & get<1>(t);
+    ar & get<2>(t);
+    ar & get<3>(t);
+    ar & get<4>(t);
+    ar & get<5>(t);
 }
 
 

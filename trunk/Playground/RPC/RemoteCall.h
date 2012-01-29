380a5cb937c519c14e9ed28cf45e996254e6c751
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-01-29 17:31:33 +0000

    Do a little stress-testing.

--- trunk/Playground/RPC/RemoteCall.h
+++ trunk/Playground/RPC/RemoteCall.h
@@ -174,36 +174,36 @@ struct Batch;
  *
  * A simple `Add` function for adding two numbers could be defined as:
  *
- *     // -> Declare call with a name and signature.
- *     typedef std::pair<int, int> Pair;
- *     RPC_CALL(Add, int(Pair)
+ *     // Declare call with a name and signature.
+ *     RPC_CALL(Add, int(pair<int, int>)
  *
- *     // -> A helper class with the given name is generated.
- *     //    You need to provide an implementation for its `execute` method:
- *     int Add::execute(const Pair & value)
+ * A helper class with the given name is generated.
+ * You need to provide an implementation for its `execute` method:
+ *
+ *     int Add::execute(const pair<int, int> & value)
  *     {
  *         return value.first + value.second;
  *     }
  *
- *     // -> You can now use it as follows:
+ * You can now use it as follows:
+ *
  *     int seven = Add(make_pair(3, 4)).send();
  *
- *     // -> There is also the `Batch` utility that allows to apply same
- *     //    method on multiple values:
- *     std::vector<Pair> pairs = get_some_pairs();
+ * There is also the `Batch` utility that allows to apply same method on multiple values:
+ *
+ *     std::vector<pair<int, int> > pairs = ...;
  *     std::vector<int> sums = Batch<Add>(pairs).send();
  *
  * Serialization works out-of-the box for builtin types, most standard containers,
  * boost tuple types and any combination of these. User defined structs and
- * classes must be made serializable by providing a `serialize` function.
- * See the boost serialization documentation for more info.
+ * classes must be made serializable. See the boost documentation for more info.
  */
-RPC_CALL(CreateStopwatch, RemoteStopwatch(std::string))
-RPC_CALL(GetStopwatches, RemoteStopwatches(Void))
-RPC_CALL(StartStopwatch, Void(RemoteStopwatch))
-RPC_CALL(CheckStopwatch, unsigned(RemoteStopwatch))
-RPC_CALL(StopStopwatch, unsigned(RemoteStopwatch))
-RPC_CALL(DestroyStopwatch, Void(RemoteStopwatch))
+RPC_CALL(CreateStopwatch,  RemoteStopwatch(std::string) )
+RPC_CALL(GetStopwatches,   RemoteStopwatches(Void)      )
+RPC_CALL(StartStopwatch,   Void(RemoteStopwatch)        )
+RPC_CALL(CheckStopwatch,   unsigned(RemoteStopwatch)    )
+RPC_CALL(StopStopwatch,    unsigned(RemoteStopwatch)    )
+RPC_CALL(DestroyStopwatch, Void(RemoteStopwatch)        )
 
 
 #endif // RPC_COMMAND_H

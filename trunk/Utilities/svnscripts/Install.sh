01e78de26a7e4596b026739d430ec41fb28f258e
francis.rammeloo@gmail.com <francis.rammeloo@gmail.com@78535834-c32d-11dd-927d-974589228060>
2012-03-31 21:40:20 +0000

    Add svnup script. (Performs svn update --ignore-externals.)

--- trunk/Utilities/svnscripts/Install.sh
+++ trunk/Utilities/svnscripts/Install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SVN_SCRIPTS="svnstatus svnadds svnchanges svnconflicts svndiff svndiffer svnfind svnmissing svnunknowns svnlog svnlogimpl"
+SVN_SCRIPTS="svnstatus svnadds svnchanges svnconflicts svndiff svndiffer svnfind svnmissing svnunknowns svnlog svnlogimpl svnup"
 
 for FILE in $SVN_SCRIPTS; do
     chmod +x $FILE

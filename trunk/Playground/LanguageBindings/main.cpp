0800bf69db9fb8be56b974ca7cbfe825c698ac77
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-04-20 16:11:06 +0000

    Add Calculator sample.

--- trunk/Playground/LanguageBindings/main.cpp
+++ trunk/Playground/LanguageBindings/main.cpp
@@ -1,3 +1,53 @@
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <boost/lexical_cast.hpp>
+
+
+struct Calculator
+{
+    int sum(int a, int b) { return a + b; }
+
+    int multiply(int a, int b) { return a * b; }
+};
+
+
+// Bindings to Tcl
+struct TclCalculator
+{
+    typedef std::string Arg;
+    typedef std::vector<Arg> Args;
+
+    std::string sum(const Args & args)
+    {
+        if (args.size() != 2)
+        {
+            throw std::invalid_argument("argc");
+        }
+
+        int result = c.sum(boost::lexical_cast<int>(args[0]),
+                         boost::lexical_cast<int>(args[1]));
+
+        return boost::lexical_cast<std::string>(result);
+    }
+
+    std::string multiply(const Args & args)
+    {
+        if (args.size() != 2)
+        {
+            throw std::invalid_argument("argc");
+        }
+
+        int result = c.multiply(boost::lexical_cast<int>(args[0]),
+                              boost::lexical_cast<int>(args[1]));
+
+        return boost::lexical_cast<std::string>(result);
+    }
+
+
+    Calculator c;
+};
+
 
 int main()
 {

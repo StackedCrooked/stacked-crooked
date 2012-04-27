d0c3062b22953ff9f5e25cc6ad91eacda476d8da
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-04-27 20:56:14 +0000

    Cleanup.

--- trunk/Playground/Phoenix/main.cpp
+++ trunk/Playground/Phoenix/main.cpp
@@ -1,7 +1,22 @@
-#include <boost/phoenix.hpp>
-#include <boost/phoenix/stl/algorithm.hpp>
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
+#include <boost/phoenix/function.hpp>
 #include <cassert>
+#include <cmath>
 #include <iostream>
+#include <utility>
+
+
+namespace {
+
+
+// a, b and c represent the equation arguments: ax2 + bx + c
+boost::phoenix::expression::argument<1>::type a;
+boost::phoenix::expression::argument<2>::type b;
+boost::phoenix::expression::argument<3>::type c;
+
+
+auto D = b * b - 4 * a * c;
 
 
 struct sqrt_impl
@@ -17,31 +32,46 @@ struct sqrt_impl
 };
 
 
+boost::phoenix::function<sqrt_impl> sq;
+
+
+/**
+ * @brief get_x1 generates the first result of the equation.
+ * @return lazy return value.
+ * @example
+ *     auto value = get_x1(-1, 2, -1); // get the lazy result
+ *     double x1 = value(); // get the actual result
+ */
+auto get_x1 = (-b + sq(D)) / (2 * a);
+
+/**
+ * @brief get_x1 generates the second result of the equation.
+ * @return lazy return value.
+ */
+auto get_x2 = (-b - sq(D)) / (2 * a);
+
+
+struct solve_impl
+{
+    typedef std::pair<double, double> result_type;
+
+    struct result { typedef result_type type; };
+
+    result_type operator()(double a, double b, double c) const
+    {
+        return std::make_pair(get_x1(a, b, c), get_x2(a, b, c));
+    }
+};
+
+
+boost::phoenix::function<solve_impl> solve_quadratic_equation;
+
+
+} // anonymous namespace
+
+
 int main()
 {
-    boost::phoenix::expression::argument<1>::type a;
-    boost::phoenix::expression::argument<2>::type b;
-    boost::phoenix::expression::argument<3>::type c;
-
-
-    // discriminant = b^2 - 4ac
-    auto D = b * b - 4 * a * c;
-    std::cout << "D(1, -1, -2): " << D(1.0, -1.0, -2.0) << std::endl;
-
-    // square root
-    boost::phoenix::function<sqrt_impl> sq;
-
-    // first solution
-    auto x1 = (-b + sq(D)) / (2 * a);
-
-    // second solution
-    auto x2 = (-b - sq(D)) / (2 * a);
-
-    auto solve = [&x1, &x2](double a, double b, double c) -> std::pair<double, double> { return std::make_pair(x1(a, b, c), x2(a, b, c)); };
-    auto result = solve(1.0, -1.0, -2.0);
-    std::cout << "x1: " << result.first << ", x2: " << result.second << std::endl;
-
-    // empty solution set (nan)
-    result = solve(1, 2, 3);
-    std::cout << "x1: " << result.first << ", x2: " << result.second << std::endl;
+    auto p = solve_quadratic_equation(1, -1, -2)();
+    std::cout << p.first << ", " << p.second << std::endl;
 }

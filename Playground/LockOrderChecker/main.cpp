90d14a0eb43e9c66a68a596ad38ab11851b97192
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-04-28 03:58:52 +0000

    Remove to previous code.

--- Playground/LockOrderChecker/main.cpp
+++ Playground/LockOrderChecker/main.cpp
@@ -1,386 +1,429 @@
-#include <algorithm>
+#include <boost/bind.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/shared_ptr.hpp>
 #include <cassert>
 #include <iostream>
-#include <memory>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
 #include <vector>
 
 
-template<class Node,
-         class Preceding = std::vector<Node> >
-const Node * FindCycle(const Node & node, Preceding preceding = Preceding())
+#define VerifyWithFileAndWithLine(file, line, statement) \
+    if (true == (statement)) \
+    { \
+        std::cout << file << ":" << line << ": " << #statement << ": OK\n"; \
+    } \
+    else \
+    { \
+        std::cout << file << ":" << line << ": " << #statement << ": *** FAIL ***\n"; \
+    }
+
+
+#define Verify(statement) VerifyWithFileAndWithLine(__FILE__, __LINE__, statement);
+
+
+template<class T>
+struct ContainerPolicy_Set
 {
-    if (node.empty())
+    typedef std::set<T> Container;
+    static void insert(Container & ioContainer, const T & inValue)
     {
-        return nullptr;
+        ioContainer.insert(inValue);
     }
 
-    if (std::find(preceding.begin(), preceding.end(), node) != preceding.end())
+    static size_t size(const Container & ioContainer)
     {
-        return &node;
+        return ioContainer.size();
     }
 
-    preceding.push_back(node);
-
-    for (const Node & child : node)
+    static bool empty(const Container & ioContainer)
     {
-        if (const Node * found = FindCycle(child, preceding))
+        return ioContainer.empty();
+    }
+};
+
+
+template<class T>
+struct ContainerPolicy_Vector
+{
+    typedef std::vector<T> Container;
+    static void insert(Container & ioContainer, const T & inValue)
+    {
+        ioContainer.push_back(inValue);
+    }
+
+    static size_t size(const Container & ioContainer)
+    {
+        return ioContainer.size();
+    }
+
+    static bool empty(const Container & ioContainer)
+    {
+        return ioContainer.empty();
+    }
+};
+
+
+template <class PointeeType>
+struct PointerPolicy_Normal_NoOwnership
+{
+    typedef PointeeType * PointerType;
+
+    static PointeeType * getRaw(PointerType p)
+    {
+        return p;
+    }
+
+    static void destroy(PointerType p) { }
+};
+
+
+template <class PointeeType>
+struct PointerPolicy_Normal_WithOwnership
+{
+    typedef PointeeType * PointerType;
+
+    static PointeeType * getRaw(PointerType p)
+    {
+        return p;
+    }
+
+    static void destroy(PointerType p)
+    {
+        delete p;
+    }
+};
+
+
+template <class PointeeType>
+struct PointerPolicy_Shared
+{
+    typedef boost::shared_ptr<PointeeType> PointerType;
+
+    static PointeeType * getRaw(PointerType p)
+    {
+        return p.get();
+    }
+
+    static void destroy(PointerType p) { }
+};
+
+
+template <class TDataType,
+          template <class> class ContainerPolicy,
+          template <class> class PointerPolicy>
+class node_t
+{
+public:
+    typedef TDataType DataType;
+
+    node_t() { }
+
+    node_t(const DataType & inData) : mData(inData) { }
+
+    typedef node_t<DataType, ContainerPolicy, PointerPolicy> This;
+    typedef typename PointerPolicy<This>::PointerType ChildPtr;
+    typedef typename ContainerPolicy<ChildPtr>::Container Container;
+
+    typedef typename Container::iterator iterator;
+    typedef typename Container::const_iterator const_iterator;
+
+    ~node_t()
+    {
+        const_iterator it = this->begin(), endIt = this->end();
+        for (; it != endIt; ++it)
         {
-            return found;
+            PointerPolicy<This>::destroy(*it);
         }
     }
 
-    return nullptr;
-}
+    iterator begin()
+    {
+        return mChildren.begin();
+    }
 
+    iterator end()
+    {
+        return mChildren.end();
+    }
 
-template<typename T>
-struct Node
-{
-    typedef std::vector<Node<T> > container_type;
+    const_iterator begin() const
+    {
+        return mChildren.begin();
+    }
 
-    Node(Node<T> * parent, const T & t);
+    const_iterator end() const
+    {
+        return mChildren.end();
+    }
 
-    const Node<T> * parent() const;
-    Node<T> * parent();
+    void insert(This * inItem)
+    {
+        ChildPtr item(inItem);
+        ContainerPolicy<ChildPtr>::insert(mChildren, item);
+    }
 
-    const T & value() const;
+    size_t size() const
+    {
+        return ContainerPolicy<ChildPtr>::size(mChildren);
+    }
 
-    T & value();
+    bool empty() const
+    {
+        return ContainerPolicy<ChildPtr>::empty(mChildren);
+    }
 
-    bool empty() const;
+    DataType & data()
+    {
+        return mData;
+    }
 
-    void push_back(const T & value);
+    const DataType & data() const
+    {
+        return mData;
+    }
 
-    const Node<T> & back() const;
-    Node<T> & back();
-
-    void pop_back();
-
-    const std::vector<Node<T> > & children() const;
-    std::vector<Node<T> > & children();
-
-    struct const_iterator;
-    const_iterator begin() const;
-    const_iterator end() const;
-
-    bool equals(const Node<T> & rhs) const;
+    void setData(const DataType & inData)
+    {
+        mData = inData;
+    }
 
 private:
-    const container_type & container() const;
-    container_type & container();
-
-    struct Impl;
-    std::shared_ptr<Impl> mImpl;
+    DataType mData;
+    Container mChildren;
 };
 
 
-template<typename T>
-struct Node<T>::Impl
+template<class T,
+         template<class> class ContainerType,
+         template<class> class PointerType>
+bool HasCycles(const node_t<T, ContainerType, PointerType> & node,
+               std::vector<const T*> node_path = std::vector<const T*>())
 {
-    typedef std::vector<Node<T> > Children;
+    typedef node_t<T, ContainerType, PointerType> Node;
+    typedef typename Node::Container Container;
 
-    explicit Impl(Node<T> * parent, const T & value) :
-        mParent(parent),
-        mValue(value)
+    if (!node.empty())
     {
+        for (typename Container::const_iterator it = node.begin(), end = node.end(); it != end; ++it)
+        {
+            node_t<T, ContainerType, PointerType> & child = **it;
+            if (std::find(node_path.begin(), node_path.end(), &child.data()) != node_path.end())
+            {
+                return true;
+            }
+            else
+            {
+                node_path.push_back(&node.data());
+
+                // Previous nodes object is passed by value!
+                // This is an important aspect of the algorithm!
+                if (HasCycles(child, node_path))
+                {
+                    return true;
+                }
+            }
+        }
     }
-
-    ~Impl()
-    {
-    }
-
-    const Node<T> * parent() const { return mParent; }
-    Node<T> * parent() { return mParent; }
-
-    const Node<T> & back() const { return children().back(); }
-    Node<T> & back() { return children().back(); }
-
-    const T & value() const { return mValue; }
-    T & value() { return mValue; }
-
-    const Children & children() const { return mChildren; }
-    Children & children() { return mChildren; }
-
-    void push_back(const Node<T> & value) { children().push_back(value); }
-
-    void pop_back() { children().pop_back(); }
-
-    Node<T> * mParent;
-    T mValue;
-    Children mChildren;
-};
-
-
-template<typename T>
-struct Node<T>::const_iterator
-{
-    typedef typename Node<T>::container_type        container_type;
-    typedef typename container_type::const_iterator iterator_type;
-
-    explicit const_iterator(iterator_type it) :
-        mIterator(it)
-    {
-    }
-
-    const_iterator & operator++()
-    {
-        ++mIterator;
-        return *this;
-    }
-
-    const Node<T> & operator*() const
-    {
-        const Node<T> & node = *mIterator;
-        return node;
-    }
-
-    bool equals(const const_iterator & rhs) const
-    {
-        return mIterator == rhs.mIterator;
-    }
-
-    iterator_type mIterator;
-};
-
-
-template<typename ConstIterator>
-bool operator==(const ConstIterator & lhs, const ConstIterator & rhs)
-{
-    return lhs.equals(rhs);
+    return false;
 }
-
-
-template<typename ConstIterator>
-bool operator!=(const ConstIterator & lhs, const ConstIterator & rhs)
-{
-    return !lhs.equals(rhs);
-}
-
-
-template<typename T>
-Node<T>::Node(Node<T> * parent, const T & value = T()) :
-    mImpl(new Impl(parent, value))
-{
-}
-
-
-template<typename T>
-const Node<T> * Node<T>::parent() const
-{
-    return mImpl->parent();
-}
-
-
-template<typename T>
-Node<T> * Node<T>::parent()
-{
-    return mImpl->parent();
-}
-
-
-template<typename T>
-bool Node<T>::equals(const Node<T> & rhs) const
-{
-    return this->mImpl.get() == rhs.mImpl.get();
-}
-
-
-
-template<typename T>
-bool Node<T>::empty() const
-{
-    return container().empty();
-}
-
-
-template<typename T>
-const typename Node<T>::container_type & Node<T>::container() const
-{
-    return mImpl->mChildren;
-}
-
-
-template<typename T>
-typename Node<T>::container_type & Node<T>::container()
-{
-    return mImpl->mChildren;
-}
-
-
-template<typename T>
-typename Node<T>::const_iterator Node<T>::begin() const
-{
-    return const_iterator(container().begin());
-}
-
-
-template<typename T>
-typename Node<T>::const_iterator Node<T>::end() const
-{
-    return const_iterator(container().end());
-}
-
-
-template<typename T>
-void Node<T>::push_back(const T & value)
-{
-    mImpl->push_back(Node<T>(this, value));
-}
-
-
-template<typename T>
-const Node<T> & Node<T>::back() const
-{
-    return mImpl->back();
-}
-
-
-template<typename T>
-Node<T> & Node<T>::back()
-{
-    return mImpl->back();
-}
-
-
-template<typename T>
-void Node<T>::pop_back()
-{
-    mImpl->pop_back();
-}
-
-
-template<typename T>
-const T & Node<T>::value() const
-{
-    return mImpl->value();
-}
-
-
-template<typename T>
-T & Node<T>::value()
-{
-    return mImpl->value();
-}
-
-
-template<typename T>
-const std::vector<Node<T> > & Node<T>::children() const
-{
-    return mImpl->_children;
-}
-
-
-template<typename T>
-std::vector<Node<T> > & Node<T>::children()
-{
-    return mImpl->children();
-}
-
-
-struct Mutex;
-static Node<Mutex*> gRoot(nullptr);
-static Node<Mutex*> * gCurrent = &gRoot;
 
 
 struct Mutex
 {
-    Mutex(const std::string & inName) : mName(inName)
-    {
-    }
+    Mutex(char inChar) : mChar(inChar) {}
 
-    Mutex(const Mutex &) = delete;
-    Mutex & operator=(const Mutex &) = delete;
+    void lock() {}
+    void unlock() {}
 
-    const std::string & name() const { return mName; }
-
-    void lock()
-    {
-        gCurrent->push_back(this);
-        gCurrent = &gCurrent->back();
-        assert(!FindCycle(gRoot));
-    }
-
-    void unlock()
-    {
-        assert(gCurrent->value() == this);
-        gCurrent = gCurrent->parent();
-    }
-
-    std::string mName;
+    char mChar;
 };
 
 
-struct Lock
+template<class T>
+void Lock(T & a, T & b)
 {
-    Lock(Mutex & mutex) : mutex(mutex)
-    {
-        mutex.lock();
-    }
-
-    Lock(const Lock &) = delete;
-    Lock & operator=(const Lock &) = delete;
-
-    ~Lock()
-    {
-        mutex.unlock();
-    }
-
-    Mutex & mutex;
-};
-
-
-template<typename T>
-std::ostream & operator<<(std::ostream & os, const Node<T> & node)
-{
-    static int fDepth = 0;
-    std::string tab = std::string("  ");
-    std::string indent = std::string(tab.size() * fDepth, ' ');
-    os << std::endl << indent << node.value() << " ";
-    for (const auto & child : node)
-    {
-        fDepth++;
-        os << child;
-        if (--fDepth == 0)
-        {
-            os << std::endl;
-        }
-    }
-    return os;
+    a.insert(&b);
 }
 
 
-std::ostream & operator<<(std::ostream & os, const Mutex * mutex)
+template<class T>
+void Lock(T & a, T & b, T& c)
 {
-    return os << (mutex ? mutex->name() : "null");
+    a.insert(&b);
+    b.insert(&c);
+}
+
+
+template<class T>
+void Lock(T & a, T & b, T& c, T & d)
+{
+    a.insert(&b);
+    b.insert(&c);
+    c.insert(&d);
+}
+
+
+void testNode()
+{
+    // Defined the tree with PointerPolicy_Normal_NoOwnership to void deleting stack-allocated objects.
+    typedef node_t<Mutex, ContainerPolicy_Set, PointerPolicy_Normal_NoOwnership> MutexNode;
+    MutexNode a('a');
+    MutexNode b('b');
+    MutexNode c('c');
+    MutexNode d('d');
+
+    Lock(a, b);
+    Verify(!HasCycles(a));
+    Verify(!HasCycles(b));
+    Verify(!HasCycles(c));
+    Verify(!HasCycles(d));
+
+    Lock(a, c);
+    Verify(!HasCycles(a));
+    Verify(!HasCycles(b));
+    Verify(!HasCycles(c));
+    Verify(!HasCycles(d));
+
+    Lock(b, d);
+    Verify(!HasCycles(a));
+    Verify(!HasCycles(b));
+    Verify(!HasCycles(c));
+    Verify(!HasCycles(d));
+
+    Lock(a, b, c);
+    Verify(!HasCycles(a));
+    Verify(!HasCycles(b));
+    Verify(!HasCycles(c));
+    Verify(!HasCycles(d));
+
+    Lock(a, b, d);
+    Verify(!HasCycles(a));
+    Verify(!HasCycles(b));
+    Verify(!HasCycles(c));
+    Verify(!HasCycles(d));
+
+    Lock(a, b, c, d);
+    Verify(!HasCycles(a));
+    Verify(!HasCycles(b));
+    Verify(!HasCycles(c));
+    Verify(!HasCycles(d));
+
+    Lock(a, b, d, c);
+    Verify(HasCycles(a));
+    Verify(HasCycles(b));
+    Verify(HasCycles(c));
+    Verify(HasCycles(d));
+}
+
+
+template<class T>
+struct LockMany : boost::noncopyable
+{
+    // No ownership here to prevent cycles causing infinite recursion on destruction.
+    typedef node_t<Mutex*, ContainerPolicy_Set, PointerPolicy_Normal_NoOwnership> MutexNode;
+
+    LockMany() :
+        mRootNode(),
+        mLastNode(&mRootNode)
+    {
+    }
+
+    ~LockMany()
+    {
+        unlockAll();
+    }
+
+    void lock(Mutex & inMutex)
+    {
+        MutexNode * node = getMutexNode(&inMutex);
+        mLastNode->insert(node);
+        mLastNode = node;
+    }
+
+    void unlockAll()
+    {
+        mLastNode = &mRootNode;
+    }
+
+    MutexNode * getMutexNode(Mutex * inMutex)
+    {
+        typename MutexNodes::iterator it = mMutexNodes.find(inMutex);
+        if (it == mMutexNodes.end())
+        {
+            MutexNode * theMutexNode = new MutexNode(inMutex);
+            mMutexNodes.insert(std::make_pair(inMutex, theMutexNode));
+            return theMutexNode;
+        }
+        return it->second.get();
+    }
+
+    typedef std::map<Mutex*, boost::shared_ptr<MutexNode> > MutexNodes;
+    MutexNodes mMutexNodes;
+    MutexNode mRootNode;
+    MutexNode * mLastNode;
+};
+
+
+void testLockMany()
+{
+    LockMany<char> locker;
+
+    Mutex a('1');
+    Mutex b('2');
+    Mutex c('3');
+
+    // Ok
+    locker.lock(a);
+    locker.lock(b);
+    Verify(!HasCycles(locker.mRootNode));
+    Verify(!HasCycles(*locker.getMutexNode(&a)));
+    Verify(!HasCycles(*locker.getMutexNode(&b)));
+    locker.unlockAll();
+
+    // Ok
+    locker.lock(a);
+    locker.lock(b);
+    Verify(!HasCycles(locker.mRootNode));
+    Verify(!HasCycles(*locker.getMutexNode(&a)));
+    Verify(!HasCycles(*locker.getMutexNode(&b)));
+    locker.unlockAll();
+
+    // Ok
+    locker.lock(b);
+    locker.lock(c);
+    Verify(!HasCycles(locker.mRootNode));
+    Verify(!HasCycles(*locker.getMutexNode(&a)));
+    Verify(!HasCycles(*locker.getMutexNode(&b)));
+    Verify(!HasCycles(*locker.getMutexNode(&c)));
+    locker.unlockAll();
+
+    // Ok
+    locker.lock(a);
+    locker.lock(c);
+    Verify(!HasCycles(locker.mRootNode));
+    Verify(!HasCycles(*locker.getMutexNode(&a)));
+    Verify(!HasCycles(*locker.getMutexNode(&b)));
+    Verify(!HasCycles(*locker.getMutexNode(&c)));
+    locker.unlockAll();
+
+    // Test inconsistent locking order here:
+    locker.lock(b);
+    locker.lock(a);
+    Verify(HasCycles(locker.mRootNode));
+    Verify(HasCycles(*locker.getMutexNode(&a)));
+    Verify(HasCycles(*locker.getMutexNode(&b)));
+    Verify(!HasCycles(*locker.getMutexNode(&c))); // no cycles here
+    locker.unlockAll();
 }
 
 
 int main()
 {
-    Node<Mutex*> & theRoot = gRoot;
-    Mutex m1("m1");
-    Mutex m2("m2");
-    Mutex m3("m3");
-    Mutex m4("m4");
-    {
-        Lock l1(m1);
-        Lock l2(m2);
-    }
-    {
-        Lock l4(m4);
-        Lock l3(m3);
-    }
-    {
-        Lock l1(m1);
-        Lock l4(m4);
-    }
-    std::cout << theRoot << std::endl;
-    if (auto node = FindCycle(theRoot))
-    {
-        std::cout << "Found cycle at " << node->value()->name() << std::endl;
-    }
-    else
-    {
-        std::cout << "No cycles detected." << std::endl;
-    }
+    testNode();
+    testLockMany();
+    std::cout << "Everything turned out better than expected." << std::endl << std::flush;
+    return 0;
 }

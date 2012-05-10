04b1f5a2b9e4edf73992c4d9c58f8e01a7d37475
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-05-10 17:57:41 +0000

    Add stuff.

--- trunk/Playground/MACCompare/main.cpp
+++ trunk/Playground/MACCompare/main.cpp
@@ -1,10 +1,48 @@
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
 #include <iostream>
 
 
 extern std::uint8_t cBytes[];
 extern std::size_t cBytesLength;
+
+
+struct A
+{
+    uint8_t bytes[6];
+};
+
+
+inline bool operator<(const A & lhs, const A & rhs)
+{
+    return std::memcmp(lhs.bytes, rhs.bytes, sizeof(lhs.bytes)) < 0;
+}
+
+struct B
+{
+    std::uint32_t first;
+    std::uint16_t second;
+};
+
+
+inline bool operator<(const B & lhs, const B & rhs)
+{
+    return lhs.first < rhs.first || (lhs.first == rhs.first && lhs.second < rhs.second);
+}
+
+
+struct C
+{
+    std::uint16_t first;
+    std::uint32_t second;
+};
+
+
+inline bool operator<(const C & lhs, const C & rhs)
+{
+    return lhs.first < rhs.first || (lhs.first == rhs.first && lhs.second < rhs.second);
+}
 
 
 int main()

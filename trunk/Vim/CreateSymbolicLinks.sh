2ccb534edbdd17b54165e6bd2ad19cab97c1c1a1
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-06-09 22:49:11 +0000

    Remove misleading comment.

--- trunk/Vim/CreateSymbolicLinks.sh
+++ trunk/Vim/CreateSymbolicLinks.sh
@@ -3,6 +3,4 @@ ln -fs `pwd`/_gvimrc $HOME/.gvimrc
 if [  `uname` == "Darwin" ]; then
     echo "Link ~/bin/gvim to MacVim"
     ln -fs `pwd`/MacVim $HOME/bin/gvim
-else
-    echo "No gvim on this platform. OK."
 fi

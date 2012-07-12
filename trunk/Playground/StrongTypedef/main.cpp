6dd70bb54fab8498a911c1c28efa6e78bbcc3ee0
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-07-12 19:17:29 +0000

    Add little tes.t

--- trunk/Playground/StrongTypedef/main.cpp
+++ trunk/Playground/StrongTypedef/main.cpp
@@ -8,11 +8,23 @@ FUTILE_STRONG_TYPEDEF(int, Height)
 
 int Area(Width w, Height h)
 {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
     return w * h;
 }
+
+template<typename T>
+void Test(T);
+
 
 
 int main()
 {
     std::cout << Area(Width(3), Height(4)) << std::endl;
+
+
+#if 0
+    // Invoke compiler error to see the type name
+    Test(Width(3));
+    Test(Height(3));
+#endif 
 }

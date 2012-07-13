0b688fb226c7136d9ce6364f0f5250eb505c5ce4
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-07-13 19:28:27 +0000

    Show image works now.

--- Playground/QtSample/ImageViewer.cpp
+++ Playground/QtSample/ImageViewer.cpp
@@ -1,2 +1,56 @@
 #include "ImageViewer.h"
 
+
+struct ImageViewer::Impl
+{
+    Impl(ImageViewer & inImageViewer) :
+        mImageViewer(inImageViewer),
+        mScene(&inImageViewer),
+        mView(&mScene)
+    {
+        mImageViewer.resize(1280, 1024);
+        mImageViewer.setCentralWidget(&mView);
+        mView.show();
+        mScene.update();
+    }
+
+    ~Impl()
+    {
+
+    }
+
+    QWidget & centralWidget()
+    {
+        return *mImageViewer.centralWidget();
+    }
+
+
+
+    void setImage(const std::string & inPath)
+    {
+        mScene.addPixmap(QPixmap(inPath.c_str()));
+    }
+
+    ImageViewer & mImageViewer;
+    QGraphicsScene mScene;
+    QGraphicsView mView;
+};
+
+
+ImageViewer::ImageViewer() :
+    QMainWindow(),
+    mImpl(new Impl(*this))
+{
+}
+
+
+ImageViewer::~ImageViewer()
+{
+    delete mImpl;
+}
+
+
+void ImageViewer::setImage(const std::string & inFile)
+{
+    mImpl->setImage(inFile);
+}

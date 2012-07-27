274707fb2650704d70bd5ae9d1414497d3a20b1a
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-07-27 14:38:40 +0000

    Improve.

--- trunk/Playground/Obtainer/main.cpp
+++ trunk/Playground/Obtainer/main.cpp
@@ -1,30 +1,129 @@
-#include <iostream>
+#ifndef ITEMOBTAINER_H
+#define ITEMOBTAINER_H
+
+
+#include <cassert>
 #include <functional>
 #include <map>
 #include <memory>
 #include <stdexcept>
-#include <typeindex>
+#include <typeinfo>
 
 
-#define ITEMOBTAINER_DEFINE_EXCEPTION(NAME, INFO) \
+// Workaround for type_index which is not yet supported on GCC 4.5
+// Once we have upgraded to GCC 4.7+ we can replace "nonstd::type_index" with "std::type_index".
+namespace nonstd {
+
+
+struct type_index
+{
+    type_index(const std::type_info & inInfo) :
+        mInfo(&inInfo)
+    {
+    }
+
+    friend bool operator<(const type_index & lhs, const type_index & rhs)
+    {
+        return lhs.mInfo->before(*rhs.mInfo);
+    }
+
+    friend bool operator==(const type_index & lhs, const type_index & rhs)
+    {
+        return lhs.mInfo == rhs.mInfo;
+    }
+
+    const std::type_info * mInfo;
+};
+
+
+} // namespace nonstd
+
+
+#define ITEMOBTAINER_EXCEPTION(NAME, INFO) \
     struct NAME : std::runtime_error { \
         NAME() : std::runtime_error(INFO) { } \
         virtual ~NAME() throw() {} \
     }
 
 
+/**
+ * ItemFactory is a object factory that allows you to create objects using string ids.
+ *
+ * @example
+ *
+ *  // Define types
+ *  struct Base {
+ *      virtual ~Base() {}
+ *  };
+ *
+ *  struct A : Base {};
+ *  struct B : Base {};
+ *
+ *  // Create a factory for objects that inherit "Base".
+ *  ItemFactory<Base> factory;
+ *
+ *  // Register types A as "a" and B as "b".
+ *  factory.registerItem<A>("a");
+ *  factory.registerItem<B>("b");
+ *
+ *  // Create an object and specifying its type as a template argument.
+ *  std::unique_ptr<A> a = factory.create<A>();
+ *
+ *  // Create an object and specify its type using a string identifier.
+ *  // In this case an object of the Base type is returned.
+ *  std::unique_ptr<Base> b = factory.create("b");
+ *
+ *  // Use dynamic cast to get an object of derived type.
+ *  B & b = dynamic_cast<B&>(*b);
+ *
+ */
 template<typename BaseType>
 struct ItemFactory
 {
-    ITEMOBTAINER_DEFINE_EXCEPTION(NotRegistered, "No create function.");
-    ITEMOBTAINER_DEFINE_EXCEPTION(AlreadyRegistered, "Already registered.");
+    ITEMOBTAINER_EXCEPTION(NotRegistered, "No create function.");
+    ITEMOBTAINER_EXCEPTION(AlreadyRegistered, "Already registered.");
 
     template<typename SubType>
     void registerItem(const std::string & inName)
     {
         static_assert(std::is_base_of<BaseType, SubType>::value, "Provided type is not a subtype of base type.");
-        std::type_index id(typeid(SubType));
-        auto it = mCreateFunctions.find(inName);
+        nonstd::type_index itemIndex = GetId<SubType>();
+        registerItemType(inName, itemIndex);
+        registerCreateFunction<SubType>(itemIndex);
+    }
+
+    template<typename SubType>
+    std::unique_ptr<BaseType> createItem()
+    {
+        return createItemImpl(GetId<SubType>());
+    }
+
+    std::unique_ptr<BaseType> createItem(const std::string & inItemName)
+    {
+        return createItemImpl(getItemId(inItemName));
+    }
+
+protected:
+    template<typename T>
+    static nonstd::type_index GetId()
+    {
+        return nonstd::type_index(typeid(T));
+    }
+
+    void registerItemType(const std::string & inName, const nonstd::type_index & inTypeIndex)
+    {
+        if (mItemTypes.find(inName) != mItemTypes.end())
+        {
+            throw AlreadyRegistered();
+        }
+
+        mItemTypes.insert(std::make_pair(inName, inTypeIndex));
+    }
+
+    template<typename SubType>
+    void registerCreateFunction(const nonstd::type_index & inTypeIndex)
+    {
+        auto it = mCreateFunctions.find(inTypeIndex);
         if (it != mCreateFunctions.end())
         {
             throw AlreadyRegistered();
@@ -39,12 +138,12 @@ struct ItemFactory
             }
         };
 
-        mCreateFunctions.insert(std::make_pair(inName, std::bind(&Create::create)));
+        mCreateFunctions.insert(std::make_pair(inTypeIndex, std::bind(&Create::create)));
     }
 
-    std::unique_ptr<BaseType> createItem(const std::string & inName)
+    std::unique_ptr<BaseType> createItemImpl(const nonstd::type_index & inTypeIndex)
     {
-        auto it = mCreateFunctions.find(inName);
+        auto it = mCreateFunctions.find(inTypeIndex);
         if (it == mCreateFunctions.end())
         {
             throw NotRegistered();
@@ -52,53 +151,104 @@ struct ItemFactory
         return it->second();
     }
 
+    nonstd::type_index getItemId(const std::string & inName) const
+    {
+        auto it = mItemTypes.find(inName);
+        if (it == mItemTypes.end())
+        {
+            throw NotRegistered();
+        }
+        return it->second;
+    }
+
 private:
     typedef std::function<std::unique_ptr<BaseType>()> CreateFunction;
-    typedef std::map<std::string, CreateFunction> CreateFunctions;
+    typedef std::map<std::string, nonstd::type_index> ItemTypes;
+    ItemTypes mItemTypes;
+
+    typedef std::map<nonstd::type_index, CreateFunction> CreateFunctions;
     CreateFunctions mCreateFunctions;
 };
 
 
+/**
+ * ItemObtainer is a lazy container.
+ *
+ * @example
+ *
+ *  // Define types
+ *  struct Base {
+ *      virtual ~Base() {}
+ *  };
+ *
+ *  struct A : Base {};
+ *  struct B : Base {};
+ *
+ *  ItemObtainer<Base> obtainer;
+ *  obtainer.register<A>("a");
+ *  obtainer.obtainItem("a"); // creates and returns a new instance of type A
+ *  obtainer.obtainItem("a"); // returns previously created instance of type A
+ *  obtainer.obtainItem<A>(); // returns previously created instance of type A
+ */
 template<typename BaseType>
 struct ItemObtainer : ItemFactory<BaseType>
 {
+public:
     BaseType & obtainItem(const std::string & inName)
     {
-        auto it = mItems.find(inName);
+        return obtainItemImpl(this->getItemId(inName));
+    }
+
+    template<typename SubType>
+    SubType & obtainItem()
+    {
+        nonstd::type_index id = this->template GetId<SubType>();
+        BaseType & base = obtainItemImpl(id);
+        return dynamic_cast<SubType&>(base);
+    }
+
+protected:
+    BaseType & obtainItemImpl(const nonstd::type_index & inTypeIndex)
+    {
+        auto it = mItems.find(inTypeIndex);
         if (it == mItems.end())
         {
-            std::unique_ptr<BaseType> basePtr = this->createItem(inName);
-            mItems.insert(std::make_pair(inName, std::move(basePtr)));
+            std::shared_ptr<BaseType> basePtr(this->createItemImpl(inTypeIndex).release());
+            mItems.insert(std::make_pair(inTypeIndex, basePtr));
             return *basePtr;
         }
         return *it->second;
     }
 
 private:
-    typedef std::unique_ptr<BaseType> ItemPtr;
-    typedef std::map<std::string, ItemPtr> Items;
+    typedef std::shared_ptr<BaseType> ItemPtr;
+    typedef std::map<nonstd::type_index, ItemPtr> Items;
     Items mItems;
 };
 
 
-struct ItemBase
+struct Base
 {
-    virtual ~ItemBase() {}
+    virtual ~Base() {}
 };
 
-struct ItemA : ItemBase {};
-struct ItemB : ItemBase {};
-struct ItemC : ItemBase {};
+
+struct A : Base {};
+struct B : Base {};
+struct C : Base {};
 
 
 int main()
 {
-    ItemObtainer<ItemBase> f;
-    f.registerItem<ItemA>("A");
-    f.registerItem<ItemB>("B");
-    f.registerItem<ItemC>("C");
+    ItemObtainer<Base> obtainer;
+    obtainer.registerItem<A>("a");
+    obtainer.registerItem<B>("b");
+    obtainer.registerItem<C>("c");
 
-    ItemBase & base_a = f.obtainItem("A");
-    ItemA & a = dynamic_cast<ItemA&>(base_a);
-    (void)a;
+    A & a = obtainer.obtainItem<A>();
+    Base & base_a = obtainer.obtainItem("a");
+    assert(&a == &base_a);
 }
+
+
+#endif // ITEMOBTAINER_H

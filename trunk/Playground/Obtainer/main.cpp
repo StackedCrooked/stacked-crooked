acb7c80988610a6b7af7cc1921c439f9ba9cc79d
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-07-27 13:11:51 +0000

    Implement obtainer with factory.

--- trunk/Playground/Obtainer/main.cpp
+++ trunk/Playground/Obtainer/main.cpp
@@ -21,36 +21,67 @@ struct ItemFactory
     ITEMOBTAINER_DEFINE_EXCEPTION(AlreadyRegistered, "Already registered.");
 
     template<typename SubType>
-    void createItem()
-    {
-        static_assert(std::is_base_of<BaseType, SubType>::value, "Provided type is not a subtype of base type.");
-        auto it = mCreateFunctions.find(std::type_index(typeid(SubType)));
-        if (it == mCreateFunctions.end())
-        {
-            throw NoCreateFunction();
-        }
-        return *it->second;
-    }
-
-    template<typename SubType>
-    void registerItem()
+    void registerItem(const std::string & inName)
     {
         static_assert(std::is_base_of<BaseType, SubType>::value, "Provided type is not a subtype of base type.");
         std::type_index id(typeid(SubType));
-        auto it = mCreateFunctions.find(id);
+        auto it = mCreateFunctions.find(inName);
         if (it != mCreateFunctions.end())
         {
             throw AlreadyRegistered();
         }
-        auto fun = [](){ return std::unique_ptr<BaseType>(new SubType); };
-        mCreateFunctions.insert(std::make_pair(id, fun));
+
+        // Use local functor instead of lambda because lambda currently messes up my editor's indention logic.
+        struct Create
+        {
+            static std::unique_ptr<BaseType> create()
+            {
+                return std::unique_ptr<BaseType>(new SubType);
+            }
+        };
+
+        mCreateFunctions.insert(std::make_pair(inName, std::bind(&Create::create)));
     }
 
+    std::unique_ptr<BaseType> createItem(const std::string & inName)
+    {
+        auto it = mCreateFunctions.find(inName);
+        if (it == mCreateFunctions.end())
+        {
+            throw NoCreateFunction();
+        }
+        return it->second();
+    }
 
+private:
     typedef std::function<std::unique_ptr<BaseType>()> CreateFunction;
-    typedef std::map<std::type_index, CreateFunction> CreateFunctions;
+    typedef std::map<std::string, CreateFunction> CreateFunctions;
     CreateFunctions mCreateFunctions;
 };
+
+
+template<typename BaseType>
+struct ItemObtainer : ItemFactory<BaseType>
+{
+    BaseType & obtainItem(const std::string & inName)
+    {
+        auto it = mItems.find(inName);
+        if (it == mItems.end())
+        {
+            std::unique_ptr<BaseType> basePtr = this->createItem(inName);
+            mItems.insert(std::make_pair(inName, std::move(basePtr)));
+            return *basePtr;
+        }
+        return *it->second;
+    }
+
+private:
+    typedef std::unique_ptr<BaseType> ItemPtr;
+    typedef std::map<std::string, ItemPtr> Items;
+    Items mItems;
+};
+
+
 
 
 struct ItemBase
@@ -65,8 +96,12 @@ struct ItemC : ItemBase {};
 
 int main()
 {
-    ItemFactory<ItemBase> f;
-    f.registerItem<ItemA>();
-    f.registerItem<ItemB>();
-    f.registerItem<ItemC>();
+    ItemObtainer<ItemBase> f;
+    f.registerItem<ItemA>("A");
+    f.registerItem<ItemB>("B");
+    f.registerItem<ItemC>("C");
+
+    ItemBase & base_a = f.obtainItem("A");
+    ItemA & a = dynamic_cast<ItemA&>(base_a);
+    (void)a;
 }

867b3d4a28bd2ffbe9fa41e2805ab171261e4cab
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-07-28 23:02:16 +0000

    Generalize the FileMenu into Menu and MenuItem.

--- Playground/QtSamples/ListViewSample/MainWindow.h
+++ Playground/QtSamples/ListViewSample/MainWindow.h
@@ -3,42 +3,102 @@
 
 
 #include "CentralWidget.h"
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
 
 
-class FileMenu : public QMenu
+class Menu;
+
+
+class MenuEvent : public QObject
 {
     Q_OBJECT
 
 public:
+    MenuEvent(Menu * inMenu, const QString & inText, int inIndex) :
+        QObject(),
+        mMenu(inMenu),
+        mText(inText),
+        mIndex(inIndex)
+    {
+    }
+
+    int getIndex() const
+    {
+        return mIndex;
+    }
+
+    QString getText() const
+    {
+        return mText;
+    }
+
+    const Menu & getMenu() const
+    {
+        return *mMenu;
+    }
+
+    typedef boost::function<void(MenuEvent&)> Callback;
+
+    void connectAction(QAction * inAction, const Callback & inCallback)
+    {
+        this->connect(inAction, SIGNAL(triggered(bool)), this, SLOT(onMenuTriggered()));
+        mCallback = inCallback;
+    }
+
+private Q_SLOTS:
+    void onMenuTriggered()
+    {
+        mCallback(*this);
+    }
+
+private:
+    Menu * mMenu;
+    QString mText;
+    int mIndex;
+    Callback mCallback;
+};
+
+
+class Menu : public QMenu
+{
+public:
     struct EventHandler
     {
-        virtual void onFileMenuTriggered(FileMenu&) = 0;
+        virtual void onMenuTriggered(Menu&, MenuEvent&) = 0;
     };
 
-    FileMenu(QMenuBar * inMenuBar, const QString & inTitle, EventHandler & inEventHandler) :
+    Menu(QMenuBar * inMenuBar, const QString & inTitle, EventHandler & inEventHandler) :
         QMenu(inMenuBar),
         mEventHandler(inEventHandler)
     {
-        QAction * action = addAction("&Open...");
-        action->setShortcut(QKeySequence("Ctrl+O"));
-        connect(action, SIGNAL(triggered(bool)), this, SLOT(onOpen()));
         setTitle(inTitle);
         inMenuBar->addMenu(this);
     }
 
-private Q_SLOTS:
-    void onOpen()
+    MenuEvent * addMenuItem(const QString & inText, const QKeySequence & inShortCut)
     {
-        mEventHandler.onFileMenuTriggered(*this);
+        QAction * action = addAction(inText);
+        action->setShortcut(inShortCut);
+
+        MenuEvent * theMenuEvent = new MenuEvent(this, inText, mMenuEvents.size());
+        theMenuEvent->connectAction(action, boost::bind(&Menu::onOpen, this, boost::ref(*theMenuEvent)));
+        return theMenuEvent;
     }
 
 private:
+    void onOpen(MenuEvent & inMenuEvent)
+    {
+        mEventHandler.onMenuTriggered(*this, inMenuEvent);
+    }
+
     EventHandler & mEventHandler;
+    QList<MenuEvent*> mMenuEvents;
 };
 
 
 class MainWindow : public QMainWindow,
-                   FileMenu::EventHandler
+                   Menu::EventHandler
 {
     Q_OBJECT
     
@@ -47,9 +107,10 @@ public:
     ~MainWindow();
 
 private:
-    void onFileMenuTriggered(FileMenu&);
+    void onMenuTriggered(Menu&, MenuEvent&);
 
     CentralWidget * mCentralWidget;
+    MenuEvent * mFileOpenEvent;
 };
 
 

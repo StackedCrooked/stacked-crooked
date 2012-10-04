dedf64661b359803ea45efe92e54de4e250a8dcc
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-10-04 22:19:40 +0000

    Cleanup copy.

--- Playground/CopyBenchmarks/main.cpp
+++ Playground/CopyBenchmarks/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <array>
 #include <cassert>
 #include <cxxabi.h>
@@ -8,17 +9,20 @@
 #include <sstream>
 #include <typeinfo>
 
-template<typename T> char * get_binary(T & t);
-template<typename T> const char * get_const_binary(const T & t);
+template<typename T> uint8_t* get_binary(T & t);
+template<typename T> const uint8_t* get_const_binary(const T & t);
 
 template<typename T>
 std::string make_hex(const T & t)
 {
     std::stringstream ss;
-    const char * c = get_const_binary(t);
-    ss << "0x";
+    const uint8_t* c = get_const_binary(t);
     for (unsigned i = 0; i < sizeof(t); ++i)
     {
+        if (i != 0)
+        {
+            ss << " ";
+        }
         ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(c[i]);        
     }
     return ss.str();
@@ -37,16 +41,16 @@ std::string make_string(const T & t)
 
 
 template<typename T>
-const char * get_const_binary(const T & t)
+const uint8_t* get_const_binary(const T & t)
 {
-    return static_cast<const char *>(static_cast<const void*>(&t));    
+    return static_cast<const uint8_t*>(static_cast<const void*>(&t));    
 }
 
 
 template<typename T>
-char * get_binary(T & t)
+uint8_t* get_binary(T & t)
 {
-    return const_cast<char*>(get_const_binary(t));
+    return const_cast<uint8_t*>(get_const_binary(t));
 }
 
 
@@ -56,35 +60,41 @@ struct copy_impl;
 template<typename Destination, unsigned Index, unsigned Size>
 struct copy_impl
 {
-    void operator()(char * dst, const char * src)
+    void operator()(uint8_t* dst, const uint8_t* src)
     {
         dst[Index] = src[Index];
         copy_impl<Destination, Index + 1, Size>()(dst, src);
     }
 };
 
-template<typename Destination, unsigned Index>
-struct copy_impl<Destination, Index, Index>
+template<typename Destination, unsigned I>
+struct copy_impl<Destination, I, I>
 {
-    void operator()(char *, const char * )
-    {
-    }
+    void operator()(uint8_t*, const uint8_t*) { }
 };
 
 template<typename Destination>
-void copy(Destination & dst, const char * data)
+void copy(Destination & dst, const uint8_t* data)
 {
-    char * bytes = get_binary(dst);
+    uint8_t* bytes = get_binary(dst);
     copy_impl<Destination, 0, sizeof(Destination)>()(bytes, data);
     
+}
+
+template<typename T>
+T make(const uint8_t* data)
+{
+    T result;
+    copy(result, data);
+    return result;
 }
 
 
 template<typename T>
 void assert_eq(const T & a, const T & b)
 {
-    std::cout << "assert_eq(" << make_string(a) << ", " << make_string(b) << ")" << std::flush;
-    std::cout << ((0 == memcmp(&a, &b, sizeof(T))) ? " => PASS" : " => FAIL") << std::endl;
+    std::cout << "assert_eq:\n\t" << make_string(a) << "\n\t" << make_string(b) << std::endl;
+    std::cout << ((0 == memcmp(&a, &b, sizeof(T))) ? " => PASS" : " => FAIL") << std::endl << std::endl;
 }
 
 
@@ -92,16 +102,22 @@ int main()
 {
     struct Header
     {
-        uint32_t a;
-        uint16_t b, c;
+        uint32_t i;
+        uint16_t s[2];
+        uint8_t c[4];
     };
     
     
-    Header h1 = { 1, 20, 21 };
-    char * buf = get_binary(h1);    
-    Header h2;
-    copy(h2, buf);
+    Header h1;
+    uint8_t* c = reinterpret_cast<uint8_t*>(&h1);
+    for (unsigned i = 0; i < sizeof(h1); ++i)
+    {
+        c[i] = i;
+    }
     
+    Header h2 = h1;
+    assert_eq(h1, h2);    
     
-    assert_eq(h1, h2);
+    Header h3 = make<Header>(c);
+    assert_eq(h2, h3);
 }

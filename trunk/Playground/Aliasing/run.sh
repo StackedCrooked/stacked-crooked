1b1fc6b3f0b170b11b93b448721ea8cdfd6bb244
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-10-04 15:36:46 +0000

    Update run.sh.

--- trunk/Playground/Aliasing/run.sh
+++ trunk/Playground/Aliasing/run.sh
@@ -1,1 +1,1 @@
-for i in `seq 0 3` ; do echo "With optimization level $i: $(g++ -o test -fstrict-aliasing -O$i main.cpp && ./test)" ; done
+for i in `seq 0 3` ; do echo "With optimization level $i: $(g++ -o test -O$i main.cpp && ./test)" ; done

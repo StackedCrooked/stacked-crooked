897f8a98ad4b4a185bde430d5ab7cbff2a1d006a
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-10-05 23:48:17 +0000

    Simplifications.

--- trunk/Playground/CopyBenchmarks/main.cpp
+++ trunk/Playground/CopyBenchmarks/main.cpp
@@ -8,58 +8,65 @@
 
 
 template<typename T>
-char * binary_cast(T & t) { return static_cast<char*>(static_cast<void*>(&t)); }
+char * binary_cast(T & t)
+{
+    return static_cast<char*>(static_cast<void*>(&t));
+}
+
 
 template<typename T>
-const char * binary_cast(const T & t) { return static_cast<const char*>(static_cast<const void*>(&t)); }
+const char * binary_cast(const T & t)
+{
+    return static_cast<const char*>(static_cast<const void*>(&t));
+}
 
 
-template<typename T>
-T net_decode(const uint8_t * data);
-
-
-void net_decode(const uint8_t * data, uint8_t & value)
+void decode(const uint8_t * data, uint8_t & value)
 {
     value = data[0];
 }
 
 
-void net_decode(const uint8_t * data, uint16_t & value)
+void decode(const uint8_t * data, uint16_t & value)
 {       
     value = data[0] << 8 | data[1];
 }
- 
 
-void net_decode(const uint8_t * data, uint32_t & value)
-{
-    value = net_decode<uint16_t>(data) << 16 | net_decode<uint16_t>(data + 2);
-}
-
-void net_decode(const uint8_t * data, uint64_t & value)
-{       
-    value = uint64_t(net_decode<uint32_t>(data)) << 32 | net_decode<uint32_t>(data + 4);
-
-#if 0
-    value = (uint64_t(c[0]) << 56) | (uint64_t(c[1]) << 48)
-          | (uint64_t(c[2]) << 40) | (uint64_t(c[3]) << 32)
-          | (uint64_t(c[4]) << 24) | (uint64_t(c[5]) << 16)
-          | (uint64_t(c[6]) <<  8) | (uint64_t(c[7]) << 0);
-#endif
-}
 
 template<typename T>
-void net_decode(const uint8_t* data, T & t)
+T decode(const uint8_t * data);
+ 
+
+void decode(const uint8_t * data, uint32_t & value)
 {
+    value = decode<uint16_t>(data) << 16 | decode<uint16_t>(data + 2);
+}
+
+
+void decode(const uint8_t * data, uint64_t & value)
+{
+    value = uint64_t(decode<uint32_t>(data)) << 32 | decode<uint32_t>(data + 4);
+}
+
+
+//! Fallback method for non-integral types.
+template<typename T>
+void decode(const uint8_t* data, T & t)
+{
+    static_assert(!std::is_integral<T>::value, "");
     std::copy(data, data + sizeof(t), binary_cast(t));
 }
 
+
 template<typename T>
-T net_decode(const uint8_t * data)
+T decode(const uint8_t * data)
 {
         T t;
-        net_decode(data, t);
+        decode(data, t);
         return t;
 }
+
+
 void print_binary(std::ostream & os, const uint8_t * data, unsigned length)
 {
     for (unsigned i = 0; i != length; ++i)
@@ -68,11 +75,13 @@ void print_binary(std::ostream & os, const uint8_t * data, unsigned length)
     }
     os << std::endl;
 }
+
  
 void print_binary(const uint8_t * data, unsigned length)
 {
     print_binary(std::cout, data, length);
 }
+
  
 int main()
 {
@@ -84,33 +93,33 @@ int main()
     
     {
         std::cout << "parse 16-bit integer: ";
-        auto n = net_decode<uint16_t>(network_data);
+        auto n = decode<uint16_t>(network_data);
         print_binary(reinterpret_cast<uint8_t*>(&n), sizeof(n));
     }
     
     {
         std::cout << "parse 32-bit integer: ";
-        auto n = net_decode<uint32_t>(network_data);
+        auto n = decode<uint32_t>(network_data);
         print_binary(reinterpret_cast<uint8_t*>(&n), sizeof(n));
     }
     
     {
         std::cout << "parse 64-bit integer: ";
-        auto n = net_decode<uint64_t>(network_data);
+        auto n = decode<uint64_t>(network_data);
         print_binary(reinterpret_cast<uint8_t*>(&n), sizeof(n));
     }
     
     {
         std::cout << "parse 64-bit non-integral object: ";
         struct obj { uint16_t s[4]; };
-        auto n = net_decode<obj>(network_data);
+        auto n = decode<obj>(network_data);
         print_binary(reinterpret_cast<uint8_t*>(&n), sizeof(n));
     }
     
     {
         std::cout << "parse array of length 4: ";
         typedef std::array<uint8_t, 4> ip_address;
-        auto n = net_decode<ip_address>(network_data);
+        auto n = decode<ip_address>(network_data);
         print_binary(reinterpret_cast<uint8_t*>(&n), sizeof(n));
     }
 }

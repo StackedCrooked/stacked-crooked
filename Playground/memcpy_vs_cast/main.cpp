bf8d798f4d190ff6d4be1c8bea4836fe5a0bd968
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-10-29 20:53:44 +0000

    Use demangling.

--- Playground/memcpy_vs_cast/main.cpp
+++ Playground/memcpy_vs_cast/main.cpp
@@ -5,8 +5,10 @@
 #include <ctime>
 #include <iomanip>
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 #include <chrono>
+#include <cxxabi.h> // for demangler
 
 class Stopwatch
 {
@@ -67,7 +69,7 @@ struct test_std_copy
 
 enum
 {
-    iterations = 20000,
+    iterations = 10000,
     container_size = 2000
 };
 
@@ -82,8 +84,40 @@ std::vector<char> get_binary_data()
     return bytes;
 }
 
+
+std::string demangle(const char * name)
+{
+    int st;
+    char * const p = abi::__cxa_demangle(name, 0, 0, &st);
+
+    if (st != 0)
+    {
+        switch (st)
+        {
+            case -1: throw std::runtime_error("A memory allocation failure occurred.");
+            case -2: throw std::runtime_error("Not a valid name under the GCC C++ ABI mangling rules.");
+            case -3: throw std::runtime_error("One of the arguments is invalid.");
+            default: throw std::runtime_error("Unexpected demangle status");
+        }
+    }
+
+    std::string result(p);
+    free(p);
+    return result;
+}
+
+
+template<typename T>
+std::string demangle(const T &)
+{
+    return demangle(typeid(T).name());
+}
+
+
+//! Measures the time required to parse the binary data.
+//! Returns time elapsed in milliseconds.
 template<typename Function>
-unsigned benchmark(const Function & function, unsigned & counter)
+unsigned measure(const Function & function, unsigned & counter)
 {
     std::vector<char> binary_data = get_binary_data();
     Stopwatch sw;
@@ -95,18 +129,27 @@ unsigned benchmark(const Function & function, unsigned & counter)
             counter += function(c);
         }
     }
-    return unsigned(0.5 + 1000.0 * sw.elapsed());
+    return static_cast<unsigned>(0.5 + 1000.0 * sw.elapsed());
+}
+
+template<typename Function>
+void benchmark(const Function & function, unsigned & counter)
+{
+    auto elapsedMs = measure(function, counter);
+    auto name = demangle(function);
+    std::cout << name << std::setw(18 - name.size()) << elapsedMs << " ms" << std::endl;
 }
 
 int main()
 {
     srand(time(0));
-    unsigned counter = 0;
 
-    std::cout << "cast:      " << benchmark(test_cast(),     counter) << " ms" << std::endl;
-    std::cout << "memcpy:    " << benchmark(test_memcpy(),   counter) << " ms" << std::endl;
-    std::cout << "memmove:   " << benchmark(test_memmove(),   counter) << " ms" << std::endl;
-    std::cout << "std::copy: " << benchmark(test_std_copy(), counter) << " ms" << std::endl;
+    unsigned counter = 0;
+    benchmark(test_cast(),     counter);
+    benchmark(test_memcpy(),   counter);
+    benchmark(test_memmove(),  counter);
+    benchmark(test_std_copy(), counter);
+
     std::cout << "(counter:  " << counter << ")" << std::endl << std::endl;
 
 }

6eb1c18d0ccfd30189dd5d8a5ea4d9750fae1a1e
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-11-03 01:57:39 +0000

    Syscalls.

--- Playground/ptrace/main.cpp
+++ Playground/ptrace/main.cpp
@@ -1,31 +1,39 @@
-#include <sys/ptrace.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
-#include <sys/user.h>
 #include <iostream>
-#include <cstddef>
+#include <string>
+#include <cassert>
 #include <asm/unistd.h>
 
+unsigned banned_syscalls[] = {
+    __NR_fork,
+    __NR_mknod,
+    __NR_chdir,
+    __NR_chmod,
+    __NR_ptrace,
+    __NR_kill,
+    __NR_rename,
+    __NR_mkdir,
+    __NR_rmdir,
+    __NR_dup,
+    __NR_pipe,
+    __NR_setgid,
+    __NR_getgid,
+    // __NR_signal, // 32-bit
+    __NR_geteuid,
+    __NR_getegid,
+    __NR_umask,
+    __NR_chroot,
+    __NR_dup2,
+    // __NR_sigsuspend, // 32-bit
+    __NR_symlink,
+    __NR_reboot,
+    __NR_clone,
+    // __NR_socketcall, // 32-bit
+    __NR_chown,
+    __NR_getcwd,
+    __NR_vfork
+};
 
 int main()
 {
-    std::cout << "GetAXOffset: " << offsetof(user_regs_struct, orig_rax) << std::endl;
-
-    pid_t child;
-    long orig_eax;
-    child = fork();
-    if(child == 0)
-    {
-        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        execl("/bin/ls", "ls", NULL);
-    }
-    else {
-
-        wait(NULL);
-        orig_eax = ptrace(PTRACE_PEEKUSER, child, offsetof(user_regs_struct, orig_rax), NULL);
-        std::cout << "The child made a system call: " << orig_eax << std::endl;
-        ptrace(PTRACE_CONT, child, NULL, NULL);
-    }
-    return 0;
 }
+

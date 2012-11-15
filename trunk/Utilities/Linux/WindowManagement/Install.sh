0f0905321bfe3079db209afe4fa15d943bce1c29
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-11-15 10:32:50 +0000

    Add wm-maximize.sh.

--- trunk/Utilities/Linux/WindowManagement/Install.sh
+++ trunk/Utilities/Linux/WindowManagement/Install.sh
@@ -1,3 +1,3 @@
 set -x
 type xdotool &>/dev/null || { echo "xdotool not found." >&2; exit 1; }
-for i in `ls w*` ; do ln -s `pwd`/$i ~/bin/ ; done
+for i in `ls w*` ; do ln -fs `pwd`/$i ~/bin/ ; done

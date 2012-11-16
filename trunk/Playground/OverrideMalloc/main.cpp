1c53ce24720ca765d5902cce5c70d763b38b68f0
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-11-16 20:26:38 +0000

    Malloc override.

--- trunk/Playground/OverrideMalloc/main.cpp
+++ trunk/Playground/OverrideMalloc/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
-
-
-void *malloc(size_t size);
+#include <stdexcept>
+#include <stdlib.h>
 
 
 int main()
 {
     unsigned n = 1;
     unsigned sum = 0;
-    while (sum < 1000 * 1000)
+    while (sum < 10 * 1000 * 1000)
     {
-        malloc(n);
+        void * ptr = malloc(n);
+        std::cout << "ptr: " << ptr << std::endl;
+        if (!ptr)
+        {
+            throw std::runtime_error("Failed to allocate " + std::to_string(n) + " more bytes.");
+        }
         sum += n;
         n *= 2;
         std::cout << "sum: " << unsigned(sum) << " bytes" << std::endl;

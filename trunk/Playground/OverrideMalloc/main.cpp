ac3ddfe54d11ce099eab626269c9be5a315b8cf1
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-11-16 21:05:21 +0000

    Add gdb-commands.txt.

--- trunk/Playground/OverrideMalloc/main.cpp
+++ trunk/Playground/OverrideMalloc/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     auto n = 1ull;
     auto sum = 0ull;
-    while (sum < 10 * 1000 * 1000)
+    while (sum < 1000)
     {
         auto ptr = malloc(n);
         std::cout << "ptr: " << ptr << std::endl;
@@ -19,5 +19,6 @@ int main()
         sum += n;
         n *= 2;
         std::cout << "sum: " << unsigned(sum) << " bytes" << std::endl;
+        free(ptr);
     }
 }
--- trunk/Playground/OverrideMalloc/malloc_hook.cpp
+++ trunk/Playground/OverrideMalloc/malloc_hook.cpp
@@ -16,45 +16,99 @@ namespace {
 std::size_t gSum = 0;
 std::unordered_map<void*, std::size_t> gAllocations;
 
-}
+std::size_t gNumAllocations = 0;
+std::size_t gNumRejectedAllocations = 0;
+std::size_t gNumUnmanagedFrees = 0;
+std::size_t gNumManagedFrees = 0;
+
+
+#define PRINT_COUNTER(name) fprintf(stderr, #name ": %lu\n", name);
+
+struct info
+{
+    info()
+    {
+        fprintf(stderr, "Initial alloc: %lu bytes.\n", gSum);
+    }
+    ~info()
+    {
+        PRINT_COUNTER(gSum);
+        PRINT_COUNTER(gNumAllocations);
+        PRINT_COUNTER(gNumRejectedAllocations);
+        PRINT_COUNTER(gNumUnmanagedFrees);
+        PRINT_COUNTER(gNumManagedFrees);
+    }
+} gInfo;
+
+
+class ScopedToggle
+{
+public:
+    ScopedToggle(bool & b) : b(b) { b = !b; }
+
+    ~ScopedToggle() { b = !b; }
+
+private:
+    ScopedToggle(const ScopedToggle&) = delete;
+    ScopedToggle& operator=(const ScopedToggle&) = delete;
+    bool & b;
+};
+
+
+} // anonymous namespace
 
 
 void *malloc(size_t size)
 {
-	typedef void* (*AllocFunction)(size_t);
-	static AllocFunction real_malloc = reinterpret_cast<decltype(real_malloc)>(dlsym(RTLD_NEXT, "malloc"));
+    typedef void* (*AllocFunction)(size_t);
+    static AllocFunction real_malloc = reinterpret_cast<decltype(real_malloc)>(dlsym(RTLD_NEXT, "malloc"));
 
-	gSum += size;
-	if (gSum > COLIRU_SANDBOX_MEMORY_LIMIT)
-	{
-		return 0;
-	}
-	else
-	{
-		return real_malloc(size);
-	}
+    static bool fInsideMalloc = false;
+    if (fInsideMalloc)
+    {
+        return real_malloc(size);
+    }
+    ScopedToggle toggle(fInsideMalloc);
+
+    gSum += size;
+    if (gSum > COLIRU_SANDBOX_MEMORY_LIMIT)
+    {
+        gNumRejectedAllocations++;
+        return 0;
+    }
+    else
+    {
+        gNumAllocations++;
+        auto result = real_malloc(size);
+        gAllocations.insert(std::make_pair(result, size));
+        return result;
+    }
 }
 
 void free( void* ptr )
 {
-	typedef void (*FreeFunction)(void*);
-	static FreeFunction real_free = reinterpret_cast<FreeFunction>(dlsym(RTLD_NEXT, "free"));
+    typedef void (*FreeFunction)(void*);
+    static FreeFunction real_free = reinterpret_cast<FreeFunction>(dlsym(RTLD_NEXT, "free"));
+
+    static bool fInsideFree = false;
+    if (fInsideFree)
+    {
+        real_free(ptr);
+        return;
+    }
+    ScopedToggle toggle(fInsideFree);
 
     auto it = gAllocations.find(ptr);
-    if (it == gAllocations.end())
+    if (it != gAllocations.end())
     {
-        fprintf(stderr, "free: unkown pointer: %p", ptr);
-        return;
+        auto amount = it->second;
+        gSum -= amount;
+        it->second = 0;
+        gNumManagedFrees++;
     }
-
-    auto amount = it->second;
-    if (amount > gSum)
+    else
     {
-        fprintf(stderr, "Amount that is to be freed is less than sum. Amount=%lu, sum=%lu", amount, gSum);
-        return;
+        gNumUnmanagedFrees++;
     }
-
-    gSum -= amount;
-    gAllocations.erase(it);
     real_free(ptr);
 }

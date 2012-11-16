df98167693e27c6597c72827e84226910c4ebd5f
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-11-16 22:15:22 +0000

    Cleanup.

--- trunk/Playground/OverrideMalloc/main.cpp
+++ trunk/Playground/OverrideMalloc/main.cpp
@@ -1,5 +1,4 @@
 #include <cassert>
-#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
@@ -9,26 +8,9 @@
 #endif
 
 
-#define TEST_MALLOC 0
-#define TEST_NEW    1
-#define TEST_CASE   TEST_NEW
-
 int main()
 {
-#if TEST_CASE == TEST_MALLOC
-    assert(malloc(COLIRU_SANDBOX_MEMORY_LIMIT - 1));
-    assert(malloc(1));
-    assert(!malloc(1));
-#elif TEST_CASE == TEST_NEW
     new char[COLIRU_SANDBOX_MEMORY_LIMIT - 1]; // OK
     new char[1]; // OK
-    try
-    {
-        new char[1]; // Error!
-        assert(false);
-    }
-    catch (std::bad_alloc& exc)
-    {
-    }
-#endif
+    new char[1]; // Should throw bad_alloc
 }
--- trunk/Playground/OverrideMalloc/malloc_hook.cpp
+++ trunk/Playground/OverrideMalloc/malloc_hook.cpp
@@ -1,18 +1,14 @@
-#include <stdexcept>
-#include <iostream>
-#include <dlfcn.h>
-
-
 #ifndef COLIRU_SANDBOX_MEMORY_LIMIT
 #error Build command should specify -DCOLIRU_SANDBOX_MEMORY_LIMIT=...
 #endif
 
 
-namespace {
+#include <stdexcept>
+#include <iostream>
+#include <dlfcn.h>
 
-std::size_t gSum = 0;
 
-}
+//namespace { std::size_t gSum = 0; }
 
 
 void *malloc(size_t size)
@@ -20,6 +16,7 @@ void *malloc(size_t size)
     typedef void* (*AllocFunction)(size_t);
     static AllocFunction real_malloc = reinterpret_cast<decltype(real_malloc)>(dlsym(RTLD_NEXT, "malloc"));
 
+    static std::size_t gSum = 0;
     gSum += size;
     if (gSum > COLIRU_SANDBOX_MEMORY_LIMIT)
     {

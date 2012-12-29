887e3f546e9cb50e51c0a7c98353be3d83d0e639
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-12-29 10:46:09 +0000

    Use Networking.

---
+++ Playground/LongRunningRequest/Bridge.h
@@ -0,0 +1,145 @@
+#include <algorithm>
+#include <condition_variable>
+#include <future>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <set>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <vector>
+
+
+void replace_all(std::string & str, const std::string & oldStr, const std::string & newStr)
+{
+    size_t pos = 0;
+    while ((pos = str.find(oldStr, pos)) != std::string::npos)
+    {
+        str.replace(pos, oldStr.length(), newStr);
+        pos += newStr.length();
+    }
+}
+
+std::string escape(std::string data)
+{
+    replace_all(data, "\t", "\\t");
+    return data;
+}
+
+std::string unescape(std::string data)
+{
+    replace_all(data, "\\t", "\t");
+    return data;
+}
+
+std::string join(const std::string & lhs, const std::string & rhs)
+{
+    return escape(lhs) + "\t" + escape(rhs);
+}
+
+std::vector<std::string> split(const std::string & s, char delim = '\t')
+{
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> result;
+    while (std::getline(ss, item, delim))
+    {
+        result.push_back(item);
+    }
+    return result;
+}
+
+
+struct Bridge
+{
+    void listen(std::string & answer)
+    {
+        while (true)
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            condition.wait_for(lock, std::chrono::seconds(10));
+            if (!delegated.empty())
+            {
+            for (const auto & pair : delegated)
+                {
+                    const auto & question = pair.first;
+                    answer = question;
+                    break; // assume only one request for now
+                }
+            }
+            else
+            {
+                answer = "[]";
+            }
+        }
+    }
+
+    std::string MakeRequest(const std::string & command, const std::string & arg)
+    {
+        return join(command, arg);
+    }
+
+    std::future<std::string> delegate(const std::string & command, const std::string & arg)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        auto promPtr = std::make_shared<std::promise<std::string> >();
+        std::promise<std::string> & prom = *promPtr;
+        auto fut = prom.get_future();
+        delegated.insert(std::make_pair(MakeRequest(command, arg), promPtr));
+        condition.notify_one();
+        return fut;
+    }
+
+    void finished(const std::string & question, const std::string & answer)
+    {
+        auto it = delegated.find(question);
+        if (it == delegated.end())
+        {
+            throw std::runtime_error("no matching request");
+        }
+        std::promise<std::string> & prom = *it->second;
+        prom.set_value(answer);
+    }
+
+    std::mutex mtx;
+    std::condition_variable condition;
+    std::map<std::string, std::shared_ptr<std::promise<std::string> > > delegated;
+};
+
+
+struct Server
+{
+    std::string handle(const std::string & request)
+    {
+        auto parts = split(request, '\t');
+        if (parts.size() != 2)
+        {
+            throw std::runtime_error("Invalid request.");
+        }
+        std::string answer;
+        handle(unescape(parts[0]), unescape(parts[1]), answer);
+        return answer;
+    }
+
+    void handle(const std::string & command, const std::string & arg, std::string & answer)
+    {
+        if (command == "listen")
+        {
+            bridge.listen(answer);
+        }
+        else if (command == "finished")
+        {
+            auto sep = arg.find("\t");
+            auto question = arg.substr(0, sep);
+            auto answer = arg.substr(sep + 1, std::string::npos);
+            bridge.finished(question, answer);
+        }
+        else
+        {
+            bridge.delegate(command, arg);
+        }
+    }
+
+    Bridge bridge;
+};

8867bbb178592ffaea7bfbbc1183549f220ebdd0
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-12-29 11:25:58 +0000

    Improved logic.

--- Playground/LongRunningRequest/main.cpp
+++ Playground/LongRunningRequest/main.cpp
@@ -7,10 +7,21 @@
 
 int main()
 {
-    Server http_server;
-
-    UDPServer udp_server(9100, [&](const Request & req){
-        return Response(http_server.handle(req));
+    boost::thread listener_thread([&](){
+        Server server;
+        UDPServer(9100, [&](const Request & req) {
+            return Response(server.handle(req));
+        });
     });
 
+    boost::thread worker_thread([](){
+        UDPClient client("localhost", 9100);
+        std::string req = client.send("listen");
+
+        // send the request itself as reply
+        UDPSender("localhost", 9100).send(join("finished", req));
+    });
+
+    worker_thread.join();
+    listener_thread.join();
 }

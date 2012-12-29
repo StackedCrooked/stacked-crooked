a1f097c0a6caecac80e538abbc05afdbf4b8e3e8
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-12-29 20:14:37 +0000

    Update install script.

--- trunk/InstallLinux.sh
+++ trunk/InstallLinux.sh
@@ -1,6 +1,7 @@
 sudo apt-get -y install python-software-properties
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get -y install g++-4.7 ccache 
 mkdir -p $HOME/bin
 echo 'ccache g++-4.7 $*' >  $HOME/bin/g++ && chmod 755 $HOME/bin/g++
@@ -12,3 +13,4 @@ cd Vim && ./Install.sh && cd -
 cd Utilities/CodeFormatter && ./Install.sh && cd -
 cd Utilities/svnscripts && ./Install.sh && cd -
 sudo apt-get -y install synaptic aptitude
+sudo apt-get install libboost1.50-dev libpoco-dev

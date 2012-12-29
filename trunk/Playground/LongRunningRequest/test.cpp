82357e87787930b45dba0a54abd246491927c08f
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-12-29 09:57:33 +0000

    Update the bridge.

--- trunk/Playground/LongRunningRequest/test.cpp
+++ trunk/Playground/LongRunningRequest/test.cpp
@@ -1,36 +1,72 @@
-//#include <iostream>
+#include <algorithm>
+#include <condition_variable>
+#include <deque>
+#include <mutex>
+#include <thread>
 
 
-//struct Server
-//{
-//    void handle(const std::string & inRequest, std::string & outResponse)
-//    {
-//        outResponse = "(" + inRequest + ")";
-//    }
-//};
+struct Server
+{
+    void handle(const std::string & question, std::string & answer)
+    {
+        if (question == "flip")
+        {
+            std::reverse(answer.begin(), answer.end());
+        }
+        else
+        {
+            // GIGO
+            answer = question;
+        }
+    }
+};
 
 
-//struct Bridge
-//{
-//    Bridge(Server & inServer) : server(&mServer) {}
+struct Bridge
+{
+    Bridge(Server & s) : server(&s) {}
 
-//    void handle(const std::string &, std::string & outResponse)
-//    {
-//        while (!mQuit)
-//        {
-//            std::unique_lock<std::mutex> lock(mMutex);
-//            mCondition.wait(lock);
-//            outResponse = std::move(mRequest);
-//        }
-//    }
+    void handle(const std::string & question, std::string & answer)
+    {
+        if (question == "listen")
+        {
+            while (true)
+            {
+                std::unique_lock<std::mutex> lock(mtx);
+                condition.wait_for(lock, std::chrono::seconds(10));
+                if (!questions.empty())
+                {
+                    answer = "[" + questions.front() + "]";
+                    questions.pop_front();
+                }
+                else
+                {
+                    answer = "[]";
+                }
+            }
+        }
+        else
+        {
+            answer = std::move(question);
+        }
+    }
+
+    void delegate(const std::string & q)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        questions.push_back(q);
+        condition.notify_one();
+    }
 
 
-//    Server & mServer;
-//    std::string mRequest;
-//};
+    Server * server;
+    std::mutex mtx;
+    std::condition_variable condition;
+    std::deque<std::string> questions;
+};
 
 
-//int main()
-//{
+int main()
+{
 
-//}
+}

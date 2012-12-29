e1947c6bfe86fc0e1960ae93581e970bd527c126
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2012-12-29 20:06:33 +0000

    Add Linux install script.

---
+++ InstallLinux.sh
@@ -0,0 +1,6 @@
+set -x
+sudo apt-get install vim
+cd Vim && ./Install.sh && cd -
+cd Utilities/CodeFormatter && ./Install.sh && cd -
+cd Utilities/svnscripts && ./Install.sh && cd -
+sudo apt-get install synaptic aptitude

31ccd8652c3829fe2a8673fbeea04b0ef567c4ab
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-12-29 20:09:43 +0000

    Update linux install script.

--- trunk/InstallLinux.sh
+++ trunk/InstallLinux.sh
@@ -1,6 +1,14 @@
+sudo apt-get -y install python-software-properties
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get -y install g++-4.7 ccache 
+mkdir -p $HOME/bin
+echo 'ccache g++-4.7 $*' >  $HOME/bin/g++ && chmod 755 $HOME/bin/g++
+echo 'ccache gcc-4.7 $*' >  $HOME/bin/gcc && chmod 755 $HOME/bin/gcc
+cd $HOME/bin && ln -fs `pwd`/g++ c++ && ln -fs `pwd`/gcc cc && cd -
 set -x
-sudo apt-get install vim
+sudo apt-get -y install vim
 cd Vim && ./Install.sh && cd -
 cd Utilities/CodeFormatter && ./Install.sh && cd -
 cd Utilities/svnscripts && ./Install.sh && cd -
-sudo apt-get install synaptic aptitude
+sudo apt-get -y install synaptic aptitude

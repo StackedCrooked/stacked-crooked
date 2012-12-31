47991a0cab172ec6287d5f03aded70e4b120d56b
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2012-12-31 09:30:53 +0000

    Simple time server.

--- trunk/Playground/HTTPServer/main.cpp
+++ trunk/Playground/HTTPServer/main.cpp
@@ -1,5 +1,25 @@
 #include "http_server.h"
+#include <ctime>
+
+
+struct TimeServer : HTTP::Server
+{
+public:
+    TimeServer(const std::string & host, unsigned short port) :
+        HTTP::Server(host, port)
+    {
+
+    }
+
+private:
+    std::string do_handle(const std::string &)
+    {
+        return std::to_string(time(0));
+    }
+};
+
 
 int main()
 {
+    TimeServer ts("localhost", 8080);
 }

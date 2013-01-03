27455ce01df73eebeff3e3d089bb7db653005e4e
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2013-01-03 20:40:54 +0000

    Actor much improved.

--- trunk/Playground/HTTPServer/main.cpp
+++ trunk/Playground/HTTPServer/main.cpp
@@ -1,36 +1,12 @@
-#include "http_server.h"
-#include <ctime>
-#include <iostream>
-#include <string>
+#include <boost/asio.hpp>
 
 
-using namespace http;
-
-
-struct TimeServer : http::Server
+class TCPSession
 {
-public:
-    TimeServer(const std::string & host, unsigned short port) :
-        http::Server(host, port)
-    {
 
-    }
-
-private:
-    std::string do_handle(const Request & req)
-    {
-        for (auto c : req.payload)
-        {
-            std::cout << '[' << c << ']' << std::flush;
-        }
-        std::cout << std::endl;
-        return std::to_string(time(0));
-    }
 };
-
 
 int main()
 {
-    TimeServer ts("localhost", 8080);
-    ts.run();
+
 }

39a2d78107648c9b7f1806512e7959a675c17846
francis.rammeloo@gmail.com <francis.rammeloo@gmail.com@78535834-c32d-11dd-927d-974589228060>
2013-02-13 21:34:21 +0000

    Update.

--- trunk/Playground/FastStack/main.cpp
+++ trunk/Playground/FastStack/main.cpp
@@ -1,12 +1,117 @@
+#include <algorithm>
 #include <array>
+#include <cassert>
+#include <deque>
 #include <iostream>
+#include <vector>
 #include <stdint.h>
+
+
+typedef std::vector<uint8_t> DynamicBuffer;
+typedef std::vector<DynamicBuffer> DynamicBuffers;
+
+
+class Packet
+{
+public:
+    Packet() = default; // enable default constructor.
+
+    template<typename T>
+    explicit Packet(const T & inInitialData)
+    {
+        push_front(inInitialData);
+    }
+
+    explicit Packet(DynamicBuffer inData)
+    {
+        push_front(std::move(inData));
+    }
+
+    // Move constructor.
+    Packet(Packet&& inPacket) : mParts(std::move(inPacket.mParts)) { }
+
+    Packet & operator=(Packet && inPacket)
+    {
+        mParts = std::move(inPacket.mParts);
+        return *this;
+    }
+
+    void push_front(const uint8_t * inData, std::size_t inSize)
+    {
+        mParts.push_front(DynamicBuffer(inData, inData + inSize));
+    }
+
+    void push_front(const DynamicBuffer & inData)
+    {
+        mParts.push_front(inData);
+    }
+
+    void push_back(const DynamicBuffer & inData)
+    {
+        mParts.push_back(inData);
+    }
+
+    template<typename T>
+    void push_back(const T & inHeader)
+    {
+        mParts.push_back(std::move(ConvertHeaderToString(inHeader)));
+    }
+
+    std::size_t getTotalSize() const
+    {
+        auto op = [](std::size_t sum, const DynamicBuffer & str) -> std::size_t {
+            return sum + str.size();
+        };
+        return std::accumulate(mParts.begin(), mParts.end(), 0u, op);
+    }
+
+    void get(DynamicBuffer & outData) const
+    {
+        outData.clear();
+        outData.reserve(getTotalSize());
+
+        for (const auto & str : mParts)
+        {
+            outData.insert(outData.end(), str.begin(), str.end());
+        }
+    }
+
+    DynamicBuffer getBuffer() const
+    {
+        DynamicBuffer result;
+        get(result);
+        return result;
+    }
+
+private:
+    template<typename T>
+    static DynamicBuffer ConvertHeaderToString(const T & inHeader)
+    {
+        static_assert(std::is_pod<T>::value, "Header type must be POD type!");
+        const uint8_t * data = reinterpret_cast<const uint8_t*>(&inHeader);
+        return DynamicBuffer(data, data + sizeof(inHeader));
+    }
+
+    Packet(const Packet&) = delete; // disable copy
+    Packet& operator=(const Packet&) = delete; // disable assigment
+
+    std::deque<DynamicBuffer> mParts;
+};
+
+
+inline DynamicBuffer GetPacketData(const Packet & inPacket)
+{
+    DynamicBuffer result;
+    inPacket.get(result);
+    return result;
+}
+
 
 
 template<typename T>
 struct InvalidEnumerator : std::runtime_error
 {
-    InvalidEnumerator(T t) : std::runtime_error("InvalidEnumerator") {}
+    InvalidEnumerator(T t) : std::runtime_error("InvalidEnumerator"), t(t) {}
     ~InvalidEnumerator() throw() {}
     
     T getEnumerator() const
@@ -18,7 +123,7 @@ struct InvalidEnumerator : std::runtime_error
 };
 
 template<typename T>
-InvalidEnumerator<T> InvalidEnumerator(T t)
+InvalidEnumerator<T> MakeInvalidEnumerator(T t)
 {
     return InvalidEnumerator<T>(t);
 }
@@ -26,11 +131,14 @@ InvalidEnumerator<T> InvalidEnumerator(T t)
 template<typename T, typename /*disambiguator*/>
 struct StrongTypedef
 {
-    StrongTypedef(const T data = T()) : data_(data) {}
+    template<typename ...Args>
+    StrongTypedef(Args && ...args) : data_(std::forward<Args>(args)...) {}
     operator const T & () const
     {
         return data_;
     }
+    T & get() { return data_; }
+    const T & get() const { return data_; }
     T data_;
 };
 
@@ -151,7 +259,12 @@ struct ICMPMessage
     Net16 mSequenceNumber;
 };
 
-using Raw = std::pair<uint8_t *, uint8_t *>;
+struct Raw : std::pair<uint8_t*, uint8_t*>
+{
+    template<typename ...Args>
+    Raw(Args && ...args) : std::pair<uint8_t*, uint8_t*>(std::forward<Args>(args)...) {}
+};
+
 
 Raw Inc(Raw raw, unsigned n)
 {
@@ -180,27 +293,24 @@ IPProtNum GetHLPId(IPPacket ip)
     return static_cast<IPProtNum>(ip.mProtocol);
 }
 
-template<typename Tail>
-auto GetHLPId(std::pair<Raw, Tail> msg) -> decltype(GetHLPId(msg.second))
-{
-    return GetHLPId(msg.second);
-}
-
 void pop(const Raw & inRaw);
-void pop(std::pair<Raw, EthernetFrame> data);
-template<typename Tail> void pop(std::pair<Raw, std::pair<IPPacket     , Tail> > data);
-template<typename Tail> void pop(std::pair<Raw, std::pair<ICMPMessage  , Tail> > data);
+void pop(std::pair<Raw, EthernetFrame> msg);
+template<typename Tail> void pop(std::pair<Raw, EthernetFrame> msg);
+template<typename Tail> void pop(std::pair<Raw, std::pair<IPPacket     , Tail> > msg);
+template<typename Tail> void pop(std::pair<Raw, std::pair<ARPMessage   , Tail> > msg);
+template<typename Tail> void pop(std::pair<Raw, std::pair<ICMPMessage  , Tail> > msg);
 
 template<typename Head>
 std::pair<Raw, Head> Decode(Raw raw)
 {
-    return std::make_pair(Inc<Head>(raw), Decode<Head>(raw));
+    return std::make_pair(Inc<Head>(raw), *reinterpret_cast<Head*>(raw.first));
 }
 
 template<typename Head, typename Tail>
 std::pair<Raw, std::pair<Head, Tail> > Decode(const std::pair<Raw, Tail> & msg)
 {
-    return std::make_pair(Inc<Head>(msg.first), std::make_pair(Decode<Head>(msg.first), msg.second));
+    auto p = Decode<Head>(msg.first);
+    return std::make_pair(p.first, std::make_pair(p.second, msg.second));
 }
 
 void pop(Raw raw)
@@ -208,37 +318,45 @@ void pop(Raw raw)
     return pop(Decode<EthernetFrame>(raw));
 }
 
-template<typename Header, typename Tail>
-auto GetHLPId(std::pair<Header, Tail> data) -> decltype(GetHLPId(data.second))
+template<typename Header>
+auto GetHLPId(std::pair<Raw, Header> msg) -> decltype(GetHLPId(msg.second))
 {
-    return GetHLPId(data.first);
+    return GetHLPId(msg.second);
 }
 
-void pop(std::pair<Raw, EthernetFrame> data)
+template<typename Header, typename Tail>
+auto GetHLPId(std::pair<Header, Tail> msg) -> decltype(GetHLPId(msg.first))
 {
-    switch (GetHLPId(data))
+    return GetHLPId(msg.first);
+}
+
+void pop(std::pair<Raw, EthernetFrame> msg)
+{
+    auto hlpId = GetHLPId(msg);
+    switch (hlpId)
     {
         case EtherType::ARP:
         {
-            pop(Decode<ARPMessage>(data));
+            pop(Decode<ARPMessage>(msg));
             break;
         }
-        case EtherType::IP :
+        case EtherType::IP:
         {
-            pop(Decode<IPPacket>(data));
+            pop(Decode<IPPacket>(msg));
             break;
         }
         default:
         {
-            throw InvalidEnumerator(data.first.mEtherType);
+            throw MakeInvalidEnumerator(hlpId);
         }
     }
 }
 
 template<typename Tail>
-void pop(std::pair<Raw, std::pair<IPProtNum, Tail> > msg)
+void pop(std::pair<Raw, std::pair<IPPacket, Tail> > msg)
 {
-    switch (GetHLPId(msg))
+    auto hlpId = GetHLPId(msg);
+    switch (hlpId)
     {
         case IPProtNum::ICMP:
         {
@@ -247,18 +365,37 @@ void pop(std::pair<Raw, std::pair<IPProtNum, Tail> > msg)
         }
         default:
         {
-             throw InvalidEnumerator(protocol);
+             throw MakeInvalidEnumerator(hlpId);
         }
     }
 }
 
-template<typename T> Flip(T) {}
+template<typename T> T Flip(T t)
+{
+    return t;
+}
 
 template<typename Tail>
 void pop(std::pair<Raw, std::pair<ICMPMessage, Tail> > msg)
 {
     Packet packet;
-    packet.push(Flip(msg.first));
-    push(packet, msg.second);
+    auto flipped = Flip(msg.second.first);
+    static_assert(std::is_pod<decltype(flipped)>::value, "");
+    uint8_t* bytes = reinterpret_cast<uint8_t*>(&flipped);
+    packet.push_front(bytes, sizeof(flipped));
+    //push(packet, msg.second);
 }
 
+template<typename Tail>
+void pop(std::pair<Raw, std::pair<ARPMessage, Tail> > msg)
+{
+    Packet packet;
+    Raw raw = msg.first;
+    packet.push_front(raw.first, raw.second - raw.first);
+    //push(msg.second);
+}
+
+
+int main()
+{
+}

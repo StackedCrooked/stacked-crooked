c633ac553d212e42535604344f5c7ac96d468045
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-02-27 16:35:08 +0000

    Add.

---
+++ Playground/Asio/TCPServer.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include <memory>
+#include <stdint.h>
+
+
+class TCPServer
+{
+public:
+    TCPServer(uint16_t port);
+    
+private:
+    struct Impl;
+    std::unique_ptr<Impl> mImpl;
+};

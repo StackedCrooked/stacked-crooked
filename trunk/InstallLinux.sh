0e1c3c249c5e54c271ee9ad2f6242842c58e2126
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2013-02-28 21:06:10 +0000

    Don't install synaptic.

--- trunk/InstallLinux.sh
+++ trunk/InstallLinux.sh
@@ -1,8 +1,8 @@
-sudo apt-get -y install python-software-properties subversion vim
+sudo apt-get install -y python-software-properties subversion vim
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get -y install g++-4.7 ccache 
+sudo apt-get install -y g++-4.7 ccache 
 mkdir -p $HOME/bin
 echo 'ccache g++-4.7 $*' >  $HOME/bin/g++ && chmod 755 $HOME/bin/g++
 echo 'ccache gcc-4.7 $*' >  $HOME/bin/gcc && chmod 755 $HOME/bin/gcc
@@ -12,5 +12,5 @@ sudo apt-get -y install vim
 cd Vim && ./Install.sh && cd -
 cd Utilities/CodeFormatter && ./Install.sh && cd -
 cd Utilities/svnscripts && ./Install.sh && cd -
-sudo apt-get -y install synaptic aptitude
-sudo apt-get -y install libboost1.50-dev libpoco-dev
+sudo apt-get install -y aptitude
+sudo apt-get install -y libboost1.50-dev libpoco-dev

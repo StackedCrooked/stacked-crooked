7efdddd0169dc1bf8409d22841a6b5ed47afad5b
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-03-21 22:12:33 +0000

    Variadic printer.

--- Playground/GenerateMessage/main.cpp
+++ Playground/GenerateMessage/main.cpp
@@ -5,12 +5,38 @@
 
 GENERATE_MESSAGE(Point,
                  ( (int)(x) )
-                 ( (int)(y) ));
+                 ( (int)(y) )
+                 ( (int)(z) ));
+
+
+template<unsigned N>
+struct printer : printer<N - 1 >
+{
+    template<typename ...Args>
+    printer(const std::tuple<Args...> & tup) : printer<N - 1 >(tup)
+    {
+        enum { index = N - 1 };
+        std::cout << std::get<index>(tup) << " ";
+    }
+};
+
+template<>
+struct printer<0>
+{
+    template<typename T>
+    printer(const T &) {}
+};
+
+template<typename ...Args>
+void print(const std::tuple<Args...> & tup)
+{
+    printer<sizeof...(Args)>{tup};
+}
 
 
 int main()
 {
-    Point p(1, 2);
-    std::cout << "(" << p.get_x() << ", " << p.get_y() << ")" << std::endl;
+    Point p(1, 2, 3);
+    print(p);
 }
 

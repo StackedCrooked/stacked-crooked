88f9eb91f2ecba049e01ec5d82bc07ff1433e45a
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-03-21 22:36:25 +0000

    Update.

--- Playground/GenerateMessage/GenerateMessage.h
+++ Playground/GenerateMessage/GenerateMessage.h
@@ -2,36 +2,30 @@
 #include <boost/preprocessor/seq/for_each_i.hpp>
 #include <boost/preprocessor/cat.hpp>
 
-#define HEAD BOOST_PP_SEQ_HEAD
-#define TAIL BOOST_PP_SEQ_TAIL
-#define EACH BOOST_PP_SEQ_FOR_EACH
-#define ELEM BOOST_PP_SEQ_ELEM
-#define FIRST(Seq) ELEM(0, Seq)
-#define SECOND(Seq) ELEM(1, Seq)
+#define SEQ_FIRST(Seq) BOOST_PP_SEQ_ELEM(0, Seq)
+#define SEQ_SECOND(Seq) BOOST_PP_SEQ_ELEM(1, Seq)
 
 #define GENERATE_COMMA() ,
 #define GENERATE_SEMICOLON() ;
 #define GENERATE_NOTHING()
 
-#define FIRST_(r, GenSep, Pair) GenSep() FIRST(Pair)
-#define SECOND_(r, GenSep, Pair) GenSep() SECOND(Pair)
-
+#define FIRST_(r, GenSep, Pair) GenSep() SEQ_FIRST(Pair)
+#define SECOND_(r, GenSep, Pair) GenSep() SEQ_SECOND(Pair)
 
 #define MAP_FIRST(Fields, GenSep) \
-    FIRST(HEAD(Fields)) EACH(FIRST_, GenSep, TAIL(Fields))
+    SEQ_FIRST(BOOST_PP_SEQ_HEAD(Fields)) BOOST_PP_SEQ_FOR_EACH(FIRST_, GenSep, BOOST_PP_SEQ_TAIL(Fields))
 
 #define MAP_SECOND(Fields, GenSep) \
-    SECOND(HEAD(Fields)) EACH(SECOND_, GenSep, TAIL(Fields))
-
+    SEQ_SECOND(BOOST_PP_SEQ_HEAD(Fields)) BOOST_PP_SEQ_FOR_EACH(SECOND_, GenSep, BOOST_PP_SEQ_TAIL(Fields))
 
 #define GEN_CONSTRUCTOR_ARGUMENT(r, GenSep, Field) \
-    GenSep() const FIRST(Field) & SECOND(Field)
+    GenSep() const SEQ_FIRST(Field) & SEQ_SECOND(Field)
 
 #define GEN_CONSTRUCTOR_ARGUMENTS(Fields) \
-    const FIRST(HEAD(Fields)) & SECOND(HEAD(Fields)) EACH(GEN_CONSTRUCTOR_ARGUMENT, GENERATE_COMMA, TAIL(Fields))
+    const SEQ_FIRST(BOOST_PP_SEQ_HEAD(Fields)) & SEQ_SECOND(BOOST_PP_SEQ_HEAD(Fields)) BOOST_PP_SEQ_FOR_EACH(GEN_CONSTRUCTOR_ARGUMENT, GENERATE_COMMA, BOOST_PP_SEQ_TAIL(Fields))
 
 #define GEN_METHOD_(r, GenSep, Index, Field) \
-    const FIRST(Field) & BOOST_PP_CAT(get_, SECOND(Field)) () const { return std::get< Index >(*this); }
+    const SEQ_FIRST(Field) & BOOST_PP_CAT(get_, SEQ_SECOND(Field)) () const { return std::get< Index >(*this); }
 
 #define GEN_METHODS(Fields) \
     BOOST_PP_SEQ_FOR_EACH_I(GEN_METHOD_, GENERATE_SEMICOLON, Fields)

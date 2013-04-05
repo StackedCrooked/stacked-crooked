8a88427441045d03aee8351f14dd39494c694165
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2013-04-05 05:42:15 +0000

    Add comments.

--- trunk/Playground/HeapSort/main.cpp
+++ trunk/Playground/HeapSort/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
 
+
 template<typename T>
 using Heap = std::vector<T>;
 
-// BuildMaxHeap
-// MaxHeapify
+
+// MaxHeapify:
+// BuildMaxHeap: applies MaxHeapify on the heap nodes, starting with the leaf nodes.
 
 
 int main()

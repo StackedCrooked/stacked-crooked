99707666e73adabcfef8c7ee38e99a916b5c1ac8
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2013-04-06 18:04:59 +0000

    Update install linux script.

--- trunk/InstallLinux.sh
+++ trunk/InstallLinux.sh
@@ -1,17 +1,24 @@
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y python-software-properties subversion vim
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install -y g++-4.7 ccache 
+#!/bin/bash
+set -x
+
+if [ $(whoami) != "root" ] ; then
+	echo "Installation requires root permissions. Exiting." 1>&2
+	exit 1
+fi
+
+apt-get install -y software-properties-common python-software-properties
+add-apt-repository ppa:ubuntu-toolchain-r/test
+apt-get update
+apt-get upgrade -y
+BOOST=$(aptitude search boost | grep -E 'libboost[0-9]\.[0-9]+-all-dev[^:]' | awk '{print $2}' | sort -n -r | head -n1)
+apt-get install -y g++-4.7 ccache subversion vim aptitude ${BOOST} libpoco-dev
 mkdir -p $HOME/bin
 echo 'ccache g++-4.7 $*' >  $HOME/bin/g++ && chmod 755 $HOME/bin/g++
 echo 'ccache gcc-4.7 $*' >  $HOME/bin/gcc && chmod 755 $HOME/bin/gcc
 cd $HOME/bin && ln -fs `pwd`/g++ c++ && ln -fs `pwd`/gcc cc && cd -
-set -x
-sudo apt-get -y install vim
 cd Vim && ./Install.sh && cd -
 cd Utilities/CodeFormatter && ./Install.sh && cd -
 cd Utilities/svnscripts && ./Install.sh && cd -
-sudo apt-get install -y aptitude
-sudo apt-get install -y libboost1.50-dev libpoco-dev
+
+echo 'export LC_ALL="en_US.UTF-8"' >> ~/.bashrc
+echo 'LC_ALL=C' >> /etc/environment

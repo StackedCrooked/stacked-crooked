bfce42018c34914ab70d57212f3baaaa4666c45c
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2013-04-08 14:18:44 +0000

    Cleanup.

--- trunk/Playground/Asio/TestClient.cpp
+++ trunk/Playground/Asio/TestClient.cpp
@@ -2,44 +2,19 @@
 #include <thread>
 
 
-int main(int argc, char * argv[])
+int main()
 {
-    try
+    MessageClient client("127.0.0.1", 9999);
+
+    std::thread t(boost::bind(&boost::asio::io_service::run, &get_io_service()));
+
+    std::string line;
+    while (std::cin >> line)
     {
-        if (argc != 3)
-        {
-            std::cerr << "Usage: chat_client <host> <port>\n";
-            return 1;
-        }
-
-        boost::asio::io_service ioservice;
-
-        boost::asio::ip::tcp::resolver resolver(ioservice);
-        boost::asio::ip::tcp::resolver::query query(argv[1], argv[2]);
-        boost::asio::ip::tcp::resolver::iterator iterator = resolver.resolve(query);
-
-        MessageClient c(ioservice, iterator);
-
-        std::thread t(boost::bind(&boost::asio::io_service::run, &ioservice));
-
-        char line[Message::max_body_length + 1];
-        while (std::cin.getline(line, Message::max_body_length + 1))
-        {
-            using namespace std; // For strlen and memcpy.
-            Message msg;
-            msg.body_length(strlen(line));
-            memcpy(msg.body(), line, msg.body_length());
-            msg.encode_header();
-            c.write(msg);
-        }
-
-        c.close();
-        t.join();
-    }
-    catch (std::exception & e)
-    {
-        std::cerr << "Exception: " << e.what() << "\n";
+        client.write(line);
     }
 
+    client.close();
+    t.join();
     return 0;
 }

5628ca74c88db174a1c04a1139e2eb42122530c0
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-04-11 09:57:41 +0000

    More cleanup.

--- Playground/Asio/TestClient.cpp
+++ Playground/Asio/TestClient.cpp
@@ -8,12 +8,13 @@ int main()
 
     std::thread t(boost::bind(&boost::asio::io_service::run, &MessageProtocol::get_io_service()));
 
-    std::string line;
-    while (std::cin >> line)
+    
+    for (int i = 0; i != 10; ++i)
     {
-        client.send(line, [](const std::string & str) {
-            std::cout << "received: " << str << std::endl;
+        client.send(std::to_string(i), [i](const std::string & str) {
+            std::cout << "received " << i << ": " << str << std::endl;
         });
+        std::cout << "sent " << i << std::endl;
     }
 
     client.close();

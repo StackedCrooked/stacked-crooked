d6a136e94bd962746d4fcdeb48b4639daea7cf56
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2013-04-11 23:22:02 +0000

    Add CallbackFuture.

---
+++ trunk/Playground/Callback2Future/main.cpp
@@ -0,0 +1,60 @@
+#include <thread>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <vector>
+
+
+using Callback = std::function<void(std::string)>;
+std::vector<std::thread> threads;
+
+void http_get(const std::string & url, const Callback & callback)
+{
+    threads.emplace_back([=]() {
+        try {
+            callback("<html><body>got " + url + "</body></html>");
+        } catch (const std::exception & exc) {
+            std::cout << "http_get: " << exc.what() << std::endl;
+        }
+    });
+}
+
+std::future<std::string> callback2future(const std::string & url)
+{
+    auto shared_promise = std::make_shared<std::promise<std::string>>();
+    auto result = shared_promise->get_future();
+    http_get(url, [=](const std::string & result) {
+        shared_promise->set_value(result);
+    });
+    return result;
+}
+
+
+void callback2future2callback(const std::string & url, const Callback & cb)
+{
+    threads.emplace_back([=]() {
+        try {
+            cb(callback2future(url).get());
+        } catch (const std::exception & exc) {
+            std::cout << "callback2future2callback: " << exc.what() << std::endl;
+        }
+    });
+}
+
+
+int main()
+{
+//    callback2future2callback("c", [](const std::string & result) {
+//        std::cout << result << std::endl;
+//    });
+
+    http_get("a", [](const std::string & result) { std::cout << result << std::endl; });
+    http_get("b", [](const std::string & result) { std::cout << result << std::endl; });
+    callback2future2callback("c", [](const std::string & result) { std::cout << result << std::endl; });
+    
+    for (auto & t : threads)
+    {
+        t.join();
+    }
+}
+

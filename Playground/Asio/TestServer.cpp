3fc219b49a09e04dc60a13e2b98dcc14bb1c4c36
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-04-11 11:07:12 +0000

    Restore previous code.

--- Playground/Asio/TestServer.cpp
+++ Playground/Asio/TestServer.cpp
@@ -3,7 +3,5 @@
 
 int main()
 {
-    MessageProtocol::MessageServer server(9999, [](const MessageProtocol::MessageSession & session, const std::string & req) {
-        return std::to_string(reinterpret_cast<long>(&session)) + " " + req;
-    });
+    MessageProtocol::MessageServer server(9999);
 }

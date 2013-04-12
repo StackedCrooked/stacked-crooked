7ff25f4d4e991be3f43ebfeb4bcee9b4ac87faea
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2013-04-12 08:46:18 +0000

    Use variables.

--- trunk/Playground/AnimeRatings/GetMALScore.sh
+++ trunk/Playground/AnimeRatings/GetMALScore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Requesting score for $1"
 URL=http://myanimelist.net/api/anime/search.xml
-wget --output-document=result.xml --user=$USER --password=$PASSWORD "$URL?q=$1" 2>/dev/null
+wget --output-document=result.xml --user=$USER --password=$PASSWORD "$URL?q=$1" 
 cat result.xml | grep \<title\> | sed 's/^.*\<title\>//' | sed 's/\<\/title\>//'
 cat result.xml | grep \<score\> | sed 's/^.*\<score\>//' | sed 's/\<\/score\>//'
--- trunk/Playground/AnimeRatings/SearchMAL.sh
+++ trunk/Playground/AnimeRatings/SearchMAL.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
-wget --output-document=result.xml --user=AnimeRatings --password=Chrome $@ 1>/dev/null 2>/dev/null
-cat result.xml
-rm result.xml
+#!/bin/bash
+USERFILE="$HOME/.mal/user"
+PASSWORDFILE="$HOME/.mal/password"
+URL=http://myanimelist.net/api/anime/search.xml
+[ -f $USERFILE ] || { echo "$USERFILE not found. Exiting" 1>&2 ; exit ; }
+[ -f $PASSWORDFILE ] || { echo "$PASSWORDFILE not found. Exiting" 1>&2 ; exit ; }
+USER=$(cat $USERFILE)
+PASSWORD=$(cat $PASSWORDFILE)
+wget -nv --user=$USER --password=$PASSWORD -O - "$URL?q=$1" 

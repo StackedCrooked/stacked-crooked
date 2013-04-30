2ac38d6496932108b385cada95a2a97fbff272c9
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-04-30 19:55:20 +0000

    Update checkout script.

--- Clang/checkout-clang.sh
+++ Clang/checkout-clang.sh
@@ -1,9 +1,4 @@
-svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
-
-cd llvm/tools
-svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
-cd ../..
-
-cd llvm/projects
-svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
-cd ../..
+(svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm)
+(cd llvm/tools && svn co http://llvm.org/svn/llvm-project/cfe/trunk clang)
+(cd llvm/tools/clang/tools && svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra)
+(cd llvm/projects && svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt)

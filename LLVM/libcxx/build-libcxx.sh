fae084d45a542caf3b56cc46604abed0ccc3a52d
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-05-01 23:41:26 +0000

    Update build script for linux.

--- LLVM/libcxx/build-libcxx.sh
+++ LLVM/libcxx/build-libcxx.sh
@@ -1,3 +1,19 @@
-cd libcxx/lib
-export TRIPLE=-apple-
-./buildit
+#!/bin/bash
+if [ "$(uname)" == "Darwin" ] ; then
+    cd libcxx/lib
+    export TRIPLE=-apple-
+    ./buildit
+    exit
+fi
+
+
+# Linx build
+mkdir -p build && cd build
+export CC=clang
+export CXX=clang++
+cmake -G "Unix Makefiles" \
+    -DLIBCXX_CXX_ABI=libsupc++ \
+    -DLIBCXX_LIBSUPCXX_INCLUDE_PATHS="/usr/include/c++/4.8/;/usr/include/x86_64-linux-gnu/c++/4.8" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    ../libcxx

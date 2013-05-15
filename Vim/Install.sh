5e9bab23a711267a69844989a905c3347c2e4d73
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-05-15 18:46:54 +0000

    udpate vimrc

--- Vim/Install.sh
+++ Vim/Install.sh
@@ -1,6 +1,15 @@
+set -x
+set -e
 ln -fs `pwd`/_vimrc $HOME/.vimrc
 ln -fs `pwd`/_gvimrc $HOME/.gvimrc
 if [  `uname` == "Darwin" ]; then
     echo "Link ~/bin/gvim to MacVim"
     ln -fs `pwd`/MacVim $HOME/bin/gvim
 fi
+
+apt-get install git || port install git || exit 
+[ ! -d ~/.vim/bundle/vundle ] || { echo "Vundle is already installed. Ok." ; exit ; }
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/gmarik/vundle ~/.vim/bundle/vundle
+vim +BundleInstall +qall

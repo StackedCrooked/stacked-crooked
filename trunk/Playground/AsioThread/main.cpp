48e83c60d6a3237161cd2a8a5393847352ebf47c
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2013-07-06 21:22:31 +0000

    Waiting for schedule.

--- trunk/Playground/AsioThread/main.cpp
+++ trunk/Playground/AsioThread/main.cpp
@@ -1,4 +1,4 @@
-#include <boost/asio.hpp>
+#include "tbb/concurrent_queue.h"
 #include <future>
 #include <iostream>
 #include <unistd.h>
@@ -28,40 +28,71 @@ void set_promise(std::promise<void>& p, F f)
 
 struct Stack
 {
-    Stack()
+    Stack() :
+        mQuit(std::make_shared<std::atomic<bool>>(false)),
+        mThread([=]{
+            try {
+                for (;;) {
+                    std::function<void()> f;
+                    this->q.pop(f);
+                    f();
+                }
+            } catch (int) {
+                std::cout << "Stopping the thread!" << std::endl;
+            }
+        })
     {
-        std::thread([&]{ for(;;) this->ios.poll_one(); }).detach();
     }
     
     ~Stack()
     {
-        ios.stop();
+        *mQuit = true;
+        q.push([]{throw 1;});
+        mThread.join();
     }
     
     Stack(const Stack&) = delete;
     Stack& operator=(const Stack&) = delete;
-    
+
     template<typename F>
     auto dispatch(F f) -> std::future<decltype(f())>
     {
         auto p = make_shared_promise(f);
-        this->ios.dispatch([=]{
+        q.push([=]{
             set_promise(*p, f);
         });
         return p->get_future();
     }
+
+    template<typename F>
+    auto schedule(F f, unsigned ms) -> std::future<decltype(f())>
+    {
+        // start thread
+        auto p = make_shared_promise(f);
+        std::thread([=]{
+            usleep(1000 * ms);
+            if (*mQuit) return;
+            set_promise(*p, [=]{ return dispatch(f).get(); });
+        }).detach();
+        return p->get_future();
+    }
     
-    boost::asio::io_service ios;
+    std::shared_ptr<std::atomic<bool>> mQuit;
+    tbb::concurrent_bounded_queue<std::function<void()>> q;
+    std::thread mThread;
 };
 
 
 int main()
 {
-    std::thread([]{ sleep(1); std::cout << "TIMEOUT!!" << std::endl;throw 1; }).detach();
+    std::thread([]{ sleep(5); std::cout << "Timeout!" << std::endl; std::abort(); }).detach();
     Stack s;
     s.dispatch([]{});
-    
-    std::future<int> future_n = s.dispatch([] { return 3; });
-    std::cout << future_n.get() << std::endl;
+
+    std::cout << s.dispatch([] { return "Hello dispatch!"; }).get() << std::endl;
+
+    s.schedule([] { std::cout << "1ms" << std::endl; }, 1);
+    s.schedule([] { std::cout << "2ms" << std::endl; }, 2);
+    s.schedule([] { std::cout << "3ms" << std::endl; }, 3).wait();
 }
 

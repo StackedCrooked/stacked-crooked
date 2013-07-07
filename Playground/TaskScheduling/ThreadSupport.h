addffd6ecdacf056e58bbb327881ca064734592f
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-07-07 13:58:02 +0000

    Fix.

--- Playground/TaskScheduling/ThreadSupport.h
+++ Playground/TaskScheduling/ThreadSupport.h
@@ -6,24 +6,52 @@ namespace TreadSupport {
 
 
 template<typename F>
-auto make_shared_promise(F f) -> std::shared_ptr<std::promise<decltype(f())>>
+auto MakeSharedPromise(F f) -> std::shared_ptr<std::promise<decltype(f())>>
 {
     return std::make_shared<std::promise<decltype(f())>>();
 }
 
 
 template<typename R, typename F>
-void set_promise(std::promise<R>& p, F f)
+void SetPromise(std::promise<R>& p, F f)
 {
     p.set_value(f());
 }
 
 
 template<typename F>
-void set_promise(std::promise<void>& p, F f)
+void SetPromise(std::promise<void>& p, F f)
 {
     f();
     p.set_value();
+}
+
+
+template<typename SinkFunction>
+struct Dispatcher
+{
+    Dispatcher(SinkFunction sink_function) :
+        sink_function_(sink_function)
+    {
+    }
+
+    template<typename F>
+    auto operator()(F f) const -> std::future<decltype(f())>
+    {
+        auto p = MakeSharedPromise(f);
+        sink_function_([=]{ SetPromise(*p, f); });
+        return p->get_future();
+    }
+
+    SinkFunction sink_function_;
+};
+
+
+
+template<typename SinkFunction>
+Dispatcher<SinkFunction> MakeSink(SinkFunction sink)
+{
+    return Dispatcher<SinkFunction>(sink);
 }
 
 

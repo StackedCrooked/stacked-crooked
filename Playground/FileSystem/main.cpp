7c536d0ad2e6c0b630261948a7face901ad6276e
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-07-23 20:45:35 +0000

    Add FileSystem.

---
+++ Playground/FileSystem/main.cpp
@@ -0,0 +1,99 @@
+#define BOOST_FILESYSTEM_DYN_LINK 1
+#include <deque>
+#include <future>
+#include <iostream>
+#include <set>
+#include <string>
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem.hpp>
+#include "tbb/concurrent_queue.h"
+
+using boost::filesystem::is_directory;
+using boost::filesystem::is_regular_file;
+using boost::filesystem::exists;
+
+std::string get_extension(const std::string& file)
+{
+    return file.substr(file.rfind('.') + 1);
+}
+
+
+struct Importer
+{
+    Importer(const std::string& dir) :
+        fut(std::async(std::launch::async, [=]{ 
+            std::cout << __LINE__ << std::endl;
+            this->import(dir);
+        }))
+    {   
+        std::cout << __LINE__ << std::endl;
+    }
+    
+    std::string pop()
+    {
+        std::string file;
+        queue.pop(file);
+        return file;
+    }
+    
+private:
+    void import_dir(const std::string& dir)
+    {
+        using namespace boost::filesystem;
+        for (auto it = directory_iterator(dir), end = directory_iterator(); it != end; ++it)
+        {
+            auto entry = *it;
+            if (is_directory(entry))
+            {
+                import_dir(entry.path().string());
+            }
+            else if (is_regular_file(entry))
+            {
+                import_file(entry.path().string());
+            }
+            else
+            {
+                std::cerr << "Not a file nor a dir: " << entry << std::endl;
+            }
+        }
+    }
+    
+    void import_file(const std::string& file)
+    {
+        // check extension
+        // if image then push to queue
+        if (is_supported(file))
+        {
+            queue.push(file);
+        }
+    }
+    
+    bool is_supported(std::string file)
+    {
+        static const std::set<std::string> fExtensions = { "jpg", "png" };
+        boost::to_lower(file);
+        return fExtensions.count(get_extension(file));
+    }
+    
+    void import(std::string dir)
+    {
+        import_dir(dir);
+    }
+    
+    
+    std::deque<std::string> imported;
+    std::future<void> fut;
+    tbb::concurrent_bounded_queue<std::string> queue;
+};
+
+int main()
+{
+    std::cout << __LINE__ << std::endl;
+    Importer importer(".");
+    for (int i = 0; i != 10; ++i)
+    {
+        std::cout << __LINE__ << std::endl;
+        std::cout << importer.pop() << std::endl;
+    }
+    std::cout << __LINE__ << std::endl;
+}

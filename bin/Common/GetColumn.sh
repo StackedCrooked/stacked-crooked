30f5c01e5f87040bc5b8f9504009f0bdbf6594e9
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-08-13 21:28:19 +0000

    Cleanup.

--- bin/Common/GetColumn.sh
+++ bin/Common/GetColumn.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-set -x
 [ "$#" == "1" ] || {
     echo "Usage $(basename $0) ColumnIndex" 1>&2
     echo "Example: ps -eouid,comm | grep 123 | $(basename $0) 2" 1>&2
@@ -8,5 +7,4 @@ set -x
 
 COLUMN="$1"
 COMMAND="awk '{print \$$1}'"
-echo "COMMAND: ${COMMAND}"
 eval "$COMMAND"

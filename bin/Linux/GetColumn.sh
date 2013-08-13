d332252d33572ce913eef28d31202e7c4552fc76
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-08-13 20:58:05 +0000

    Add Linux bin.

---
+++ bin/Linux/GetColumn.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -x
+[ "$#" == "1" ] || {
+    echo "Usage $(basename $0) ColumnIndex" 1>&2
+    echo "Example: ps -eouid,comm | grep 123 | $(basename $0) 2" 1>&2
+    exit 1
+}
+
+COLUMN="$1"
+COMMAND="awk '{print \$$1}'"
+echo "COMMAND: ${COMMAND}"
+eval "$COMMAND"

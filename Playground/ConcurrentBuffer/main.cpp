f813db9687bc2a4c9bf771f3672bb3f608f5bb41
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-08-24 11:10:45 +0000

    Some changes but still bugful.

--- Playground/ConcurrentBuffer/main.cpp
+++ Playground/ConcurrentBuffer/main.cpp
@@ -1,11 +1,69 @@
 #include <boost/circular_buffer.hpp>
 #include <atomic>
+#include <iomanip>
 #include <iostream>
 #include <mutex>
 #include <sstream>
 #include <thread>
 #include <vector>
 #include <stdint.h>
+
+
+std::vector<uint8_t> generateTestData()
+{
+    std::vector<uint8_t> data;
+    data.reserve(1000);
+    while (data.size() != data.capacity())
+    {
+        data.push_back(data.size());
+    }
+    return data;
+}
+
+
+void print(std::string str)
+{
+    static std::mutex fMutex;
+    std::lock_guard<std::mutex> lock(fMutex);
+    std::cout << str << '\n';
+}
+
+
+inline std::ostream& operator<<(std::ostream& os, const std::vector<uint8_t>& vec)
+{
+    for (auto& el : vec)
+    {
+        os << std::setw(2) << std::setfill('0') << static_cast<int>(el) << ' ';
+    }
+    return os;
+}
+
+
+class MakeString
+{
+public:
+    template <typename T>
+    MakeString & operator<<(const T & datum)
+    {
+        mBuffer << datum;
+        return *this;
+    }
+
+    MakeString & operator<<(uint8_t n)
+    {
+        mBuffer << std::setw(2) << std::setfill('0') << static_cast<int>(n) << ' ';
+        return *this;
+    }
+
+    operator std::string() const
+    { return mBuffer.str(); }
+
+    std::string str() const
+    { return mBuffer.str(); }
+
+private:
+    std::ostringstream mBuffer;
+};
 
 
 struct ConcurrentBuffer
@@ -29,8 +87,10 @@ struct ConcurrentBuffer
 
     std::size_t read(uint8_t* buf, std::size_t len)
     {
-        std::size_t numRead = mNumRead;
+        // first make a copy of the number of written bytes and use it as
+        // an upper bound for the amount of data that we can read.
         std::size_t numWritten = mNumWritten;
+        std::size_t numRead = mNumRead;
 
         assert(numWritten >= numRead);
 
@@ -40,26 +100,40 @@ struct ConcurrentBuffer
             return 0;
         }
 
-        auto begin = mBuffer.begin() + mNumRead;
         mBuffer.resize(mBuffer.size() + numUnread);
+        auto begin = mBuffer.begin() + mNumRead;
 
-        memcpy(buf, &*begin, numUnread);
+        for (auto i = 0UL; i != numUnread; ++i)
+        {
+            *buf++ = *begin++;
+        }
         mNumRead += numUnread;
         return numUnread;
     }
 
     std::size_t write(uint8_t* buf, std::size_t len)
     {
-        std::size_t numRead = mNumRead;
         std::size_t numWritten = mNumWritten;
+        std::size_t numRead = mNumRead;
 
         assert(numWritten <= numRead);
-        auto availableSpace = std::min(len, numWritten - numRead);
 
+        auto availableSpace = std::min(len, mBuffer.size() - (numWritten - numRead));
+        if (availableSpace == 0)
+        {
+            return 0;
+        }
 
-        auto begin = mBuffer.begin() + mNumWritten;
         mBuffer.resize(mBuffer.size() + availableSpace);
-        memcpy(buf, &*begin, availableSpace);
+        auto begin = mBuffer.begin() + mNumWritten;
+        auto end = begin + availableSpace;
+        print(MakeString() << "Writing: " << std::vector<uint8_t>(begin, end));
+
+        for (auto it = begin; it != end; ++it)
+        {
+            print(MakeString() << "Writing byte: " << int(*buf));
+            *it++ = *buf++;
+        }
 
         mNumWritten = availableSpace;
         return availableSpace;
@@ -72,68 +146,50 @@ struct ConcurrentBuffer
 };
 
 
-std::vector<uint8_t> generateTestData()
-{
-    std::vector<uint8_t> data;
-    data.reserve(500);
-    while (data.size() != data.capacity())
-    {
-        data.push_back(data.size());
-    }
-    return data;
-}
-
-
-class MakeString
-{
-public:
-    template <typename T>
-    MakeString & operator<<(const T & datum)
-    {
-        mBuffer << datum;
-        return *this;
-    }
-
-    operator std::string() const
-    { return mBuffer.str(); }
-
-    std::string str() const
-    { return mBuffer.str(); }
-
-private:
-    std::ostringstream mBuffer;
-};
-
-
-void print(std::string str)
-{
-    static std::mutex fMutex;
-    std::lock_guard<std::mutex> lock(fMutex);
-    std::cout << str << std::endl;
-}
-
-
 int main()
 {
+    print("Creating the ConcurrentBuffer");
     ConcurrentBuffer buf(100);
 
+    print("Generate the test data");
     auto testData = generateTestData();
-    auto numWritten = 0UL;
 
+    print("Start the reader thread");
     std::thread([&] {
-        std::vector<uint8_t> receiveData;
-        receiveData.reserve(100);
+        print("Inside the reader thread");
+        std::vector<uint8_t> receiveData(testData.size());
         auto numReceived = 0UL;
-        while (numReceived != buf.size()) {
+        while (numReceived != testData.size()) {
             if (!buf.empty()) {
-                numReceived += buf.read(&receiveData[0], receiveData.size() - numReceived);
-                print(MakeString() << "numReceived=" << numReceived);
+                print("READER: buffer is empty vOv");
+                continue;
             }
+            numReceived += buf.read(
+                &receiveData[0] + numReceived,
+                receiveData.size() - numReceived);
+            print(MakeString() << "READER: numReceived=" << numReceived);
         }
+        print(MakeString() << "testData=" << testData);
+        print(MakeString() << "receiveData=" << receiveData);
+        assert(testData == receiveData);
+
     }).detach();
 
+    print("Start writing in the main thread.");
+    auto numWritten = 0UL;
     while (numWritten != testData.size()) {
-        numWritten += buf.write(testData.data(), testData.size());
-        print(MakeString() << "numWritten=" << numWritten);
+        if (auto n = buf.write(
+                testData.data() + numWritten,
+                testData.size() - numWritten)) {
+
+            print(MakeString() << "n=" << n << " numWritten" << numWritten << " size=" << testData.size());
+            if (numWritten == testData.size()) {
+                print("write buffer is complete.");
+                break;
+            }
+        } else {
+            //print(MakeString() << "writer: write() returned 0, numWritten=" << numWritten);
+            //usleep(1000);
+        }
     }
 }

b65aa0ac317054988fab9d3ea58a015ccc64d412
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-08-24 02:10:09 +0000

    Add usage note to RChop.sh.

--- bin/RChop.sh
+++ bin/RChop.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+[[ "$#" == "1" ]] || {
+    echo "Usage: $(basename $0) num" 1>&2
+    exit 1
+}
+
 while read line ; do
     line_length="${#line}"
     offset="$(($line_length - ${1}))"

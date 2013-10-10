d14c8f8867b89a2a04eb235aa02f57d9a43016b6
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-10-10 21:21:00 +0000

    Add StrongTypedefAgain.

---
+++ Playground/StrongTypedefAgain/main.cpp
@@ -0,0 +1,219 @@
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <type_traits>
+
+
+namespace Detail {
+
+
+template<typename El, typename Tuple, int Index, int Size>
+struct IndexOf_
+{
+    static constexpr int value =
+        std::is_same<typename std::tuple_element<Index, Tuple>::type, El>::value
+            ? Index
+            : IndexOf_<El, Tuple, Index + 1, Size>::value;
+};
+
+
+template<typename El, typename Tuple, int Index>
+struct IndexOf_<El, Tuple, Index, Index>
+{
+    static constexpr int value = -1;
+};
+
+
+} // namespace Detail
+
+
+template<typename T, typename ...TupleTypes>
+struct IndexOf;
+
+
+template<typename T, typename ...TupleTypes>
+struct IndexOf<T, std::tuple<TupleTypes...> >
+{
+private:
+    typedef std::tuple<TupleTypes...> tuple_type;
+    static constexpr int tuple_size = std::tuple_size<tuple_type>::value;
+
+public:
+    static constexpr int value = Detail::IndexOf_<T, tuple_type, 0, tuple_size>::value;
+};
+
+
+template<typename T, typename ...Args>
+struct IsAnyOf;
+
+
+template<typename T, typename argument_type>
+struct IsAnyOf<T, argument_type>
+{
+    static constexpr bool value = std::is_same<T, argument_type>::value;
+};
+
+
+template<typename T, typename Head, typename ...Tail>
+struct IsAnyOf<T, Head, Tail...>
+{
+    static constexpr bool value = std::is_same<T, Head>::value || IsAnyOf<T, Tail...>::value;
+};
+
+
+template<typename Head, typename ...Tail>
+struct HasDuplicates;
+
+
+template<typename Head>
+struct HasDuplicates<Head>
+{
+    static constexpr bool value = false;
+};
+
+
+template<typename Head, typename Tail>
+struct HasDuplicates<Head, Tail>
+{
+    static constexpr bool value = std::is_same<Head, Tail>::value;
+};
+
+
+template<typename Head, typename Next, typename ...Tail>
+struct HasDuplicates<Head, Next, Tail...>
+{
+    static constexpr bool value =
+        std::is_same<Head, Next>::value ||
+        IsAnyOf<Head, Tail...>::value ||
+        IsAnyOf<Next, Tail...>::value;
+};
+
+
+
+// A "record" is a tuple where are elements are of different type.
+// This enables access by type instead of index.
+template<typename ...Args>
+auto MakeRecord(Args && ...args) -> std::tuple<Args...>
+{
+    static_assert(!HasDuplicates<Args...>::value, "The argument list must not contain two objects of same type.");
+    return std::make_tuple(std::forward<Args>(args)...);
+}
+
+
+template<typename T, typename ...U>
+T & Get(std::tuple<U...> & record)
+{
+    static_assert(!HasDuplicates<U...>::value, "The argument list must not contain two objects of same type.");
+    static constexpr unsigned index = IndexOf<T, std::tuple<U...>>::value;
+    static_assert(index < (sizeof...(U)), "Out of bounds");
+    return std::get< index >(record);
+}
+
+
+template<typename T, typename ...U>
+const T & Get(const std::tuple<U...> & record)
+{
+    static_assert(!HasDuplicates<U...>::value, "The argument list must not contain two objects of same type.");
+    static constexpr unsigned index = IndexOf<T, std::tuple<U...>>::value;
+    static_assert(index < (sizeof...(U)), "Out of bounds");
+    return std::get< index >(record);
+}
+
+
+
+
+
+template<typename T, typename Tag>
+struct StrongTypedef
+{
+    template<typename ...Args>
+    explicit StrongTypedef(Args&& ...args) :
+        t(std::forward<Args>(args)...)
+    {
+    }
+
+    explicit operator const T & () const
+    {
+        return t;
+    }
+    explicit operator T & ()
+    {
+        return t;
+    }
+
+    const T & get() const
+    {
+        return t;
+    }
+    T & get()
+    {
+        return t;
+    }
+
+    friend std::ostream & operator<<(std::ostream & os, const StrongTypedef<T, Tag> & obj)
+    {
+        return os << obj.t;
+    }
+
+    T t;
+};
+
+
+
+
+template<typename ...Args>
+struct Handy : std::tuple<Args...>
+{
+    template<typename ...A>
+    Handy(A&& ...a) : std::tuple<Args...>(std::forward<A>(a)...)
+    {
+    }
+
+    template<typename El>
+    El & get()
+    {
+        return Get<El>(*this);
+    }
+
+    template<typename El>
+    const El & get() const
+    {
+        return Get<El>(*this);
+    }
+
+    template<typename T>
+    const T & get(T) const
+    {
+        return get<T>();
+    }
+
+    template<typename T>
+    T & get(T)
+    {
+        return get<T>();
+    }
+};
+
+
+int main()
+{
+    using ServerId = StrongTypedef<std::string, struct ServerId_>;
+    using InterfaceId = StrongTypedef<std::string, struct InterfaceId_>;
+    using PortId = StrongTypedef<std::string, struct PortId_>;
+
+    Handy<ServerId, InterfaceId, PortId> handy(ServerId("s1"), InterfaceId("i1"), PortId("p1"));
+    handy.get<ServerId>() = ServerId{"s2"};
+    handy.get<PortId>() = PortId {"p2"};
+
+    auto record = MakeRecord(ServerId("s1"), PortId("p2"));
+    std::cout << Get<ServerId>(record) << std::endl;
+
+    std::cout << Get<ServerId>(handy) << std::endl;
+    std::cout << handy.get<PortId>() << std::endl;
+
+    std::cout << handy.get(PortId {}) << std::endl;
+
+    handy.get(PortId()) = PortId("Port2");
+    std::cout << handy.get(PortId {}) << std::endl;
+}
+

155d8af2b92630e8049784d689eaf9bfee2b299d
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-11-06 18:05:49 +0000

    Add memcopy_benchmark.

---
+++ Playground/memcpy_benchmark/main.cpp
@@ -0,0 +1,31 @@
+#include <string.h>
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+
+using namespace std::chrono;
+using Clock = std::chrono::high_resolution_clock;
+
+
+
+int main()
+{
+    long size = 10000;
+    long iterations = 10000;
+    
+    auto src = malloc(size);
+    auto dst = malloc(size);
+
+    memset(dst, 0, size);
+
+    auto start = Clock::now();
+    for (long i = 0; i != iterations; ++i)
+    {
+        memcpy(dst, src, size);
+    }    
+
+    auto megabyte_per_second = iterations * size / duration_cast<microseconds>(Clock::now() - start).count();
+
+    std::cout << megabyte_per_second << "MB/s" << std::endl;
+}

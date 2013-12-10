da229dcd160cdff06442742997b50f957bf90e8a
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2013-12-10 22:55:40 +0000

    Fix.

--- Playground/memcpy_benchmark/main.cpp
+++ Playground/memcpy_benchmark/main.cpp
@@ -9,22 +9,42 @@ using namespace std::chrono;
 using Clock = std::chrono::high_resolution_clock;
 
 
+long size = 1500;
+long iterations = 1000 * 1000;
+
+
+
+
+
+void* get(long size)
+{
+	static std::vector<void*> buf;
+	if (buf.size() < 1000)
+	{
+		buf.push_back(malloc(size));
+		return buf.back();
+	}
+	static unsigned i = 0;
+	return buf[i++ % buf.size()];
+}
+
+
+void* dst(long size) { return get(size); }
+void* src(long size) { return get(size); }
+
+
+
+
+
 
 int main()
 {
-    long size = 1500;
-    long iterations = 1000 * 1000;
     
-    auto src = malloc(size);
-    memset(src, 0, size);
-
-    auto dst = malloc(size);
-
     auto start = Clock::now();
 
     for (long i = 0; i != iterations; ++i)
     {
-        memcpy(dst, src, size);
+		memcpy(dst(size), src(size), size);
     }
 
     auto us = duration_cast<microseconds>(Clock::now() - start).count();

a1493616c6bdbe4d5eedb75c9e7ac2828fd35cd8
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2014-01-01 06:23:40 +0000

    Add new key if not yet exists.

--- bin/IncrementField.sh
+++ bin/IncrementField.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 [ "$#" == "2" ] || {
     echo "Usage: $(basename $0) key_name file_name" 1>&2
     echo "File must contains key-value pairs using the format: 'key=value\n'. Each line must contain only one key=value statement."
@@ -8,5 +9,10 @@
 
 key_name="$1"
 file_name="$2"
+grep $key_name $file_name >/dev/null 2>&1 || {
+    echo "${key_name}=1" >> $file_name 
+    echo "Added new key: ${key_name}=1" 1>&2
+    exit
+}
 cmd="perl -pe 's,${key_name}=(\d+),\$n=\$1+1; \"${key_name}=\$n\",e'  -i $file_name"
 eval "$cmd"

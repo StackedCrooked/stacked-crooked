8bc3b86b1809c7a5591d2b8d5a8a76cb042df096
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2014-02-14 02:41:50 +0000

    Comment.

--- bin/Detox.sh
+++ bin/Detox.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Removes [bla] and (bla) tags from input lines.  
+
 while read line ; do 
     echo "$line" | perl -pe 's,(.*?)\[.*?\](.*),\1 \2,g; s,_, ,g ; s,(.*?)\(.*?\)(.*),\1 \2,g ; s,\s+(.*)\s+,\1,g' 
 done

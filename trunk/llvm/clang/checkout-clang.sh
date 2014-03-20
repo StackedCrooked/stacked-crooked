468dd5f61b47145b6280608df428e2b68e45275f
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2014-03-20 03:11:45 +0000

    Use release 34.

--- trunk/llvm/clang/checkout-clang.sh
+++ trunk/llvm/clang/checkout-clang.sh
@@ -1,4 +1,5 @@
-(svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm)
-(cd llvm/tools && svn co http://llvm.org/svn/llvm-project/cfe/trunk clang)
-(cd llvm/tools/clang/tools && svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra)
-(cd llvm/projects && svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt)
+set -e
+(svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_34 llvm)
+(cd llvm/tools && svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_34 clang)
+(cd llvm/tools/clang/tools && svn co http://llvm.org/svn/llvm-project/clang-tools-extra/tags/RELEASE_34 extra)
+(cd llvm/projects && svn co http://llvm.org/svn/llvm-project/compiler-rt/tags/RELEASE_34 compiler-rt)

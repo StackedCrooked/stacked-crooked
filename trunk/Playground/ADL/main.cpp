5459285d09539ca2765184d9e9fb553a52bc4c6c
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2014-05-08 18:39:42 +0000

    Add make.sh executable.

--- trunk/Playground/ADL/main.cpp
+++ trunk/Playground/ADL/main.cpp
@@ -1,4 +1,81 @@
+#include <string>
+
+struct Protocol
+{
+    typedef void(*PopFunction)(void*, const std::string&);
+
+    Protocol(PopFunction inPopFunction) :
+        mPopFunction(inPopFunction)
+    {
+
+    }
+
+    void pop(const std::string& str)
+    {
+        mPopFunction(*this, str);
+    }
+
+    typedef void(*PopFunction)(void*, const std::string&);
+};
+
+
+template<typename CLP>
+struct BasicProtocol : Protocol
+{
+    BasicProtocol() : Protocol(&BasicProtocol<CLP>::static_pop) {}
+
+    void pop(const std::string& s)
+    {
+        DecoderType dec(s);
+        static_cast<CLP&>(*this).do_pop(dec);
+    }
+
+private:
+
+    static void static_pop(Protocol& p, const std::string& s)
+    {
+        static_cast<this_type&>(p).pop(s):
+    }
+};
+
+
+template<typename CLP, typename CLS, typename SessionKey>
+struct BasicSessionManager
+{
+    template<typename DecoderType>
+    void do_pop(const DecoderType& dec)
+    {
+        this->sessionPop(dec);
+    }
+
+    template<typename DecoderType>
+    void sessionPop(const DecoderType& dec)
+    {
+        auto key = GetSessionKey(dec);
+
+        auto it = mSessions.find(key);
+        if (it != mSessions.end())
+        {
+            it->second->pop(GetPayload(dec));
+            return;
+        }
+
+        auto cls = static_cast<CLP&>(*this).createSession(dec);
+        mSessions.insert(std::make_pair(key, cls));
+        cls->pop(GetPayload(dec));
+    }
+
+private:
+    std::map<SessionKey, std::shared_ptr<CLS>> mSessions;
+};
+
+
+struct IPv4Protocol : BasicProtocol<IPv4Protocol>, BasicSessionManager<IPv4Protocol>
+{
+};
+
 
 int main()
 {
+    IPv4Protocol theIPv4Protocol;
 }

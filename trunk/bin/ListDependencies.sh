472970811495d835d2283c1566479812e9203088
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2014-05-09 22:38:07 +0000

    Also find if in bundle.

--- trunk/bin/ListDependencies.sh
+++ trunk/bin/ListDependencies.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+[ "$#" == "1" ] || {
+    echo "Usage: $(basename $0) file" 1>&2 
+    exit 1
+}
+
 [ "$(uname)" == "Darwin" ] || {
     echo "$(basename $0) only works on Darwin." 1>&2
     exit 1
 }
 
-otool -L $1 | tail -n +2 | perl -pe 's,\s*(\S+).*,\1,g' | grep -v '^@'
+file="$1"
+
+[ "$(echo "$file"| grep '\.app$')" ] && {
+    file="$(find $file -name $(basename $file | sed -e 's,.app,,'))"
+}
+
+
+otool -L "$file" | tail -n +2 | perl -pe 's,\s*(\S+).*,\1,g' | grep -v '^@'

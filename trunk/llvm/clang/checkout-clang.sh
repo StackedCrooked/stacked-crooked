5c9dedff3a376bbfd01316449303ed3aed3777a1
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2014-08-27 19:08:30 +0000

    Change update

--- trunk/llvm/clang/checkout-clang.sh
+++ trunk/llvm/clang/checkout-clang.sh
@@ -1,5 +1,6 @@
 set -e
-(svn co http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_34/final llvm)
-(cd llvm/tools && svn co http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_34/final clang)
-(cd llvm/tools/clang/tools && svn co http://llvm.org/svn/llvm-project/clang-tools-extra/tags/RELEASE_34/final extra)
-(cd llvm/projects && svn co http://llvm.org/svn/llvm-project/compiler-rt/tags/RELEASE_34/final compiler-rt)
+svn_tag=RELEASE_342
+(svn co -q http://llvm.org/svn/llvm-project/llvm/tags/${svn_tag}/final llvm)
+(cd llvm/tools && svn co -q http://llvm.org/svn/llvm-project/cfe/tags/${svn_tag}/final clang)
+(cd llvm/tools/clang/tools && svn co -q http://llvm.org/svn/llvm-project/clang-tools-extra/tags/${svn_tag}/final extra)
+(cd llvm/projects && svn co -q http://llvm.org/svn/llvm-project/compiler-rt/tags/${svn_tag}/final compiler-rt)
--- trunk/llvm/clang/rebuild-clang.sh
+++ trunk/llvm/clang/rebuild-clang.sh
@@ -1,1 +1,3 @@
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
 rm -rf build && mkdir -p build && cd build && ../llvm/configure --enable-optimized=yes --enable-assertions=no && make "$@"

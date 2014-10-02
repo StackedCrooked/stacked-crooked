8b72b0a0e1fbb25e08c1dc7207e0cccf1b601a10
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2014-10-02 21:39:38 +0000

    Dirteir.

--- Playground/FunctionWrapper/main.cpp
+++ Playground/FunctionWrapper/main.cpp
@@ -1,63 +1,69 @@
-#include <boost/bind.hpp>
+﻿#include <boost/bind.hpp>
+#include <boost/lexical_cast.hpp>
+#include <atomic>
+#include <deque>
+#include <vector>
 #include <cassert>
 #include <cstddef>
 #include <functional>
 #include <iostream>
+#include "tbb/concurrent_queue.h"
 #include <new>
 #include <utility>
 #include <array>
 
 
+#include <cxxabi.h>
+
+
+std::string Demangle(char const * name)
+{
+    int st;
+    char * const p = abi::__cxa_demangle(name, 0, 0, &st);
+    switch (st)
+    {
+        case 0: { return std::unique_ptr<char, decltype(&std::free)>(p, &std::free).get(); }
+        case -1: throw std::runtime_error("A memory allocation failure occurred.");
+        case -2: throw std::runtime_error("Not a valid name under the GCC C++ ABI mangling rules.");
+        case -3: throw std::runtime_error("One of the arguments is invalid.");
+        default: throw std::runtime_error("unexpected demangle status");
+    }
+}
+
 template<typename Signature>
 struct Function;
 
 
+template<int> struct Size;
+
 template<typename R, typename ...Args>
 struct Function<R(Args...)>
 {
-    Function() : storage()
-    {}
+    typedef Function<R(Args...)> this_type;
+
+    Function() = delete;
 
     template<typename F>
-    Function(F f)
+    Function(const F& f) noexcept
     {
-        static_assert(alignof(F) <= alignof(Function), "");
-        static_assert(sizeof(f) <= sizeof(storage), "");
+        static_assert(!std::is_same<F, this_type>::value, "");
+        static_assert(sizeof(Impl<F>) <= sizeof(storage), "");
 
-        new (storage.data()) Impl<F>(std::move(f));
+        static_assert(!std::is_reference<F>::value, "");
+        static_assert(alignof(F) <= alignof(this_type), "");
+
+        new (&storage) Impl<F>(f);
     }
 
-    Function(const Function& rhs) :
-        storage()
-    {
-        if (rhs.valid())
-        {
-            rhs.base().copy_to(data());
-        }
-    }
+    Function(Function&& rhs) = delete;
+    Function(const Function& rhs)  = delete;
 
-    Function(Function&& rhs) noexcept :
-        storage(rhs.storage)
-    {
-        if (rhs.valid())
-        {
-            rhs.move_to(*this);
-        }
-    }
-
-    Function& operator=(Function rhs) noexcept
-    {
-        unset();
-        if (rhs.valid())
-        {
-            rhs.move_to(*this);
-        }
-        return *this;
-    }
+    Function& operator=(const Function& rhs) = delete;
+    Function& operator=(Function&& rhs) = delete;
 
     ~Function()
     {
-        unset();
+        base().~Base();
     }
 
     explicit operator bool() const
@@ -67,10 +73,6 @@ struct Function<R(Args...)>
 
     R operator()(Args&& ...args) const
     {
-        if (!valid())
-        {
-            throw std::bad_function_call();
-        }
         return base().call(std::forward<Args>(args)...);
     }
 
@@ -78,37 +80,20 @@ private:
     struct Base
     {
         virtual ~Base() {}
-        virtual R call(Args&& ...args) const = 0;
-        virtual void copy_to(void* where) const = 0;
-        virtual void move_to(void* where) = 0;
+        virtual R call(const Args& ...args) const= 0;
     };
 
 
     template<typename F>
     struct Impl : Base
     {
-        Impl(F f) : f(std::move(f)) {}
+        Impl(const F& f) : f(f) {}
 
-        R call(Args&& ...args) const override final
-        { return f(std::forward<Args>(args)...); }
-
-        void copy_to(void* where) const override final
-        { new (where) Impl<F>(*this); }
-
-        void move_to(void* where) override final
-        { new (where) Impl<F>(std::move(*this)); }
+        virtual R call(const Args& ...args) const override final
+        { return f(args...); }
 
         F f;
     };
-
-    void move_to(Function& dst)
-    {
-        assert(valid());
-        dst.unset();
-
-        base().move_to(dst.data());
-        unset();
-    }
 
     void unset()
     {
@@ -124,11 +109,32 @@ private:
     bool valid() const
     { return storage != Storage(); }
 
+//    void mark_deleted()
+//    {
+//        for (auto&& i : storage)
+//        {
+//            static_assert(std::is_unsigned<Storage::value_type>::value, "");
+//            i = Storage::value_type(-1);
+//        }
+//    }
+
+//    bool is_deleted() const
+//    {
+//        for (auto&& i : storage)
+//        {
+//            if (i != Storage::value_type(-1))
+//            {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
     const void* data() const
-    { return static_cast<const void*>(storage.data()); }
+    { return static_cast<const void*>(&storage); }
 
     void* data()
-    { return static_cast<void*>(storage.data()); }
+    { return static_cast<void*>(&storage); }
 
     const Base& base() const
     { assert(valid()); return *static_cast<const Base*>(data()); }
@@ -136,85 +142,84 @@ private:
     Base& base()
     { assert(valid()); return *static_cast<Base*>(data()); }
 
-    typedef std::array<char, 48> Storage;
-    alignas(alignof(std::max_align_t)) Storage storage;
+    typedef std::array<uint64_t, 2> Storage;
+    alignas(alignof(max_align_t)) Storage storage;
 };
+
+
+
+#include "tbb/concurrent_queue.h"
+#include <thread>
+
+
+const int64_t num_iterations = 10000000;
+
+using namespace std::chrono;
+
+typedef steady_clock Clock;
+
+
+int64_t total_sum = 0;
+
+
+
+template<typename Task>
+void test_fast()
+{
+    auto now = []() -> int64_t { return duration_cast<microseconds>(Clock::now().time_since_epoch()).count(); };
+
+    int64_t sum = 0;
+    std::deque<Task> queue;
+
+    auto t1 = now();
+
+    std::string s = "asdfasdfasdfasd";
+
+    for (long i = 0; i != num_iterations; ++i)
+    {
+        s.back() = char(i);
+        queue.emplace_back([&s]{ return s.size() + total_sum; });
+    }
+
+    auto t2 = now();
+
+    for (auto&& task : queue) sum += task();
+
+    auto t3 = now();
+
+    std::cout << (t2 - t1) << "+" << (t3 - t2) << "=" << (t3 - t1) << '\t';
+    total_sum += sum;
+
+
+
+
+
+}
+
+
+void run()
+{
+    typedef Function<int()> MyFunction;
+    std::cout << "MyFunction:  ";
+    test_fast<MyFunction>();
+    test_fast<MyFunction>();
+    std::cout << std::endl;
+
+    typedef std::function<int()> StdFunction;
+    std::cout << "StdFunction: ";
+    test_fast<StdFunction>();
+    test_fast<StdFunction>();
+    std::cout << std::endl;
+
+
+}
 
 
 int main()
 {
-    Function<int(int)> increment = [](int n) {
-        return n + 1;
-    };
-    
-    Function<int(int)> decrement = [](int n) {
-        return n - 1;
-    };
-
-
-#undef assert
-    #define assert(cmd) std::cout << #cmd << std::flush << ": " << ((cmd) ? "OK" : (std::abort(), "FAIL")) << std::endl;
-
-
-    assert(increment(3) == 4);
-    assert(decrement(3) == 2);
-
-    auto a = increment;
-    auto b = decrement;
-
-    assert(a(3) == 4);
-    assert(b(3) == 2);
-
-    std::swap(a, b);
-
-    assert(a(3) == 2);
-    assert(b(3) == 4);
-
-    a = b;
-
-    assert(a(3) == 4);
-    assert(b(3) == 4);
-
-    b = a;
-
-    assert(a(3) == 4);
-    assert(b(3) == 4);
-
-
-    b = std::move(a);
-
-    assert(!a);
-    assert(b(3) == 4);
-
-    a = b;
-
-    assert(a(3) == 4);
-    assert(b(3) == 4);
-
-    b = std::move(a);
-    a = std::move(b);
-    b = a;
-
-    assert(a(3) == 4);
-    assert(b(3) == 4);
-
-    std::swap(increment, decrement);
-
-    assert(increment(3) == 2);
-    assert(decrement(3) == 4);
-
-    increment = std::move(decrement);
-    std::cout << increment(3) << std::endl;
-
-
-    // calling the moved-from decrement
-    try {
-        decrement(3);
-        assert(false);
-    } catch (std::bad_function_call& ) {
-        std::cout << "OK: got bad_function_call as exptected." << std::endl;
-    }
-
-
-
+    std::thread([]{ sleep(1); for (int i = 0; i != 10; ++i) { std::cout << '[' << i << ']'; } }).detach();
+    run();
+    run();
+    run();
+    std::cout << std::endl << " total_sum=" << total_sum << std::endl;
 }

e3a5836033d0606c4041823fd2d6bd5a35f86257
francis.rammeloo <francis.rammeloo@78535834-c32d-11dd-927d-974589228060>
2014-10-10 18:28:26 +0000

    cleanups.

--- trunk/bin/Detox.sh
+++ trunk/bin/Detox.sh
@@ -3,5 +3,5 @@
 # Removes [bla] and (bla) tags from input lines.  
 
 while read line ; do 
-    echo "$(echo "$line" | perl -pe 's,(.*?)\[.*?\](.*),\1 \2,g; s,_, ,g ; s,(.*?)\(.*?\)(.*),\1 \2,g ; s,\s+(.*)\s+,\1,g' )"
+    echo "$(echo "$line" | perl -pe 's,(.*?)\[.*?\](.*),\1 \2,g; s,&,,g; s, ,,g ; s,(.*?)\(.*?\)(.*),\1 \2,g ; s,\s+(.*)\s+,\1,g' )"
 done
--- trunk/bin/MesmerizeHere.sh
+++ trunk/bin/MesmerizeHere.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 [ "$#" == "0" ] && {
-    MESMERIZE_AUTOLOAD="$(pwd)" open -n /Applications/Mesmerize.app
+    MESMERIZE_AUTOLOAD="$(pwd)" open -n /Users/francis/programming/projects/.builds/Mesmerize/release/Mesmerize.app
     exit
 }
 
 for i in "$@" ; do
-    (cd "$i" && echo "pwd=$(pwd)" && MESMERIZE_AUTOLOAD="$(pwd)" open -n /Applications/Mesmerize.app)
+    (cd "$i" && echo "pwd=$(pwd)" && MESMERIZE_AUTOLOAD="$(pwd)" open -n /Applications/Mesmerize.app/Contents/MacOS/Mesmerize)
 done

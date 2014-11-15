3a1ba32ef5e43b6947e7bb3f5c3f0644c8148cbf
francis.rammeloo <StackedCrooked@users.noreply.github.com>
2014-11-15 11:15:39 +0000

    More improvements.

--- Playground/GeneralizedFunctor/main.cpp
+++ Playground/GeneralizedFunctor/main.cpp
@@ -10,10 +10,14 @@
 #include <array>
 
 
+#define TRACE() std::cout << __FILE__ << ":" << __LINE__ << ": " << __PRETTY_FUNCTION__ << std::endl
 
 
 template<typename T>
 using Invoke = typename T::type;
+
+template <typename Condition, typename T = void>
+using EnableIf = Invoke<std::enable_if<Condition::value, T>>;
 
 template <typename Condition, typename T = void>
 using DisableIf = Invoke<std::enable_if<!Condition::value, T>>;
@@ -25,19 +29,6 @@ template <typename T, typename U>
 struct IsRelated : std::is_same<RemoveCV<T>, RemoveCV<U>> {};
 
 
-#if 0
-template <class _Alloc>
-promise<void>::promise(allocator_arg_t, const _Alloc& __a0)
-{
-    typedef typename _Alloc::template rebind<__assoc_sub_state_alloc<_Alloc> >::other _A2;
-    typedef __allocator_destructor<_A2> _D2;
-    _A2 __a(__a0);
-    unique_ptr<__assoc_sub_state_alloc<_Alloc>, _D2> __hold(__a.allocate(1), _D2(__a, 1));
-    ::new(__hold.get()) __assoc_sub_state_alloc<_Alloc>(__a0);
-    __state_ = __hold.release();
-}
-#endif
-
 template<typename Signature>
 struct Function;
 
@@ -46,35 +37,15 @@ template<typename R, typename ...Args>
 struct Function<R(Args...)>
 {
     // this constructor accepts lambda, function pointer or functor
-    template<typename F, DisableIf<IsRelated<Function, F>>...>
+    template<typename F, typename Enabler = DisableIf<IsRelated<F, Function>>>
     Function(F&& f) :
         mImpl(new Impl<F>(std::forward<F>(f)))
     {
     }
 
     // this constructor accepts lambda, function pointer or functor
-    template<typename Alloc, typename F, DisableIf<IsRelated<Function, F>>...>
-    Function(std::allocator_arg_t, Alloc alloc, F&& f)
-    {
-        struct ImplPlus : public Impl<F>, private Alloc
-        {
-            ImplPlus(F&& f, Alloc& alloc) : Impl<F>(std::forward<F>(f)), Alloc(alloc) {}
-
-            using Impl<F>::operator ();
-
-            void destroy()
-            {
-                Impl<F>& impl = *this;
-                impl.~Impl<F>();
-
-                Alloc& alloc = *this;
-                typename Alloc::template rebind<ImplPlus>::other real_alloc(alloc);
-                real_alloc.deallocate(this, 1);
-            }
-        };
-
-        mImpl.reset(new (typename Alloc::template rebind<ImplPlus>::other(alloc).allocate(1)) ImplPlus(std::forward<F>(f), alloc), [](ImplPlus* impl) { impl->destroy(); });
-    }
+    template<typename Alloc, typename F, typename Enabler = DisableIf<IsRelated<F, Function>>>
+    Function(std::allocator_arg_t, Alloc alloc, F&& f);
 
     Function(Function&&) noexcept = default;
     Function& operator=(Function&&) noexcept = default;
@@ -98,9 +69,12 @@ struct Function<R(Args...)>
     template<typename F>
     struct Impl : ImplBase
     {
-        Impl(F&& f) : f(std::forward<F>(f)) {}
+        Impl(F&& f) : f(std::forward<F>(f)) { std::cout << "Impl::Impl()" << std::endl; }
+        ~Impl() { std::cout << "Impl::~Impl" << std::endl; }
+
         virtual R operator()(Args ...args)
         {
+            std::cout << "Impl::operator(...)" << std::endl;
             return f(args...);
         }
 
@@ -111,6 +85,26 @@ struct Function<R(Args...)>
 };
 
 
+    // this constructor accepts lambda, function pointer or functor
+    template<typename R, typename ...Args>
+    template<typename Alloc, typename F, typename>
+    Function<R(Args...)>::Function(std::allocator_arg_t, Alloc alloc, F&& f)
+    {
+        typedef typename Alloc::template rebind<Impl<F>>::other Other;
+        Other other(alloc);
+
+        struct Deallocator : private Other
+        {
+            Deallocator(Other& other) : Other(other) {}
+            void operator()(Impl<F>* impl)
+            {
+                std::cout << "Deallocator::operator(Impl<F>*)" << std::endl;
+                static_cast<Other&>(*this).deallocate(impl, 1);
+            }
+        };
+        mImpl.reset(new (other.allocate(1)) Impl<F>(std::forward<F>(f)), Deallocator{other});
+    }
+
 int main()
 {
     Function<int(int)> increment([=](int n) { return n + 1; });
@@ -119,8 +113,9 @@ int main()
     auto copy = increment;
     std::cout << copy(3) << std::endl;
 
-    Function<std::string(std::string)> append(std::allocator_arg, std::allocator<char>(), [=](std::string s) { return s + "a"; });
-    std::cout << append("abc") << std::endl;
+
+    Function<std::string(std::string)> aa(std::allocator_arg, std::allocator<char>(), [=](std::string s) { return s + "a"; });
+    auto bb = aa;
 
 
 

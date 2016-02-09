0e0d1df1cc90c84f37255d1e3c25984e719db485
StackedCrooked <francis.rammeloo@gmail.com>
2016-02-09 19:10:00 +0100

    Segment and Packet classes.

---
+++ Playground/PacketProcessing/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <array>
+
+
+struct Packet
+{
+};
+
+
+struct Segment
+{
+    enum
+    {
+        max_packets = 65536,
+        max_bytes   = max_packets * 64
+    };
+
+    std::array<uint16_t, max_packets> mPacketOffsets;
+    std::array<uint8_t, max_bytes> mPacketData;
+};
+
+struct NetworkInterface
+{
+    Segment receiveSegment()
+    {
+        return Segment();
+    }
+};
+
+
+
+
+
+int main()
+{
+
+}

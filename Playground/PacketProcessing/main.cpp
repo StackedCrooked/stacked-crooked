21003735824dedd203a93183725784c5f6065318
StackedCrooked <francis.rammeloo@gmail.com>
2016-02-13 09:35:52 +0100

    Updates.

--- Playground/PacketProcessing/main.cpp
+++ Playground/PacketProcessing/main.cpp
@@ -27,40 +27,76 @@ struct rdtsc_clock
     }
 };
 
-
 using Clock = rdtsc_clock;
+
+int64_t get_frequency_mhz()
+{
+    static auto frequency_mhz = []
+    {
+        using HRC_t = std::chrono::high_resolution_clock;
+        auto c1 = Clock::now();
+        auto t1 = HRC_t::now();
+        auto now = t1;
+        while (now < t1 + std::chrono::milliseconds(10))
+        {
+            now = HRC_t::now();
+        }
+        return 1000 * (Clock::now() - c1) / (now - t1).count();
+    }();
+    return frequency_mhz;
+}
+
+double get_frequency_ghz()
+{
+    static auto frequency_ghz = get_frequency_mhz() / 1000.0;
+    return frequency_ghz;
+}
+
+int64_t get_frequency_hz()
+{
+    static auto frequency_hz = get_frequency_mhz() * 1000000;
+    return frequency_hz;
+}
+
+
+
 
 void test_one_processor()
 {
+    std::cout << "DETECTED FREQ=" << get_frequency_mhz() << "Mhz\n";
     std::vector<Header> headers;
+    headers.reserve(10000);
 
     std::vector<Processor> processors;
+    processors.reserve(20);
 
-    IPv4Address src_ip(1, 1, 1, 1);
-    IPv4Address dst_ip(1, 1, 1, 1);
     uint16_t src_port = 1024;
     uint16_t dst_port = 1024;
 
 
-    for (auto i = 1ul; i <= 16u; ++i)
+    for (auto i = 1ul; i <= processors.capacity(); ++i)
     {
-        auto r = 0;//rand() % 2;
-        Processor proc(src_ip, dst_ip, src_port + i, dst_port + i + r);
+        IPv4Address src_ip(1, 1, 1, i);
+        IPv4Address dst_ip(1, 1, 2, i);
+        Processor proc(src_ip, dst_ip, src_port, dst_port);
         processors.push_back(proc);
     }
 
-    for (auto i = 0; i != 1000; ++i)
+    for (auto i = 1ul; i <= 10000u; ++i)
     {
-        auto r1 = 0;//rand() % 2;
-        auto r2 = 0;//rand() % 2;
-        Header  header(src_ip, dst_ip, src_port + (i % processors.size()) + r1, dst_port + (i % processors.size()) + r2);
+        IPv4Address src_ip(1, 1, 1, 1 + i % processors.size());
+        IPv4Address dst_ip(1, 1, 2, 1 + i % processors.size());
+        Header  header(src_ip, dst_ip, src_port, dst_port);
         headers.push_back(header);
     }
 
 
+    std::random_shuffle(headers.begin(), headers.end());
+
+
 
     auto total_counter = 0ul;
-    for (auto i = 0; i != 10; ++i)
+    for (auto i = 0; i != 1; ++i)
     {
         auto start_time = Clock::now();
         for (const Header& header : headers)
@@ -74,16 +110,19 @@ void test_one_processor()
         }
         auto elapsed_time = Clock::now() - start_time;
         std::cout << "Total=" << elapsed_time
-            << " time/packet=" << (1.0 * elapsed_time / headers.size())
-            << " time/packet/filter=" << (1.0 * elapsed_time / headers.size() / processors.size()) << '\n';
+            << " cycles/packet=" << (1.0 * elapsed_time / headers.size())
+            << " ns/packet=" << (1.0 * elapsed_time / get_frequency_ghz() / headers.size())
+            << " time/packet/filter=" << (1.0 * elapsed_time / headers.size() / processors.size())
+            << " ns=" << (1.0 * elapsed_time / headers.size() / processors.size()) / get_frequency_ghz()
+            << '\n';
 
     }
 
     for (Processor& p : processors)
     {
         std::cout
-            << " TotalOk=" << p.getOkCounted() << '/' << total_counter
-            << " HashedOnly=" << p.getOkHashedOnly()
+            << " TotalOk=" << p.getOkCounted() << '/' << total_counter << " (" << int(0.5 + 100.0 * p.getOkCounted() / total_counter)  << "%)"
+            << " HashMatched=" << p.getOkHashedOnly() << " (" << int(0.5 + 100.0 * p.getOkHashedOnly() / p.getOkCounted()) << "%) "
             << " ProcessedOnly=" << p.getOkProcessedOnly()
             << std::endl;
     }

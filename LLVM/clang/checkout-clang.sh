38e598b6280918d50f3a75f57d1d4f4770fb14ee
StackedCrooked <francis.rammeloo@gmail.com>
2016-04-22 15:51:05 +0200

    Automatically pick most recent svn tag.

--- LLVM/clang/checkout-clang.sh
+++ LLVM/clang/checkout-clang.sh
@@ -1,5 +1,6 @@
 set -e
-svn_tag=RELEASE_380
+set -x
+export svn_tag="$(svn list http://llvm.org/svn/llvm-project/llvm/tags | grep RELEASE_ | tail -n1 | sed -e 's,/,,g')"
 (svn co -q http://llvm.org/svn/llvm-project/llvm/tags/${svn_tag}/final llvm)
 (cd llvm/tools && svn co -q http://llvm.org/svn/llvm-project/cfe/tags/${svn_tag}/final clang)
 (cd llvm/tools/clang/tools && svn co -q http://llvm.org/svn/llvm-project/clang-tools-extra/tags/${svn_tag}/final extra)
--- LLVM/clang/rebuild-clang.sh
+++ LLVM/clang/rebuild-clang.sh
@@ -1,3 +1,1 @@
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
-rm -rf build && mkdir -p build && cd build && ../llvm/configure --enable-optimized=yes --enable-assertions=no && make "$@"
+rm -rf build && mkdir -p build && cd build && ../llvm/configure --enable-optimized=yes --enable-assertions=no && make "$@" -j3

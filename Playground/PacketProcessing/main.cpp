943f65edaa845a1ff9ff7e6c5873eb8f9fdb11b6
StackedCrooked <francis.rammeloo@gmail.com>
2016-05-03 12:38:17 +0200

    Cleanup.

--- Playground/PacketProcessing/main.cpp
+++ Playground/PacketProcessing/main.cpp
@@ -308,127 +308,19 @@ void test(int num_packets, int num_processors)
 
     std::random_shuffle(packets.begin(), packets.end());
 
-
-
     auto total_counter = 0ul;
 
     auto start_time = Clock::now();
 
-#if PREFETCH == 16
-    for (auto i = 0ul; i != packets.size(); i += 16)
-    {
-        __builtin_prefetch(packets[i+16].data(), 0, 0);
-        __builtin_prefetch(packets[i+17].data(), 0, 0);
-        __builtin_prefetch(packets[i+18].data(), 0, 0);
-        __builtin_prefetch(packets[i+19].data(), 0, 0);
-        __builtin_prefetch(packets[i+20].data(), 0, 0);
-        __builtin_prefetch(packets[i+21].data(), 0, 0);
-        __builtin_prefetch(packets[i+22].data(), 0, 0);
-        __builtin_prefetch(packets[i+23].data(), 0, 0);
-        __builtin_prefetch(packets[i+24].data(), 0, 0);
-        __builtin_prefetch(packets[i+25].data(), 0, 0);
-        __builtin_prefetch(packets[i+26].data(), 0, 0);
-        __builtin_prefetch(packets[i+27].data(), 0, 0);
-        __builtin_prefetch(packets[i+28].data(), 0, 0);
-        __builtin_prefetch(packets[i+29].data(), 0, 0);
-        __builtin_prefetch(packets[i+30].data(), 0, 0);
-        __builtin_prefetch(packets[i+31].data(), 0, 0);
-        total_counter += 16;
-        for (Processor& processor : processors)
-        {
-            processor.process(packets[i+0].data(), packets[i+0].size());
-            processor.process(packets[i+1].data(), packets[i+1].size());
-            processor.process(packets[i+2].data(), packets[i+2].size());
-            processor.process(packets[i+3].data(), packets[i+3].size());
-            processor.process(packets[i+4].data(), packets[i+4].size());
-            processor.process(packets[i+5].data(), packets[i+5].size());
-            processor.process(packets[i+6].data(), packets[i+6].size());
-            processor.process(packets[i+7].data(), packets[i+7].size());
-            processor.process(packets[i+8].data(), packets[i+8].size());
-            processor.process(packets[i+9].data(), packets[i+9].size());
-            processor.process(packets[i+10].data(), packets[i+10].size());
-            processor.process(packets[i+11].data(), packets[i+11].size());
-            processor.process(packets[i+12].data(), packets[i+12].size());
-            processor.process(packets[i+13].data(), packets[i+13].size());
-            processor.process(packets[i+14].data(), packets[i+14].size());
-            processor.process(packets[i+15].data(), packets[i+15].size());
-        }
-    }
-#elif PREFETCH == 8
-    for (auto i = 0ul; i != packets.size(); i += 8)
-    {
-        __builtin_prefetch(packets[i+ 8].data(), 0, 0);
-        __builtin_prefetch(packets[i+ 9].data(), 0, 0);
-        __builtin_prefetch(packets[i+10].data(), 0, 0);
-        __builtin_prefetch(packets[i+11].data(), 0, 0);
-
-        __builtin_prefetch(packets[i+12].data(), 0, 0);
-        __builtin_prefetch(packets[i+13].data(), 0, 0);
-        __builtin_prefetch(packets[i+14].data(), 0, 0);
-        __builtin_prefetch(packets[i+15].data(), 0, 0);
-        total_counter += 8;
-        for (Processor& processor : processors)
-        {
-            processor.process(packets[i+0].data(), packets[i+0].size());
-            processor.process(packets[i+1].data(), packets[i+1].size());
-            processor.process(packets[i+2].data(), packets[i+2].size());
-            processor.process(packets[i+3].data(), packets[i+3].size());
-            processor.process(packets[i+4].data(), packets[i+4].size());
-            processor.process(packets[i+5].data(), packets[i+5].size());
-            processor.process(packets[i+6].data(), packets[i+6].size());
-            processor.process(packets[i+7].data(), packets[i+7].size());
-        }
-    }
-#elif PREFETCH == 4
-    for (auto i = 0ul; i != packets.size(); i += 4)
-    {
-        __builtin_prefetch(packets[i+4].data(), 0, 0);
-        __builtin_prefetch(packets[i+5].data(), 0, 0);
-        __builtin_prefetch(packets[i+6].data(), 0, 0);
-        __builtin_prefetch(packets[i+7].data(), 0, 0);
-        total_counter += 4;
-        for (Processor& processor : processors)
-        {
-            processor.process(packets[i+0].data(), packets[i+0].size());
-            processor.process(packets[i+1].data(), packets[i+1].size());
-            processor.process(packets[i+2].data(), packets[i+2].size());
-            processor.process(packets[i+3].data(), packets[i+3].size());
-        }
-    }
-
-#elif PREFETCH == 2
-    for (auto i = 0ul; i != packets.size(); i += 2)
-    {
-        __builtin_prefetch(packets[i+3].data(), 0, 0);
-        __builtin_prefetch(packets[i+4].data(), 0, 0);
-        total_counter += 2;
-        for (Processor& processor : processors)
-        {
-            processor.process(packets[i+0].data(), packets[i+0].size());
-            processor.process(packets[i+1].data(), packets[i+1].size());
-        }
-    }
-#elif PREFETCH == 1
     for (auto i = 0ul; i != packets.size(); i += 1)
     {
         total_counter += 1;
-        for (Processor& processor : processors)
-        {
-            __builtin_prefetch(packets[i+1].data(), 0, 0);
-            processor.process(packets[i+0].data(), packets[i+0].size());
-        }
-    }
- #elif PREFETCH == 0
-    for (auto i = 0ul; i != packets.size(); i += 1)
-    {
-        total_counter += 1;
+        __builtin_prefetch(packets[i+4].data(), 0, 0);
         for (Processor& processor : processors)
         {
             processor.process(packets[i+0].data(), packets[i+0].size());
         }
     }
-#endif
-
 
     auto elapsed_time = Clock::now() - start_time;
 

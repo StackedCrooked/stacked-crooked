168ba73d4e3896ca312ddb72ab6caa00cbc0a5d2
StackedCrooked <francis.rammeloo@gmail.com>
2016-07-01 20:55:03 +0200

    Fix.

--- LLVM/libcxx/checkout-libcxx.sh
+++ LLVM/libcxx/checkout-libcxx.sh
@@ -1,1 +1,2 @@
-svn co http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_34/final libcxx
+export svn_tag="$(svn list http://llvm.org/svn/llvm-project/llvm/tags | grep RELEASE_ | tail -n1 | sed -e 's,/,,g')"
+svn co http://llvm.org/svn/llvm-project/libcxx/tags/${svn_tag}/final libcxx

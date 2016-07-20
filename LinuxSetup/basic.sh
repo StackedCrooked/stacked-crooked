29ca93df0494ab33cb805664ebeb56ac5c586b40
StackedCrooked <francis.rammeloo@gmail.com>
2016-07-20 21:28:08 +0200

    Rename Install to basic.sh.

--- LinuxSetup/basic.sh
+++ LinuxSetup/basic.sh
@@ -1,4 +1,9 @@
+[ -d ~/stacked-crooked ] && {
+	echo "Basic setup seems to be already done."
+	exit 
+}
+
 sudo apt-get -y install subversion vim
 svn co https://github.com/StackedCrooked/stacked-crooked.git/trunk ~/stacked-crooked
 cp ~/stacked-crooked/Profile/_profile ~/.profile
-cp ~/stacked-crooked/Vim/_vimrc_minimal ~/.vimrc
+cp  ~/stacked-crooked/Vim/_vimrc_minimal ~/.vimrc

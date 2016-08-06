f3fef444cc125e12a444c96f729ee950283a7692
StackedCrooked <francis.rammeloo@gmail.com>
2016-08-06 18:25:26 +0200

    Update delay.sh

--- Utilities/EmulateKeyboardLag/delay.sh
+++ Utilities/EmulateKeyboardLag/delay.sh
@@ -1,5 +1,5 @@
 # Emulate typing delay.
 # Usage example:
-#  >source delay # load the delay function
-#  >delay 0.1 # set typing delay to 0.1 s
+#  >source delay.sh # load the delay function
+#  >delay 0.1 # set typing delay to 0.1 s (stop with CTRL-C)
 delay() { rm -f /tmp/fifo; mkfifo /tmp/fifo; while true ; do cat /tmp/fifo; sleep $1; done & while true ; do read -rsn1 n; { [ "$n" == "" ] && printf "\n" || printf "$n"; } >/tmp/fifo; done; }

751296de0efcf5f51990248b05a2883d9403aaa6
StackedCrooked <francis.rammeloo@gmail.com>
2016-09-10 03:42:00 +0200

    Timing.

--- Playground/FrameBlasting/main.cpp
+++ Playground/FrameBlasting/main.cpp
@@ -1,7 +1,10 @@
+#include <boost/container/flat_set.hpp>
 #include <atomic>
 #include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <algorithm>
+#include <deque>
 #include <thread>
 #include <cstdint>
 #include <mutex>
@@ -9,91 +12,221 @@
 #include <vector>
 
 
-using Packet = std::string;
+struct Packet
+{
+    explicit Packet(int flowId) : mFlowId(flowId) {}
+    std::size_t size() const { return mData.size(); }
+    std::string mData;
+    int mFlowId = 0;
+};
 
+using Duration  = std::chrono::milliseconds;
 using Clock = std::chrono::steady_clock;
 using Timestamp = Clock::time_point;
-using Duration  = std::chrono::nanoseconds;
+
+
+Timestamp gStartTime = Clock::now();
+
+
+
+struct BBInterface;
+
 
 
 struct Flow
 {
-    void consume(std::vector<Packet*>& packets, Timestamp current_time)
+    Flow() :
+        mPacket([]{ static int flowId; return flowId++; }())
+    {
+    }
+
+    void consume(std::deque<Packet*>& packets, Timestamp current_time)
     {   
-        if (current_time >= mNextTransmission)
-        {   
-            packets.push_back(&mPacket);
-            mNextTransmission += mInterframeGap;
+        if (mPacketCount == 0)
+        {
+            return;
+        }
+        for (auto i = 0; i != 4; ++i)
+        {
+            if (current_time >= mNextTransmission)
+            {   
+                packets.push_back(&mPacket);
+                mTxBytes += mPacket.size();
+                mPacketCount--;
+                mNextTransmission += mInterval;
+            }
         }
     }
     
     Packet mPacket;
     Timestamp mNextTransmission;
-    Duration mInterframeGap;
+    Duration mInterval;
+    int64_t mTxBytes = 0;
+    int64_t mPacketCount = 40;
 };
+
+
+struct BBInterface
+{
+    void set_rate_limit(double bytes_per_ns)
+    {
+        mBytesPerMs = bytes_per_ns;
+    }
+
+    void add_flow(Flow& flow)
+    {
+        mFlows.insert(&flow);
+    }
+
+    void remove_flow(Flow& flow)
+    {
+        mFlows.erase(&flow);
+    }
+
+    void consume(std::deque<Packet*>& packets, Timestamp current_time, int max_packets = 2)
+    {
+        for (auto i = 0; i != max_packets; ++i)
+        {
+            if (mPackets.empty())
+            {
+                for (Flow* flow : mFlows)
+                {
+                    flow->consume(mPackets, current_time);
+                }
+                if (mPackets.empty())
+                {
+                    return;
+                }
+            }
+
+            Packet* front_packet = mPackets.front();
+            auto full_size = front_packet->size() + 24;
+
+            if (mBytesPerMs && mBucket < 0)
+            {
+                if (mLastTime == Timestamp())
+                {
+                    mLastTime = current_time;
+                }
+                std::chrono::milliseconds elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - mLastTime);
+
+                auto bucket_increment = elapsed_ms.count() * mBytesPerMs;
+                auto new_bucket = mBucket + bucket_increment;
+                if (new_bucket < 0)
+                {
+                    return;
+                }
+
+                mBucket = new_bucket;
+                mLastTime = current_time;
+            }
+
+
+            packets.push_back(front_packet);
+            mTransmittedPackets.emplace_back(std::chrono::duration_cast<std::chrono::milliseconds>(current_time - gStartTime), full_size);
+            mTxBytes += full_size;
+            mPackets.pop_front();
+
+            if (mBytesPerMs)
+            {
+                mBucket -= full_size;
+             }
+        }
+    }
+
+    void print(int index)
+    {
+        for (auto& el : mTransmittedPackets)
+        {
+            std::cout << "BB" << index << ' ' << std::get<0>(el).count() << ' ' << std::get<1>(el) << '\n';
+        }
+    }
+
+    double mBytesPerMs = 50;
+    double mBucket = 4000;
+    Timestamp mLastTime = Timestamp();
+    boost::container::flat_set<Flow*> mFlows;
+    std::deque<Packet*> mPackets;
+    std::deque<std::tuple<std::chrono::milliseconds, int32_t>> mTransmittedPackets;
+    int64_t mTxBytes = 0;
+};
+
+
 
 
 struct Socket
 {
-    void send_batch(Timestamp ts, const std::vector<Packet*>& packets)
+    void send_batch(Timestamp ts, const std::deque<Packet*>& packets)
     {
-        auto us = std::chrono::duration_cast<std::chrono::microseconds>(ts - mStartTime);
-        std::cout << us.count() << ' ';
+        assert(!packets.empty());
+        if (!mDirty)
+        {
+            auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(ts - gStartTime);
+            std::cout << std::setw(5) << ms.count() << "ms: ";
+        }
         for (Packet* p : packets)
         {
-            std::cout << *p << " ";
+            std::cout << p->size() << ' ';
         }
-        std::cout << std::endl;
+        mDirty = true;
     }
-    Clock::time_point mStartTime = Clock::now();
+
+    void flush()
+    {
+        if (mDirty)
+        {
+            std::cout << std::endl;
+            mDirty = false;
+        }
+    }
+
+    bool mDirty = false;
 };
 
 
-struct Transmitter
+struct PhysicalInterface
 {
-    Transmitter() :
-        mThread(&Transmitter::run_thread, this)
+    PhysicalInterface(std::size_t num_interfaces) :
+        mBBInterfaces(num_interfaces),
+        mThread(&PhysicalInterface::run_thread, this)
     {
     }
     
-    Transmitter(const Transmitter&) = delete;
-    Transmitter& operator=(const Transmitter&) = delete;
+    PhysicalInterface(const PhysicalInterface&) = delete;
+    PhysicalInterface& operator=(const PhysicalInterface&) = delete;
     
-    ~Transmitter()
+    ~PhysicalInterface()
     {
+        stop();
+    }
+
+    void stop()
+    {
+        if (!mQuit.exchange(true))
         {
-            std::lock_guard<std::mutex> lock(mMutex);
-            mQuit = true;
+            mThread.join();
         }
-        mThread.join();
     }
-    
-    void add_flow(Flow& flow)
+
+    std::vector<BBInterface>& getBBInterfaces()
     {
-        std::lock_guard<std::mutex> lock(mMutex);
-        mFlows.push_back(&flow);
-    }
-    
-    void remove_flow(Flow& flow)
-    {
-        std::lock_guard<std::mutex> lock(mMutex);
-        mFlows.erase(std::remove(mFlows.begin(), mFlows.end(), &flow), mFlows.end());
+        return mBBInterfaces;
     }
 
 private:
     void run_thread()
     {
-        std::vector<Packet*> packets;
+        std::deque<Packet*> packets;
         
+        auto now = Clock::now();
         while (!mQuit)
         {
             std::lock_guard<std::mutex> lock(mMutex);
             
-            auto now = Clock::now();
-            
-            for (Flow* flow : mFlows)
+
+            for (BBInterface& bbinterface : mBBInterfaces)
             {
-                flow->consume(packets, now);
+                bbinterface.consume(packets, now);
             }
 
             if (!packets.empty())
@@ -101,47 +234,48 @@ private:
                 mSocket.send_batch(now, packets);
                 packets.clear();
             }
+            else
+            {
+                now += std::chrono::milliseconds(10);
+                mSocket.flush();
+            }
         }
     }
     
     std::atomic<bool> mQuit{false};
-    std::vector<Flow*> mFlows;
+    std::vector<BBInterface> mBBInterfaces;
     Socket mSocket;
     std::mutex mMutex;
     std::thread mThread;
 };
 
 
-enum
-{
-    num_flows = 4
-};
-
-
-
-
-
-
 int main()
 {
-    Transmitter tx;
+    PhysicalInterface physicalInterface(48);
+    BBInterface* bbInterfaces = physicalInterface.getBBInterfaces().data();
 
-    std::vector<Flow> flows(num_flows);
+    enum { num_flows = 5 };
+    Flow flows[num_flows];
 
-    
     auto now = Clock::now();
+
     for (auto i = 0; i != num_flows; ++i)
     {
         Flow& flow = flows[i];
-        flow.mPacket.push_back('A' + i);
-        flow.mInterframeGap = std::chrono::microseconds(200 + 100 * i);
-        flow.mNextTransmission = now + flows[i].mInterframeGap;
-        tx.add_flow(flow);
+        flow.mPacket.mData.resize((i + 1) * 64);
+        flow.mInterval = std::chrono::milliseconds(1);
+        flow.mNextTransmission = now + flow.mInterval;
+        bbInterfaces[0].add_flow(flow);
     }
 
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
+    physicalInterface.stop();
+
+//    for (BBInterface& b : physicalInterface.getBBInterfaces())
+//    {
+//        b.print(&b - &physicalInterface.getBBInterfaces()[0]);
+//    }
 }
-

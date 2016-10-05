f51883d152cfb04bddb618b62184c61976b75303
StackedCrooked <francis.rammeloo@gmail.com>
2016-10-05 22:13:19 +0200

    Split-up prefetching code.

--- Playground/PacketProcessing/main.cpp
+++ Playground/PacketProcessing/main.cpp
@@ -260,18 +260,31 @@ void test(uint32_t num_packets, uint32_t num_flows)
         __builtin_prefetch(packets[i].data(), 0, 0);
     }
 
-    for (auto i = 0ul; i != packets.size(); ++i)
+    for (auto i = 0ul; i < prefetch; ++i)
     {
-        Packet& packet = packets[i];
+        __builtin_prefetch(packets[i + prefetch].data(), 0, 0);
+    }
 
-        if (prefetch > 0 && i + prefetch < packets.size())
+    for (auto i = 0ul; i + prefetch < packets.size(); ++i)
+    {
+        if (prefetch > 0)
         {
             __builtin_prefetch(packets[i + prefetch].data(), 0, 0);
         }
 
+        Packet& packet = packets[i];
+
         for (auto flow_index = 0ul; flow_index != flows.size(); ++flow_index)
         {
             matches[flow_index] += flows[flow_index].match(packet.data(), packet.size());
+        }
+    }
+
+    for (auto i = packets.size() - prefetch; i != packets.size(); ++i)
+    {
+        if (prefetch > 0)
+        {
+            __builtin_prefetch(packets[i + prefetch].data(), 0, 0);
         }
     }
 

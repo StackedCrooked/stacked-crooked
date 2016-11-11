d3bf4af4b2a11fede28c1a20ebc478e752817141
StackedCrooked <francis.rammeloo@gmail.com>
2016-11-11 21:33:29 +0100

    Use 1e6 packets.

--- Playground/PacketProcessing/with-hash.cpp
+++ Playground/PacketProcessing/with-hash.cpp
@@ -237,7 +237,7 @@ void do_run(uint32_t num_packets, uint32_t num_flows)
 
 
 
-void run(uint32_t num_packets = 400 * 1000)
+void run(uint32_t num_packets = 1000 * 1000)
 {
     int flow_counts[] = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048 };
 

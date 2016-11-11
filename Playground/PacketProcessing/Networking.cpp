a3306efe82ea79cac188f9694f290ebae25789ae
StackedCrooked <francis.rammeloo@gmail.com>
2016-11-11 02:58:26 +0100

    Add static asserts.

--- Playground/PacketProcessing/Networking.cpp
+++ Playground/PacketProcessing/Networking.cpp
@@ -9,3 +9,15 @@ std::ostream& operator<<(std::ostream& os, IPv4Address ip)
               << static_cast<int>(ip.mData[2]) << '.'
               << static_cast<int>(ip.mData[3]);
 }
+
+
+static_assert(std::is_trivially_copyable<Net16>::value, "");
+static_assert(std::is_trivially_copyable<MACAddress>::value, "");
+static_assert(std::is_trivially_copyable<IPv4Address>::value, "");
+static_assert(std::is_trivially_copyable<EthernetHeader>::value, "");
+static_assert(std::is_trivially_copyable<IPv4Header>::value, "");
+static_assert(std::is_trivially_copyable<TCPHeader>::value, "");
+
+
+static_assert(sizeof(IPv4Header) == 20, "");
+static_assert(sizeof(TCPHeader) == 20, "");

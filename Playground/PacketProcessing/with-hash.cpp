3c7f5df4ad907d0a73e2932175c17bb476918518
StackedCrooked <francis.rammeloo@gmail.com>
2016-11-15 13:25:10 +0100

    Only test up to 512 flows.

--- Playground/PacketProcessing/with-hash.cpp
+++ Playground/PacketProcessing/with-hash.cpp
@@ -239,7 +239,7 @@ void do_run(uint32_t num_packets, uint32_t num_flows)
 
 void run(uint32_t num_packets = 1000 * 1000)
 {
-    int flow_counts[] = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048 };
+    int flow_counts[] = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
 
     for (auto flow_count : flow_counts)
     {

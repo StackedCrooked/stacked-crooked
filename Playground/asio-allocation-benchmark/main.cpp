9271c094408ee641e829696db9f8b07a28ecd31b
StackedCrooked <francis.rammeloo@gmail.com>
2016-12-03 02:04:46 +0100

    Use operator new. No allocations!.

--- Playground/asio-allocation-benchmark/main.cpp
+++ Playground/asio-allocation-benchmark/main.cpp
@@ -1,32 +1,52 @@
 #include <boost/asio.hpp>
 #include <boost/function.hpp>
+#include <boost/container/static_vector.hpp>
+#include <atomic>
 #include <thread>
-#include <iostream>
+#include <stdint.h>
+#include <stdio.h>
 
 
+#define trace() printf("... thread_id=0x%lx main.cpp:%d: %s\n", reinterpret_cast<intptr_t>(pthread_self()), __LINE__, __FUNCTION__)
+#define say_trace(s) printf(">>> thread_id=0x%lx main.cpp:%d: %s MESSAGE=%s\n", reinterpret_cast<intptr_t>(pthread_self()), __LINE__, __FUNCTION__, s)
+#define nice_trace() printf("+++ thread_id=0x%lx main.cpp:%d: %s\n", reinterpret_cast<intptr_t>(pthread_self()), __LINE__, __FUNCTION__)
+#define super_trace() printf("*** thread_id=0x%lx main.cpp:%d: %s\n", reinterpret_cast<intptr_t>(pthread_self()), __LINE__, __FUNCTION__)
 
+std::atomic<bool> enable_trace{true};
+
+#define ohno_trace() if (enable_trace) []{printf("DEL thread_id=0x%lx main.cpp:%d:%s\n", reinterpret_cast<intptr_t>(pthread_self()), __LINE__, __FUNCTION__); }()
+#define newo_trace(n) if (enable_trace) [n] { printf("NEW thread_id=0x%lx main.cpp:%d:%s: size=%d\n", reinterpret_cast<intptr_t>(pthread_self()), __LINE__, __FUNCTION__, int(n)); }()
 
 
 struct Worker
 {
     Worker(boost::asio::io_service& ios) : ios(ios)
     {
-        mTasks.reserve(100);
     }
 
     template<typename F>
     void post(F f)
     {
-        std::cout << std::this_thread::get_id() << " PostedTask::post mTasks.size=" << mTasks.size() << std::endl;
+        trace();
 
-        auto was_empty = mTasks.empty();
-        mTasks.push_back(f);
-        if (was_empty)
+        const auto old_size = mTasks.size();
+
+        if (old_size < mTasks.capacity())
         {
+            mTasks.push_back(f);
+        }
+        else
+        {
+            say_trace("mAdditionalTasks push!");
+            mAdditionalTasks.push_back(f);
+        }
+
+        if (old_size == 0)
+        {
+            super_trace();
             ios.post(PostedTask(*this));
         }
     }
-
 
     struct PostedTask
     {
@@ -35,7 +55,7 @@ struct Worker
 
         void operator()() const
         {
-            std::cout << std::this_thread::get_id() << " PostedTask::call: worker_=" << worker_ << std::endl;
+            super_trace();
             if (!worker_)
             {
                 throw std::runtime_error("std::bad");
@@ -45,29 +65,29 @@ struct Worker
 
         friend void* asio_handler_allocate(std::size_t size, PostedTask* task)
         {
-            std::cout << std::this_thread::get_id() << " asio_handler_allocate size=" << size << " sizeof(*task)=" << sizeof(*task) << " sizeof(Storage)=" << sizeof(Storage) << std::endl;
             if (!task->worker_->mStorageInUse)
             {
-                std::cout << std::this_thread::get_id() << " REUSE size=" << size << " sizeof(PostedTask)" << sizeof(PostedTask) << " &task->worker_->mStorage=" << (&task->worker_->mStorage) << std::endl;
+                super_trace();
                 task->worker_->mStorageInUse = true;
                 return &task->worker_->mStorage;
             }
             else
             {
-                std::cout << std::this_thread::get_id() << " MALLOC size=" << size << " sizeof(PostedTask)" << sizeof(PostedTask) << " &task->worker_->mStorage=" << (&task->worker_->mStorage) << std::endl;
+                ohno_trace();
                 return ::operator new (size);
             }
         }
 
-        friend void asio_handler_deallocate(void* pointer, std::size_t size, PostedTask* task)
+        friend void asio_handler_deallocate(void* pointer, std::size_t /*size*/, PostedTask* task)
         {
-            std::cout << std::this_thread::get_id() << " asio_handler_deallocate size=" << size << " sizeof(*task)=" << sizeof(*task) << " pointer=" << pointer << " task=" << task << std::endl;(void)size; (void)task;
             if (pointer == &task->worker_->mStorage)
             {
+                super_trace();
                 task->worker_->mStorageInUse = false;
             }
             else
             {
+                ohno_trace();
                 ::operator delete (pointer);
             }
         }
@@ -77,58 +97,126 @@ struct Worker
 
     void run_tasks()
     {
-        std::cout << std::this_thread::get_id() << " run_tasks: mTasks.size=" << mTasks.size() << std::endl;
+        super_trace();
+
         for (auto& task : mTasks)
         {
             task();
         }
+
         mTasks.clear();
 
+
+        for (auto& task : mAdditionalTasks)
+        {
+            say_trace("mAdditionalTasks invocation!");
+            task();
+        }
+
+        mAdditionalTasks.clear();
     }
 
     using Function  = boost::function<void()>;
-
     using Storage = std::aligned_storage<sizeof(PostedTask), 32>::type;
-    std::vector<Function> mTasks;
-    bool mStorageInUse = false;
+
+    boost::container::static_vector<Function, 2> mTasks;
+    std::vector<Function> mAdditionalTasks;
     Storage mStorage;
+    bool mStorageInUse = false;
     boost::asio::io_service& ios;
 };
 
 
-    #define L(cmd) std::cout << __FILE__ << ":" << __LINE__ << ": " << std::flush << #cmd << std::endl; cmd
+
+
+void run(boost::asio::io_service& ios)
+{
+    printf("<run>\n");
+    trace();
+    {
+        trace();
+        {
+            printf("<section n=2>\n");
+            trace();
+            Worker worker(ios);
+            trace();
+            worker.post([]{ nice_trace(); });
+            trace();
+            worker.post([]{ nice_trace(); });
+            trace();
+            ios.run();
+            trace();
+            ios.reset();
+            printf("</section>\n");
+        }
+        {
+            printf("<section n=3>\n");
+            trace();
+            Worker worker(ios);
+            trace();
+            worker.post([]{ nice_trace(); });
+            trace();
+            worker.post([]{ nice_trace(); });
+            trace();
+            worker.post([]{ nice_trace(); });
+            trace();
+            worker.post([]{ nice_trace(); });
+            trace();
+            worker.post([]{ nice_trace(); });
+            trace();
+            worker.post([]{ nice_trace(); });
+            trace();
+            ios.run();
+            trace();
+            ios.reset();
+            printf("</section>\n");
+        }
+        trace();
+    }
+    trace();
+    printf("</run>\n");
+
+}
+
 
 int main()
 {
-    std::cout << std::this_thread::get_id() << " Start of program" << std::endl;
-
+    printf("<main>\n");
     {
+        printf("<io_service>\n");
         boost::asio::io_service ios;
-
-        {
-            Worker worker(ios);
-            ios.post([]{ std::cout << "I'm in a post!" << std::endl; });
-            ios.run(); ios.reset();
-            std::cout << "main.cpp:" << __LINE__ << std::endl;
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            std::cout << "main.cpp:" << __LINE__ << std::endl;
-            ios.run(); ios.reset();
-            std::cout << "main.cpp:" << __LINE__ << std::endl;
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            std::cout << "main.cpp:" << __LINE__ << std::endl;
-            worker.post([]{ std::cout << std::this_thread::get_id() << " inside worker post! (post on main.cpp:" << __LINE__ << ")" << std::endl; });
-            std::cout << "main.cpp:" << __LINE__ << std::endl;
-            worker.ios.run();
-            std::cout << "main.cpp:" << __LINE__ << std::endl;
-        }
-        std::cout << "main.cpp:" << __LINE__ << std::endl;
+        run(ios);
+        printf("</io_service>\n");
     }
-    std::cout << "main.cpp:" << __LINE__ << std::endl;
+    printf("</main>\n");
+}
 
+
+
+void * operator new(std::size_t n) throw(std::bad_alloc)
+{
+    newo_trace(n);
+    return malloc(n);
+}
+
+
+
+void operator delete(void* p) throw()
+{
+    ohno_trace();
+    free(p);
+}
+
+
+void *operator new[](std::size_t s) throw(std::bad_alloc)
+{
+    newo_trace(s);
+    return malloc(s);
+}
+
+
+void operator delete[](void *p) throw()
+{
+    ohno_trace();
+    free(p);
 }

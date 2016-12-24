4619eb17c68a0c38356ad54e67b6031213199e33
StackedCrooked <francis.rammeloo@gmail.com>
2016-12-24 21:15:09 +0100

    Cleanup whitespace.

--- Playground/PCAPWriter/main.cpp
+++ Playground/PCAPWriter/main.cpp
@@ -5,9 +5,9 @@ uint8_t buf[1514]; // standard ethernet size backet
 
 int main()
 {
-	PCAPWriter pcap("pcap.pcap");
-	for (auto i = 0;  i != 1e6; ++i)
-	{
-		pcap.push_back(buf, sizeof(buf));
-	}
+    PCAPWriter pcap("pcap.pcap");
+    for (auto i = 0;  i != 1e6; ++i)
+    {
+        pcap.push_back(buf, sizeof(buf));
+    }
 }

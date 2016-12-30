34e7f9699814abb3dc62b0aa7483be77c2de7441
StackedCrooked <francis.rammeloo@gmail.com>
2016-12-30 21:50:22 +0100

    simple: experimental: use modulo dst_port as a simple hash.

--- Playground/PacketProcessing/Networking.h
+++ Playground/PacketProcessing/Networking.h
@@ -47,6 +47,11 @@ struct IPv4Address
         return lhs.toInteger() == rhs.toInteger();
     }
 
+    friend bool operator<(IPv4Address lhs, IPv4Address rhs)
+    {
+        return lhs.toInteger() < rhs.toInteger();
+    }
+
     friend std::ostream& operator<<(std::ostream& os, IPv4Address ip);
 
     std::array<uint8_t, 4> mData;

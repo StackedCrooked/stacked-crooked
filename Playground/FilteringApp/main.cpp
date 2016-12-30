a7b49257393602a56c601e0b7283ca771f81cc23
StackedCrooked <francis.rammeloo@gmail.com>
2016-12-31 00:16:49 +0100

    Add basic layout.

--- Playground/FilteringApp/main.cpp
+++ Playground/FilteringApp/main.cpp
@@ -1,9 +1,74 @@
+#include "Range.h"
+#include <vector>
 #include <iostream>
 
-using namespace std;
 
-int main(int argc, char *argv[])
+struct RxPacket
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    const uint8_t* data() const;
+    uint32_t size() const;
+};
+
+
+
+struct BBPort
+{
+    void receive(RxPacket /*packet*/)
+    {
+    }
+};
+
+
+struct BBInterface
+{
+    void receive(RxPacket packet)
+    {
+        BBPort& bbPort = get_bb_port(packet);
+        bbPort.receive(packet);
+    }
+
+    BBPort& get_bb_port(RxPacket);
+};
+
+
+struct PhysicalInterface
+{
+    // Receive packet from socket
+    // - Dispatch to flow-based counters (5-tuple based)
+    // - Dispatch to special counters (multicast and broadcast counters)
+    // - Dispatch to bpf-based processors (user provided a BPF string)
+    // - Dispatch to stack
+    void receive(RxPacket packet)
+    {
+        BBInterface& bbInterface = get_bb_interface(packet);
+        bbInterface.receive(packet);
+    }
+
+    BBInterface& get_bb_interface(RxPacket);
+
+private:
+    std::vector<BBInterface> mBBInterfaces;
+};
+
+
+struct BBServer
+{
+    void run()
+    {
+    }
+
+};
+
+
+int main()
+{
+    std::cout << "Hello!" << std::endl;
+
+    BBServer bbServer;
+    bbServer.run();
+
+    std::cout << "Goodbye!" << std::endl;
 }
+
+
+

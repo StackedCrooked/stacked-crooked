52cdba5abc49fd954dd2f5dcb3d2d785b6048c35
StackedCrooked <francis.rammeloo@gmail.com>
2017-01-18 17:25:40 +0100

    VectorFilter: precalculate mask.

--- Playground/PacketProcessing/VectorFilter.cpp
+++ Playground/PacketProcessing/VectorFilter.cpp
@@ -1,1 +1,29 @@
 #include "VectorFilter.h"
+
+
+VectorFilter::VectorFilter(uint8_t protocol, IPv4Address src_ip, IPv4Address dst_ip, uint16_t src_port, uint16_t dst_port)
+{
+    // Composite of IPv4 + TCP/UDP header fields from TTL to DestinationPort.
+    struct TransportHeader
+    {
+        uint8_t ttl;
+        uint8_t protocol;
+        uint16_t checksum;
+        IPv4Address src_ip;
+        IPv4Address dst_ip;
+        Net16 src_port;
+        Net16 dst_port;
+    };
+
+    auto h = TransportHeader();
+    h.protocol = protocol;
+    h.src_ip = src_ip;
+    h.dst_ip = dst_ip;
+    h.src_port = src_port;
+    h.dst_port = dst_port;
+
+    field_ = _mm_loadu_si128((__m128i*)&h);
+}
+
+
+__m128i VectorFilter::static_mask_;
--- Playground/PacketProcessing/VectorFilter.h
+++ Playground/PacketProcessing/VectorFilter.h
@@ -9,49 +9,32 @@
 
 struct VectorFilter
 {
-    VectorFilter(uint8_t protocol, IPv4Address src_ip, IPv4Address dst_ip, uint16_t src_port, uint16_t dst_port)
+    VectorFilter(uint8_t protocol, IPv4Address src_ip, IPv4Address dst_ip, uint16_t src_port, uint16_t dst_port);
+
+    bool match(const uint8_t* packet_data, uint32_t /*len*/) const
     {
-        // Composite of IPv4 + TCP/UDP header fields from TTL to DestinationPort.
-        struct TransportHeader
-        {
-            uint8_t ttl;
-            uint8_t protocol;
-            uint16_t checksum;
-            IPv4Address src_ip;
-            IPv4Address dst_ip;
-            Net16 src_port;
-            Net16 dst_port;
-        };
+        enum { offset = sizeof(EthernetHeader) + sizeof(IPv4Header) + sizeof(uint16_t) + sizeof(uint16_t) - sizeof(static_mask_) };
 
-        auto h = TransportHeader();
-        h.protocol = protocol;
-        h.src_ip = src_ip;
-        h.dst_ip = dst_ip;
-        h.src_port = src_port;
-        h.dst_port = dst_port;
+        auto mask_result = _mm_and_si128(static_mask_, _mm_loadu_si128((__m128i*)(packet_data + offset)));
+        auto compare_result = _mm_xor_si128(field_, mask_result);
+        return _mm_testz_si128(compare_result, compare_result);
+    }
 
-        field_ = _mm_loadu_si128((__m128i*)&h);
-
+private:
+    static __m128i GetMask()
+    {
         uint8_t mask_bytes[16] = {
             0x00, 0xff, 0x00, 0x00, // ttl, protocol and checksum
             0xff, 0xff, 0xff, 0xff, // source ip
             0xff, 0xff, 0xff, 0xff, // destination ip
             0xff, 0xff, 0xff, 0xff  // source and destination ports
         };
-        mask_ = _mm_loadu_si128((__m128i*)&mask_bytes[0]);
-    }
 
-    bool match(const uint8_t* packet_data, uint32_t /*len*/) const
-    {
-        enum { offset = sizeof(EthernetHeader) + sizeof(IPv4Header) + sizeof(uint16_t) + sizeof(uint16_t) - sizeof(mask_) };
-
-        auto mask_result = _mm_and_si128(mask_, _mm_loadu_si128((__m128i*)(packet_data + offset)));
-        auto compare_result = _mm_xor_si128(field_, mask_result);
-        return _mm_testz_si128(compare_result, compare_result);
+        return _mm_loadu_si128((__m128i*)&mask_bytes[0]);
     }
 
     __m128i field_;
-    __m128i mask_;
+    static __m128i static_mask_;
 };
 
 

77b2d0c40c8e62ed5303ceffc2518c47f14fefe6
StackedCrooked <francis.rammeloo@gmail.com>
2017-02-25 11:39:10 +0100

    Cleanup.

--- Playground/FilteringApp/UDPFlow.cpp
+++ Playground/FilteringApp/UDPFlow.cpp
@@ -1,9 +1,6 @@
 #include "UDPFlow.h"
 
 
-//static_assert(sizeof(UDPFlow) == 32, "");
-
-
 UDPFlow::UDPFlow(IPv4Address src_ip, IPv4Address dst_ip, uint16_t src_port, uint16_t dst_port) :
     mFilter(ProtocolId::kUDP, src_ip, dst_ip, src_port, dst_port)
 {

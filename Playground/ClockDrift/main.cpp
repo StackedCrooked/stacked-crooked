d24c09660bc3b9f377680eb72986ba1d7fe1b53b
StackedCrooked <francis.rammeloo@gmail.com>
2017-03-06 17:30:23 +0100

    Read values.

--- Playground/ClockDrift/main.cpp
+++ Playground/ClockDrift/main.cpp
@@ -1,5 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+//raw_timeval
+// "current_time"
+std::vector<int64_t> get_values(const std::string& file)
+{
+    std::vector<int64_t> result;
+
+    std::ifstream ifs(file);
+    int64_t value = 0;
+    while (ifs >> value)
+    {
+        result.push_back(value);
+    }
+
+    return result;
+}
 
 
 int main()
 {
+    std::cout << get_values("raw_timeval").size() << std::endl;
+    std::cout << get_values("current_time").size() << std::endl;
 }

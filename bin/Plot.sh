02e0af97340c18fa438f663595315a9af52c64a4
StackedCrooked <francis.rammeloo@gmail.com>
2017-04-06 20:00:34 +0200

    Add XPlot.

--- bin/Plot.sh
+++ bin/Plot.sh
@@ -1,3 +1,3 @@
 num_columns="$(awk '{print NF}' "${1}" | sort -nu | tail -n 1)"
-command="set terminal png size 1280,640; set output '$1.png'; set key outside; plot for [col=1:$num_columns] '$1' using 0:col with lines title columnheader"
-gnuplot -e "$command" && open "${1}.png"
+command="set terminal png size 1600,300; set grid; set output '$1.png'; set key outside; plot for [col=1:$num_columns] '$1' using 0:col with points title columnheader"
+gnuplot -e "$command" && xdg-open "${1}.png"

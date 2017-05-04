6a94e833054df2a8b60ced1a35168f5bafee76d5
StackedCrooked <francis.rammeloo@gmail.com>
2017-05-04 05:06:18 +0200

    BBPort::pop is slightly faster when not inlined.

--- Playground/FilteringApp/BBPort.h
+++ Playground/FilteringApp/BBPort.h
@@ -19,50 +19,7 @@ struct BBPort
 
     void addUDPFlow(uint16_t dst_port);
 
-    void pop(RxPacket packet)
-    {
-        if (is_local_mac(packet))
-        {
-            mStats.mUnicastCounter++;
-        }
-        else if (is_broadcast(packet))
-        {
-            mStats.mBroadcastCounter++;
-        }
-        else if (is_multicast(packet))
-        {
-            mStats.mMulticastCounter++;
-        }
-        else
-        {
-            return;
-        }
-
-        if (is_ipv4(packet))
-        {
-            for (auto& flow : mUDPFlows)
-            {
-                if (flow->match(packet, mLayer3Offset)) // BBPort knows its layer-3 offset
-                {
-                    flow->accept(packet);
-                    mStats.mUDPAccepted++;
-                    return;
-                }
-            }
-
-            // If we didn't match any UDP flows then the IP may wrong. So we still need to check it.
-            auto dst_ip = Decode<IPv4Header>(packet.data() + mLayer3Offset).mDestinationIP;
-
-            if (dst_ip != mLocalIP && !dst_ip.isBroadcast() && !dst_ip.isMulticast())
-            {
-                // Invalid destination IP.
-                return;
-            }
-        }
-
-        // Send it to the stack.
-        handle_other(packet);
-    }
+    void pop(RxPacket packet);
 
     bool is_ipv4(RxPacket packet) const
     {

d78297c9ba7afcef19dd995fa51deee0085daef7
StackedCrooked <francis.rammeloo@gmail.com>
2017-05-17 20:41:07 +0200

    Cleanups.

--- Playground/UnixDomainSockets/stream_client.cpp
+++ Playground/UnixDomainSockets/stream_client.cpp
@@ -34,18 +34,19 @@ int main(int argc, char* argv[])
         stream_protocol::socket socket(io_service);
         socket.connect(stream_protocol::endpoint(argv[1]));
 
-        using namespace std; // For strlen.
-        std::cout << "Enter message: ";
-        char request[max_length];
-        std::cin.getline(request, max_length);
-        size_t request_length = strlen(request);
-        boost::asio::write(socket, boost::asio::buffer(request, request_length));
+        for (;;)
+        {
+            using namespace std; // For strlen.
+            char request[max_length];
+            if (!std::cin.getline(request, max_length)) break;
+            size_t request_length = strlen(request);
+            boost::asio::write(socket, boost::asio::buffer(request, request_length));
 
-        char reply[max_length];
-        size_t reply_length = socket.read_some(boost::asio::buffer(reply));
-        std::cout << "Reply is: ";
-        std::cout.write(reply, reply_length);
-        std::cout << "\n";
+            char reply[max_length];
+            size_t reply_length = socket.read_some(boost::asio::buffer(reply));
+            std::cout.write(reply, reply_length);
+            std::cout << "\n";
+        }
     }
     catch (std::exception& e)
     {

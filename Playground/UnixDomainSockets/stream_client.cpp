4d51bb92449f9e0d7630a80150cc6ebc0be07950
StackedCrooked <francis.rammeloo@gmail.com>
2017-05-17 20:37:27 +0200

    Fixes.

--- Playground/UnixDomainSockets/stream_client.cpp
+++ Playground/UnixDomainSockets/stream_client.cpp
@@ -21,39 +21,38 @@ enum { max_length = 1024 };
 
 int main(int argc, char* argv[])
 {
-  try
-  {
-    if (argc != 2)
+    try
     {
-      std::cerr << "Usage: stream_client <file>\n";
-      return 1;
+        if (argc != 2)
+        {
+            std::cerr << "Usage: stream_client <file>\n";
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+
+        stream_protocol::socket socket(io_service);
+        socket.connect(stream_protocol::endpoint(argv[1]));
+
+        using namespace std; // For strlen.
+        std::cout << "Enter message: ";
+        char request[max_length];
+        std::cin.getline(request, max_length);
+        size_t request_length = strlen(request);
+        boost::asio::write(socket, boost::asio::buffer(request, request_length));
+
+        char reply[max_length];
+        size_t reply_length = socket.read_some(boost::asio::buffer(reply));
+        std::cout << "Reply is: ";
+        std::cout.write(reply, reply_length);
+        std::cout << "\n";
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
-    boost::asio::io_service io_service;
-
-    stream_protocol::socket s(io_service);
-    s.connect(stream_protocol::endpoint(argv[1]));
-
-    using namespace std; // For strlen.
-    std::cout << "Enter message: ";
-    char request[max_length];
-    std::cin.getline(request, max_length);
-    size_t request_length = strlen(request);
-    boost::asio::write(s, boost::asio::buffer(request, request_length));
-
-    char reply[max_length];
-    size_t reply_length = boost::asio::read(s,
-        boost::asio::buffer(reply, request_length));
-    std::cout << "Reply is: ";
-    std::cout.write(reply, reply_length);
-    std::cout << "\n";
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
+    return 0;
 }
 
 #else // defined(BOOST_ASIO_HAS_LOCAL_SOCKETS)
--- Playground/UnixDomainSockets/stream_server.cpp
+++ Playground/UnixDomainSockets/stream_server.cpp
@@ -10,6 +10,9 @@
 
 #include <cstdio>
 #include <iostream>
+#include <deque>
+#include <string>
+#include <vector>
 #include <boost/array.hpp>
 #include <boost/bind.hpp>
 #include <boost/enable_shared_from_this.hpp>
@@ -20,120 +23,168 @@
 
 using boost::asio::local::stream_protocol;
 
-class session
-  : public boost::enable_shared_from_this<session>
+class Session : public boost::enable_shared_from_this<Session>
 {
 public:
-  session(boost::asio::io_service& io_service)
-    : socket_(io_service)
-  {
-  }
-
-  stream_protocol::socket& socket()
-  {
-    return socket_;
-  }
-
-  void start()
-  {
-    socket_.async_read_some(boost::asio::buffer(data_),
-        boost::bind(&session::handle_read,
-          shared_from_this(),
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-  }
-
-  void handle_read(const boost::system::error_code& error,
-      size_t bytes_transferred)
-  {
-    if (!error)
+    Session(boost::asio::io_service& io_service)
+        : mSocket(io_service)
     {
-      boost::asio::async_write(socket_,
-          boost::asio::buffer(data_, bytes_transferred),
-          boost::bind(&session::handle_write,
-            shared_from_this(),
-            boost::asio::placeholders::error));
     }
-  }
 
-  void handle_write(const boost::system::error_code& error)
-  {
-    if (!error)
+    stream_protocol::socket& socket()
     {
-      socket_.async_read_some(boost::asio::buffer(data_),
-          boost::bind(&session::handle_read,
-            shared_from_this(),
-            boost::asio::placeholders::error,
-            boost::asio::placeholders::bytes_transferred));
+        return mSocket;
     }
-  }
 
+    void start()
+    {
+        mSocket.async_read_some(boost::asio::buffer(mData),
+                                boost::bind(&Session::handle_read,
+                                            shared_from_this(),
+                                            boost::asio::placeholders::error,
+                                            boost::asio::placeholders::bytes_transferred));
+    }
+
+
+    std::string generate_reply(const char* request_data, int request_size)
+    {
+        static const std::string open_tag = "<SSH>";
+        static const std::string close_tag = "</SSH>";
+
+        std::string result;
+        result.reserve(open_tag.size() + request_size + close_tag.size());
+        result += open_tag;
+        result.insert(result.end(), request_data, request_data + request_size);
+        result += close_tag;
+        return result;
+    }
+
+    void handle_read(const boost::system::error_code& error,
+                     size_t bytes_transferred)
+    {
+        if (error)
+        {
+            return;
+        }
+            
+        auto was_empty = mReplies.empty();
+
+        mReplies.push_back(generate_reply(mData.data(), bytes_transferred));
+        std::cout << "reply=[" << mReplies.back() << "]" << std::endl;
+
+        if (was_empty)
+        {
+            boost::asio::async_write(
+                mSocket,
+                boost::asio::buffer(mReplies.front().data(), mReplies.front().size()),
+                boost::bind(
+                    &Session::handle_write,
+                    shared_from_this(),
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
+        }
+    }
+
+    void handle_write(const boost::system::error_code& error, size_t bytes_transferred)
+    {
+        if (error)
+        {
+            return;
+        }
+
+        std::cout << "Written reply: " << mReplies.front() << std::endl;
+        std::cout << "bytes_transferred=" << bytes_transferred << std::endl;
+
+        mReplies.pop_front();
+
+        if (!mReplies.empty())
+        {
+            boost::asio::async_write(
+                mSocket,
+                boost::asio::buffer(mReplies.front().data(), mReplies.front().size()),
+                boost::bind(
+                    &Session::handle_write,
+                    shared_from_this(),
+                    boost::asio::placeholders::error,
+                    boost::asio::placeholders::bytes_transferred));
+            return;
+        }
+
+        mSocket.async_read_some(
+            boost::asio::buffer(mData),
+            boost::bind(
+                &Session::handle_read,
+                shared_from_this(),
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
+        }
 private:
-  // The socket used to communicate with the client.
-  stream_protocol::socket socket_;
+    // The socket used to communicate with the client.
+    stream_protocol::socket mSocket;
 
-  // Buffer used to store data received from the client.
-  boost::array<char, 1024> data_;
+    // Buffer used to store data received from the client.
+    boost::array<char, 1024> mData;
+    std::deque<std::string> mReplies;
 };
 
-typedef boost::shared_ptr<session> session_ptr;
+typedef boost::shared_ptr<Session> session_ptr;
 
-class server
+class Server
 {
 public:
-  server(boost::asio::io_service& io_service, const std::string& file)
-    : io_service_(io_service),
-      acceptor_(io_service, stream_protocol::endpoint(file))
-  {
-    session_ptr new_session(new session(io_service_));
-    acceptor_.async_accept(new_session->socket(),
-        boost::bind(&server::handle_accept, this, new_session,
-          boost::asio::placeholders::error));
-  }
-
-  void handle_accept(session_ptr new_session,
-      const boost::system::error_code& error)
-  {
-    if (!error)
+    Server(boost::asio::io_service& io_service, const std::string& file)
+        : io_service_(io_service),
+          acceptor_(io_service, stream_protocol::endpoint(file))
     {
-      new_session->start();
+        session_ptr new_session(new Session(io_service_));
+        acceptor_.async_accept(new_session->socket(),
+                               boost::bind(&Server::handle_accept, this, new_session,
+                                           boost::asio::placeholders::error));
     }
 
-    new_session.reset(new session(io_service_));
-    acceptor_.async_accept(new_session->socket(),
-        boost::bind(&server::handle_accept, this, new_session,
-          boost::asio::placeholders::error));
-  }
+    void handle_accept(session_ptr new_session,
+                       const boost::system::error_code& error)
+    {
+        if (!error)
+        {
+            new_session->start();
+        }
+
+        new_session.reset(new Session(io_service_));
+        acceptor_.async_accept(new_session->socket(),
+                               boost::bind(&Server::handle_accept, this, new_session,
+                                           boost::asio::placeholders::error));
+    }
 
 private:
-  boost::asio::io_service& io_service_;
-  stream_protocol::acceptor acceptor_;
+    boost::asio::io_service& io_service_;
+    stream_protocol::acceptor acceptor_;
 };
 
 int main(int argc, char* argv[])
 {
-  try
-  {
-    if (argc != 2)
+    try
     {
-      std::cerr << "Usage: stream_server <file>\n";
-      std::cerr << "*** WARNING: existing file is removed ***\n";
-      return 1;
+        if (argc != 2)
+        {
+            std::cerr << "Usage: stream_server <file>\n";
+            std::cerr << "*** WARNING: existing file is removed ***\n";
+            return 1;
+        }
+
+        boost::asio::io_service io_service;
+
+        std::remove(argv[1]);
+        Server s(io_service, argv[1]);
+
+        io_service.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
-    boost::asio::io_service io_service;
-
-    std::remove(argv[1]);
-    server s(io_service, argv[1]);
-
-    io_service.run();
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "Exception: " << e.what() << "\n";
-  }
-
-  return 0;
+    return 0;
 }
 
 #else // defined(BOOST_ASIO_HAS_LOCAL_SOCKETS)

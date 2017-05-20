04162bb786dac018d30483f66799421e2e5ba88c
StackedCrooked <francis.rammeloo@gmail.com>
2017-05-20 02:51:29 +0200

    Quick HTTPS server.

---
+++ Playground/NodeJS_HTTPS/run.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+set -x
+set -e
+
+openssl genrsa -out key.pem
+openssl req -new -key key.pem -out csr.pem
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+rm csr.pem
+
+node https.js

38221f7b8f1ab7884384d9c484a2cf02586d2e01
StackedCrooked <francis.rammeloo@gmail.com>
2017-11-04 12:59:43 +0100

    Update install_packages.sh.

--- LinuxSetup/basic.sh
+++ LinuxSetup/basic.sh
@@ -3,7 +3,11 @@
 
 mkdir -p ~/bin
 
+sudo apt-get update
+sudo apt-get update --fix-missing
+sudo apt-get upgrade
 sudo apt-get -y install subversion vim
 svn co https://github.com/StackedCrooked/stacked-crooked.git/trunk ~/stacked-crooked
 cp ~/stacked-crooked/Profile/_profile ~/.profile
-cp  ~/stacked-crooked/Vim/_vimrc_minimal ~/.vimrc
+cp ~/stacked-crooked/Vim/_vimrc_minimal ~/.vimrc
+

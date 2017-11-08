b5021e0d4a31a3d98fb1e124e4be1a94228d4a46
StackedCrooked <francis.rammeloo@gmail.com>
2017-11-08 15:41:27 +0100

    Update basic.sh

--- LinuxSetup/basic.sh
+++ LinuxSetup/basic.sh
@@ -5,8 +5,8 @@ mkdir -p ~/bin
 
 sudo apt-get update
 sudo apt-get update --fix-missing
-sudo apt-get upgrade
-sudo apt-get -y install subversion vim
+sudo apt-get -y upgrade
+sudo apt-get -y install subversion vim-common
 svn co https://github.com/StackedCrooked/stacked-crooked.git/trunk ~/stacked-crooked
 cp ~/stacked-crooked/Profile/_profile ~/.profile
 cp ~/stacked-crooked/Vim/_vimrc_minimal ~/.vimrc

7c1ba9fdbbed918c11f5ee3ad679e752f8a2a1fa
StackedCrooked <francis.rammeloo@gmail.com>
2017-11-29 21:43:36 +0100

    Add LinearCongruentialGenerator.

---
+++ Playground/LinearCongruentialGenerator/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <ctime>
+#include <cstdint>
+
+
+auto seed = time(0);
+
+
+inline uint64_t next(uint64_t seed)
+{
+    return seed * 6364136223846793005 + 1442695040888963407;
+}
+
+
+int main()
+{
+    enum { max = 100 };
+
+    uint64_t seed = time(0);
+
+    for (auto i = 0; i != 100 * max; ++i)
+    {
+        auto r = next(seed);
+        std::cout << r % max << "\n";
+    }
+}
+

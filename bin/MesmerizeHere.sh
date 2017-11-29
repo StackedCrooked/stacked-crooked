987c5325b263f3e5ebe1ce1de672db89bf877318
StackedCrooked <francis.rammeloo@gmail.com>
2017-11-29 20:55:23 +0100

    Fix MH.

--- bin/MesmerizeHere.sh
+++ bin/MesmerizeHere.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 [ "$#" == "0" ] && {
-    MESMERIZE_AUTOLOAD="$(pwd)" open -n /Users/francis/programming/projects/.builds/Mesmerize/release/Mesmerize.app
+    MESMERIZE_AUTOLOAD="$(pwd)" open -n /Users/francis/programming/projects/build-Mesmerize-release/Mesmerize.app
     exit
 }
 
 for i in "$@" ; do
     (cd "$i" && echo "pwd=$(pwd)" && MESMERIZE_AUTOLOAD="$(pwd)" open -n /Applications/Mesmerize.app/Contents/MacOS/Mesmerize)
 done
+
+#A

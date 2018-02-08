033da2225d5e09ca0395cae28b9baa8452ba459e
StackedCrooked <francis.rammeloo@gmail.com>
2018-02-08 23:11:13 +0100

    Update XPlot.

--- bin/XPlot.sh
+++ bin/XPlot.sh
@@ -1,3 +1,2 @@
-num_columns="$(awk '{print NF}' "${1}" | sort -nu | tail -n 1)"
 command="set terminal png size 1600,900; set grid; set output '$1.png'; set key outside; plot '$1' using 1:2:xtic(1) with linespoints"
 gnuplot -e "$command" && xdg-open "${1}.png"

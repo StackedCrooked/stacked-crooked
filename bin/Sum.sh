863fea8e90429f3f4da8430ad1d1af12eb5df3fb
StackedCrooked <francis.rammeloo@gmail.com>
2018-03-16 12:52:25 +0100

    Sum.sh now also works with command line arg.

--- bin/Sum.sh
+++ bin/Sum.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+[ "$#" == 1 ] && {
+    echo "$1"
+    export sum=0
+    cat "$1" | while read line; do sum="$(($line + $sum))"; done
+    echo "$sum"
+    exit 
+}
+    
 sum=0
 while read line; do sum="$(($line + $sum))"; done
 echo "$sum"

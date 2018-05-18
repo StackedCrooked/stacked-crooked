8efabc095a621dd2f8b80bbdfc3a01d824404af4
StackedCrooked <francis.rammeloo@gmail.com>
2018-05-18 23:02:41 +0200

    Add BisonExample.

---
+++ Playground/BisonExample/main.c
@@ -0,0 +1,73 @@
+/*
+ * main.c file
+ */
+
+#include "Expression.h"
+#include "Parser.h"
+#include "Lexer.h"
+ 
+#include <stdio.h>
+ 
+int yyparse(SExpression **expression, yyscan_t scanner);
+ 
+SExpression *getAST(const char *expr)
+{
+    SExpression *expression;
+    yyscan_t scanner;
+    YY_BUFFER_STATE state;
+ 
+    if (yylex_init(&scanner)) {
+        // could not initialize
+        return NULL;
+    }
+ 
+    state = yy_scan_string(expr, scanner);
+ 
+    if (yyparse(&expression, scanner)) {
+        // error parsing
+        return NULL;
+    }
+ 
+    yy_delete_buffer(state, scanner);
+ 
+    yylex_destroy(scanner);
+ 
+    return expression;
+}
+ 
+int evaluate(SExpression *e)
+{
+    switch (e->type) {
+        case eVALUE:
+            return e->value;
+        case eMULTIPLY:
+            return evaluate(e->left) * evaluate(e->right);
+        case ePLUS:
+            return evaluate(e->left) + evaluate(e->right);
+        default:
+            // should not be here
+            return 0;
+    }
+}
+ 
+void test(const char* text)
+{
+    SExpression *e = NULL;
+ 
+    e = getAST(text);
+ 
+    int result = evaluate(e);
+ 
+    printf("Result of '%s' is %d\n", text, result);
+ 
+    deleteExpression(e);
+ 
+}
+ 
+int main(void)
+{
+    test("4 ~ 3");
+    test("4 + 3");
+    test("4 + 2");
+    return 0;
+}

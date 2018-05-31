514947361fea856aa848ab11a5ab4e30c9b95f00
StackedCrooked <francis.rammeloo@gmail.com>
2018-05-31 13:51:47 +0200

    Revert.

--- Playground/BPFParsing/main.cpp
+++ Playground/BPFParsing/main.cpp
@@ -1,9 +1,27 @@
+#include <boost/tokenizer.hpp>
 #include <deque>
-#include <cassert>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <set>
+
+
+typedef boost::tokenizer<boost::char_separator<char>> tokenizer;
+
+
+std::deque<std::string> tokenize(const std::string& str)
+{
+    std::deque<std::string> result;
+
+    boost::char_separator<char> sep(" ", "()=");
+
+    tokenizer tokens(str, sep);
+    for (tokenizer::iterator tok_iter = tokens.begin(); tok_iter != tokens.end(); ++tok_iter)
+    {
+        result.push_back(*tok_iter);
+    }
+
+    return result;
+}
 
 
 static std::string indent(int level)
@@ -20,26 +38,12 @@ static std::string indent(int level)
 
 struct Expression
 {
-    static Expression And()
-    {
-        Expression result;
-        result.mType = Type::And;
-        return result;
-    }
-
     static Expression And(Expression lhs, Expression rhs)
     {
         Expression result;
         result.mType = Type::And;
         result.mChildren.push_back(lhs);
         result.mChildren.push_back(rhs);
-        return result;
-    }
-
-    static Expression Or()
-    {
-        Expression result;
-        result.mType = Type::Or;
         return result;
     }
 
@@ -66,11 +70,6 @@ struct Expression
         Or,
         Leaf
     };
-
-    void add(Expression e)
-    {
-        mChildren.push_back(e);
-    }
 
     void print(int level = 0)
     {
@@ -114,122 +113,33 @@ struct Expression
 };
 
 
-/**
- * simplified from https://github.com/the-tcpdump-group/libpcap/blob/master/grammar.y
- * depend on the functionality of data plane
- * BPF: http://biot.com/capstats/bpf.html
- *
- *
- *  letter = "A-Za-z"
- *  digit = "0-9"
- *
- *
- *  node_name = letter { letter | digit | "_" }
- *  node_type = letter { letter }
- *
- *
- *  node = node_name "::" node_type
- *  edge = node_name [ "[" lexpr "]" ] "->" [ "[" lexpr "]" ] node_name
- *
- *
- *  attribute = "true" | "false" | bpf_expr | nf_expr
- *  bpf_expr = bpf_term bpf_id
- *
- *
- *  bpf_id = bpf_ipv4 | digit {digit}
- *  bpf_ipv4 = bpf_ipv4_octet "." bpf_ipv4_octet "." bpf_ipv4_octet "." bpf_ipv4_octet
- *  bpf_ipv4_octet = "0-9" | "1-9" "0-9" | "1" "0-9" "0-9" | "2" "0-4" "0-9" | "2" "5" "0-5"
- *
- *
- *  bpf_term = [bpf_proto] [bpf_dir] [bpf_type] bpf_id
- *  bpf_proto = "ether" | "fddi" | "tr" | "wlan" | "ip" | "ip6" | "arp" | "rarp" | "decnet" | "tcp" | "udp"
- *  bpf_dir = "src" | "dst" | "src or dst" | "src and dst"
- *  bpf_type = "host" | "net" | "port" | "portrange"
- *
- *
- *  nf_expr = nf_type nf_attr
- *  // nf_type = load nf types from user specification
- *  // nf_attr = load nf-specific attributes from user specification
- *
- *
- *  lexpr =
- *    "(" lexpr ")"     |
- *    lexpr "and" lexpr |
- *    lexpr "or" lexpr  |
- *    attribute
- *
- */
 struct Parser
 {
     explicit Parser(const std::string& text) :
-        mOriginalText(text),
-        mText(mOriginalText.data())
+        mTokens(tokenize(text))
     {
-        mProtocols = {
-            "ether", "ppp", "ip", "ip6", "udp", "tcp"
-        };
-
-        assert(is_attribute("ether"));
-        assert(!is_attribute("pether"));
     }
 
     Expression parse_bpf_expression()
     {
-        if (consume_token("("))
+        if (next_token("ip") || next_token("ip6") || next_token("udp") || next_token("tcp") || next_token("ether") || next_token("ppp"))
         {
-            Expression result = parse_bpf_expression();
+            auto leaf = parse_leaf_expression();
 
-            if (!consume_token(")"))
+            return parse_binary_expression(leaf);
+        }
+        else if (consume_token("("))
+        {
+            auto result = parse_bpf_expression();
+
+            if (consume_token(")"))
+            {
+                return parse_binary_expression(result);
+            }
+            else
             {
                 return error(__FILE__, __LINE__);
             }
-
-            return result;
-        }
-        else
-        {
-            return parse_or_expression();
-        }
-    }
-
-    Expression parse_or_expression()
-    {
-        auto result = parse_and_expression();
-
-        if (consume_token("or"))
-        {
-            result = Expression::Or(result, parse_bpf_expression());
-        }
-
-        return result;
-    }
-
-    Expression parse_and_expression()
-    {
-        if (is_attribute())
-        {
-            auto attribute = Expression::Leaf(pop_token());
-
-            if (consume_token("and"))
-            {
-                auto result = Expression::And();
-                result.add(attribute);
-
-                while (consume_token("and"))
-                {
-                    result.add(parse_bpf_expression());
-                }
-
-                return result;
-            }
-            auto result = Expression::And(attribute, parse_bpf_expression());
-
-            while (consume_token("and"))
-            {
-                result.mChildren.push_back(parse_bpf_expression());
-            }
-
-            return result;
         }
         else
         {
@@ -237,67 +147,61 @@ struct Parser
         }
     }
 
-    bool is_attribute() const
+    Expression parse_binary_expression(Expression result)
     {
-        return is_attribute(mText);
+        if (consume_token("and"))
+        {
+            return Expression::And(result, parse_bpf_expression());
+        }
+        else if (consume_token("or"))
+        {
+            return Expression::Or(result, parse_bpf_expression());
+        }
+        else
+        {
+            return result;
+        }
     }
 
-    bool is_attribute(const std::string& text) const
+    Expression parse_leaf_expression()
     {
-        for (const std::string& protocol : mProtocols)
-        {
-            if (text.find(protocol.c_str()) == 0)
-            {
-                return true;
-            }
-        }
-        return false;
+        return Expression::Leaf(pop_token());
     }
 
     bool consume_token(const std::string& s)
     {
-        auto backup = mText;
-        for (auto& c : s)
+        if (mTokens.empty() || mTokens.front() != s)
         {
-            if (mText[0] != c)
-            {
-                mText = backup;
-                return false;
-            }
+            return false;
         }
 
+        pop_token();
         return true;
+    }
+
+    bool next_token(const std::string& s) const
+    {
+        return !mTokens.empty() && mTokens.front() == s;
     }
 
     std::string pop_token()
     {
-        assert(mText && *mText);
-
-        std::string result;
-
-        while (mText && *mText)
-        {
-            result.push_back(*mText++);
-        }
-
+        assert(!mTokens.empty());
+        auto result = mTokens.front();
+        mTokens.pop_front();
         return result;
     }
 
     Expression error(const char* file, int line)
     {
-        std::cout
-            << file << ":" << line << ":\n"
-            << mOriginalText << "\n"
-            << indent(mText - mOriginalText.data()) << "^\n"
-            << std::endl;
+        std::cout << file << ":" << line << ": " <<
+            (mTokens.empty() ? std::string("Expected more tokens") : ("Unexpected token: " + mTokens.front())) << std::endl;
         exit(1);
         throw 1;
     }
 
 
-    std::string mOriginalText;
-    const char* mText;
-    std::set<std::string> mProtocols;
+    std::deque<std::string> mTokens;
 };
 
 
@@ -313,6 +217,15 @@ void test(const char* str)
 
 int main()
 {
+
+    test("ip");
     test("ip and udp");
-    test("(ip or ip6) and (tcp or udp)");
+
+
+    test("(ip and udp) or (ip6 and tcp)");
+    test("ip and udp or ip6 and tcp");  // => TODO: AND should have precedence over OR
+
+    test("((ip and udp) or (ip6 and tcp)) and (ether or ppp)");
+
+    test("((((ip and udp) or (ip and tcp)) or (ip6 and udp or (ip6 and tcp))))");
 }

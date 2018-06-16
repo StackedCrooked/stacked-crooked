2226e27d7cd88ac067eab69f26850fdbfac5bf23
StackedCrooked <francis.rammeloo@gmail.com>
2018-06-16 02:29:19 +0200

    HPlot: use diagonal labels for x-axis.

--- bin/HPlot.sh
+++ bin/HPlot.sh
@@ -4,5 +4,6 @@ set auto x
 set boxwidth 0.8 relative
 set style fill solid 1.0
 set output '$1.png'
+set xtic rotate by -45 scale 0
 plot '$1' using 2:xtic(1) with boxes" | perl -pe 's,\n,;,g')"
 gnuplot -e "$command" && xdg-open "${1}.png"

1fb7b6852e52b16cb89c57901c305a75a1f530a0
StackedCrooked <francis.rammeloo@gmail.com>
2018-11-01 19:56:30 +0100

    Update

--- bin/start-vnc.sh
+++ bin/start-vnc.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+
+# Kill previous SSH reverse tunnels
+ps -eopgid,comm,args | grep 'ssh -NR 5901:localhost:5901' | grep -v grep | awk '{print $1}' | while read line; do kill -9 -${line}; done
+
+
+# Create a new SSH reverse tunnel 
 while true; do ssh -NR 5901:localhost:5901 root@85.159.210.140; sleep 60; done & disown
+
+# Start the VNC server
 vncserver -kill :1
-vncserver :1 -geometry 2560x1440
+vncserver :1 -geometry 1920x1080
+
+
+#vncserver :1 -geometry 2560x1440

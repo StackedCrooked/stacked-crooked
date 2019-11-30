de20487f4a497d5008018f37d5e38ce4e955df4f
StackedCrooked <francis.rammeloo@gmail.com>
2019-11-30 01:52:46 +0100

    Print more info.

--- Playground/StackStatusTree/Input.h
+++ Playground/StackStatusTree/Input.h
@@ -1,5 +1,9 @@
 #include <cstdint>
+#include <cstdint>
+#include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
 extern std::vector<std::pair<int64_t, int64_t>> gInput;
+extern std::unordered_map<int64_t, std::string> gDescriptions;
--- Playground/StackStatusTree/main.cpp
+++ Playground/StackStatusTree/main.cpp
@@ -13,9 +13,37 @@ struct Node
     {
     }
 
+    const std::string& name() const
+    {
+        return gDescriptions[mId];
+    }
+
+    bool is_connected() const
+    {
+        if (mId == 0)
+        {
+            return true;
+        }
+
+        return mParent && mParent->is_connected();
+    }
+
+    int64_t num_elements() const
+    {
+        auto result = 1;
+
+        for (auto& el : mChildren)
+        {
+            result += el.second->num_elements();
+        }
+
+        return result;
+    }
+
     int64_t mId = 0;
     Node* mParent = nullptr;
     std::unordered_map<int64_t, Node*> mChildren;
+    bool mIsConnected = mId == 0;
 };
 
 
@@ -42,7 +70,10 @@ Node& obtain_node(int64_t node_id)
 
 void print_node(const Node& node, int indent = 0)
 {
-    std::cout << std::string(indent, ' ') << node.mId << std::endl;
+    std::cout << std::string(indent, ' ')
+        //<< node.mId << " => "
+        << node.name()
+        << std::endl;
     for (auto& el : node.mChildren)
     {
         print_node(*el.second, indent + 4);
@@ -74,8 +105,23 @@ int main()
     }
 
 
+    std::cout << "all_nodes.size=" << all_nodes.size() << " root_node.num_elements=" << root_node.num_elements() << std::endl;
     print_node(root_node);
 
+
+
+
+    std::cout << "Not connected: ";
+    for (auto& el : all_nodes)
+    {
+        Node& node = *el.second;
+        if (!node.is_connected())
+        {
+            std::cout << node.mId << '(' << node.name() << ") ";
+        }
+    }
+
+    std::cout << std::endl;
 
 }
 

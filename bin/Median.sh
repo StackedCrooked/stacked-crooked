2f5ab9f4bea57fdf6ec0ec69ff646c4caf922ac9
StackedCrooked <francis.rammeloo@gmail.com>
2020-02-21 03:46:58 +0100

    Median.sh can handle multiple columns.

--- bin/Median.sh
+++ bin/Median.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 sort -n | awk '{
     for (i = 1; i <= NF; i++) {
-        numbers[NF][NR] = $1
+        numbers[i][NR] = $i
     }
-    count++
+    num_columns = NF
+    num_lines++
 } END {
-    if (count % 2 != 0) {
-        # Be careful to avoid indexing with a non-integer (count/2 is no integer)
-        for (i = 1; i <= NF; i++) {
-            printf "%d", numbers[i][(count+1)/2]
-            if (i < NF) {
+    if (num_lines % 2 != 0) {
+        # Be careful to avoid indexing with a non-integer (num_lines/2 is no integer)
+        for (i = 1; i <= num_columns; i++) {
+            printf "%1$s", numbers[i][(num_lines+1)/2]
+            if (i < num_columns) {
                 printf " "
             }
         }
     } else {
-        for (i = 1; i <= NF; i++) {
-            a = numbers[i][count/2]
-            b = numbers[i][count/2 + 1]
-            printf "%d",(a + b)/2
-            if (i < NF) {
+        for (i = 1; i <= num_columns; i++) {
+            a = numbers[i][num_lines/2]
+            b = numbers[i][num_lines/2 + 1]
+            avg = (a + b)/2
+            printf "%1s",avg
+            if (i < num_columns) {
                 printf " "
             }
         }

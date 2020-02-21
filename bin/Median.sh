299e32b75c567f97d071b529aa5a9b0d7389dca5
StackedCrooked <francis.rammeloo@gmail.com>
2020-02-21 03:22:20 +0100

    Median.sh now uses awk.

--- bin/Median.sh
+++ bin/Median.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 sort -n | awk '{
-    numbers[NR] = $1
+    for (i = 1; i <= NF; i++) {
+        numbers[NF][NR] = $1
+    }
     count++
 } END {
     if (count % 2 != 0) {
         # Be careful to avoid indexing with a non-integer (count/2 is no integer)
-        print numbers[(count+1)/2]
+        for (i = 1; i <= NF; i++) {
+            printf "%d", numbers[i][(count+1)/2]
+            if (i < NF) {
+                printf " "
+            }
+        }
     } else {
-        a = numbers[count/2]
-        b = numbers[count/2 + 1]
-        print (a + b)/2
+        for (i = 1; i <= NF; i++) {
+            a = numbers[i][count/2]
+            b = numbers[i][count/2 + 1]
+            printf "%d",(a + b)/2
+            if (i < NF) {
+                printf " "
+            }
+        }
     }
+    printf "\n"
 }'

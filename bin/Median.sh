709d5905326cc32132dd0349b3be7a937628b51e
StackedCrooked <francis.rammeloo@gmail.com>
2020-02-21 02:31:42 +0100

    Median.sh: printf uses -- to indicate end of options.

--- bin/Median.sh
+++ bin/Median.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 buffer=""
 count=0
-newline="$(printf "\n")"
+newline="$(printf -- "\n")"
 while read line; do
-    buffer="$(printf "${line}\n${buffer}")"
+    buffer="$(printf -- "${line}\n${buffer}")"
     count=$(($count+1))
 done
 
 #printf "BUFFER:\n$buffer\b"
-printf "$buffer" | sort -n | head -n $(($count/2)) | tail -n1
+printf -- "$buffer" | sort -n | head -n $(($count/2)) | tail -n1

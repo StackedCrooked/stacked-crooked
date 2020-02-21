8f0930fa82023fd011208befa4e82356d684e563
StackedCrooked <francis.rammeloo@gmail.com>
2020-02-21 02:31:07 +0100

    Delta.sh now uses awk and can handle multiple columns.

--- bin/Delta.sh
+++ bin/Delta.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-i=0
-while read line; do prev=$line; break; done
-while read line; do echo $(($line - $prev)); prev=$line; done
+awk '{
+    for (i=1; i <= NF; i++) {
+        a[NR][i] = $i
+        if (NR > 1) {
+            printf "%d", (a[NR][i] - a[NR - 1][i])
+            if (i < NF) {
+                printf " "
+            }
+        }
+    }
+    if (NR >= 2) {
+        printf "\n"
+    }
+}'

e5e2066f4172676c4b5405ccce1177ce1836bd5c
StackedCrooked <francis.rammeloo@gmail.com>
2020-02-21 02:47:14 +0100

    Sum.sh now uses awk and supports summing multiple columns and decimal numbers.

--- bin/Sum.sh
+++ bin/Sum.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-[ "$#" == 1 ] && {
-    echo "$1"
-    export sum=0
-    cat "$1" | while read line; do sum="$(($line + $sum))"; done
-    echo "$sum"
-    exit 
-}
-    
-sum=0
-while read line; do sum="$(($line + $sum))"; done
-echo "$sum"
 
+awk '{
+    columns = NF
+    for (i=1; i <= columns; i++) {
+        sum[i] += $i
+    }
+}
+END {
+    for (i = 1; i <= columns; i++) {
+        printf "%d", sum[i]
+
+        # Print space after each column, except the last one
+        if (i < columns) {
+            printf " "
+        }
+    }
+    printf "\n"
+}'

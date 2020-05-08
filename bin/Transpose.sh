0fda6c392c4b3d715d74882eed935234fa49848b
StackedCrooked <francis.rammeloo@gmail.com>
2020-05-08 06:11:04 +0200

    Transpose.sh: cleanup.

--- bin/Transpose.sh
+++ bin/Transpose.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-awk '
-{ 
-    for (i=1; i<=NF; i++)  {
+awk '{ 
+    for (i = 1; i <= NF; i++)  {
         a[NR,i] = $i
     }
+
+    if (NF > num_fields) {
+        num_fields = NF
+    }
 }
-NF>p { p = NF }
 END {    
-    for(j=1; j<=p; j++) {
-        str=a[1,j]
-        for(i=2; i<=NR; i++){
-            str=str" "a[i,j];
+    for (j = 1; j <= num_fields; j++) {
+        str = a[1,j]
+        for(i = 2; i <= NR; i++){
+            str = str" "a[i,j];
         }
         print str
     }

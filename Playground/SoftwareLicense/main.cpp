b3fd87d49597d447ac123b609f69c3900a55d867
StackedCrooked <francis.rammeloo@gmail.com>
2020-05-19 14:44:35 +0200

    Show error when command line doesn't have action .

--- Playground/SoftwareLicense/main.cpp
+++ Playground/SoftwareLicense/main.cpp
@@ -519,6 +519,10 @@ int main(int argc, char** argv)
 {
     try
     {
+        if (argc < 2)
+        {
+            throw std::runtime_error("Action required: create, show, check or validate");
+        }
         run(argc - 1, argv + 1);
     }
     catch (const std::exception& e)

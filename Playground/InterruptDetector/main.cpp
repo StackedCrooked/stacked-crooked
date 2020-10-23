bdef26832802e92072b0d33e2ac03a4f7750f287
StackedCrooked <francis.rammeloo@gmail.com>
2020-10-24 00:30:56 +0200

    Parse range.

--- Playground/InterruptDetector/main.cpp
+++ Playground/InterruptDetector/main.cpp
@@ -1,14 +1,25 @@
 #include <boost/lockfree/spsc_queue.hpp>
 #include <chrono>
+#include <condition_variable>
 #include <deque>
 #include <iostream>
+#include <mutex>
 #include <sstream>
 #include <string>
-#include <vector>
 #include <thread>
+#include <vector>
 
 
 using Clock = std::chrono::system_clock;
+
+
+void pin_current_thread_to_core(int core_id)
+{
+    std::cout << "Pinning thread to core " << core_id << std::endl;
+    auto cpuset = cpu_set_t();
+    CPU_SET(core_id, &cpuset);
+    pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
+}
 
 
 struct Snapshot
@@ -79,12 +90,8 @@ struct ThreadRunner
 private:
     void run_thread()
     {
-        {
-            std::cout << "Pinning thread to core " << mCoreId << std::endl;
-            auto cpuset = cpu_set_t();
-            CPU_SET(mCoreId, &cpuset);
-            pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
-        }
+        std::cout << "ThreadRunner: entered thread function, pinning thread to core " << mCoreId << std::endl;
+        pin_current_thread_to_core(mCoreId);
 
         auto start_time = Clock::now();
         auto next_snapshot_time = start_time + std::chrono::seconds(1);
@@ -117,12 +124,13 @@ private:
             // print its result.
             if (iteration_counter++ > 0)
             {
-                std::cout
-                    << "num_samples=" << snapshot.num_samples
-                    << " sum_samples=" << snapshot.sum_samples
-                    << " average_sample_duration=" << (snapshot.num_samples ? snapshot.sum_samples / snapshot.num_samples : 0)
-                    << " max _sample=" << snapshot.max_sample
-                    << std::endl;
+//                std::cout
+//                    << "num_samples=" << snapshot.num_samples
+//                    << " sum_samples=" << snapshot.sum_samples
+//                    << " average_sample_duration=" << (snapshot.num_samples ? snapshot.sum_samples / snapshot.num_samples : 0)
+//                    << " max _sample=" << snapshot.max_sample
+//                    << std::endl;
+                mSnapshots.push(snapshot);
             }
 
             next_snapshot_time += std::chrono::seconds(1);
@@ -136,6 +144,45 @@ private:
 };
 
 
+/// Parses a range parameter.
+/// Examples:
+///     "1-5" will return { 1, 5 } as an std::pair
+///
+/// Single number is treated as a range of one element:
+///     "3" will return { 3, 3 } as an std::pair
+///
+std::pair<int, int> ParseRange(const std::string& str)
+{
+    auto dash_position = str.find('-');
+    if (dash_position == std::string::npos)
+    {
+        int core_id = std::stoi(str);
+        return { core_id, core_id };
+    }
+
+    auto from = std::stoi(str.substr(0, dash_position));
+    auto to = std::stoi(str.substr(dash_position + 1));
+
+    return { from, to };
+}
+
+std::vector<int> GetCoreIds(const std::string& str)
+{
+    std::pair<int, int> core_id_range = ParseRange(str);
+    assert(core_id_range.first <= core_id_range.second);
+
+    std::vector<int> result;
+    result.reserve(core_id_range.second - core_id_range.first + 1);
+
+    for (auto core_id = core_id_range.first; core_id <= core_id_range.second; ++core_id)
+    {
+        result.push_back(core_id);
+    }
+
+    return result;
+}
+
+
 
 int main(int argc, char** argv)
 {
@@ -145,8 +192,8 @@ int main(int argc, char** argv)
     {
         std::string program_name = args[0];
         std::string basename = program_name.substr(program_name.rfind('/') + 1);
-        std::cerr << "Usage:\n\t" << basename << " <core_id> <priority>\n";
-        std::cerr << "\nExample:\n\t" << basename << " 1 SCHED_RR\n";
+        std::cerr << "Usage:\n\t" << basename << " <core_id_range> <priority>\n";
+        std::cerr << "\nExample:\n\t" << basename << " 1-12 SCHED_RR\n";
         std::cerr << "\nExample:\n\t" << basename << " 1 SCHED_OTHER\n";
         std::cerr << "\nNote: SCHED_OTHER is the default Linux scheduling policy.\n";
         return 1;
@@ -181,25 +228,53 @@ int main(int argc, char** argv)
     std::cout << std::endl;
 
 
+    /// The main thread is on core zero.
+    pin_current_thread_to_core(0);
+
     std::deque<ThreadRunner> runners;
-    runners.emplace_back(core_id);
+
+    for (int core_id : GetCoreIds(args[1]))
+    {
+        if (core_id == 0)
+        {
+            throw std::runtime_error("Core 0 not allowed.");
+        }
+
+        std::cout << "Starting thread for core " << core_id << std::endl;
+        runners.emplace_back(core_id);
+    }
 
     for (ThreadRunner& runner : runners)
     {
-        Snapshot snapshot;
-        if (runner.consume(snapshot))
-        {
-            std::cout
-                << "CoreId: " << runner.getCoreId()
-                << " num_samples=" << snapshot.num_samples
-                << " sum_samples=" << snapshot.sum_samples
-                << " average_sample_duration=" << (snapshot.num_samples ? snapshot.sum_samples / snapshot.num_samples : 0)
-                << " max _sample=" << snapshot.max_sample
-                << std::endl;
-
-        }
+        runner.start();
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    for (;;)
+    {
 
+        for (ThreadRunner& runner : runners)
+        {
+            Snapshot snapshot;
+            if (runner.consume(snapshot))
+            {
+                std::cout
+                    << "Consumed ThreadRunner with core id: " << runner.getCoreId()
+                    << " num_samples=" << snapshot.num_samples
+                    << " sum_samples=" << snapshot.sum_samples
+                    << " average_sample_duration=" << (snapshot.num_samples ? snapshot.sum_samples / snapshot.num_samples : 0)
+                    << " max _sample=" << snapshot.max_sample
+                    << std::endl;
+
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    std::mutex m;
+    std::condition_variable c;
+    std::unique_lock<std::mutex> lock(m);
+    c.wait(lock);
+
+    std::cout << "Exiting program." << std::endl;
 }

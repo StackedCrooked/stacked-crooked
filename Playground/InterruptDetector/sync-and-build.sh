1fe3ece417c825e64550c86428e642fce644fa59
StackedCrooked <francis.rammeloo@gmail.com>
2020-10-23 23:01:39 +0200

    Add sync-and-build.sh.

---
+++ Playground/InterruptDetector/sync-and-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+export remote_host="byteblower-dev-5100-2.lab.byteblower.excentis.com"
+export remote_path="/root/tmp/InterruptChecker/"
+
+
+scp -C main.cpp Makefile root@$remote_host:$remote_path
+
+
+ssh root@$remote_host "
+    set -e
+    set -x
+    cd $remote_path &&
+    make &&
+    ./a.out 2
+"

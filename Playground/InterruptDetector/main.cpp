292bf3d2c755c6329d329710bba9b9f63ba238b3
StackedCrooked <francis.rammeloo@gmail.com>
2020-10-24 00:16:22 +0200

    Add ThreadRunner.

--- Playground/InterruptDetector/main.cpp
+++ Playground/InterruptDetector/main.cpp
@@ -1,8 +1,11 @@
+#include <boost/lockfree/spsc_queue.hpp>
 #include <chrono>
+#include <deque>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <vector>
+#include <thread>
 
 
 using Clock = std::chrono::system_clock;
@@ -34,58 +37,103 @@ struct Snapshot
 };
 
 
-void run(int core_id)
+struct ThreadRunner
 {
+    explicit ThreadRunner(int core_id) :
+        mCoreId(core_id),
+        mThread()
     {
-        std::cout << "Pinning thread to core " << core_id << std::endl;
-        auto cpuset = cpu_set_t();
-        CPU_SET(core_id, &cpuset);
-        pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
     }
 
-    auto start_time = Clock::now();
-    auto next_snapshot_time = start_time + std::chrono::seconds(1);
+    ThreadRunner(const ThreadRunner&) = default;
+    ThreadRunner& operator=(const ThreadRunner&) = default;
 
-    Snapshot snapshot;
+    ThreadRunner(ThreadRunner&&) noexcept = default;
+    ThreadRunner& operator=(ThreadRunner&&) noexcept  = default;
 
-    auto iteration_counter = 0ul;
-
-    for (;;)
+    ~ThreadRunner()
     {
-        Clock::time_point t1 = Clock::now();
+        if (mThread.joinable())
+        {
+            mThread.join();
+        }
+    }
+
+    void start()
+    {
+        mThread = std::thread([this]{
+            run_thread();
+        });
+    }
+
+    int getCoreId() const
+    {
+        return mCoreId;
+    }
+
+    bool consume(Snapshot& snapshot)
+    {
+        return mSnapshots.pop(&snapshot, 1);
+    }
+
+private:
+    void run_thread()
+    {
+        {
+            std::cout << "Pinning thread to core " << mCoreId << std::endl;
+            auto cpuset = cpu_set_t();
+            CPU_SET(mCoreId, &cpuset);
+            pthread_setaffinity_np(pthread_self(), sizeof(cpuset), &cpuset);
+        }
+
+        auto start_time = Clock::now();
+        auto next_snapshot_time = start_time + std::chrono::seconds(1);
+
+        Snapshot snapshot;
+
+        auto iteration_counter = 0ul;
 
         for (;;)
         {
-            Clock::time_point t2 = Clock::now();
+            Clock::time_point t1 = Clock::now();
 
-            if (t1 < next_snapshot_time)
+            for (;;)
             {
-                uint64_t sample = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
-                snapshot.add(sample);
-                t1 = t2;
+                Clock::time_point t2 = Clock::now();
+
+                if (t1 < next_snapshot_time)
+                {
+                    uint64_t sample = std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+                    snapshot.add(sample);
+                    t1 = t2;
+                }
+                else
+                {
+                    break;
+                }
             }
-            else
+
+            // The first iteration is always slow, so treat is as a warmup iteration and don't
+            // print its result.
+            if (iteration_counter++ > 0)
             {
-                break;
+                std::cout
+                    << "num_samples=" << snapshot.num_samples
+                    << " sum_samples=" << snapshot.sum_samples
+                    << " average_sample_duration=" << (snapshot.num_samples ? snapshot.sum_samples / snapshot.num_samples : 0)
+                    << " max _sample=" << snapshot.max_sample
+                    << std::endl;
             }
-        }
 
-        // The first iteration is always slow, so treat is as a warmup iteration and don't
-        // print its result.
-        if (iteration_counter++ > 0)
-        {
-            std::cout
-                << "num_samples=" << snapshot.num_samples
-                << " sum_samples=" << snapshot.sum_samples
-                << " average_sample_duration=" << (snapshot.num_samples ? snapshot.sum_samples / snapshot.num_samples : 0)
-                << " max_sample=" << snapshot.max_sample
-                << std::endl;
+            next_snapshot_time += std::chrono::seconds(1);
+            snapshot.clear();
         }
-
-        next_snapshot_time += std::chrono::seconds(1);
-        snapshot.clear();
     }
-}
+
+    boost::lockfree::spsc_queue<Snapshot, boost::lockfree::capacity<1>> mSnapshots;
+    uint32_t mCoreId;
+    std::thread mThread;
+};
 
 
 
@@ -132,5 +180,26 @@ int main(int argc, char** argv)
 
     std::cout << std::endl;
 
-    run(core_id);
+
+    std::deque<ThreadRunner> runners;
+    runners.emplace_back(core_id);
+
+    for (ThreadRunner& runner : runners)
+    {
+        Snapshot snapshot;
+        if (runner.consume(snapshot))
+        {
+            std::cout
+                << "CoreId: " << runner.getCoreId()
+                << " num_samples=" << snapshot.num_samples
+                << " sum_samples=" << snapshot.sum_samples
+                << " average_sample_duration=" << (snapshot.num_samples ? snapshot.sum_samples / snapshot.num_samples : 0)
+                << " max _sample=" << snapshot.max_sample
+                << std::endl;
+
+        }
+    }
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
 }
